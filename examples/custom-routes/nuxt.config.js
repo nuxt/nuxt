6ab9f138c4dc5c58e0cba57a725bb91f5c2e3256
Sébastien Chopin <seb@chopin.io>
2016-11-27 18:12:45 +0100

    Clean custom-routes example

--- examples/custom-routes/nuxt.config.js
+++ examples/custom-routes/nuxt.config.js
@@ -1,11 +1,4 @@
-const axios = require('axios')
-
 module.exports = {
-  env: {
-    token: 'Hello :)',
-    num: 3,
-    bool: true
-  },
   router: {
     routes: [
       { name: 'user', path: '/users/:id(\\d+)', component: 'pages/_user' }
@@ -13,17 +6,5 @@ module.exports = {
   },
   build: {
     vendor: ['axios']
-  },
-  generate: {
-    routeParams: {
-      '/users/:id(\\d+)': function () {
-        return axios.get('http://jsonplaceholder.typicode.com/users')
-        .then((res) => {
-          return res.data.map((user) => {
-            return { id: user.id }
-          })
-        })
-      }
-    }
   }
 }
