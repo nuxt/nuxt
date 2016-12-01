ba8327abf4135b5e028f1373293334572c4cad72
Sébastien Chopin <seb@chopin.io>
2016-12-01 19:01:59 +0100

    Add cached-components example

---
+++ examples/cached-components/nuxt.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  cache: true
+}
---
+++ examples/cached-components/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Cached components</h1>
+    <p>Look at the source code and see how the timestamp is not reloaded before 10s after refreshing the page.</p>
+    <p>Timestamp: {{ date }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'date',
+  serverCacheKey () {
+    // Will change every 10 secondes
+    return Math.floor(Date.now() / 10000)
+  },
+  data () {
+    return { date: Date.now() }
+  }
+}
+</script>
