2c4a512dda7329a1a1317ab03dd33764c63f0492
Alexandre Chopin <wikeo65@gmail.com>
2016-12-12 21:54:02 +0100

    generate ok

--- examples/custom-routes/nuxt.config.js
+++ examples/custom-routes/nuxt.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   build: {
     vendor: ['axios']
+  },
+  generate: {
+    routeParams: {
+      '/users/:id': [{id: 1}, {id: 2}, {id: 3}, {id: 4}, {id: 5}],
+      '/posts/:slug': [{slug: 'welcome'}, {slug: 'foo'}, {slug: 'bar'}],
+      '/posts/:slug/comments': [{slug: 'welcome'}, {slug: 'foo'}, {slug: 'bar'}],
+      '/posts/:slug/:name': [{slug: 'welcome', name: 'a'}, {slug: 'foo', name: 'b'}, {slug: 'bar', name: 'a'}],
+      '/projects/:slug': [{slug: 'toto'}, {slug: 'titi'}, {slug: 'tutu'}]
+    }
   }
 }
