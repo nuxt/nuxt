b6b1fc3073a7b03713d40d65eba1d8d9aed2bf87
Alexandre Chopin <wikeo65@gmail.com>
2016-12-13 17:42:41 +0100

    add component nuxt-child

--- examples/custom-routes/pages/posts.vue
+++ examples/custom-routes/pages/posts.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
     <h1>Dynamic nested route example</h1>
-    <router-view></router-view>
+    <nuxt-child></nuxt-child>
   </div>
 </template>
---
+++ lib/app/components/nuxt-child.vue
@@ -0,0 +1,38 @@
+<template>
+  <transition :name="transition.name" :mode="transition.mode">
+    <router-view></router-view>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'nuxt-child',
+  props: {
+    transition: {
+      type: Object,
+      default: () => {
+        return {
+          mode: 'out-in', // out-in ou in-out
+          name: 'fade',
+          type: '', // animation ou transition
+          enter: null, // isFunc = js, is String = css
+          enterActive: '',
+          beforeEnter: null, // Func
+          afterEnter: null,
+          enterCancelled: null,
+          leave: null, // isFunc = js, is String = css
+          leaveActive: '',
+          beforeLeave: null, // Func
+          afterLeave: null,
+          leaveCancelled: null,
+          appear: null, // isFunc = js, is String = css
+          appearClass: '',
+          appearActiveClass: '',
+          beforeAppear: null,
+          afterAppear: null
+        }
+      }
+    }
+  }
+}
+</script>
