38ef413e5d417c95ccf7cb58c585777511ffdb25
Sébastien Chopin <seb@chopin.io>
2016-12-21 15:27:30 +0100

    test for nuxt.js

---
+++ test/fixtures/with-config/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    counter: 0
+  },
+  mutations: {
+    increment (state) {
+      state.counter++
+    }
+  }
+})
+
+export default store
