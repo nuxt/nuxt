27bfabd4d2cfd9be28df176dfe9d3b7a7550d904
Alexandre Chopin <wikeo65@gmail.com>
2016-12-23 17:31:52 +0100

    add test folder dynamic routes

---
+++ test/fixtures/dynamic-routes/pages/test/users/_id.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="user">
+    <h3>{{ name }}</h3>
+    <h4>@{{ username }}</h4>
+    <p>Email : {{ email }}</p>
+    <p><nuxt-link to="/">List of users</nuxt-link></p>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  validate ({ params }) {
+    return !isNaN(+params.id)
+  },
+  data ({ params, error }) {
+    return axios.get(`https://jsonplaceholder.typicode.com/users/${+params.id}`)
+    .then((res) => res.data)
+    .catch(() => {
+      error({ message: 'User not found', statusCode: 404 })
+    })
+  }
+}
+</script>
+
+<style scoped>
+.user {
+  text-align: center;
+  margin-top: 100px;
+  font-family: sans-serif;
+}
+</style>
