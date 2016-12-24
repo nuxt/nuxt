3a0fcdee73d3230259d4db05517692e6ba1c9d0f
Sébastien Chopin <seb@chopin.io>
2016-12-24 12:34:41 +0100

    Layout with code-splitting!

--- examples/custom-layout/layouts/error.vue
+++ examples/custom-layout/layouts/error.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1>Custom error page</h1>
+  <div class="container">
+    <h1>Sorry, page not found</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
 </template>
 
 <script>
@@ -7,3 +10,14 @@ export default {
   props: ['error']
 }
 </script>
+
+<style scoped>
+.container {
+  font-family: sans-serif;
+  padding-top: 10%;
+  text-align: center;
+}
+h1 {
+  font-size: 20px;
+}
+</style>
--- lib/app/App.vue
+++ lib/app/App.vue
@@ -1,6 +1,6 @@
 <template>
   <nuxt-container>
-    <component :is="layout"></component>
+    <component v-if="layout" :is="layout"></component>
   </nuxt-container>
 </template>
 
@@ -9,19 +9,37 @@ let layouts = {
 <%
 var layoutsKeys = Object.keys(layouts);
 layoutsKeys.forEach(function (key, i) { %>
-  _<%= key %>: require('<%= layouts[key] %>')<%= (i + 1) < layoutsKeys.length ? ',' : '' %>
+  _<%= key %>: process.BROWSER_BUILD ? () => System.import('<%= layouts[key] %>') : require('<%= layouts[key] %>')<%= (i + 1) < layoutsKeys.length ? ',' : '' %>
 <% }) %>
 }
 
 export default {
   data () {
-    return { layout: layouts._default }
+    return { layout: null }
   },
   methods: {
     setLayout (layout) {
       if (!layout || !layouts['_' + layout]) layout = 'default'
-      this.layout = layouts['_' + layout]
-      return layout
+      let _layout = '_' + layout
+      if (typeof layouts[_layout] === 'function') {
+        return this.loadLayout(_layout)
+      }
+      this.layout = layouts[_layout]
+      return Promise.resolve(this.layout)
+    },
+    loadLayout (_layout) {
+      return layouts[_layout]()
+      .then((Component) => {
+        layouts[_layout] = Component
+        this.layout = layouts[_layout]
+        return this.layout
+      })
+      .catch((e) => {
+        if (this.$nuxt) {
+          return this.$nuxt.error({ statusCode: 500, message: e.message })
+        }
+        console.error(e)
+      })
     }
   }
 }
