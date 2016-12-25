cb4dcf0b37e06007c37caa2c0e204cfef22b2de8
Sébastien Chopin <seb@chopin.io>
2016-12-25 22:55:00 +0100

    Update custom layout example

--- examples/custom-layouts/layouts/error.vue
+++ examples/custom-layouts/layouts/error.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-    <h1>Sorry, page not found</h1>
+    <h1 v-if="error.statusCode === 404">Page not found</h1>
+    <h1 v-else>An error occured</h1>
     <nuxt-link to="/">Home page</nuxt-link>
   </div>
 </template>
