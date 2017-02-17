bea7b73ad5fd627386159ef3a5260ec7247a727a
Sébastien Chopin <seb@chopin.io>
2017-02-17 16:12:20 +0100

    Example with JSX

---
+++ examples/hello-world-jsx/pages/about.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  data ({ req }) {
+    return {
+      name: req ? 'server' : 'client'
+    }
+  },
+  render (h) {
+    return <div>
+      <p>Hi from {this.name}</p>
+      <nuxt-link to="/">Home page</nuxt-link>
+    </div>
+  }
+}
+</script>
