b3e909a704215bd67ffc681cb7fe50a7a88f8a86
Charlie Hield <charles.hield@gmail.com>
2017-02-24 21:37:05 -0600

    Add Firebase example

---
+++ examples/with-firebase/pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>Avatar</th>
+          <th>Name</th>
+          <th>Title</th>
+          <th>Profile</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(user, key) in users">
+          <td>
+            <nuxt-link :to="{ path: `/users/${key}`}">
+              <img :src="user.avatar" class="rounded" alt="avatar">
+            </nuxt-link>
+          </td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.title }}</td>
+          <td>
+            <nuxt-link :to="{ path: `/users/${key}`}">View profile &rarr;</nuxt-link>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from '~plugins/axios'
+
+export default {
+  async data() {
+    const { data } = await axios.get('users.json')
+    return {
+      users: data
+    }
+  }
+}
+</script>
