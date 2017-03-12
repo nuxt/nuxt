f584a221ee86856f1c5cb61bdd12e48d0858a4cf
rohitpal <rohit.1401322808@gmail.com>
2017-03-12 05:43:34 +0530

    service worker registraion

--- examples/offline-nuxt/plugins/init-offline.js
+++ examples/offline-nuxt/plugins/init-offline.js
@@ -1,3 +1,20 @@
 if (process.BROWSER_BUILD && process.env.NODE_ENV === 'production') {
   require('offline-plugin/runtime').install()
+  window.onNuxtReady((app) => {
+    if ('serviceWorker' in navigator) {
+      if (navigator.serviceWorker.controller) {} else {
+        navigator.serviceWorker.register('/_nuxt/tcmpsw.js', {scope: '/'}).then(function(res) {
+          console.log('sw loaded...')
+        }).catch(function(err) {
+          console.log(err);
+        });
+      }
+    } else if (window.applicationCache) {
+      // register appcache code
+      var iframe = document.createElement('iframe');
+      iframe.style.display = "none";
+      iframe.src = '/_nuxt/appcache/manifest.html';
+      document.body.appendChild(iframe);
+    }
+  })
 }
