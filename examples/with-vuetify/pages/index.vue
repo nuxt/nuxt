18ec7507ad1c578e89eb028fb232b1a5173b7c71
bratberg <bratberg@gmail.com>
2017-03-14 21:13:39 +0100

    Add Vuetify.js example

---
+++ examples/with-vuetify/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app top-toolbar footer>
+    <v-toolbar>
+      <v-toolbar-title>Toolbar</v-toolbar-title>
+    </v-toolbar>
+    <main>
+      <v-content>
+        <v-container fluid>
+          <div class="title">Main Content</div>
+        </v-container>
+      </v-content>
+    </main>
+  </v-app>
+</template>
+
+<style>
+  .title {
+    padding-left: 14px;
+  }
+</style>
