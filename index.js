e17e05e86f70c119887c8ac490ef0192c5309b27
Sébastien Chopin <seb@chopin.io>
2017-03-24 02:15:13 +0100

    Disable deprecation warning

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 ** @Atinux & @alexchopin
 */
 
+// Until babel-loader 7 is released
+process.noDeprecation = true
+
 var Nuxt = require('./dist/nuxt.js')
 
 module.exports = Nuxt.default ? Nuxt.default : Nuxt
