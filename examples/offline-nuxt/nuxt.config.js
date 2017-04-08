ddbb4b96d1330c961c50f58c97bd8c8ba21b6752
Sébastien Chopin <seb@chopin.io>
2017-04-08 11:30:06 +0200

    Set ssr: false

--- examples/offline-nuxt/nuxt.config.js
+++ examples/offline-nuxt/nuxt.config.js
@@ -1,4 +1,6 @@
 module.exports = {
   offline: true, // true or https://github.com/NekR/offline-plugin/blob/master/docs/options.md
-  plugins: ['~plugins/init-offline.js']
+  plugins: [
+    { src: '~plugins/init-offline.js', ssr: false }
+  ]
 }
