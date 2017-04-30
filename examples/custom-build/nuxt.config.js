6ee5d958a76babc10e5714db789232377dbc7c18
Pooya Parsa <pooya@pi0.ir>
2017-04-30 17:24:14 +0430

    cleanups

--- examples/custom-build/nuxt.config.js
+++ examples/custom-build/nuxt.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   build: {
     filenames: {
-        css: 'styles.[chunkhash].css',         // default: common.[chunkhash].css
+        css: 'styles.[chunkhash].css',    // default: common.[chunkhash].css
         manifest: 'manifest.[hash].js',   // default: manifest.[hash].js
         vendor: 'vendor.[hash].js',       // default: vendor.bundle.[hash].js
         app: 'app.[chunkhash].js'         // default: nuxt.bundle.[chunkhash].js
