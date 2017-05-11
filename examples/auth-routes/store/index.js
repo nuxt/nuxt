2f881dfb31958c3f047e8c4305e7e3e8dd51462a
kodekrendel <kode.krendel@yandex.ru>
2017-05-10 23:06:43 -0400

    fixing auth-routes example

--- examples/auth-routes/store/index.js
+++ examples/auth-routes/store/index.js
@@ -1,41 +1,45 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
 import axios from 'axios'
 
-export const state = {
-  authUser: null
-}
+Vue.use(Vuex)
 
-export const mutations = {
-  SET_USER: function (state, user) {
-    state.authUser = user
-  }
-}
-
-export const actions = {
-  nuxtServerInit ({ commit }, { req }) {
-    if (req.session && req.session.authUser) {
-      commit('SET_USER', req.session.authUser)
+const store = new Vuex.Store({
+  state: {
+    authUser: null
+  },
+  mutations: {
+    SET_USER: function (state, user) {
+      state.authUser = user
     }
   },
-  login ({ commit }, { username, password }) {
-    return axios.post('/api/login', {
-      username,
-      password
-    })
-    .then((res) => {
-      commit('SET_USER', res.data)
-    })
-    .catch((error) => {
-      if (error.response.status === 401) {
-        throw new Error('Bad credentials')
+  actions: {
+    nuxtServerInit ({ commit }, { req }) {
+      if (req.session && req.session.authUser) {
+        commit('SET_USER', req.session.authUser)
       }
-    })
-  },
-
-  logout ({ commit }) {
-    return axios.post('/api/logout')
-    .then(() => {
-      commit('SET_USER', null)
-    })
+    },
+    login ({ commit }, { username, password }) {
+      return axios.post('/api/login', {
+        username,
+        password
+      })
+      .then((res) => {
+        commit('SET_USER', res.data)
+      })
+      .catch((error) => {
+        if (error.response.status === 401) {
+          throw new Error('Bad credentials')
+        }
+      })
+    },
+    logout ({ commit }) {
+      return axios.post('/api/logout')
+      .then(() => {
+        commit('SET_USER', null)
+      })
+    }
   }
+})
 
-}
+export default store
