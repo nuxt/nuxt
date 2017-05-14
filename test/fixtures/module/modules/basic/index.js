58842f3c03abf033458a4aafc921f66f01495c1b
Pooya Parsa <pooya@pi0.ir>
2017-05-15 03:10:09 +0430

    [tests] use promise

--- test/fixtures/module/modules/basic/index.js
+++ test/fixtures/module/modules/basic/index.js
@@ -1,17 +1,20 @@
 const path = require('path')
 
-module.exports = async function basicModule (options) {
-  // Add simple vendor
-  this.addVendor('lodash')
+module.exports = function basicModule (options) {
+  return new Promise((resolve,reject) => {
+    // Add simple vendor
+    this.addVendor('lodash')
 
-  // Add a plugin
-  this.addPlugin(path.resolve(__dirname, 'reverse.js'))
+    // Add a plugin
+    this.addPlugin(path.resolve(__dirname, 'reverse.js'))
 
-  // Add simple api endpoint
-  this.addServerMiddleware({
-    path: '/api',
-    handler (req, res, next) {
-      res.end('It works!')
-    }
+    // Add simple api endpoint
+    this.addServerMiddleware({
+      path: '/api',
+      handler (req, res, next) {
+        res.end('It works!')
+      }
+    })
+    resolve()
   })
 }
