96cf4a0ec4db0e4a4e4471cf8ba7eb14338eca10
Sebastien Chopin <atinux@gmail.com>
2017-05-29 17:03:28 +0200

    Add ts types

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+//These declarations allow TypeScript to import non-js/ts files without the file extensions (such as .vue files)
+
+declare module "~components/*" {}
+declare module "~layouts/*" {}
+declare module "~pages/*" {}
+declare module "~assets/*" {}
+declare module "~static/*" {}
