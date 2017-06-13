c519c3edff47c9fe805a36729a85bb1eee76b6ad
Pooya Parsa <pooya@pi0.ir>
2017-06-14 02:39:03 +0430

    fixes + improvements

--- test/basic.fail.generate.test.js
+++ test/basic.fail.generate.test.js
@@ -7,29 +7,14 @@ test('Fail with routes() which throw an error', async t => {
     rootDir: resolve(__dirname, 'fixtures/basic'),
     dev: false,
     generate: {
-      routes: function () {
-        return new Promise((resolve, reject) => {
-          reject(new Error('Not today!'))
-        })
+      async routes () {
+        throw new Error('Not today!')
       }
     }
   }
   const nuxt = new Nuxt(options)
-  return new Promise((resolve) => {
-    var oldExit = process.exit
-    var oldCE = console.error // eslint-disable-line no-console
-    var _log = ''
-    console.error = (s) => { _log += s } // eslint-disable-line no-console
-    process.exit = (code) => {
-      process.exit = oldExit
-      console.error = oldCE // eslint-disable-line no-console
-      t.is(code, 1)
-      t.true(_log.includes('Could not resolve routes'))
-      resolve()
-    }
-    nuxt.generate()
+  return nuxt.generate()
     .catch((e) => {
       t.true(e.message === 'Not today!')
     })
-  })
 })
--- test/index.test.js
+++ test/index.test.js
@@ -21,44 +21,28 @@ test.serial('Nuxt.js Instance', async t => {
   t.is(nuxt.initialized, true)
 })
 
-test.serial('Fail to build when no pages/ directory but is in the parent', async t => {
+test.serial('Fail to build when no pages/ directory but is in the parent', t => {
   const nuxt = new Nuxt({
-    dev: true,
+    dev: false,
     rootDir: resolve(__dirname, 'fixtures', 'empty', 'pages')
   })
-  return new Promise((resolve) => {
-    let oldExit = process.exit
-    let oldCE = console.error // eslint-disable-line no-console
-    let _log = ''
-    console.error = (s) => { _log += s } // eslint-disable-line no-console
-    process.exit = (code) => {
-      process.exit = oldExit
-      console.error = oldCE // eslint-disable-line no-console
-      t.is(code, 1)
-      t.true(_log.includes('No `pages` directory found. Did you mean to run `nuxt` in the parent (`../`) directory?'))
-      resolve()
-    }
-    nuxt.build()
+  return nuxt.build().catch(err => {
+    let s = String(err)
+    t.true(s.includes('No `pages` directory found'))
+    t.true(s.includes('Did you mean to run `nuxt` in the parent (`../`) directory?'))
+    resolve()
   })
 })
 
-test.serial('Fail to build when no pages/ directory', async t => {
+test.serial('Fail to build when no pages/ directory', t => {
   const nuxt = new Nuxt({
-    dev: true,
+    dev: false,
     rootDir: resolve(__dirname)
   })
-  return new Promise((resolve) => {
-    let oldExit = process.exit
-    let oldCE = console.error // eslint-disable-line no-console
-    let _log = ''
-    console.error = (s) => { _log += s } // eslint-disable-line no-console
-    process.exit = (code) => {
-      process.exit = oldExit
-      console.error = oldCE // eslint-disable-line no-console
-      t.is(code, 1)
-      t.true(_log.includes('Couldn\'t find a `pages` directory. Please create one under the project root'))
-      resolve()
-    }
-    nuxt.build()
+  return nuxt.build().catch(err => {
+    let s = String(err)
+    t.true(s.includes('Couldn\'t find a `pages` directory'))
+    t.true(s.includes('Please create one under the project root'))
+    resolve()
   })
 })
