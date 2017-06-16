2df044664a77b5434523ed908833e66ce9cde9f0
Pooya Parsa <pooya@pi0.ir>
2017-06-16 18:13:45 +0430

    code coverage

--- src/core/renderer.js
+++ src/core/renderer.js
@@ -235,7 +235,7 @@ export default class Renderer extends Tapable {
       res.end(html, 'utf8')
       return html
     } catch (err) {
-      /* istanbul ignore if  */
+      /* istanbul ignore if */
       if (context.redirected) {
         console.error(err) // eslint-disable-line no-console
         return err
@@ -306,8 +306,7 @@ export default class Renderer extends Tapable {
     if (!jsdom) {
       try {
         jsdom = require('jsdom')
-      } catch (e) {
-        // istanbul ignore next
+      } catch (e) /* istanbul ignore next */{
         console.error('Fail when calling nuxt.renderAndGetWindow(url)') // eslint-disable-line no-console
         console.error('jsdom module is not installed') // eslint-disable-line no-console
         console.error('Please install jsdom with: npm install --save-dev jsdom') // eslint-disable-line no-console
