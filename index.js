582e8179f45275f2e7077f7d594b9e85b2ef2aa6
Sébastien Chopin <seb@chopin.io>
2017-06-18 15:48:25 +0200

    remove NUXT_APP_DIR env

--- index.js
+++ index.js
@@ -1,30 +1,21 @@
 /*!
  * Nuxt.js
  * (c) 2016-2017 Chopin Brothers
+ * Core maintainer: Pooya (@pi0)
  * Released under the MIT License.
  */
-
-const path = require('path')
-
-process.noDeprecation = true
 
 // Node Source Map Support
 // https://github.com/evanw/node-source-map-support
 require('source-map-support').install();
 
+// Fix babel flag
+process.noDeprecation = true
+
 // Require Core
 const Core = require('./dist/core.js')
-// Object.assign(exports, Core.default || Core)
 
 // Require Builder
 const Builder = require('./dist/builder')
-// Object.assign(exports, Builder.default || Builder)
 
-// FIXME
-Object.assign(Core.default || Core, Builder.default || Builder)
-module.exports = Core.default || Core
-
-// Use special env flag to specify app dir without modify builder
-if (!process.env.NUXT_APP_DIR) {
-  process.env.NUXT_APP_DIR = path.resolve(__dirname, 'lib/app')
-}
+module.exports = Object.assign(Core, Builder)
