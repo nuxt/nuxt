6146de34f87ffbf8fb8ba8b5425980bf10616646
Pooya Parsa <pooya@pi0.ir>
2017-06-18 12:40:24 +0430

    temporary for for Core.Builder

--- index.js
+++ index.js
@@ -14,11 +14,15 @@ require('source-map-support').install();
 
 // Require Core
 const Core = require('./dist/core.js')
-Object.assign(exports, Core.default || Core)
+// Object.assign(exports, Core.default || Core)
 
 // Require Builder
 const Builder = require('./dist/builder')
-Object.assign(exports, Builder.default || Builder)
+// Object.assign(exports, Builder.default || Builder)
+
+// FIXME
+Object.assign(Core.default || Core, Builder.default || Builder)
+module.exports = Core.default || Core
 
 // Use special env flag to specify app dir without modify builder
 if (!process.env.NUXT_APP_DIR) {
