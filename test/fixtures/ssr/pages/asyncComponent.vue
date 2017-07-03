bfd8475979e2474aa174ce8dd9a7575574a95f6b
Pooya Parsa <pooya@pi0.ir>
2017-07-03 22:59:38 +0430

    test(ssr): async components

---
+++ test/fixtures/ssr/pages/asyncComponent.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <async-test/>
+</div>
+</template>
+
+<script>
+
+export default {
+  async asyncData({ error }) {
+    try {
+      this.components.AsyncTest = await import(`@/components/test.vue`)
+    } catch (e) {
+      error({ statusCode: 404, message: 'Can not load test component' })
+    }
+  }
+}
+</script>
