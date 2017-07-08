a0c470977789d3f4a9b86901cb437f13cb570422
Sébastien Chopin <seb@chopin.io>
2017-07-08 18:14:40 +0200

    Fix typo

--- examples/auth-routes/middleware/auth.js
+++ examples/auth-routes/middleware/auth.js
@@ -1,7 +1,5 @@
-export default function ({ store, redirect, error }) {
-  // If user not connected, redirect to /
+export default function ({ store, error }) {
   if (!store.state.authUser) {
-  //   return redirect('/')
     error({
       message: 'You are not connected',
       statusCode: 403
