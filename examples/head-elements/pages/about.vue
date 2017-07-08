1e064ac180f86b067179f42d7e8a58c818c39b23
Sébastien Chopin <seb@chopin.io>
2017-07-08 19:44:05 +0200

    Fix path

--- examples/head-elements/pages/about.vue
+++ examples/head-elements/pages/about.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import TwitterHeadCard from '~components/twitter-head-card.vue'
+import TwitterHeadCard from '~/components/twitter-head-card.vue'
 
 export default {
   head: {
