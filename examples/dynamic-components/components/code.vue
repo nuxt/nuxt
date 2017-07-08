c7ce0802c9db11c76d6d61e0391a806ed1fb6329
Sébastien Chopin <seb@chopin.io>
2017-07-09 01:44:59 +0200

    Updates dynamic examples

---
+++ examples/dynamic-components/components/code.vue
@@ -0,0 +1,20 @@
+<template>
+  <pre>{{ data }}</pre>
+</template>
+
+<script>
+export default {
+  props: {
+    data: String
+  }
+}
+</script>
+
+<style scoped>
+pre {
+    background: #222;
+    color: #eee;
+    margin: 0;
+    padding: 20px;
+}
+</style>
--- examples/dynamic-components/pages/index.vue
+++ examples/dynamic-components/pages/index.vue
@@ -1,9 +1,64 @@
 <template>
   <div>
-    <h1>Articles</h1>
+    <h1>Nuxt Chat</h1>
     <ul>
-      <li><nuxt-link to="/article-1">Article #1</nuxt-link></li>
-      <li><nuxt-link to="/article-2">Article #2</nuxt-link></li>
+      <li v-for="message in messages">
+        <component :is="message.component" :data="message.data"></component>
+      </li>
     </ul>
   </div>
 </template>
+
+<script>
+import streamMessages from '@/js/messages.js'
+// Dynamic components
+const components = {
+  vText: () => import('@/components/text.vue').then(m => m.default),
+  vImage: () => import('@/components/image.vue').then(m => m.default),
+  vCode: () => import('@/components/code.vue').then(m => m.default)
+}
+
+export default {
+  data: () => ({
+    messages: []
+  }),
+  mounted () {
+    setTimeout(() => {
+    // Listen to new messages
+    streamMessages(async (message) => {
+      // Make sure to wait for async chunk to be loaded before adding the message
+      await components[message.component]()
+      // Add the message to the list
+      this.messages.push(message)
+    })
+    }, 1000)
+  },
+  components
+}
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+  font-family: Helvetica, Arial, sans-serif;
+}
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  with: 100%;
+  max-width: 300px;
+  margin: auto;
+}
+
+ul li {
+  display: block;
+  width: 100%;
+  border-radius: 20px;
+  margin-bottom: 5px;
+  font-family: Helvetica, Arial, sans-serif;
+  background: white;
+  border: 1px #ddd solid;
+  overflow: hidden;
+}
+</style>
