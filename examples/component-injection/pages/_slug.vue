3e36219a43a9000037f2f19eb02502a09487022d
Sébastien Chopin <seb@chopin.io>
2017-07-10 08:56:02 +0200

    Update component injection example

--- examples/component-injection/pages/_slug.vue
+++ examples/component-injection/pages/_slug.vue
@@ -1,16 +1,19 @@
 <template>
-  <BlogArticle/>
+  <component :is="comp"/>
 </template>
 
 <script>
+const components = {}
+const files = require.context('@/articles', false, /\.vue$/)
+files.keys().forEach((filename) => {
+  const name = filename.replace('./', '').replace('.vue', '')
+  components[name] = () => import('@/articles/' + name + '.vue').then((m) => m.default || m)
+})
+
 export default {
   async asyncData({ params, error }) {
-    const slug = params.slug || 'hello'
-    try {
-      this.components.BlogArticle = await import(`~/articles/${slug}.vue`)
-    } catch (e) {
-      error({ statusCode: 404, message: 'Article not found' })
-    }
-  }
+    return { comp: params.slug }
+  },
+  components
 }
 </script>
