8ac2f3e6b0389da8a20e1b2908e1768e9d238256
Sebastien Chopin <seb@chopin.io>
2017-07-10 13:52:58 +0200

    Point to example

--- examples/async-component-injection/layouts/default.vue
+++ examples/async-component-injection/layouts/default.vue
@@ -5,7 +5,7 @@
     </div>
     <p class="links">
       <a href="https://twitter.com/nuxt_js" target="_blank"><img src="~assets/img/twitter.png"></a>
-      <a href="https://github.com/nuxt/nuxt.js" target="_blank"><img src="~assets/img/github.png"></a>
+      <a href="https://github.com/nuxt/nuxt.js/tree/dev/examples/async-component-injection" target="_blank"><img src="~assets/img/github.png"></a>
     </p>
     <div class="main">
       <nuxt/>
