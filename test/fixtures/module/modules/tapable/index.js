054098acf8a1cc7e83bbc7ffeb80cc6af153dbac
Pooya Parsa <pooya@pi0.ir>
2017-07-18 02:49:02 +0430

    > upgrade dependencies

--- test/fixtures/module/modules/tapable/index.js
+++ test/fixtures/module/modules/tapable/index.js
@@ -2,17 +2,17 @@ module.exports = function () {
   let ctr = 1
 
   // Add hook for module
-  this.nuxt.plugin('module', async moduleContainer => {
+  this.nuxt.plugin('module', moduleContainer => {
     this.nuxt.__module_hook = moduleContainer && ctr++
   })
 
   // Add hook for renderer
-  this.nuxt.plugin('renderer', async renderer => {
+  this.nuxt.plugin('renderer', renderer => {
     this.nuxt.__renderer_hook = renderer && ctr++
   })
 
   // Add hook for build
-  this.nuxt.plugin('build', async builder => {
+  this.nuxt.plugin('build', builder => {
     this.nuxt.__builder_hook = builder && ctr++
   })
 }
