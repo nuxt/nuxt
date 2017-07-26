3893e191ed31b8459fb15d782810a86d30d22456
SVogel <SVogel>
2017-07-26 11:27:59 +0200

    fixed vue apollo

--- examples/vue-apollo/nuxt.config.js
+++ examples/vue-apollo/nuxt.config.js
@@ -2,9 +2,6 @@ module.exports = {
   build: {
     vendor: ['vue-apollo', 'apollo-client']
   },
-  router: {
-    middleware: 'apollo'
-  },
   plugins: [
     // Will inject the plugin in the $root app and also in the context as `apolloProvider`
     { src: '~plugins/apollo.js', injectAs: 'apolloProvider' }
