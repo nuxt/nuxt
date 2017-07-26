018934eeedea83ccdfce6afe968e4e77ca19a762
SVogel <SVogel>
2017-07-26 11:34:41 +0200

    added paramterized car view

---
+++ examples/vue-apollo/pages/car/_id.vue
@@ -0,0 +1,33 @@
+<template>
+  <section>
+    <article class="car-art">
+      <span>{{Car.make}}</span><br>
+      <span>{{Car.model}}</span><br>
+      <span>{{Car.year}}</span>
+    </article>
+    <nuxt-link to="/">Home</nuxt-link>
+  </section>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+
+export default {
+  name: "Car",
+  apollo: {
+    Car: {
+      query: gql`query car($id: ID) {Car(id: $id) {make model year}}`,
+      variables () { 
+        return {
+          id: this.$route.params.id
+        }
+      }
+    }
+  },
+  data () {
+    return {
+      Car: {}
+    }
+  }
+}
+</script>
