8e157c7c0b93da64832b0d8cbbf9e8024507ace1
Sébastien Chopin <seb@chopin.io>
2017-07-30 19:49:12 +0200

    Update comment

--- examples/custom-server/server.js
+++ examples/custom-server/server.js
@@ -4,13 +4,13 @@ const { Nuxt, Builder } = require('nuxt')
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
 
-// Import and Set Nuxt.js options
+// Import and set Nuxt.js options
 let config = require('./nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
 
 const nuxt = new Nuxt(config)
 
-// Start build process if
+// Start build process in dev mode
 if (config.dev) {
   const builder = new Builder(nuxt)
   builder.build()
