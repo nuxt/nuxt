888a9ecbf246f256c159e541da2ddad074240636
Sebastien Chopin <seb@chopin.io>
2017-08-18 15:44:43 +0200

    Add spa example

---
+++ examples/spa/nuxt.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'spa',
+  loadingIndicator: {
+    /*
+    ** See https://github.com/nuxt/nuxt.js/tree/dev/lib/app/views/loading for available loading indicators
+    ** You can add a custom indicator by giving a path
+    ** Default: 'circle'
+    */
+    name: 'folding-cube',
+    /*
+    ** You can give custom options given to the template
+    ** See https://github.com/nuxt/nuxt.js/blob/dev/lib/app/views/loading/folding-cube.html
+    ** Default:
+    ** - color: '#3B8070'
+    ** - background: 'white'
+    */
+    color: '#222',
+    background: '#eee'
+  }
+}
