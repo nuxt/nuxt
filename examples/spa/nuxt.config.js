72cb339d10c9e66a2096ab488b4becad5fc5e87f
Sebastien Chopin <seb@chopin.io>
2017-08-21 11:38:30 +0200

    Update spa example to use meta tags

--- examples/spa/nuxt.config.js
+++ examples/spa/nuxt.config.js
@@ -5,6 +5,20 @@ module.exports = {
   */
   mode: 'spa',
   /*
+  ** Headers of the page (works with SPA!)
+  */
+  head: {
+    title: 'SPA mode with Nuxt.js',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '{{ description }}' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
   ** Add css for appear transition
   */
   css: ['~/assets/main.css'],
