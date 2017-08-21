d1a6b2bc80c10074228d80047774f06335510220
Sebastien Chopin <seb@chopin.io>
2017-08-21 11:42:17 +0200

    update meta description

--- examples/spa/nuxt.config.js
+++ examples/spa/nuxt.config.js
@@ -12,7 +12,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '{{ description }}' }
+      { hid: 'description', name: 'description', content: 'Single Page Application made with Nuxt.js' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
