d3867a2b705845fdc76c9af730cd5f5f81bbd3c7
Sebastien Chopin <seb@chopin.io>
2017-08-24 14:34:00 +0200

    Add css for dropdown

--- examples/uikit/pages/index.vue
+++ examples/uikit/pages/index.vue
@@ -4,7 +4,7 @@
     <button class="uk-button uk-button-default" title="Hello World" uk-tooltip>Hover</button>
     <div class="uk-inline">
       <button class="uk-button uk-button-default" type="button">Click</button>
-      <div uk-dropdown="mode: click">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</div>
+      <div uk-dropdown="mode: click" class="uk-dropdown">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</div>
     </div>
     <hr class="uk-divider-icon">
     <nuxt-link to="/about">About page</nuxt-link>
