9d43db475b86218db1b68515200a0ec887bb43a9
Sebastien Chopin <sebastien.chopin@neo9.fr>
2017-09-01 17:38:16 +0200

    Fix example

--- examples/i18n/plugins/i18n.js
+++ examples/i18n/plugins/i18n.js
@@ -3,11 +3,11 @@ import VueI18n from 'vue-i18n'
 
 Vue.use(VueI18n)
 
-export default ({ app, store }) => {
+export default ({ app, isClient, store, serverStore }) => {
   // Set i18n instance on app
   // This way we can use it in middleware and pages asyncData/fetch
   app.i18n = new VueI18n({
-    locale: store.state.locale,
+    locale: (isClient ? serverStore.locale : store.state.locale),
     fallbackLocale: 'en',
     messages: {
       'en': require('~/locales/en.json'),
