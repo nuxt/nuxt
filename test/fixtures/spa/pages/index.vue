75350cdd78d17e565b6c58d6c1aaa00c5146cbae
Pooya Parsa <pooya@pi0.ir>
2017-10-07 12:37:52 +0330

    test: add tests for spa

---
+++ test/fixtures/spa/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  Hello SPA!
+</div>
+</template>
+
+<script>
+export default {
+  layout: 'custom',
+  mounted () {
+    window.indexMounted = (+window.indexMounted) + 1
+    console.log('mounted')
+  }
+}
+</script>
