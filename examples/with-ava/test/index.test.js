d2a57bc3291b7c0d4276af78cc3811977a87b352
Clark Du <clark.duxin@gmail.com>
2017-10-20 14:41:06 +0800

    feat: update with-ava example

--- examples/with-ava/test/index.test.js
+++ examples/with-ava/test/index.test.js
@@ -1,11 +1,10 @@
 import test from 'ava'
-import Nuxt from 'nuxt'
+import { Nuxt, Builder } from 'nuxt'
 import { resolve } from 'path'
 
 // We keep the nuxt and server instance
 // So we can close them at the end of the test
 let nuxt = null
-let server = null
 
 // Init Nuxt.js and create a server listening on localhost:4000
 test.before('Init Nuxt.js', async t => {
@@ -15,9 +14,8 @@ test.before('Init Nuxt.js', async t => {
   config.rootDir = rootDir // project folder
   config.dev = false // production build
   nuxt = new Nuxt(config)
-  await nuxt.build()
-  server = new nuxt.Server(nuxt)
-  server.listen(4000, 'localhost')
+  await new Builder(nuxt).build()
+  await nuxt.listen(4000, 'localhost')
 })
 
 // Example of testing only generated html
@@ -39,6 +37,5 @@ test('Route / exits and render HTML with CSS applied', async t => {
 
 // Close server and ask nuxt to stop listening to file changes
 test.after('Closing server and nuxt.js', t => {
-  server.close()
   nuxt.close()
 })
