abe0da6a0759a63a9aece4a8192aa9533bcf8c72
Clark Du <clark.duxin@gmail.com>
2017-10-20 15:13:17 +0800

    feat: update with-sockets example

--- examples/with-sockets/server.js
+++ examples/with-sockets/server.js
@@ -1,4 +1,4 @@
-const Nuxt = require('nuxt')
+const { Nuxt, Builder } = require('nuxt')
 const app = require('express')()
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
@@ -8,17 +8,14 @@ const isProd = process.env.NODE_ENV === 'production'
 // We instantiate Nuxt.js with the options
 let config = require('./nuxt.config.js')
 config.dev = !isProd
-const nuxt = new Nuxt(config)
-app.use(nuxt.render)
 
-// Build only in dev mode
+const nuxt = new Nuxt(config)
+// Start build process in dev mode
 if (config.dev) {
-  nuxt.build()
-  .catch((error) => {
-    console.error(error) // eslint-disable-line no-console
-    process.exit(1)
-  })
+  const builder = new Builder(nuxt)
+  builder.build()
 }
+app.use(nuxt.render)
 
 // Listen the server
 server.listen(port, '0.0.0.0')
@@ -29,9 +26,9 @@ let messages = []
 io.on('connection', (socket) => {
   socket.on('last-messages', function (fn) {
     fn(messages.slice(-50))
-  });
+  })
   socket.on('send-message', function (message) {
     messages.push(message)
     socket.broadcast.emit('new-message', message)
   })
-});
+})
