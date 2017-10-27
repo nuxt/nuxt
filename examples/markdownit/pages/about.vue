1e4c95554aa725e48a04eb874e646e5e526fd1cd
Clark Du <clark.duxin@gmail.com>
2017-10-28 00:20:12 +0800

    feat: add markdown example

---
+++ examples/markdownit/pages/about.vue
@@ -0,0 +1,6 @@
+<template lang="md">
+  # Hello World!
+  Current route is: {{ $route.name }}
+
+  <nuxt-link to="/">Back home</nuxt-link>
+</template>
---
+++ examples/markdownit/pages/index.vue
@@ -0,0 +1,19 @@
+<template lang="md">
+  # Hello World!
+
+  Current route is: {{ $route.path }}
+
+  Data model is: {{ model }}
+
+  <nuxt-link to="/about">Goto About</nuxt-link>
+</template>
+
+<<script>
+export default {
+  data () {
+    return {
+      model: 'abc'
+    }
+  }
+}
+</script>
