5081789a62fd91e36eceaa7df204db2a6c9beec7
Sébastien Chopin <seb@chopin.io>
2017-10-28 11:01:23 +0200

    example: Fix pug page

--- examples/markdownit/pages/pug.vue
+++ examples/markdownit/pages/pug.vue
@@ -3,7 +3,8 @@
     h1 Pug Page
     div(language="md") Current route is: {{ $route.name }}
     div(language="md") Data model is: {{ model }}
-    a(href='/') Back Home
+    br
+    nuxt-link(to='/') Back Home
 </template>
 
 <script>
