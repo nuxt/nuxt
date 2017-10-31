dc7d4bfab7e04a295a0e59f52dd7d45216034b49
Atinux <sebastien.chopin@neo9.fr>
2017-10-31 18:33:46 +0100

    test: Add children fixPrepatch example

---
+++ test/fixtures/children/pages/patch/_id.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>_id: {{ date }}</h2>
+    <nuxt-child/>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      date: Date.now()
+    }
+  }
+}
+</script>
---
+++ test/fixtures/children/pages/patch/_id/child/_slug.vue
@@ -0,0 +1,13 @@
+<template>
+  <h4>_id: {{ date }}</h4>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    return {
+      date: Date.now()
+    }
+  }
+}
+</script>
