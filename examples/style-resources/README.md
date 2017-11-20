9fb2fdf4f5a97217d2b0f310b45afaa1dc7235db
Dmitri Efimenko <ezheg89@gmail.com>
2017-11-21 02:56:14 +0300

    build.styleResourcesLoader
    https://github.com/yenshih/style-resources-loader

---
+++ examples/style-resources/README.md
@@ -0,0 +1,3 @@
+# Using build.styleResources with Nuxt.js
+
+https://nuxtjs.org/examples
---
+++ examples/style-resources/pages/index.vue
@@ -0,0 +1,12 @@
+<style lang="scss">
+.main {
+  background: $main;
+}
+</style>
+
+<template>
+  <div class="main">
+    <h1>Welcome!</h1>
+    <nuxt-link to="/about">About page</nuxt-link>
+  </div>
+</template>
