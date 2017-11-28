40b9d23ea9866ca860a8f75076e7e3322ffe16bd
Clark Du <clark.duxin@gmail.com>
2017-11-28 22:05:17 +0800

    refactor: move redirect message to option.messges

--- test/basic.ssr.test.js
+++ test/basic.ssr.test.js
@@ -127,7 +127,7 @@ test('/redirect -> external link', async t => {
     }
   })
   t.is(headers.Location, 'https://nuxtjs.org')
-  t.true(html.includes('<div>redirecting.</div>'))
+  t.true(html.includes('<div>Redirecting to external page.</div>'))
 })
 
 test('/special-state -> check window.__NUXT__.test = true', async t => {
