7d5ecbfd08196100ffde903215442e8c261fe257
Clark Du <clark.duxin@gmail.com>
2017-12-08 22:56:14 +0800

    feat: lint jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     browser: true,
     node: true
   },
-  extends: 'standard',
+  extends: ["standard", "standard-jsx"],
   // required to lint *.vue files
   plugins: [
     'html'
