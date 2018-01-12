1dbeb132d25bdf732cbbadc15b000b956a5dd9bf
Pooya Parsa <pooya@pi0.ir>
2018-01-13 00:28:46 +0330

    test: add failing test for #2574

--- test/fixtures/module/modules/basic/index.js
+++ test/fixtures/module/modules/basic/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 module.exports = function basicModule(options, resolve) {
   // Add  vendor
   this.addVendor('lodash')
+  this.addVendor(['lodash', 'lodash'])
 
   // Add a plugin
   this.addPlugin(path.resolve(__dirname, 'reverse.js'))
