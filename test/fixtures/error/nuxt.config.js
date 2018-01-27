833f23ca7689e26e9a95cf9682ed9f67b54ef1ff
Clark Du <clark.duxin@gmail.com>
2018-01-27 23:29:42 +0800

    test: error hook

---
+++ test/fixtures/error/nuxt.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  hooks(hook) {
+    hook('build:done', nuxt => {
+      throw new Error('hook error')
+    })
+    hook('error', ({message}, from) => {
+      console.log(`[${from}]: ${message}`) // eslint-disable-line no-console
+    })
+  },
+  build: {
+    stats: false
+  }
+}
