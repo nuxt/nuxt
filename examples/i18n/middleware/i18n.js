03f89421c2cf9daf05f6b03174cb40c8a5a32eb4
Clark Du <clark.duxin@gmail.com>
2018-03-01 21:03:36 +0800

    fix: i18n lint issue

--- examples/i18n/middleware/i18n.js
+++ examples/i18n/middleware/i18n.js
@@ -1,5 +1,5 @@
 export default function ({ isHMR, app, store, route, params, error, redirect }) {
-  const defaultLocale = app.i18n.fallbackLocale;
+  const defaultLocale = app.i18n.fallbackLocale
   // If middleware is called from hot module replacement, ignore it
   if (isHMR) return
   // Get locale from params
@@ -11,10 +11,9 @@ export default function ({ isHMR, app, store, route, params, error, redirect }) 
   store.commit('SET_LANG', locale)
   app.i18n.locale = store.state.locale
   // If route is /<defaultLocale>/... -> redirect to /...
-  if (locale === defaultLocale && route.fullPath.indexOf('/'+defaultLocale) === 0) {
-
-    var toReplace = '^/'+defaultLocale
-    var re = new RegExp(toReplace); 
+  if (locale === defaultLocale && route.fullPath.indexOf('/' + defaultLocale) === 0) {
+    const toReplace = '^/' + defaultLocale
+    const re = new RegExp(toReplace)
     return redirect(
       route.fullPath.replace(re, '/')
     )
