0a219699e99b5186b1eca0f726559b745adf9e89
Nicolas Pennec <nicolas.pennec@corp.ovh.com>
2018-03-22 00:41:16 +0100

    copy props wholesale into component without function

---
+++ test/fixtures/with-config/components/test.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Hello {{ name }}!</h1>
+</template>
+
+<script>
+  export default {
+    props: ['name']
+  }
+</script>
--- test/fixtures/with-config/nuxt.config.js
+++ test/fixtures/with-config/nuxt.config.js
@@ -16,6 +16,14 @@ export default {
         {
           path: '/redirect/about-bis',
           redirect: '/about-bis'
+        },
+        {
+          name: 'hello-world',
+          path: '/hello-world',
+          component: '~/components/test.vue',
+          props: {
+            name: 'world'
+          }
         }
       ]
     }
