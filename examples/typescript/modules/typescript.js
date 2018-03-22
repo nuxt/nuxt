3efbd1b54529420c815d957771bff46305e16307
Clark Du <clark.duxin@gmail.com>
2018-03-22 17:16:06 +0800

    refactor: remove duplicate loader

--- examples/typescript/modules/typescript.js
+++ examples/typescript/modules/typescript.js
@@ -18,12 +18,6 @@ export default function () {
         tsLoader
       )
     )
-    // Add TypeScript loader for vue files
-    for (let rule of config.module.rules) {
-      if (rule.loader === 'vue-loader') {
-        rule.options.loaders.ts = tsLoader
-      }
-    }
     // Add .ts extension in webpack resolve
     if (config.resolve.extensions.indexOf('.ts') === -1) {
       config.resolve.extensions.push('.ts')
