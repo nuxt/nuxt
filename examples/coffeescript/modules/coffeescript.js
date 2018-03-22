919e3e7f8040c25962b43cf9ec5c887fb81af7de
Clark Du <clark.duxin@gmail.com>
2018-03-22 17:16:06 +0800

    refactor: remove duplicate loader

--- examples/coffeescript/modules/coffeescript.js
+++ examples/coffeescript/modules/coffeescript.js
@@ -9,12 +9,6 @@ export default function () {
   this.extendBuild(config => {
     // Add CoffeeScruot loader
     config.module.rules.push(coffeeLoader)
-    // Add CoffeeScript loader for vue files
-    for (let rule of config.module.rules) {
-      if (rule.loader === 'vue-loader') {
-        rule.options.loaders.coffee = coffeeLoader
-      }
-    }
     // Add .coffee extension in webpack resolve
     if (config.resolve.extensions.indexOf('.coffee') === -1) {
       config.resolve.extensions.push('.coffee')
