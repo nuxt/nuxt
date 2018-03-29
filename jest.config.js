7c9c4a5017133632e466e5f5f6bc33d87a78b227
Clark Du <clark.duxin@gmail.com>
2018-03-29 15:20:48 +0800

    test(coverage): ignore forked vue-ssr plugin

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   testEnvironment: 'node',
   coverageDirectory: './coverage/',
+  collectCoverageFrom: [
+    '!<rootDir>/lib/builder/webpack/plugins/vue/'
+  ],
   setupTestFrameworkScriptFile: './test/utils/setup',
   testPathIgnorePatterns: ['test/fixtures/.*/.*?/'],
   moduleFileExtensions: ['js', 'mjs', 'json'],
