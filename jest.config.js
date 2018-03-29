026541516e6898a95e0dbf6fc7d002fcc068e596
Clark Du <clark.duxin@gmail.com>
2018-03-29 17:30:16 +0800

    test(coverage): ignore lib/*.js coverage

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,8 @@ module.exports = {
   coverageDirectory: './coverage/',
   coveragePathIgnorePatterns: [
     '<rootDir>/node_modules/',
-    '<rootDir>/lib/builder/webpack/plugins/vue'
+    '<rootDir>/lib/builder/webpack/plugins/vue',
+    '<rootDir>/lib/[^/]*\\.js'
   ],
   setupTestFrameworkScriptFile: './test/utils/setup',
   testPathIgnorePatterns: ['test/fixtures/.*/.*?/'],
