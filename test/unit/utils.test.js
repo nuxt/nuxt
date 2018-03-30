84133d91c750b4f1cce2fca1fd2c42999b9f52be
Clark Du <clark.duxin@gmail.com>
2018-03-30 11:10:49 +0800

    test: perf-loader

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -1,4 +1,7 @@
+
+import path from 'path'
 import { Utils } from '../utils'
+import PerfLoader from '../../lib/builder/webpack/utils/perf-loader'
 
 describe('utils', () => {
   test('encodeHtml', () => {
@@ -196,6 +199,34 @@ describe('utils', () => {
 
     const chainedFn = Utils.chainFn(firstFn, secondFn)
     expect(chainedFn({}, 10)).toEqual({ foo: 11, bar: 12 })
+  })
+
+  test('performance loader', () => {
+    const perfLoader = new PerfLoader({
+      dev: true,
+      build: {
+        parallel: true,
+        cache: true
+      }
+    })
+    const js = { name: 'js', poolTimeout: Infinity }
+    const css = { name: 'css', poolTimeout: Infinity }
+    expect(perfLoader.workerPools).toMatchObject({ js, css })
+
+    const warmup = jest.fn()
+    perfLoader.threadLoader = { warmup }
+    perfLoader.warmup()
+    expect(warmup).toHaveBeenCalledTimes(2)
+    expect(warmup).toHaveBeenCalledWith(js, ['babel-loader', 'babel-preset-env'])
+    expect(warmup).toHaveBeenCalledWith(css, ['css-loader'])
+
+    const loaders = perfLoader.pool('js', { loader: 'test-perf-loader' })
+    const cacheDirectory = path.resolve('node_modules/.cache/cache-loader')
+    expect(loaders).toMatchObject([
+      { loader: 'cache-loader', options: { cacheDirectory } },
+      { loader: 'thread-loader', options: js },
+      { loader: 'test-perf-loader' }
+    ])
   })
 })
 
