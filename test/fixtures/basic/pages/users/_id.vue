cbe1fbde256bf3efc3060271bf7185309f5cce28
Daniel Sim <daniel.ssq89@gmail.com>
2018-04-05 23:44:10 +0800

    add a simple e2e test

--- test/fixtures/basic/pages/users/_id.vue
+++ test/fixtures/basic/pages/users/_id.vue
@@ -1,9 +1,19 @@
 <template>
-  <h1>User: {{ id }}</h1>
+  <div>
+    <h1>User: {{ id }}</h1>
+    <h2>Type: {{ typeof numericId }}</h2>
+    <h3>Plus One: {{ numericId + 1 }}</h3>
+  </div>
 </template>
 
 <script>
 export default {
+  props: ['numericId'],
+  routeToProps (route) {
+    return {
+      numericId: parseInt(route.params.id)
+    }
+  },
   asyncData({ params, payload }) {
     if (payload) return payload
     return { id: params.id }
