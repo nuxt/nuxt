b7d54b2c754fac0473514daff4bbdd3e2b243657
Clark Du <clark.duxin@gmail.com>
2018-04-16 00:26:45 +0800

    test: add open-in-editor and youch test

--- test/unit/basic.dev.test.js
+++ test/unit/basic.dev.test.js
@@ -1,4 +1,4 @@
-import { loadFixture, getPort, Nuxt, Builder } from '../utils'
+import { loadFixture, getPort, Nuxt, Builder, rp } from '../utils'
 
 let port
 const url = route => 'http://localhost:' + port + route
@@ -9,6 +9,7 @@ describe('basic dev', () => {
   beforeAll(async () => {
     const config = loadFixture('basic', {
       dev: true,
+      debug: true,
       buildDir: '.nuxt-dev',
       build: {
         stats: 'none'
@@ -46,6 +47,56 @@ describe('basic dev', () => {
   //     t.is(err.response.body, '')
   //   }
   // })
+
+  test('/__open-in-editor (open-in-editor)', async () => {
+    const { body } = await rp(
+      url('/__open-in-editor?file=pages/index.vue'),
+      { resolveWithFullResponse: true }
+    )
+    expect(body).toBe('')
+  })
+
+  test('/__open-in-editor should return error (open-in-editor)', async () => {
+    await expect(rp(url('/__open-in-editor?file='))).rejects.toMatchObject({
+      statusCode: 500,
+      error: 'launch-editor-middleware: required query param "file" is missing.'
+    })
+  })
+
+  test('/error should return error stack trace (Youch)', async () => {
+    await expect(nuxt.renderAndGetWindow(url('/error'))).rejects.toMatchObject({
+      statusCode: 500
+    })
+  })
+
+  test('/error no source-map (Youch)', async () => {
+    const sourceMaps = nuxt.renderer.resources.serverBundle.maps
+    nuxt.renderer.resources.serverBundle.maps = {}
+
+    // const errorSpy = await interceptError()
+    await expect(nuxt.renderAndGetWindow(url('/error'))).rejects.toMatchObject({
+      statusCode: 500
+    })
+
+    nuxt.renderer.resources.serverBundle.maps = sourceMaps
+  })
+
+  test('/error should return json format error (Youch)', async () => {
+    const opts = {
+      headers: {
+        accept: 'application/json'
+      },
+      resolveWithFullResponse: true
+    }
+    await expect(rp(url('/error'), opts)).rejects.toMatchObject({
+      statusCode: 500,
+      response: {
+        headers: {
+          'content-type': 'text/json; charset=utf-8'
+        }
+      }
+    })
+  })
 
   // Close server and ask nuxt to stop listening to file changes
   afterAll(async () => {
