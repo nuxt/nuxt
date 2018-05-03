25d752630a59238b959fcdc9285ecf412fb0ec24
Clark Du <clark.duxin@gmail.com>
2018-05-03 20:49:37 +0100

    test: render:context hook

--- test/unit/module.test.js
+++ test/unit/module.test.js
@@ -53,6 +53,11 @@ describe('module', () => {
     expect(response).toBe('Use external middleware')
   })
 
+  test('Hooks - render context', async () => {
+    await nuxt.renderRoute('/render-context')
+    expect(nuxt.__render_context).toBeTruthy()
+  })
+
   // Close server and ask nuxt to stop listening to file changes
   afterAll(async () => {
     await nuxt.close()
