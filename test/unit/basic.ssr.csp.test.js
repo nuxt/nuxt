e8063ca7d2305d8e25e0351055662614e92bcfa1
Alexander Lichter <manniL@gmx.net>
2018-07-23 23:05:26 +0200

    fix(csp): remove duplicate sha-256 hashes

--- test/unit/basic.ssr.csp.test.js
+++ test/unit/basic.ssr.csp.test.js
@@ -1,4 +1,4 @@
-import { loadFixture, getPort, Nuxt, rp } from '../utils'
+import { getPort, loadFixture, Nuxt, rp } from '../utils'
 
 let port
 const url = route => 'http://localhost:' + port + route
@@ -41,6 +41,23 @@ describe('basic ssr csp', () => {
   )
 
   test(
+    'Contain only unique hashes in header when csp is set',
+    async () => {
+      const nuxt = await startCSPTestServer(true)
+      const { headers } = await rp(url('/stateless'), {
+        resolveWithFullResponse: true
+      })
+
+      const hashes = headers['content-security-policy'].split(' ').filter(s => s.startsWith('\'sha256-'))
+      const uniqueHashes = [...new Set(hashes)]
+
+      expect(uniqueHashes.length).toBe(hashes.length)
+
+      await nuxt.close()
+    }
+  )
+
+  test(
     'Contain Content-Security-Policy header, when csp.allowedSources set',
     async () => {
       const cspOption = {
@@ -77,7 +94,7 @@ describe('basic ssr csp', () => {
       })
 
       expect(headers['content-security-policy']).toMatch(/default-src 'none'/)
-      expect(headers['content-security-policy']).toMatch(/script-src 'self' 'sha256-.*'/)
+      expect(headers['content-security-policy']).toMatch(/script-src 'sha256-(.*)?' 'self'/)
       expect(headers['content-security-policy'].includes('https://example.com')).toBe(true)
       expect(headers['content-security-policy'].includes('https://example.io')).toBe(true)
 
@@ -101,7 +118,39 @@ describe('basic ssr csp', () => {
       })
 
       expect(headers['content-security-policy']).toMatch(/default-src 'none'/)
-      expect(headers['content-security-policy']).toMatch(/script-src 'self' 'sha256-.*'/)
+      expect(headers['content-security-policy']).toMatch(/script-src 'sha256-.*' 'self'$/)
+
+      await nuxt.close()
+    }
+  )
+
+  test(
+    'Contain only unique hashes in header when csp.policies is set',
+    async () => {
+      const policies = {
+        'default-src': [`'self'`],
+        'script-src': [`'self'`],
+        'style-src': [`'self'`]
+      }
+
+      const nuxt = await startCSPTestServer({
+        policies
+      })
+
+      for (let i = 0; i < 5; i++) {
+        await rp(url('/stateless'), {
+          resolveWithFullResponse: true
+        })
+      }
+
+      const { headers } = await rp(url('/stateful'), {
+        resolveWithFullResponse: true
+      })
+
+      const hashes = headers['content-security-policy'].split(' ').filter(s => s.startsWith('\'sha256-'))
+      const uniqueHashes = [...new Set(hashes)]
+
+      expect(uniqueHashes.length).toBe(hashes.length)
 
       await nuxt.close()
     }
