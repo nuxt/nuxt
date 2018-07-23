5637628e4d03f12fb3c459676d69fb668261ad9f
Alexander Lichter <manniL@gmx.net>
2018-07-23 21:47:14 +0200

    tests: increase timeout for appveyor

--- test/utils/setup.js
+++ test/utils/setup.js
@@ -4,8 +4,9 @@ require('consola').clear().add({
   log: jest.fn()
 })
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 1000
-
 const isAppveyor = !!process.env.APPVEYOR
+
+jest.setTimeout((isAppveyor ? 120 : 60) * 1000)
+
 describe.skip.appveyor = isAppveyor ? describe.skip : describe
 test.skip.appveyor = isAppveyor ? test.skip : test
