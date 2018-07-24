2d349325fd9c1c04cbe2a10b3caaa1e1a25bb72d
Clark Du <clark.duxin@gmail.com>
2018-07-24 15:39:12 +0100

    debug: appveyor

--- test/fixtures/ssr/ssr.test.js
+++ test/fixtures/ssr/ssr.test.js
@@ -1,5 +1,3 @@
 const { buildFixture } = require('../../utils/build')
 
-describe.skip.appveyor('cli build', () => {
-  buildFixture('ssr')
-})
+buildFixture('ssr')
--- test/unit/ssr.test.js
+++ test/unit/ssr.test.js
@@ -61,7 +61,7 @@ const stressTest = async (_url, concurrency = 2, steps = 4) => {
   expect(statusCodes[200]).toBe(concurrency * steps)
 }
 
-describe.skip.appveyor('ssr', () => {
+describe('ssr', () => {
   beforeAll(async () => {
     const config = loadFixture('ssr')
     nuxt = new Nuxt(config)
