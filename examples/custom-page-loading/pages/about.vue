4a0951cefdda25429ba22459e48013e7964903eb
Jonas Galvez <jonasgalvez@gmail.com>
2018-08-03 17:55:35 -0300

    Linting (2)

--- examples/custom-page-loading/pages/about.vue
+++ examples/custom-page-loading/pages/about.vue
@@ -3,7 +3,7 @@
     <p>About Page</p>
     <p>It should take 5 seconds for the loader to disappear</p>>
     <p>It should take 5 seconds for the route to change after you
-      <span class="link" @click="goToFinal">click here</span></p>
+    <span class="link" @click="goToFinal">click here</span></p>
   </div>
 </template>
 
