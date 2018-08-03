19032afd31a0d9d61aadbb756cc0589202df386a
Jonas Galvez <jonasgalvez@gmail.com>
2018-08-03 19:18:33 -0300

    Fix typo

--- examples/custom-page-loading/pages/about.vue
+++ examples/custom-page-loading/pages/about.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <p>About Page</p>
-    <p>It should take 5 seconds for the loader to disappear</p>>
+    <p>It should take 5 seconds for the loader to disappear</p>
     <p>It should take 5 seconds for the route to change after you
     <span class="link" @click="goToFinal">click here</span></p>
   </div>
