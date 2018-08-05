0e42e98751ac4fb91c513fba0006f05850719f13
Jonas Galvez <jonasgalvez@gmail.com>
2018-08-05 18:56:49 -0300

    Add `loading: false` option to pages (#3629)
    * Add hasManualLoading
    
    * Add hasManualLoading (2)
    
    * Debugging
    
    * Debugging (2)
    
    * Change 'manual' to false
    
    * Add custom-page-loading example
    
    * Add custom-page-loading example (2)
    
    * Changed approach
    
    * Added custom-page-loading/README.md
    
    * Change running loading time in about.vue
    
    * Patch loadAsyncComponents
    
    * Added final page
    
    * Changed approach (2)
    
    * Fixed example
    
    * Fixed example (2)
    
    * Fix example package name
    
    * Linting
    
    * Improved examples (more to the point)
    
    * Linting (2)
    
    * Fix typo
    
    * Adjust indentation
    
    * Added noloading.vue to basic fixture
    
    * Added noloading tests
    
    * Linting (3)
    
    * Debugging test
    
    * Linting (4)
    
    * Debugging test (2)
    
    * Debugging test (3)

---
+++ test/fixtures/basic/pages/noloading.vue
@@ -0,0 +1,22 @@
+<template>
+  <p>{{ name }}</p>
+</template>
+
+<script>
+export default {
+  loading: false,
+  asyncData() {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve({ name: 'Nuxt.js' }), 10)
+    })
+  },
+  watch: {
+    $route(to) {
+      this.$nuxt.$loading.start()
+    }
+  },
+  mounted() {
+    setTimeout(() => this.$nuxt.$loading.finish(), 1500)
+  }
+}
+</script>
