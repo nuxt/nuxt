b879e9303738ae26f70f14c3cc1a7002e9dead47
Jonas Galvez <jonasgalvez@gmail.com>
2018-08-05 16:15:22 -0300

    Refactored CLI test

--- test/unit/cli.test.js
+++ test/unit/cli.test.js
@@ -6,10 +6,48 @@ let port
 const rootDir = resolve(__dirname, '..', 'fixtures/cli')
 
 const url = route => 'http://localhost:' + port + route
-
 const nuxtBin = resolve(__dirname, '..', '..', 'bin', 'nuxt')
+const genHandlers = (cmd) => {
+  cmd.on()
+}
+
+const killNuxt = async (nuxtInt) => {
+  nuxtStart.kill()
+
+  // Wait max 10s for the process to be killed
+  timeout = await waitUntil(() => exitCode !== undefined, 10)
+
+  if (timeout === true) {
+    console.warn( // eslint-disable-line no-console
+      `we were unable to automatically kill the child process with pid: ${
+        nuxtStart.pid
+      }`
+    )
+  }
+}
 
 describe.skip.appveyor('cli', () => {
+
+  test('nuxt dev', async () => {
+    let stdout = ''
+    let exitcode
+    const env = process.env
+    env.PORT = port = await getPort()
+
+    const nuxtDev = spawn('node', [nuxtBin, 'dev', rootDir], { env })
+    nuxtDev.stdout.on('data', data => { stdout += data })
+    nuxtStart.on('close', code => { exitCode = code })
+    
+    const customFilePath = join(rootDir, 'custom.file')
+    await writeFile(customFilePath, 'This file is used to test custom chokidar watchers.')
+    await new Promise((resolve) => setTimeout(() => resolve()), 200)
+    expect(stdout.includes('[custom.file] changed')).toBe(true)
+
+    await killNuxt(nuxtStart)    
+    expect(exitCode).toBe(null)
+
+  })
+
   test('nuxt start', async () => {
     let stdout = ''
     let error
@@ -20,17 +58,9 @@ describe.skip.appveyor('cli', () => {
 
     const nuxtStart = spawn('node', [nuxtBin, 'start', rootDir], { env })
 
-    nuxtStart.stdout.on('data', data => {
-      stdout += data
-    })
-
-    nuxtStart.on('error', err => {
-      error = err
-    })
-
-    nuxtStart.on('close', code => {
-      exitCode = code
-    })
+    nuxtStart.stdout.on('data', data => { stdout += data })
+    nuxtStart.on('error', err => { error = err })
+    nuxtStart.on('close', code => { exitCode = code })
 
     // Wait max 20s for the starting
     let timeout = await waitUntil(() => stdout.includes('Listening on'))
@@ -45,18 +75,7 @@ describe.skip.appveyor('cli', () => {
     const html = await rp(url('/'))
     expect(html).toMatch(('<div>CLI Test</div>'))
 
-    nuxtStart.kill()
-
-    // Wait max 10s for the process to be killed
-    timeout = await waitUntil(() => exitCode !== undefined, 10)
-
-    if (timeout === true) {
-      console.warn( // eslint-disable-line no-console
-        `we were unable to automatically kill the child process with pid: ${
-          nuxtStart.pid
-        }`
-      )
-    }
+    await killNuxt(nuxtStart)
 
     expect(exitCode).toBe(null)
   })
