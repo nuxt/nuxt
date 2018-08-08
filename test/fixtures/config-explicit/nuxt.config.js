04a2cd9c9febecf4e2e57d6321c2df7225703593
Alexander Lichter <manniL@gmx.net>
2018-08-08 14:21:54 +0200

    feat: add Vue.config configuration to nuxt.config.js

---
+++ test/fixtures/config-explicit/nuxt.config.js
@@ -0,0 +1,12 @@
+import path from 'path'
+
+export default {
+  modulesDir: path.join(__dirname, '..', '..', '..', 'node_modules'),
+  transition: false,
+  vueConfig: {
+    silent: false,
+    performance: true,
+    devtools: true,
+    productTip: true,
+  }
+}
