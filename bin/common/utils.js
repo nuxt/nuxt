52780d3257aecaa52e0a413256b16597e6221bda
Alexander Lichter <manniL@gmx.net>
2018-08-08 21:18:06 +0200

    feat: add NUXT_HOST and NUXT_PORT for host retrieval (#3651)
    related: #3609

--- bin/common/utils.js
+++ bin/common/utils.js
@@ -48,9 +48,10 @@ exports.loadNuxtConfig = (argv) => {
 
 exports.getLatestHost = (argv) => {
   const port =
-    argv.port || process.env.PORT || process.env.npm_package_config_nuxt_port
+    argv.port || process.env.NUXT_PORT || process.env.PORT || process.env.npm_package_config_nuxt_port
   const host =
     argv.hostname ||
+    process.env.NUXT_HOST ||
     process.env.HOST ||
     process.env.npm_package_config_nuxt_host
 
