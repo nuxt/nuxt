ca46ad77a71f5b9e8ee65984c9342009db46c27d
Jonas Galvez <jonasgalvez@gmail.com>
2018-08-08 03:10:53 -0300

    Fix defaults

--- test/fixtures/basic/nuxt.config.js
+++ test/fixtures/basic/nuxt.config.js
@@ -26,13 +26,6 @@ export default {
     }
   },
   modulesDir: path.join(__dirname, '..', '..', '..', 'node_modules'),
-  render: {
-    dist: {
-      setHeaders (res) {
-        res.setHeader('X-Custom', 'custom header')
-      }
-    }
-  },
   hooks: {
     ready(nuxt) {
       nuxt.__hook_called__ = true
