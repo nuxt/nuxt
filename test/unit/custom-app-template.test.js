960f4fe47e389af045a5cc76147699a1f638521e
Alexander Lichter <manniL@gmx.net>
2018-08-10 17:23:09 +0200

    feat: make appTemplatePath customizable (#3678)
    related: #1925

---
+++ test/unit/custom-app-template.test.js
@@ -0,0 +1,23 @@
+import { getPort, loadFixture, Nuxt } from '../utils'
+
+let port
+let nuxt = null
+
+describe('custom-app-template', () => {
+  beforeAll(async () => {
+    const options = loadFixture('custom-app-template')
+    nuxt = new Nuxt(options)
+    port = await getPort()
+    await nuxt.listen(port, '0.0.0.0')
+  })
+  test('/', async () => {
+    const { html } = await nuxt.renderRoute('/')
+    expect(html.includes('<p>My Template</p>')).toBe(true)
+    expect(html.includes('<h1>Custom!</h1>')).toBe(true)
+  })
+
+  // Close server and ask nuxt to stop listening to file changes
+  afterAll(async () => {
+    await nuxt.close()
+  })
+})
