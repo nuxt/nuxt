248700e8b44a22be47757e15bc37616eaa728427
Jonas Galvez <jonasgalvez@gmail.com>
2018-08-14 13:36:37 -0300

    Debugging

--- test/unit/cli.test.js
+++ test/unit/cli.test.js
@@ -19,44 +19,44 @@ const close = async (nuxtInt) => {
 }
 
 describe.skip.appveyor('cli', () => {
-  test('nuxt dev', async () => {
-    let stdout = ''
-    const env = process.env
-    env.PORT = port = await getPort()
+  // test('nuxt dev', async () => {
+  //   let stdout = ''
+  //   const env = process.env
+  //   env.PORT = port = await getPort()
 
-    const nuxtDev = spawn('node', [nuxtBin, 'dev', rootDir], { env })
-    nuxtDev.stdout.on('data', (data) => { stdout += data })
+  //   const nuxtDev = spawn('node', [nuxtBin, 'dev', rootDir], { env })
+  //   nuxtDev.stdout.on('data', (data) => { stdout += data })
 
-    // Wait max 20s for the starting
-    await waitUntil(() => stdout.includes(`${port}`))
+  //   // Wait max 20s for the starting
+  //   await waitUntil(() => stdout.includes(`${port}`))
 
-    // Change file specified in `watchers` (nuxt.config.js)
-    const customFilePath = join(rootDir, 'custom.file')
-    writeFileSync(customFilePath, 'This file is used to test custom chokidar watchers.')
+  //   // Change file specified in `watchers` (nuxt.config.js)
+  //   const customFilePath = join(rootDir, 'custom.file')
+  //   writeFileSync(customFilePath, 'This file is used to test custom chokidar watchers.')
 
-    // Change file specified in `serverMiddleware` (nuxt.config.js)
-    const serverMiddlewarePath = join(rootDir, 'middleware.js')
-    writeFileSync(serverMiddlewarePath, '// This file is used to test custom chokidar watchers.\n')
+  //   // Change file specified in `serverMiddleware` (nuxt.config.js)
+  //   const serverMiddlewarePath = join(rootDir, 'middleware.js')
+  //   writeFileSync(serverMiddlewarePath, '// This file is used to test custom chokidar watchers.\n')
 
-    // Wait max 20s for picking up changes
-    await waitUntil(() => {
-      const match = stdout.match(/Compiled client/g)
-      return match && match.length === 3
-    })
+  //   // Wait max 20s for picking up changes
+  //   await waitUntil(() => {
+  //     const match = stdout.match(/Compiled client/g)
+  //     return match && match.length === 3
+  //   })
 
-    // Must see two modifications in the log
-    expect(stdout.match(/custom\.file/g).length).toBe(1)
-    expect(stdout.match(/middleware\.js/g).length).toBe(1)
+  //   // Must see two modifications in the log
+  //   expect(stdout.match(/custom\.file/g).length).toBe(1)
+  //   expect(stdout.match(/middleware\.js/g).length).toBe(1)
 
-    await close(nuxtDev)
-  })
+  //   await close(nuxtDev)
+  // })
 
   test('nuxt start', async () => {
     let stdout = ''
     let error
 
     const env = process.env
-    env.PORT = port = await getPort()
+    env.PORT = port = 4444 // await getPort()
 
     await new Promise((resolve) => {
       const nuxtBuild = spawn('node', [nuxtBin, 'build', rootDir], { env })
@@ -68,9 +68,9 @@ describe.skip.appveyor('cli', () => {
     nuxtStart.stdout.on('data', (data) => { stdout += data })
     nuxtStart.on('error', (err) => { error = err })
 
-    // Wait max 20s for the starting
-    if (await waitUntil(() => stdout.includes(`${port}`))) {
-      error = 'server failed to start successfully in 20 seconds'
+    // Wait max 40s for the starting
+    if (await waitUntil(() => stdout.includes(`${port}`), 40)) {
+      error = 'server failed to start successfully in 40 seconds'
     }
 
     expect(error).toBe(undefined)
