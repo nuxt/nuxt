b28075a3f11d5682d1726ddfce4edcdeedef0066
Jonas Galvez <jonasgalvez@gmail.com>
2018-08-25 02:45:43 -0300

    Fix test (6)

--- test/e2e/basic.browser.test.js
+++ test/e2e/basic.browser.test.js
@@ -122,12 +122,6 @@ describe('basic browser', () => {
     expect(error.statusCode).toBe(404)
     expect(error.message).toBe('This page could not be found')
   })
-  
-  test('/validate?valid=true', async () => {
-    await page.nuxt.navigate('/validate?valid=true')
-
-    expect(await page.$text('h1')).toBe('I am valid')
-  })
 
   test('/validate-async should display a 404', async () => {
     await page.nuxt.navigate('/validate-async')
@@ -136,6 +130,12 @@ describe('basic browser', () => {
 
     expect(error.statusCode).toBe(404)
     expect(error.message).toBe('This page could not be found')
+  })
+
+  test('/validate?valid=true', async () => {
+    await page.nuxt.navigate('/validate?valid=true')
+
+    expect(await page.$text('h1')).toBe('I am valid')
   })
 
   test('/validate-async?valid=true', async () => {
