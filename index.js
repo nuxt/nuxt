7cdfc1f33ae77c525b07afd80e178aa0c05805e6
Pooya Parsa <pyapar@gmail.com>
2018-08-31 03:26:15 +0430

    update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const path = require('path')
 if (fs.existsSync(path.resolve(__dirname, '.babelrc'))) {
   // Use esm version when using linked repository to prevent builds
   const requireModule = require('esm')(module, {})
-  module.exports = requireModule('./nuxt').default
+  module.exports = requireModule('./lib/index.js').default
 } else {
   // Use production bundle by default
   module.exports = require('./dist/nuxt.js')
