3b7d662c3f22e465f20af406c7401c012f16d18d
Clark Du <clark.duxin@gmail.com>
2018-08-31 17:26:45 +0100

    refactor: cache buildins map

--- build/builtins.js
+++ build/builtins.js
@@ -12,9 +12,12 @@ const builtins = (builtinModules || Object.keys(process.binding('natives')))
   .filter(x => !/^_|^(internal|v8|node-inspect)\/|\//.test(x) && !blacklist.includes(x))
   .sort()
 
-export const builtinsMap = () => builtins.reduce((obj, builtin) => {
+let builtinsObj = null
+
+const convertToObj = () => builtins.reduce((obj, builtin) => {
   obj[builtin] = true
   return obj
-}, {})
+}, (builtinsObj = {}))
 
+export const builtinsMap = () => builtinsObj || convertToObj()
 export default builtins
