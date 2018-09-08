f87992530e4ac42afa73e9108596ecb8bf623057
Alexander Lichter <manniL@gmx.net>
2018-09-08 21:48:46 +0100

    feat(webpack/client): minify extracted css assets (#3857)

---
+++ test/unit/extract-css.test.js
@@ -0,0 +1,14 @@
+import { resolve } from 'path'
+import fs from 'fs'
+import { promisify } from 'util'
+
+const readFile = promisify(fs.readFile)
+
+describe('extract css', () => {
+  test('Verify global.css has been extracted and minified', async () => {
+    const pathToMinifiedGlobalCss = resolve(__dirname, '..', 'fixtures/extract-css/.nuxt/dist/client/90516105347f397aade6.css')
+    const content = await readFile(pathToMinifiedGlobalCss, 'utf-8')
+    const expectedContent = 'h1[data-v-180e2718]{color:red}.container[data-v-180e2718]{-ms-grid-columns:60px 60px 60px 60px 60px;-ms-grid-rows:30px 30px;display:-ms-grid;display:grid;grid-auto-flow:row;grid-template-columns:60px 60px 60px 60px 60px;grid-template-rows:30px 30px}'
+    expect(content).toBe(expectedContent)
+  })
+})
