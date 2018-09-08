c25baf99837b24e8ae6a027da41dd442da0745e9
Clark Du <clark.duxin@gmail.com>
2018-09-08 23:34:19 +0100

    test: build.loaders

--- test/unit/basic.dev.test.js
+++ test/unit/basic.dev.test.js
@@ -8,6 +8,7 @@ let nuxt = null
 let builder = null
 let transpile = null
 let output = null
+let loadersOptions
 
 describe('basic dev', () => {
   beforeAll(async () => {
@@ -26,11 +27,17 @@ describe('basic dev', () => {
           'vue\\.test\\.js',
           /vue-test/
         ],
-        extend({ module: { rules }, output: wpOutput }, { isClient }) {
+        loaders: {
+          cssModules: {
+            localIdentName: '[hash:base64:6]'
+          }
+        },
+        extend({ module: { rules }, output: wpOutput }, { isClient, loaders }) {
           if (isClient) {
             const babelLoader = rules.find(loader => loader.test.test('.jsx'))
             transpile = file => !babelLoader.exclude(file)
             output = wpOutput
+            loadersOptions = loaders
           }
         }
       }
@@ -60,6 +67,16 @@ describe('basic dev', () => {
     expect(consola.warn).toBeCalledWith(
       'Notice: Please do not use contenthash in dev mode to prevent memory leak'
     )
+  })
+
+  test('Config: build.loaders', () => {
+    expect(Object.keys(loadersOptions)).toHaveLength(12)
+    expect(loadersOptions).toHaveProperty(
+      'file', 'fontUrl', 'imgUrl', 'pugPlain', 'vue',
+      'css', 'cssModules', 'less', 'sass', 'scss', 'stylus', 'vueStyle'
+    )
+    const { cssModules } = loadersOptions
+    expect(cssModules.localIdentName).toBe('[hash:base64:6]')
   })
 
   test('/stateless', async () => {
