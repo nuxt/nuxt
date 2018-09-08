b74d537cbada7b52b91be7d36a815b81c4805676
Alexander Lichter <manniL@gmx.net>
2018-09-08 21:18:14 +0100

    feat(renderer): make compression middleware customizable (#3863)

--- test/fixtures/with-config/with-config.test.js
+++ test/fixtures/with-config/with-config.test.js
@@ -1,13 +1,22 @@
 import consola from 'consola'
 import { buildFixture } from '../../utils/build'
 
+let customCompressionMiddlewareFunctionName
+const hooks = [
+  ['render:errorMiddleware', (app) => {
+    customCompressionMiddlewareFunctionName = app.stack[0].handle.name
+  }]
+]
+
 describe('with-config', () => {
   buildFixture('with-config', () => {
     expect(consola.warn).toHaveBeenCalledTimes(1)
+    expect(consola.fatal).toHaveBeenCalledTimes(0)
     expect(consola.warn.mock.calls[0]).toMatchObject([{
       message: 'Found 2 plugins that match the configuration, suggest to specify extension:',
       additional: expect.stringContaining('plugins/test.json'),
       badge: true
     }])
-  })
+    expect(customCompressionMiddlewareFunctionName).toBe('damn')
+  }, hooks)
 })
--- test/utils/build.js
+++ test/utils/build.js
@@ -1,10 +1,11 @@
 import { loadFixture, Nuxt, Builder } from './index'
 
-export const buildFixture = function (fixture, callback) {
+export const buildFixture = function (fixture, callback, hooks = []) {
   test(`Build ${fixture}`, async () => {
     const config = await loadFixture(fixture)
     const nuxt = new Nuxt(config)
     const buildDone = jest.fn()
+    hooks.forEach(([hook, fn]) => nuxt.hook(hook, fn))
     nuxt.hook('build:done', buildDone)
     const builder = await new Builder(nuxt).build()
     // 2: BUILD_DONE
