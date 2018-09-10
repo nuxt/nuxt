d8e267e64c709efee9dcf39dac2cdef71b0022e6
pimlie <pimlie@hotmail.com>
2018-09-10 21:31:11 +0200

    feat: throw fatal error when buildDir or generate.dir equals rootDir
    When setting eg generate.dir to .. you will complete remove your project when you run nuxt generate. Added a check in options.js to throw an error when this would be the case

--- test/unit/basic.fail.generate.test.js
+++ test/unit/basic.fail.generate.test.js
@@ -17,4 +17,28 @@ describe('basic fail generate', () => {
       expect(e.message).toBe('Not today!')
     })
   })
+
+  test('Fails when generate.dir lower then root dir', async () => {
+    const options = await loadFixture('basic', {
+      generate: { dir: '.' }
+    })
+
+    expect(() => {
+      new Nuxt(options) /* eslint-disable-line no-new */
+    }).toThrow(
+      'options.generate.dir cannot be lower than or equal to rootDir'
+    )
+  })
+
+  test('generate.dir can be on same level as root', async () => {
+    const options = await loadFixture('basic', {
+      generate: { dir: '../basic-dist' }
+    })
+
+    expect(() => {
+      new Nuxt(options) /* eslint-disable-line no-new */
+    }).not.toThrow(
+      'options.generate.dir cannot be lower than or equal to rootDir'
+    )
+  })
 })
