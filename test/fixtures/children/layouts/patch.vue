a4039fb4809280c64d42cd7d01c89aaab2b64b7e
pimlie <pimlie@hotmail.com>
2018-09-21 11:38:46 +0200

    keep exposing $nuxt also for NODE_ENV=test

--- test/fixtures/children/layouts/patch.vue
+++ test/fixtures/children/layouts/patch.vue
@@ -13,14 +13,3 @@
     </ul>
   </div>
 </template>
-
-<script>
-export default {
-  created() {
-    if (typeof window !== 'undefined') {
-      // expose nuxt for e2e tests
-      window.$nuxt = this.$root
-    }
-  }
-}
-</script>
