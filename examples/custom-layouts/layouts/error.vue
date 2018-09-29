1dfddd1aa30b5058da80f4b4f53ac8b93afc34b6
Veres Lajos <vlajos@gmail.com>
2018-09-29 20:26:11 +0100

    * Typofixes - https://github.com/vlajos/misspell-fixer

--- examples/custom-layouts/layouts/error.vue
+++ examples/custom-layouts/layouts/error.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occured</h1>
+    <h1 v-else>An error occurred</h1>
     <nuxt-link to="/">Home page</nuxt-link>
   </div>
 </template>
