686fa49328b88be8d3d2720ce45e4bce13a1d2be
Alexander Lichter <manniL@gmx.net>
2018-09-30 22:57:16 +0100

    refactor: add promise-result-handling util fn

--- bin/common/utils.js
+++ bin/common/utils.js
@@ -31,6 +31,15 @@ const getLatestHost = (argv) => {
   return { port, host, socket }
 }
 
+exports.runAsyncScript = (fn) => {
+  fn().then(() => { process.exit(0) })
+    .catch((err) => {
+      consola.fatal('Failed to run async Buxt script!')
+      consola.fatal(err)
+      process.exit(1)
+    })
+}
+
 exports.nuxtConfigFile = getNuxtConfigFile
 
 exports.loadNuxtConfig = async (argv) => {
