ce132438e72e8215951778ff17b9057b49267e05
Clark Du <clark.duxin@gmail.com>
2018-10-11 14:53:14 +0100

    test: improve extract css test

--- test/unit/extract-css.test.js
+++ test/unit/extract-css.test.js
@@ -1,15 +1,31 @@
 import { resolve } from 'path'
 import fs from 'fs'
 import { promisify } from 'util'
+import { loadFixture, getPort, Nuxt } from '../utils'
 
+let nuxt = null
 const readFile = promisify(fs.readFile)
 
 describe('extract css', () => {
-  // TODO: make css chunk name predictive
+  beforeAll(async () => {
+    const options = await loadFixture('extract-css')
+    nuxt = new Nuxt(options)
+    await nuxt.listen(await getPort(), '0.0.0.0')
+  })
+
   test('Verify global.css has been extracted and minified', async () => {
-    const pathToMinifiedGlobalCss = resolve(__dirname, '..', 'fixtures/extract-css/.nuxt/dist/client/7dc53e76acc7df734a24.css')
-    const content = await readFile(pathToMinifiedGlobalCss, 'utf-8')
-    const expectedContent = 'h1[data-v-180e2718]{color:red}.container[data-v-180e2718]{display:-ms-grid;display:grid;-ms-grid-columns:60px 60px 60px 60px 60px;grid-template-columns:60px 60px 60px 60px 60px;-ms-grid-rows:30px 30px;grid-template-rows:30px 30px;grid-auto-flow:row}'
-    expect(content).toBe(expectedContent)
+    const extractedIndexCss = resolve(__dirname, '..', 'fixtures/extract-css/.nuxt/dist/client/pages_index.css')
+    const content = await readFile(extractedIndexCss, 'utf-8')
+
+    const scopeCss = /^h1\[data-v-[a-zA-Z0-9]{8}\]\{color:red\}\.container\[data-v-[a-zA-Z0-9]{8}\]/
+    expect(content).toMatch(scopeCss)
+
+    const containerStyle = '{display:-ms-grid;display:grid;-ms-grid-columns:60px 60px 60px 60px 60px;grid-template-columns:60px 60px 60px 60px 60px;-ms-grid-rows:30px 30px;grid-template-rows:30px 30px;grid-auto-flow:row}'
+    expect(content).toContain(containerStyle)
+  })
+
+  test('/about should contain module style', async () => {
+    const { html } = await nuxt.renderRoute('/about')
+    expect(html).toMatch(/<h1 class="test_[a-zA-Z0-9]{5}">I'm BLUE<\/h1>/)
   })
 })
