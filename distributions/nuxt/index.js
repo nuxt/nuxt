95ee7547496270cd3c5afaeaa57d60a232643a95
Pooya Parsa <pyapar@gmail.com>
2018-10-13 01:16:29 +0330

    add all core contributors to index.js

--- distributions/nuxt/index.js
+++ distributions/nuxt/index.js
@@ -1,7 +1,11 @@
 /*!
  * Nuxt.js
  * (c) 2016-2018 Chopin Brothers
- * Core maintainers: Pooya Parsa (@pi0) - Clark Du (@clarkdo)
+ * Core maintainers:
+ * - Pooya Parsa (@pi0)
+ * - Clark Du (@clarkdo)
+ * - Jonas Galvez (galvez)
+ * - Alexander Lichter (manniL)
  * Released under the MIT License.
  */
 
