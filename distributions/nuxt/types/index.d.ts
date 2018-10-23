c0237ad6c3e3056554f6499d44f6e3df30325183
Kevin <contact@kevinmarrec.com>
2018-10-23 18:08:31 +0200

    fix(ts): Follow TypeScript Standard (semi-colons) + provide better `Context.redirect` definition

--- distributions/nuxt/types/index.d.ts
+++ distributions/nuxt/types/index.d.ts
@@ -6,47 +6,48 @@ import { MetaInfo } from 'vue-meta'
 
 declare module 'vue/types/options' {
   interface Context {
-    app: Vue,
-    isClient: boolean,
-    isServer: boolean,
-    isStatic: boolean,
-    isDev: boolean,
-    isHMR: boolean,
-    route: Route,
-    store: Store<any>,
-    env: Object,
-    params: Dictionary<string>,
-    query: Dictionary<string>,
-    req: Request,
-    res: Response,
-    redirect: Function,
-    error: Function
-    nuxtState: Object,
-    beforeNuxtRender: Function
+    app: Vue;
+    isClient: boolean;
+    isServer: boolean;
+    isStatic: boolean;
+    isDev: boolean;
+    isHMR: boolean;
+    route: Route;
+    store: Store<any>;
+    env: Object;
+    params: Dictionary<string>;
+    query: Dictionary<string>;
+    req: Request;
+    res: Response;
+    redirect(status: number, path: string, query?: object): void;
+    redirect(path: string, query?: object): void;
+    error: Function;
+    nuxtState: Object;
+    beforeNuxtRender: Function;
   }
   interface Transition {
-    name?: string
-    mode?: string,
-    css?: boolean,
-    duration?: number,
-    type?: string,
-    enterClass?: string,
-    enterToClass?: string,
-    enterActiveClass?: string,
-    leaveClass?: string,
-    leaveToClass?: string,
-    leaveActiveClass?: string
+    name?: string;
+    mode?: string;
+    css?: boolean;
+    duration?: number;
+    type?: string;
+    enterClass?: string;
+    enterToClass?: string;
+    enterActiveClass?: string;
+    leaveClass?: string;
+    leaveToClass?: string;
+    leaveActiveClass?: string;
   }
 
   interface ComponentOptions<V extends Vue> {
-    asyncData?: (ctx: Context) => object
-    fetch?: (ctx: Context) => Promise<void> | void
-    head?: MetaInfo | (() => MetaInfo)
-    layout?: string | ((ctx: Context) => string)
-    middleware?: string | string[]
-    scrollToTop?: boolean
-    transition?: string | Transition | ((to: Route, from: Route) => string)
-    validate?: (ctx: Context) => Promise<boolean> | boolean
-    watchQuery?: boolean | string[]
+    asyncData?: (ctx: Context) => object;
+    fetch?: (ctx: Context) => Promise<void> | void;
+    head?: MetaInfo | (() => MetaInfo);
+    layout?: string | ((ctx: Context) => string);
+    middleware?: string | string[];
+    scrollToTop?: boolean;
+    transition?: string | Transition | ((to: Route, from: Route) => string);
+    validate?: (ctx: Context) => Promise<boolean> | boolean;
+    watchQuery?: boolean | string[];
   }
 }
