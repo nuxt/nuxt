a74aa5f906da546ed9da85ad566d713d20ccfc98
Clark Du <clark.duxin@gmail.com>
2018-10-23 10:58:50 +0100

    ci: use travis for windows building

--- test/utils/setup.js
+++ test/utils/setup.js
@@ -1,7 +1,3 @@
-const isAppveyor = !!process.env.APPVEYOR
-describe.skip.appveyor = isAppveyor ? describe.skip : describe
-test.skip.appveyor = isAppveyor ? test.skip : test
-
 const isWin = process.platform === 'win32'
 describe.skip.win = isWin ? describe.skip : describe
 test.skip.win = isWin ? test.skip : test
