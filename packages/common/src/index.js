b34b88c8875ef652ad74270b8a55771eb818788a
Jonas Galvez <jonasgalvez@gmail.com>
2018-10-24 07:47:14 -0200

    fix

--- packages/common/src/index.js
+++ packages/common/src/index.js
@@ -1,7 +1,8 @@
+
 import * as _Utils from './utils'
 
 export { default as Modes } from './modes'
 export { default as NuxtConfig } from './nuxt.config'
 export { default as Options } from './options'
-
+export * from './utils'
 export const Utils = _Utils
