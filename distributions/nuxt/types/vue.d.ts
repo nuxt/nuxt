2351c84c95d0fe0d9cd0fb63d05b2bae786d181b
Kevin <contact@kevinmarrec.com>
2018-10-24 15:52:31 +0200

    fix(ts): Follow same definition architecture as official 'vue' and 'vuex' typedefs
    And also same codestyle (double quotes, semi-colons for imports)

---
+++ distributions/nuxt/types/vue.d.ts
@@ -0,0 +1,28 @@
+/**
+ * Extends interfaces in Vue.js
+ */
+
+import Vue, { ComponentOptions } from "vue";
+import { Route } from "vue-router";
+import { MetaInfo } from "vue-meta";
+import { Context, Transition } from "./index";
+
+declare module "vue/types/options" {
+  interface ComponentOptions<V extends Vue> {
+    asyncData?: (ctx: Context) => object;
+    fetch?: (ctx: Context) => Promise<void> | void;
+    head?: MetaInfo | (() => MetaInfo);
+    layout?: string | ((ctx: Context) => string);
+    middleware?: string | string[];
+    scrollToTop?: boolean;
+    transition?: string | Transition | ((to: Route, from: Route) => string);
+    validate?: (ctx: Context) => Promise<boolean> | boolean;
+    watchQuery?: boolean | string[];
+  }
+}
+
+declare module "vue/types/vue" {
+  interface Vue {
+    $nuxt: any; //TODO: Provide better type definition
+  }
+}
