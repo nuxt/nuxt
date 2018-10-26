2551190e83f1734a6759be536b3b9e78fbf79fae
Clark Du <clark.duxin@gmail.com>
2018-10-26 16:50:47 +0100

    feat: move @nuxtjs/babel-preset-app into nuxt mono-repo

---
+++ packages/babel-preset-app/polyfillsPlugin.js
@@ -0,0 +1,23 @@
+// add polyfill imports to the first file encountered.
+module.exports = ({ types }) => {
+  let entryFile
+  return {
+    name: 'vue-cli-inject-polyfills',
+    visitor: {
+      Program(path, state) {
+        if (!entryFile) {
+          entryFile = state.filename
+        } else if (state.filename !== entryFile) {
+          return
+        }
+
+        const { polyfills } = state.opts
+        const { createImport } = require('@babel/preset-env/lib/utils')
+        // imports are injected in reverse order
+        polyfills.slice().reverse().forEach((p) => {
+          createImport(path, p)
+        })
+      }
+    }
+  }
+}
