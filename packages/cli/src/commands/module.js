a9eff9bb51c7a2ec4c6342f52a3e0ed1b2dce7db
Jonas Galvez <jonasgalvez@gmail.com>
2018-10-27 19:20:35 -0200

    module run

--- packages/cli/src/commands/module.js
+++ packages/cli/src/commands/module.js
@@ -25,24 +25,8 @@ const loadModule = async (name) => {
 
 export default async function module(name) {
   const module = await loadModule(name)
-  // const nuxtCmd = new NuxtCommand({
-  //   description: 'Generate a static web application (server-rendered)',
-  //   usage: 'generate <dir>',
-  //   options: [ 'build' ]
-  // })
-
-  // const argv = nuxtCmd.getArgv()
-
-  // const generator = await nuxtCmd.getGenerator(
-  //   await nuxtCmd.getNuxt(
-  //     await nuxtCmd.getNuxtConfig(argv, { dev: false })
-  //   )
-  // )
-
-  // return generator.generate({
-  //   init: true,
-  //   build: argv.build
-  // }).then(() => {
-  //   process.exit(0)
-  // }).catch(err => consola.fatal(err))
+  const nuxtCmd = new NuxtCommand({ module: module.cli })
+  nuxtCmd.run.then(() => {
+    process.exit(0)
+  }).catch(err => consola.fatal(err))
 }
