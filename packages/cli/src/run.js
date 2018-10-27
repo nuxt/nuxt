4ff5abf62245c1c32fc56e853faa30e58c0c0e3a
Jonas Galvez <jonasgalvez@gmail.com>
2018-10-27 18:51:14 -0200

    revert run.js

--- packages/cli/src/run.js
+++ packages/cli/src/run.js
@@ -1,15 +1,33 @@
 import consola from 'consola'
 import * as commands from './commands'
 import setup from './setup'
-import { build } from 'gluegun'
-import moduleCommand from './commands/module'
 
-const cli = build('nuxt')
-  .src(__dirname)
-  .command(moduleCommand)
-  .defaultCommand()
-  .create()
+export default function run() {
+  const defaultCommand = 'dev'
 
-export default async function run() {
-  await cli.run()
+  const cmds = new Set([
+    defaultCommand,
+    'build',
+    'start',
+    'generate'
+  ])
+
+  let cmd = process.argv[2]
+
+  if (cmds.has(cmd)) {
+    process.argv.splice(2, 1)
+  } else {
+    cmd = defaultCommand
+  }
+
+  // Setup runtime
+  setup({
+    dev: cmd === 'dev'
+  })
+
+  return commands[cmd]() // eslint-disable-line import/namespace
+    .then(m => m.default())
+    .catch((error) => {
+      consola.fatal(error)
+    })
 }
