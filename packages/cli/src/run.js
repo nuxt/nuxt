6c4502b3927620bbf803c124595aa26312943e38
Jonas Galvez <jonasgalvez@gmail.com>
2018-10-27 18:22:31 -0200

    refactor cli/src/run to use gluegun

--- packages/cli/src/run.js
+++ packages/cli/src/run.js
@@ -1,33 +1,15 @@
 import consola from 'consola'
 import * as commands from './commands'
 import setup from './setup'
+import { build } from 'gluegun'
+import moduleCommand from './commands/module'
 
-export default function run() {
-  const defaultCommand = 'dev'
+const cli = build('nuxt')
+  .src(__dirname)
+  .command(moduleCommand)
+  .defaultCommand()
+  .create()
 
-  const cmds = new Set([
-    defaultCommand,
-    'build',
-    'start',
-    'generate'
-  ])
-
-  let cmd = process.argv[2]
-
-  if (cmds.has(cmd)) {
-    process.argv.splice(2, 1)
-  } else {
-    cmd = defaultCommand
-  }
-
-  // Setup runtime
-  setup({
-    dev: cmd === 'dev'
-  })
-
-  return commands[cmd]() // eslint-disable-line import/namespace
-    .then(m => m.default())
-    .catch((error) => {
-      consola.fatal(error)
-    })
+export default async function run() {
+  await cli.run()
 }
