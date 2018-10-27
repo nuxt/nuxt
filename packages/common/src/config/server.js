479dcea219bb667b9880ae418f91d891f073c256
Jonas Galvez <jonasgalvez@gmail.com>
2018-10-27 12:06:43 -0200

    refactor nuxt.config.js and provide runnable  nuxtServerConfig() (2)

---
+++ packages/common/src/config/server.js
@@ -0,0 +1,13 @@
+export default () => ({
+  https: false,
+  port: process.env.NUXT_PORT ||
+    process.env.PORT ||
+    process.env.npm_package_config_nuxt_port ||
+    3000,
+  host: process.env.NUXT_HOST ||
+    process.env.HOST ||
+    process.env.npm_package_config_nuxt_host ||
+    'localhost',
+  socket: process.env.UNIX_SOCKET ||
+    process.env.npm_package_config_unix_socket
+})
