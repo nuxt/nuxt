dd4f5168fe46d119fa3f193be2a44c51e8c38bb6
Jonas Galvez <jonasgalvez@gmail.com>
2018-10-27 18:51:43 -0200

    revert commands

--- packages/cli/src/commands/build.js
+++ packages/cli/src/commands/build.js
@@ -1,49 +1,46 @@
 import consola from 'consola'
 import NuxtCommand from '../command'
 
-export default {
-  name: 'build',
-  async function run() {
-    const nuxtCmd = new NuxtCommand({
-      description: 'Compiles the application for production deployment',
-      usage: 'build <dir>',
-      options: [ 'analyze', 'quiet' ]
+export default async function build() {
+  const nuxtCmd = new NuxtCommand({
+    description: 'Compiles the application for production deployment',
+    usage: 'build <dir>',
+    options: [ 'analyze', 'quiet' ]
+  })
+
+  const argv = nuxtCmd.getArgv()
+
+  // Create production build when calling `nuxt build` (dev: false)
+  const nuxt = await nuxtCmd.getNuxt(
+    await nuxtCmd.getNuxtConfig(argv, { dev: false })
+  )
+
+  // Setup hooks
+  nuxt.hook('error', err => consola.fatal(err))
+
+  let builderOrGenerator
+  if (nuxt.options.mode !== 'spa' || argv.generate === false) {
+    // Build only
+    builderOrGenerator = (await nuxtCmd.getBuilder(nuxt)).build()
+  } else {
+    // Build + Generate for static deployment
+    builderOrGenerator = (await nuxtCmd.getGenerator(nuxt)).generate({
+      build: true
     })
-
-    const argv = nuxtCmd.getArgv()
-
-    // Create production build when calling `nuxt build` (dev: false)
-    const nuxt = await nuxtCmd.getNuxt(
-      await nuxtCmd.getNuxtConfig(argv, { dev: false })
-    )
-
-    // Setup hooks
-    nuxt.hook('error', err => consola.fatal(err))
-
-    let builderOrGenerator
-    if (nuxt.options.mode !== 'spa' || argv.generate === false) {
-      // Build only
-      builderOrGenerator = (await nuxtCmd.getBuilder(nuxt)).build()
-    } else {
-      // Build + Generate for static deployment
-      builderOrGenerator = (await nuxtCmd.getGenerator(nuxt)).generate({
-        build: true
-      })
-    }
-
-    return builderOrGenerator
-      .then(() => {
-        // In analyze mode wait for plugin
-        // emitting assets and opening browser
-        if (
-          nuxt.options.build.analyze === true ||
-          typeof nuxt.options.build.analyze === 'object'
-        ) {
-          return
-        }
-
-        process.exit(0)
-      })
-      .catch(err => consola.fatal(err))
   }
+
+  return builderOrGenerator
+    .then(() => {
+      // In analyze mode wait for plugin
+      // emitting assets and opening browser
+      if (
+        nuxt.options.build.analyze === true ||
+        typeof nuxt.options.build.analyze === 'object'
+      ) {
+        return
+      }
+
+      process.exit(0)
+    })
+    .catch(err => consola.fatal(err))
 }
