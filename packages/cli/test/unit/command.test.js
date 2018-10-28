5bc5bbc0c6cd42e7ab5fb4594a82362b5176c619
Jonas Galvez <jonasgalvez@gmail.com>
2018-10-28 15:34:02 -0200

    lint

--- packages/cli/test/unit/command.test.js
+++ packages/cli/test/unit/command.test.js
@@ -18,7 +18,7 @@ describe('cli/command', () => {
   })
 
   test('builds minimist options', () => {
-    const cmd = new Command({ options: Options})
+    const cmd = new Command({ options: Options })
     const minimistOptions = cmd._getMinimistOptions()
 
     expect(minimistOptions.string.length).toBe(4)
@@ -28,7 +28,7 @@ describe('cli/command', () => {
   })
 
   test('parses args', () => {
-    const cmd = new Command({ options: Options})
+    const cmd = new Command({ options: Options })
 
     let args = ['-c', 'test-file', '-s', '-p', '3001']
     let argv = cmd.getArgv(args)
