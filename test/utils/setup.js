73050d41e364b2574d031af15be55462b567d0ae
Pooya Parsa <pyapar@gmail.com>
2018-11-01 02:42:13 +0330

    add a mock consola reporter

--- test/utils/setup.js
+++ test/utils/setup.js
@@ -1,15 +1,13 @@
+import Consola from 'consola'
+
 const isWin = process.platform === 'win32'
 describe.skip.win = isWin ? describe.skip : describe
 test.skip.win = isWin ? test.skip : test
 
 jest.setTimeout(60000)
-jest.mock('consola', () => {
-  const consola = {}
-  for (const level of [
-    'fatal', 'error', 'warn', 'log', 'info',
-    'start', 'success', 'ready', 'debug', 'trace'
-  ]) {
-    consola[level] = jest.fn()
+
+Consola.setReporters([
+  {
+    log: jest.fn()
   }
-  return consola
-})
+])
