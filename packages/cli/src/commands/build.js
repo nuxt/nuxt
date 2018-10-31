fb0ec8e7e19b7609316edf2bd5680687e4cd2b8c
Clark Du <clark.duxin@gmail.com>
2018-10-31 11:12:29 +0000

    feat: add modern argument on build command

--- packages/cli/src/commands/build.js
+++ packages/cli/src/commands/build.js
@@ -24,6 +24,17 @@ export default {
       default: true,
       description: 'Don\'t generate static version for SPA mode (useful for nuxt start)'
     },
+    modern: {
+      alias: 'm',
+      type: 'boolean',
+      description: 'Build app for modern browsers',
+      prepare(cmd, options, argv) {
+        options.build = options.build || {}
+        if (argv.modern) {
+          options.build.modern = !!argv.modern
+        }
+      }
+    },
     quiet: {
       alias: 'q',
       type: 'boolean',
