b7a3c2d55a318c509e17c93c1b3b57dbdf88b45a
Jonas Galvez <jonasgalvez@gmail.com>
2018-11-01 02:50:25 -0200

    cmd.showHelp()

--- packages/cli/src/commands/help.js
+++ packages/cli/src/commands/help.js
@@ -11,8 +11,10 @@ export default {
     if (argv.length) {
       const name = argv[0]
       // eslint-disable-next-line
-      const command = await commands[name]().then(m => m.default)
-      consola.info(command.usage)
+      const command = NuxtCommand.from(
+        await commands[name]().then(m => m.default)
+      )
+      command.showHelp()
     }
   }
 }
