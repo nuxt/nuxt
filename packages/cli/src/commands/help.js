783fbc386fac701f34f61d26c2a85c7466caecdf
Jonas Galvez <jonasgalvez@gmail.com>
2018-11-01 02:06:42 -0200

    add commands/help

---
+++ packages/cli/src/commands/help.js
@@ -0,0 +1,11 @@
+import consola from 'consola'
+import { common, server } from '../options'
+
+export default {
+  name: 'dev',
+  description: 'Shows help for <command>',
+  usage: 'help <command>',
+  async run(cmd) {
+    // todo
+  }
+}
