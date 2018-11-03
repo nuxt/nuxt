268ee6bec6004ae2c4435f47e8a7cac31890bcd3
Pooya Parsa <pyapar@gmail.com>
2018-11-03 14:49:27 +0330

    globally disable chalk for tests

--- test/utils/setup.js
+++ test/utils/setup.js
@@ -1,8 +1,11 @@
 import consola from 'consola'
+import chalk from 'chalk'
 
 const isWin = process.platform === 'win32'
 describe.skip.win = isWin ? describe.skip : describe
 test.skip.win = isWin ? test.skip : test
+
+chalk.enabled = false
 
 jest.setTimeout(60000)
 
