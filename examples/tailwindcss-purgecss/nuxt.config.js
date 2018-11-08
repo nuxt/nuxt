ec9724f7129b51ebf2faad1a416b4eecbb8ecf5e
charlie hield <charles.hield@gmail.com>
2018-11-08 03:37:38 -0600

    chore: tailwind purgecss example (#4214)
    * Add tailwindcss purgecss example
    
    * Eslint fixes

---
+++ examples/tailwindcss-purgecss/nuxt.config.js
@@ -0,0 +1,16 @@
+export default {
+  head: {
+    title: 'Nuxt Tailwind CSS + Purgecss',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'A static site powered by Nuxt.js'
+      }
+    ]
+  },
+
+  css: ['~/assets/css/tailwind.css']
+}
