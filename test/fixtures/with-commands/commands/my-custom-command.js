8ae12cbcc49c4c282d18f1652ac6010f255c17a2
Jonas Galvez <jonasgalvez@gmail.com>
2018-11-11 20:21:51 -0200

    add with-commands fixture

---
+++ test/fixtures/with-commands/commands/my-custom-command.js
@@ -0,0 +1,23 @@
+import consola from 'consola'
+
+export default {
+  name: 'my-custom-command',
+  description: 'My Custom Command',
+  usage: 'my-custom-command <foobar>',
+  options: {
+    foobar: {
+      alias: 'fb',
+      type: 'string',
+      description: 'Simple test string'
+    }
+  },
+  async run(cmd) {
+    try {
+      const argv = cmd.getArgv()
+      consola.info('argv: ', argv)
+      process.exit(0)
+    } catch (err) {
+      consola.fatal(err)
+    }
+  }
+}
