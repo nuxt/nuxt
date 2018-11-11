eccb5ec93b114ef067948d2427cebe92c600502f
Jonas Galvez <jonasgalvez@gmail.com>
2018-11-11 21:03:20 -0200

    changed signatures

--- packages/cli/src/local.js
+++ packages/cli/src/local.js
@@ -4,30 +4,30 @@ import NuxtCommand from './command'
 const filterCommands = (dir) => readdirSync(dir)
   .filter(c => c.endsWith('.js'))
 
-export default {
-  hash() {
-    const cmdsRoot =  resolve('.', 'commands')
-    const cmds = filterCommands(cmdsRoot)
-    return cmds.reduce((hash, cmd) => {
-      return Object.assign(hash, {
-        [parse(cmd).name]: import(path.join(cmdsRoot, cmd))
-      })
-    }, {})
-  }
-  exists(cmd) {
-    const cmdsHash = {}
-    const cmdsRoot =  resolve('.', 'commands')
-    if (existsSync(cmdsRoot)) {
-      return filterCommands(cmdsRoot).includes(`${cmd}.js`)
-    }
-  },
-  load (cmd) {
-    const cmdsRoot =  resolve('.', 'commands')
-    const file = filterCommands(cmdsRoot).find((c) => {
-      return parse(c).name === cmd
+export function getLocalCommands() {
+  const cmdsRoot =  resolve('.', 'commands')
+  const cmds = filterCommands(cmdsRoot)
+  return cmds.reduce((hash, cmd) => {
+    return Object.assign(hash, {
+      [parse(cmd).name]: import(path.join(cmdsRoot, cmd))
     })
-    const command = await import(path.join(cmdsRoot, command))
-      .then(m => m.default)
-    return NuxtCommand.from(command)
+  }, {})
+}
+
+export function localCommandExists(cmd) {
+  const cmdsHash = {}
+  const cmdsRoot =  resolve('.', 'commands')
+  if (existsSync(cmdsRoot)) {
+    return filterCommands(cmdsRoot).includes(`${cmd}.js`)
   }
+}
+
+export function localCommandLoad (cmd) {
+  const cmdsRoot =  resolve('.', 'commands')
+  const file = filterCommands(cmdsRoot).find((c) => {
+    return parse(c).name === cmd
+  })
+  const command = await import(path.join(cmdsRoot, command))
+    .then(m => m.default)
+  return NuxtCommand.from(command)
 }
