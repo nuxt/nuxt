195b10e49c8a8450dd811148ab97f915441027a7
Jonas Galvez <jonasgalvez@gmail.com>
2018-11-13 03:51:38 -0200

    ongoing work on run command (6)

--- packages/cli/src/commands/run.js
+++ packages/cli/src/commands/run.js
@@ -11,15 +11,9 @@ export default {
   async run(cmd) {
     const argv = cmd.getArgv()
     const customCmd = argv._[0]
-    try { 
-      NuxtCommand.ensure(cmd, '.')
-    } catch (notFoundError) {
-      if (process.argv.includes('--help') || process.argv.includes('-h')) {
-        return listCommands('.').then(process.exit)
-      } else {
-        throw notFoundError
-      }
-    }
+    
+    NuxtCommand.ensure(cmd, '.')
+    
     return NuxtCommand.load(cmd, '.')
       .then(command => command.run())
   }
