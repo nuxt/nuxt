9d6b1a4298115565625371bdc24add69c9c0a054
mottox2 <y.mottox2@gmail.com>
2018-11-18 04:33:34 +0900

    Add test

--- packages/cli/test/unit/generate.test.js
+++ packages/cli/test/unit/generate.test.js
@@ -46,6 +46,24 @@ describe('generate', () => {
     Command.prototype.getArgv = getArgv
   })
 
+  test('build with development', async () => {
+    mockGetNuxt()
+    const generator = mockGetGenerator(Promise.resolve())
+
+    const cmd = NuxtCommand.from(generate)
+    const args = ['generate', '.', '--development']
+    const argv = cmd.getArgv(args)
+    argv._ = ['.']
+
+    const options = await cmd.getNuxtConfig(argv)
+
+    await cmd.run()
+
+    expect(options.vue.config.devtools).toBe(true)
+    expect(generator).toHaveBeenCalled()
+    expect(generator.mock.calls[0][0].build).toBe(true)
+  })
+
   test('catches error', async () => {
     mockGetNuxt()
     mockGetGenerator(Promise.reject(new Error('Generator Error')))
