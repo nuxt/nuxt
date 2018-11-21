b12141b99a5a57c70d1658430de80f5682780dcf
Clark Du <clark.duxin@gmail.com>
2018-11-21 16:04:01 +0000

    hotfix: nuxt-start tries to build instead of starting (#4384)

--- distributions/nuxt-start/bin/nuxt-start.js
+++ distributions/nuxt-start/bin/nuxt-start.js
@@ -2,4 +2,7 @@
 const { name } = require('../package.json')
 const isEdge = name.indexOf('-edge') !== -1
 const cli = require(isEdge ? '@nuxt/cli-edge' : '@nuxt/cli')
+if (process.argv[2] !== 'start') {
+  process.argv.splice(2, 0, 'start')
+}
 cli.run()
