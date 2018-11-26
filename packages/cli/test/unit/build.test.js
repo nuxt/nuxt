ff632ba1375bb711340cac248fab373c87efee0d
Pooya Parsa <pyapar@gmail.com>
2018-11-26 20:05:18 +0330

    remove build with devtools tests as feature is not merged yet

--- packages/cli/test/unit/build.test.js
+++ packages/cli/test/unit/build.test.js
@@ -44,25 +44,6 @@ describe('build', () => {
     expect(process.exit).toHaveBeenCalled()
   })
 
-  test('build with devtools', async () => {
-    mockGetNuxt({
-      mode: 'universal'
-    })
-    const builder = mockGetBuilder(Promise.resolve())
-
-    const cmd = NuxtCommand.from(build)
-    const args = ['build', '.', '--devtools']
-    const argv = cmd.getArgv(args)
-    argv._ = ['.']
-
-    const options = await cmd.getNuxtConfig(argv)
-
-    await cmd.run()
-
-    expect(options.vue.config.devtools).toBe(true)
-    expect(builder).toHaveBeenCalled()
-  })
-
   test('build with modern mode', async () => {
     mockGetNuxt({
       mode: 'universal'
--- packages/cli/test/unit/generate.test.js
+++ packages/cli/test/unit/generate.test.js
@@ -46,24 +46,6 @@ describe('generate', () => {
     Command.prototype.getArgv = getArgv
   })
 
-  test('build with devtools', async () => {
-    mockGetNuxt()
-    const generator = mockGetGenerator(Promise.resolve())
-
-    const cmd = NuxtCommand.from(generate)
-    const args = ['generate', '.', '--devtools']
-    const argv = cmd.getArgv(args)
-    argv._ = ['.']
-
-    const options = await cmd.getNuxtConfig(argv)
-
-    await cmd.run()
-
-    expect(options.vue.config.devtools).toBe(true)
-    expect(generator).toHaveBeenCalled()
-    expect(generator.mock.calls[0][0].build).toBe(true)
-  })
-
   test('generate with modern mode', async () => {
     mockGetNuxt()
     mockGetGenerator(Promise.resolve())
