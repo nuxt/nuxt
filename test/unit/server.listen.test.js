1fa4d4fb3f185280f8ba6c9bf61bc9d2077a6aa3
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2018-11-27 21:46:45 -0200

    simplified tests

--- test/unit/server.listen.test.js
+++ test/unit/server.listen.test.js
@@ -2,34 +2,27 @@ import consola from 'consola'
 import { loadFixture, getPort, Nuxt } from '../utils'
 
 let port = null
-let nuxt1 = null
-let nuxt2 = null
+let nuxt = null
 
 describe('server listen', () => {
   beforeAll(async () => {
     const config = await loadFixture('empty')
-    nuxt1 = new Nuxt(config)
-    nuxt2 = new Nuxt(config)
+    nuxt = new Nuxt(config)
     port = await getPort()
   })
 
   test('should listen different ports', async () => {
-    await nuxt1.server.listen(port, 'localhost')
-    await nuxt2.server.listen(port, 'localhost')
+    await nuxt.server.listen(port, 'localhost')
+    await nuxt.server.listen(port, 'localhost')
 
-    const host = nuxt1.server.listeners[0].host
-    const port1 = nuxt1.server.listeners[0].port
-    const port2 = nuxt2.server.listeners[0].port
-
-    expect(port1).toBe(port)
-    expect(port2).not.toBe(port1)
+    expect(nuxt.server.listeners[0].port).toBe(port)
+    expect(nuxt.server.listeners[1].port).not.toBe(nuxt.server.listeners[0].port)
 
     expect(consola.warn).toHaveBeenCalledTimes(1)
-    expect(consola.warn).toHaveBeenCalledWith(`Address \`${host}:${port}\` is already in use.`)
+    expect(consola.warn).toHaveBeenCalledWith(`Address \`localhost:${port}\` is already in use.`)
   })
 
   afterAll(async () => {
-    await nuxt1.close()
-    await nuxt2.close()
+    await nuxt.close()
   })
 })
