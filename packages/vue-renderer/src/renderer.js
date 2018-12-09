1162f2ddc1b3d88032cdc3f8eb4d68599dabe486
Pooya Parsa <pyapar@gmail.com>
2018-12-10 01:07:36 +0330

    feat: use runInNewContext: true for nuxt dev (#4508)

--- packages/vue-renderer/src/renderer.js
+++ packages/vue-renderer/src/renderer.js
@@ -244,7 +244,6 @@ export default class VueRenderer {
     const hasModules = fs.existsSync(path.resolve(this.context.options.rootDir, 'node_modules'))
 
     const rendererOptions = {
-      runInNewContext: false,
       clientManifest: this.context.resources.clientManifest,
       // for globally installed nuxt command, search dependencies in global dir
       basedir: hasModules ? this.context.options.rootDir : __dirname,
