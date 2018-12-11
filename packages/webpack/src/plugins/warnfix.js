653da2a1b12589416f94ebab8b84820ca39ed520
Alexander Lichter <manniL@gmx.net>
2018-12-11 23:09:19 +0000

    fix: lint

--- packages/webpack/src/plugins/warnfix.js
+++ packages/webpack/src/plugins/warnfix.js
@@ -1,7 +1,7 @@
 export default class WarnFixPlugin {
   apply(compiler) /* istanbul ignore next */ {
     compiler.hooks.done.tap('warnfix-plugin', (stats) => {
-      stats.compilation.warnings = stats.compilation.warnings.filter((warn) =>
+      stats.compilation.warnings = stats.compilation.warnings.filter(warn =>
         !(warn.name === 'ModuleDependencyWarning' &&
           warn.message.includes(`export 'default'`) &&
           warn.message.includes('nuxt_plugin_')))
