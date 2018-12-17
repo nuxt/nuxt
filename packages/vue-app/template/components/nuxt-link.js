c0199975f791aba68984afc33219b1d175e678f0
Sébastien Chopin <seb@chopin.io>
2018-12-17 17:58:45 +0100

    feat(nuxt-link): Improve <n-link> and add automatic prefetch

--- packages/vue-app/template/components/nuxt-link.js
+++ packages/vue-app/template/components/nuxt-link.js
@@ -1,8 +1,34 @@
 <%= isTest ? '// @vue/component' : '' %>
+import Vue from 'vue'
+
 export default {
+  extends: Vue.component('RouterLink'),
   name: 'NuxtLink',
-  functional: true,
-  render(h, { data, children }) {
-    return h('router-link', data, children)
+  props: {
+    noPrefetch: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted() {
+    if (!this.noPrefetch) this.$nextTick(this.prefetch)
+  },
+  methods: {
+    prefetch() {
+      if (process.client) {
+        // Avoid including it in SSR bundle
+        var ref = this.$router.resolve(this.to, this.$route, this.append)
+        const Components = ref.resolved.matched.map((r) => r.components.default)
+        for (let i = 0; i < Components.length; i++) {
+          const Component = Components[i]
+          if (typeof Component === 'function' && !Component.__prefetched) {
+            try {
+              Component()
+              Component.__prefetched = true
+            } catch (e) {}
+          }
+        }
+      }
+    }
   }
 }
