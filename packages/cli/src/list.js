c6572a6614a74b790b09b65e22510e188cb57bc1
Jonas Galvez <jonasgalvez@gmail.com>
2018-12-18 14:38:16 -0200

    rm unused import

--- packages/cli/src/list.js
+++ packages/cli/src/list.js
@@ -1,5 +1,4 @@
 import chalk from 'chalk'
-import NuxtCommand from './command'
 import { indent, foldLines, startSpaces, optionSpaces, colorize } from './utils/formatting'
 import getCommand from './commands'
 
