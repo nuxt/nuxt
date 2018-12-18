87f9ed213210edf0aad00587cc5722555054c106
Jonas Galvez <jonasgalvez@gmail.com>
2018-12-18 01:22:48 -0200

    debugging ensureExternal

--- packages/cli/src/command.js
+++ packages/cli/src/command.js
@@ -13,7 +13,12 @@ export default class NuxtCommand {
   }
 
   static async ensureExternal() {
-    const command = process.argv.slice(0, 3).join('-')
+    const parts = process.argv.slice(1, 4)
+    if (parts[0] !== 'nuxt') {
+      parts[0] = 'nuxt'
+    }
+    const command = parts.join('-')
+    console.log('command', command)
     if (!await commandExists(command)) {
       throw new Error(`Module command ${command} failed to load!`)
     }
--- packages/cli/src/run.js
+++ packages/cli/src/run.js
@@ -7,41 +7,34 @@ import setup from './setup'
 
 export default async function run(custom = null) {
   if (custom) {
+    await custom.run()
+    return
+  }
+
+  const cmd = process.argv[2] || 'dev'
+  console.log('!')
+  const isExternal = await NuxtCommand.ensure(cmd)
+  console.log('isExternal', isExternal)
+
+  if (isExternal) {
+    process.argv.splice(2, 1)
     try {
-      custom.run()
+      spawn(process.argv[0], process.argv.slice(1))
     } catch (error) {
       consola.fatal(error)
     }
     return
   }
-
-  const cmd = process.argv[2] || 'dev'
-  try {
-    const isExternal = await NuxtCommand.ensure(cmd)
-
-    if (isExternal) {
-      process.argv.splice(2, 1)
-      try {
-        spawn(process.argv[0], process.argv.slice(1))
-      } catch (error) {
-        consola.fatal(error)
-      }
-      return
-    }
-  } catch (notFoundError) {
-    if (process.argv.includes('--help') || process.argv.includes('-h')) {
-      return listCommands().then(() => process.exit(0))
-    } else {
-      throw notFoundError
-    }
-  }
+  // } catch (notFoundError) {
+  //   if (process.argv.includes('--help') || process.argv.includes('-h')) {
+  //     return listCommands().then(() => process.exit(0))
+  //   } else {
+  //     throw notFoundError
+  //   }
+  // }
 
   process.argv.splice(2, 1)
   setup({ dev: cmd === 'dev' })
 
-  try {
-    await NuxtCommand.run(cmd)
-  } catch (error) {
-    consola.fatal(error)
-  }
+  await NuxtCommand.run(cmd)
 }
