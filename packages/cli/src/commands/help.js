7dda9a210136cbc4b9d75909af92cdc4e595106a
Pooya Parsa <pyapar@gmail.com>
2018-12-18 14:05:03 +0330

    add --help, --version for help

--- packages/cli/src/commands/help.js
+++ packages/cli/src/commands/help.js
@@ -1,13 +1,18 @@
 import consola from 'consola'
 import NuxtCommand from '../command'
 import listCommands from '../list'
+import { common } from '../options'
 
 export default {
   name: 'help',
   description: 'Shows help for <command>',
   usage: 'help <command>',
+  options: {
+    help: common.help,
+    version: common.version
+  },
   async run(cmd) {
-    const argv = cmd.getArgv()._
+    const argv = cmd.getArgv()
     const name = argv[0] || null
     if (!name) {
       return listCommands().then(() => process.exit(0))
