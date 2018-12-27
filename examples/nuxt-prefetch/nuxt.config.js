7274c99a2b1f3d50889d8fe7a4a5c22da49ef3c0
Sébastien Chopin <seb@chopin.io>
2018-12-27 21:38:01 +0100

    examples(nuxt-prefetch): Add Nuxt prefetching example

---
+++ examples/nuxt-prefetch/nuxt.config.js
@@ -0,0 +1,9 @@
+export default {
+  head: {
+    titleTemplate: '%s - NuxtJS Prefetching'
+  },
+  router: {
+    // To disable prefetching, uncomment the line
+    // prefetchLinks: false
+  }
+}
---
+++ examples/nuxt-prefetch/pages/rapid.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuxt-link v-for="link of $store.state.links" :key="link" :to="{ name: link }">/{{ link }}</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: this.$route.name
+    }
+  }
+}
+</script>
+
