d0679083b1d424d98784bd6fd72e7b80010b9bc8
Kevin <contact@kevinmarrec.com>
2019-01-05 18:48:50 +0100

    Simpler `typescript` example with a HelloWorld component

---
+++ examples/typescript/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import HelloWorld from '~/components/HelloWorld.vue'
+
+@Component({
+  components: {
+    HelloWorld
+  }
+})
+export default class Home extends Vue {}
+</script>
