0be6c4d50d53825ad7b799baa3c76781c88e58b8
Alexander Lichter <manniL@gmx.net>
2019-01-07 11:31:49 +0000

    test: use js layout

---
+++ test/fixtures/with-config/layouts/default.js
@@ -0,0 +1,5 @@
+export default {
+  render(h) {
+    return h('div', [h('h1', 'Default Layout'), h('Nuxt')])
+  }
+}
--- test/fixtures/with-config/layouts/default.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Default layout</h1>
-    <Nuxt />
-  </div>
-</template>
