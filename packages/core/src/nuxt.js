5bb068c7c306a966ea155029a41655b9595860ab
pimlie <pimlie@hotmail.com>
2019-01-13 20:39:06 +0100

    feat: show git status

--- packages/core/src/nuxt.js
+++ packages/core/src/nuxt.js
@@ -45,7 +45,7 @@ export default class Nuxt extends Hookable {
   }
 
   static get version() {
-    return global.NUXT_VERSION || version
+    return global.__NUXT_VERSION_ || version
   }
 
   async ready() {
