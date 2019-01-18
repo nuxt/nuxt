22ddd752c62660cdc7062e8cf276b8c1bc26da49
Kevin <contact@kevinmarrec.com>
2019-01-18 13:47:45 +0100

    fix(ts): add missing `process.mode` & `process.modern` typedefs

--- packages/vue-app/types/process.d.ts
+++ packages/vue-app/types/process.d.ts
@@ -6,6 +6,8 @@ declare namespace NodeJS {
   interface Process {
     browser: boolean;
     client: boolean;
+    mode: 'universal' | 'spa';
+    modern: boolean;
     server: boolean;
     static: boolean;
   }
