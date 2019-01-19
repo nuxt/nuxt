b23f5c9b4bbd86d65cda420c478bf30d59554283
Xin Du (Clark) <clark.duxin@gmail.com>
2019-01-19 18:14:54 +0000

    feat(server): timing option for `Server-Timing` header (#4800)

---
+++ packages/server/src/middleware/timing.js
@@ -0,0 +1,51 @@
+import consola from 'consola'
+import onHeaders from 'on-headers'
+import { Timer } from '@nuxt/utils'
+
+export default options => (req, res, next) => {
+  if (res.timing) {
+    consola.warn('server-timing is already registered.')
+  }
+  res.timing = new ServerTiming()
+
+  if (options && options.total) {
+    res.timing.start('total', 'Nuxt Server Time')
+  }
+
+  onHeaders(res, () => {
+    res.timing.end('total')
+
+    res.setHeader(
+      'Server-Timing',
+      []
+        .concat(res.getHeader('Server-Timing') || [])
+        .concat(res.timing.headers)
+        .join(', ')
+    )
+  })
+
+  next()
+}
+
+class ServerTiming extends Timer {
+  constructor(...args) {
+    super(...args)
+    this.headers = []
+  }
+
+  end(...args) {
+    const time = super.end(...args)
+    this.headers.push(this.formatHeader(time))
+    return time
+  }
+
+  clear() {
+    super.clear()
+    this.headers.length = 0
+  }
+
+  formatHeader(time) {
+    const desc = time.description ? `;desc="${time.description}"` : ''
+    return `${time.name};dur=${time.duration}${desc}`
+  }
+}
