f24e3c5f57915357d332ed5fce061184eb7b455e
gaoyuan <gaoyuanzlz@qq.com>
2019-01-22 20:42:48 +0800

    fixed lint check

--- packages/server/src/middleware/nuxt.js
+++ packages/server/src/middleware/nuxt.js
@@ -63,11 +63,11 @@ export default ({ options, nuxt, renderRoute, resources }) => async function nux
     }
 
     if (options.render.csp) {
-      let { allowedSources, policies } = options.render.csp
-      policies = policies ? {...policies} : null
+      const { allowedSources, policies } = options.render.csp;
+      const curPolicies = policies ? { ...policies } : null
       const cspHeader = options.render.csp.reportOnly ? 'Content-Security-Policy-Report-Only' : 'Content-Security-Policy'
 
-      res.setHeader(cspHeader, getCspString({ cspScriptSrcHashes, allowedSources, policies, isDev: options.dev }))
+      res.setHeader(cspHeader, getCspString({ cspScriptSrcHashes, allowedSources, curPolicies, isDev: options.dev }))
     }
 
     // Send response
