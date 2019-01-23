801e046719a74fbb7d3a567e4ec4988e55117027
andoshin11 <shinglish11@gmail.com>
2019-01-23 22:13:16 +0900

    Load only vue-style-loader options

--- packages/webpack/src/utils/style-loader.js
+++ packages/webpack/src/utils/style-loader.js
@@ -11,7 +11,7 @@ export default class StyleLoader {
     this.perfLoader = perfLoader
     this.rootDir = options.rootDir
     this.loaders = {
-      ...options.build.loaders,
+      vueStyle: options.build.loaders.vueStyle,
       css: options.build.loaders.css,
       cssModules: options.build.loaders.cssModules
     }
