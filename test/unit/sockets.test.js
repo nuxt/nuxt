d0aadd4357637ce3a44ed6b672c4d379428fb34e
Clark Du <clark.duxin@gmail.com>
2019-01-28 14:30:04 +0000

    fix(test): jest describe.skip does not work fine

--- test/unit/sockets.test.js
+++ test/unit/sockets.test.js
@@ -3,19 +3,14 @@ import { loadFixture, Nuxt } from '../utils'
 let nuxt = null
 
 describe.skip.win('basic sockets', () => {
-  beforeAll(async () => {
+  test('/', async () => {
     const options = await loadFixture('sockets')
     nuxt = new Nuxt(options)
     await nuxt.server.listen()
-  })
 
-  test('/', async () => {
     const { html } = await nuxt.server.renderRoute('/')
     expect(html).toContain('<h1>Served over sockets!</h1>')
-  })
 
-  // Close server and ask nuxt to stop listening to file changes
-  afterAll(async () => {
     await nuxt.close()
   })
 })
