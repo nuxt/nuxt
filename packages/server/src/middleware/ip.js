15354c9ea0940495b8411729dbc06ba4514c7d64
pooya parsa <pooya@pi0.ir>
2019-01-30 15:35:02 +0330

    code format

--- packages/server/src/middleware/ip.js
+++ packages/server/src/middleware/ip.js
@@ -1,7 +1,11 @@
 export default function () {
   return function (req, _, next) {
     if (req.ip === undefined) {
-      req.ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || (req.connection.socket ? req.connection.socket.remoteAddress : null)
+      req.ip = req.headers['x-real-ip'] ||
+        req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        (req.connection.socket ? req.connection.socket.remoteAddress : null)
     }
     next()
   }
