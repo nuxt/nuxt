2d05ce805d843697205e5849581835974620fc90
Clark Du <clark.duxin@gmail.com>
2019-01-31 13:10:36 +0000

    fix: remove cache-loader for external resources

--- packages/webpack/src/config/base.js
+++ packages/webpack/src/config/base.js
@@ -295,33 +295,33 @@ export default class WebpackBaseConfig {
       },
       {
         test: /\.(png|jpe?g|gif|svg|webp)$/i,
-        use: perfLoader.asset().concat({
+        use: {
           loader: 'url-loader',
           options: Object.assign(
             this.loaders.imgUrl,
             { name: this.getFileName('img') }
           )
-        })
+        }
       },
       {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
-        use: perfLoader.asset().concat({
+        use: {
           loader: 'url-loader',
           options: Object.assign(
             this.loaders.fontUrl,
             { name: this.getFileName('font') }
           )
-        })
+        }
       },
       {
         test: /\.(webm|mp4|ogv)$/i,
-        use: perfLoader.asset().concat({
+        use: {
           loader: 'file-loader',
           options: Object.assign(
             this.loaders.file,
             { name: this.getFileName('video') }
           )
-        })
+        }
       }
     ]
   }
