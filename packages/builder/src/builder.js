f70645e5aa0705543798e028182f795f16e4c594
Xin Du (Clark) <clark.duxin@gmail.com>
2019-02-01 11:56:26 +0000

    fix: revert templatFiles name (#4924)

--- packages/builder/src/builder.js
+++ packages/builder/src/builder.js
@@ -497,7 +497,7 @@ export default class Builder {
 
     await this.nuxt.callHook('build:templates', {
       templateVars,
-      templateFiles,
+      templatesFiles: templateFiles,
       resolve: r
     })
 
