68f6880f54a483c105cdf74990402aba73022d2a
Xin Du (Clark) <clark.duxin@gmail.com>
2019-02-06 18:45:39 +0000

    refactor(test): change cli.test to be more accurate (#4957)

--- test/unit/utils.test.js
+++
@@ -1,8 +0,0 @@
-import { waitUntil } from '../utils'
-
-describe('utils', () => {
-  test('waitUntil', async () => {
-    expect(await waitUntil(() => true, 0.1, 100)).toBe(false)
-    expect(await waitUntil(() => false, 0.1, 100)).toBe(true)
-  })
-})
