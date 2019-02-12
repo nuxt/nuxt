b8ddfe95cdcc30bd87aeb17cc072178e7b02eb8c
Xin Du (Clark) <clark.duxin@gmail.com>
2019-02-12 13:05:37 +0000

    feat: upgrade to jest 24 (#4868)

--- test/unit/sockets.test.js
+++ test/unit/sockets.test.js
@@ -1,11 +1,9 @@
 import { loadFixture, Nuxt } from '../utils'
 
-let nuxt = null
-
 describe.skip.win('basic sockets', () => {
   test('/', async () => {
     const options = await loadFixture('sockets')
-    nuxt = new Nuxt(options)
+    const nuxt = new Nuxt(options)
     await nuxt.server.listen()
 
     const { html } = await nuxt.server.renderRoute('/')
