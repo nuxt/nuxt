ad6a8cda9a27316a0514919aa4617aed2f4ed9b1
Pim <pimlie@hotmail.com>
2019-02-13 10:16:51 +0100

    test: mock enquirer in typescript tests (#5025)

--- packages/typescript/test/setup.test.js
+++ packages/typescript/test/setup.test.js
@@ -4,6 +4,9 @@ import { register } from 'ts-node'
 import { setup as setupTypeScript } from '@nuxt/typescript'
 
 jest.mock('ts-node')
+jest.mock('enquirer', () => ({
+  prompt: jest.fn(() => ({ confirmGeneration: true }))
+}))
 
 describe('typescript setup', () => {
   const rootDir = 'tmp'
