2015140d1287bcf3701b170cd7d4000b01270c82
Pooya Parsa <pyapar@gmail.com>
2019-02-14 19:26:58 +0330

    feat(vue-app): universal fetch (#5028)
    * pkg(nuxt-start): add node-fetch, unfetch
    
    * pkg(vue-app): add node-fetch, unfetch
    
    * add yarn.lock
    
    * feat(config): _app.fetch options
    
    * feat(builder): add fetch to templateVars
    
    * feat(vue-app): polyfill global with fetch
    
    * feat(fixtures/basic): /api/test
    
    * add fetch example to fixtures
    
    * remove unfetch from nuxt-start
    
    * update template snapshot
    
    * revert extra new line in server.js
    
    * single line if

---
+++ test/fixtures/basic/pages/fetch.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ data }}
+    <button @click="update">
+      Fetch
+    </button>
+    <button @click="reload">
+      Reload
+    </button>
+  </div>
+</template>
+
+<script>
+const name = process.server ? 'server' : 'client'
+const baseURL = 'http://localhost:3000/api'
+const getData = () => fetch(`${baseURL}/test`)
+  .then(r => r.text())
+  .then(r => r + ` (From ${name})`)
+
+export default {
+  async asyncData() {
+    const data = await getData()
+    return { data }
+  },
+  methods: {
+    async update() {
+      this.data = await getData()
+    },
+    reload() {
+      window.location.reload()
+    }
+  }
+}
+</script>
