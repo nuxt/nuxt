2213eb6d045deaa82ae9fc968c8fd00fad24fa73
chibo <huyach@protonmail.ch>
2019-02-14 13:26:53 +0300

    fix eslint #3

--- examples/pug-stylus-coffee/pages/index.vue
+++ examples/pug-stylus-coffee/pages/index.vue
@@ -18,11 +18,10 @@
 <script lang="coffee">
 import Logo from '~/components/Logo.vue'
 
-export default {
-  components: { Logo },
+export default
+  components: { Logo }
   computed:
     message: -> @$store.state.msgComputed
-}
 </script>
 
 <style lang="stylus">
