6d4cb5e1419fee349812d8396871ca6917ed942e
pooya parsa <pooya@pi0.ir>
2019-03-05 01:43:52 +0330

    update nuxt.test

--- packages/core/test/nuxt.test.js
+++ packages/core/test/nuxt.test.js
@@ -10,26 +10,24 @@ import Resolver from '../src/resolver'
 import { version } from '../package.json'
 
 jest.mock('@nuxt/utils')
-jest.mock('@nuxt/config', () => ({
-  getNuxtConfig: jest.fn(() => ({}))
-}))
+
 jest.mock('@nuxt/server')
+
+jest.mock('@nuxt/config', () => ({
+  getNuxtConfig: jest.fn(() => ({
+    _autoInit: false
+  }))
+}))
 
 describe('core: nuxt', () => {
   beforeEach(() => {
     jest.clearAllMocks()
-    jest.spyOn(Nuxt.prototype, 'ready').mockImplementation(() => Promise.resolve())
   })
 
-  afterEach(() => {
-    if (Nuxt.prototype.ready.mockRestore) {
-      Nuxt.prototype.ready.mockRestore()
-    }
-  })
-
-  test('should construct nuxt with options', () => {
+  test('should construct nuxt with options', async () => {
     const options = {}
     const nuxt = new Nuxt(options)
+    await nuxt.ready()
 
     expect(nuxt).toBeInstanceOf(Hookable)
     expect(getNuxtConfig).toBeCalledTimes(1)
@@ -46,15 +44,14 @@ describe('core: nuxt', () => {
     })
 
     expect(defineAlias).toBeCalledTimes(2)
-    expect(defineAlias).nthCalledWith(1, nuxt, nuxt.server, ['renderRoute', 'renderAndGetWindow', 'listen'])
-    expect(defineAlias).nthCalledWith(2, nuxt, nuxt.resolver, ['resolveAlias', 'resolvePath'])
+    expect(defineAlias).nthCalledWith(1, nuxt, nuxt.resolver, ['resolveAlias', 'resolvePath'])
+    expect(defineAlias).nthCalledWith(2, nuxt, nuxt.server, ['renderRoute', 'renderAndGetWindow', 'listen'])
 
     expect(nuxt.renderer).toBe(nuxt.server)
     expect(nuxt.render).toBe(nuxt.server.app)
     expect(nuxt.showReady).toBeInstanceOf(Function)
-    expect(nuxt.initialized).toEqual(false)
 
-    expect(nuxt.ready).toBeCalledTimes(1)
+    expect(nuxt._ready).toBeInstanceOf(Promise)
   })
 
   // TODO: Remove in next major release
@@ -70,10 +67,9 @@ describe('core: nuxt', () => {
 
   test('should display fatal message if ready failed', async () => {
     const err = new Error('nuxt ready failed')
-    Nuxt.prototype.ready.mockImplementation(() => Promise.reject(err))
     const nuxt = new Nuxt()
-
-    await nuxt._ready
+    nuxt.init = () => Promise.reject(err)
+    await nuxt.ready()
 
     expect(consola.fatal).toBeCalledTimes(1)
     expect(consola.fatal).toBeCalledWith(err)
@@ -95,8 +91,6 @@ describe('core: nuxt', () => {
 
   test('should call module/server ready in nuxt.ready', async () => {
     const nuxt = new Nuxt()
-    delete nuxt._ready
-    Nuxt.prototype.ready.mockRestore()
 
     nuxt.callHook = jest.fn()
     nuxt.server = { ready: jest.fn() }
@@ -114,22 +108,15 @@ describe('core: nuxt', () => {
 
   test('should ignore ready when _ready exists', async () => {
     const nuxt = new Nuxt()
-    Nuxt.prototype.ready.mockRestore()
     const _ready = nuxt._ready = jest.fn()
-    nuxt.server = { ready: jest.fn() }
-
     const result = await nuxt.ready()
-
     expect(result).toBe(_ready)
-    expect(nuxt.server.ready).not.toBeCalled()
   })
 
   test('should add object hooks', async () => {
     const hooks = {}
-    getNuxtConfig.mockReturnValueOnce({ hooks })
+    getNuxtConfig.mockReturnValueOnce({ hooks, _autoInit: false })
     const nuxt = new Nuxt()
-    delete nuxt._ready
-    Nuxt.prototype.ready.mockRestore()
 
     nuxt.addHooks = jest.fn()
     nuxt.server = { ready: jest.fn() }
@@ -145,8 +132,6 @@ describe('core: nuxt', () => {
     const hooks = jest.fn()
     getNuxtConfig.mockReturnValueOnce({ hooks })
     const nuxt = new Nuxt()
-    delete nuxt._ready
-    Nuxt.prototype.ready.mockRestore()
 
     nuxt.addHooks = jest.fn()
     nuxt.server = { ready: jest.fn() }
@@ -174,8 +159,6 @@ describe('core: nuxt', () => {
 
   test('should ignore non-function callback in close', async () => {
     const nuxt = new Nuxt()
-    delete nuxt._ready
-    Nuxt.prototype.ready.mockRestore()
 
     nuxt.callHook = jest.fn()
     nuxt.server = { ready: jest.fn() }
