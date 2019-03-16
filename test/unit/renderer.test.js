805e2aee88d70238e0e3511fa2d7222660dbcd91
pooya parsa <pooya@pi0.ir>
2019-03-16 11:25:11 +0330

    fix renderer test

--- test/unit/renderer.test.js
+++ test/unit/renderer.test.js
@@ -2,8 +2,8 @@ import path from 'path'
 import consola from 'consola'
 import { Nuxt } from '../utils'
 
-const NO_BUILD_MSG = 'No build files found. Use either `nuxt build` or `builder.build()` or start nuxt in development mode.'
-const NO_MODERN_BUILD_MSG = 'No modern build files found. Use either `nuxt build --modern` or `modern` option to build modern files.'
+const NO_BUILD_MSG = 'No build files found in /path/to/404/dist/server.\nUse either `nuxt build` or `builder.build()` or start nuxt in development mode.'
+const NO_MODERN_BUILD_MSG = 'No modern build files found in /path/to/404/dist/server.\nUse either `nuxt build --modern` or `modern` option to build modern files.'
 
 describe('renderer', () => {
   afterEach(() => {
@@ -39,10 +39,10 @@ describe('renderer', () => {
       mode: 'universal',
       modern: 'client',
       dev: false,
-      buildDir: path.resolve(__dirname, '..', 'fixtures', 'empty', '.nuxt')
+      buildDir: '/path/to/404'
     })
     await nuxt.ready()
-    await expect(nuxt.renderer.renderer.isReady).toBe(true)
+    await expect(nuxt.renderer.renderer.isModernReady).toBe(false)
     expect(consola.fatal).toHaveBeenCalledWith(new Error(NO_MODERN_BUILD_MSG))
   })
 })
