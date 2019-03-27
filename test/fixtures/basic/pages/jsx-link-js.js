1e07455c3769da198d880074e3d844a22facc5d5
pooya parsa <pooya@pi0.ir>
2019-03-27 19:11:44 +0430

    rename js-link to jsx-link-js to prevent dup

---
+++ test/fixtures/basic/pages/jsx-link-js.js
@@ -0,0 +1,5 @@
+import renderLink from './jsx-link-js.js'
+
+export default {
+  render: renderLink
+}
--- test/fixtures/basic/pages/jsx-link.js
+++
@@ -1,5 +0,0 @@
-import renderLink from './jsx-link.jsx'
-
-export default {
-  render: renderLink
-}
