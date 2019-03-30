fd6990098e38d71bcfb0705fc771a8d612a4e3b1
Alexander Lichter <manniL@gmx.net>
2019-03-30 14:48:08 +0100

    Update test/fixtures/with-config/with-config.test.js

--- test/fixtures/with-config/with-config.test.js
+++ test/fixtures/with-config/with-config.test.js
@@ -23,7 +23,7 @@ describe('with-config', () => {
         'additional': expect.stringContaining('plugins/test.json'),
         'message': 'Found 2 plugins that match the configuration, suggest to specify extension:'
       }],
-      ['Please use `build.postcss` in your nuxt.config.js instead of an external config file. Support for such files will be removed in Nuxt 3.'],
+      ['Please use `build.postcss` in your nuxt.config.js instead of an external config file. Support for such files will be removed in Nuxt 3 as they remove all defaults set by Nuxt and can cause severe problems with features like alias resolving inside your CSS.'],
       ['Using styleResources without the nuxt-style-resources-module is not suggested and can lead to severe performance issues.', 'Please use https://github.com/nuxt-community/style-resources-module'],
       ['Notice: Please do not deploy bundles built with analyze mode, it\'s only for analyzing purpose.']
     ])
