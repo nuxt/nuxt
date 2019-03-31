3eec1152f48970afbc5cb19ffa9aa7179c696c72
Utkarsh Gupta <utgupta1997@gmail.com>
2019-04-01 03:36:28 +0530

    examples: add docker example (#5430)

---
+++ examples/docker-build/layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+</style>
---
+++ examples/docker-build/nuxt.config.js
@@ -0,0 +1,54 @@
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
