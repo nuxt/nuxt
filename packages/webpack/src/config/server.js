6b923b59761b7878e9f56240f57ff47a7f7804c2
Clark Du <clark.duxin@gmail.com>
2019-04-02 13:43:47 +0100

    fix: wrong property

--- packages/webpack/src/config/server.js
+++ packages/webpack/src/config/server.js
@@ -56,7 +56,7 @@ export default class WebpackServerConfig extends WebpackBaseConfig {
   resolve() {
     const resolveConfig = super.resolve()
 
-    resolveConfig.mainFields = ['main', 'module']
+    resolveConfig.resolve.mainFields = ['main', 'module']
 
     return resolveConfig
   }
