3fcb17e13611f106b28777f9ed341dd4736a28e5
pooya parsa <pooya@pi0.ir>
2019-04-07 14:13:11 +0430

    fix(cli): throw error with proper code for failed imports

--- packages/cli/src/imports.js
+++ packages/cli/src/imports.js
@@ -5,24 +5,22 @@ const localNodeModules = path.resolve(process.cwd(), 'node_modules')
 
 // Prefer importing modules from local node_modules (for NPX and global bin)
 async function _import(modulePath) {
-  let m
-  for (const mp of [ path.resolve(localNodeModules, modulePath), modulePath ]) {
+  for (const mp of [
+    path.resolve(localNodeModules, modulePath),
+    modulePath
+  ]) {
     try {
-      m = await import(mp)
+      return await import(mp)
     } catch (e) {
       if (e.code !== 'MODULE_NOT_FOUND') {
         throw e
-      } else if (mp === modulePath) {
-        consola.fatal(
-          `Module ${modulePath} not found.\n\n`,
-          `Please install missing dependency:\n\n`,
-          `Using npm:  npm i ${modulePath}\n\n`,
-          `Using yarn: yarn add ${modulePath}`
-        )
       }
     }
   }
-  return m
+
+  const error = new Error(`Cannot import module '${modulePath}'`)
+  error.code = 'MODULE_NOT_FOUND'
+  throw error
 }
 
 export const builder = () => _import('@nuxt/builder')
