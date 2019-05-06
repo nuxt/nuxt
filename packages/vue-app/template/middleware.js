5ebf1cc78c565671d25766bae58a30a5a46427c7
Pooya Parsa <pooya@pi0.ir>
2019-05-06 15:16:04 +0430

    fix: avoid using aliases in templates (#5656)

--- packages/vue-app/template/middleware.js
+++ packages/vue-app/template/middleware.js
@@ -2,7 +2,7 @@ const middleware = {}
 <% middleware.forEach(m => {
    const name = m.src.replace(new RegExp(`\\.(${extensions})$`), '')
 %>
-middleware['<%= name %>'] = require('@/<%= dir.middleware %>/<%= m.src %>');
+middleware['<%= name %>'] = require('<%= relativeToBuild(srcDir, dir.middleware, m.src) %>');
 middleware['<%= name %>'] = middleware['<%= name %>'].default || middleware['<%= name %>']
 <% }) %>
 export default middleware
