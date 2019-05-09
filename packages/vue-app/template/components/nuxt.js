2a66d1952e32ad22cacb97e1f5dd5f2704ce621e
Hyunje Jun <me@noraesae.net>
2019-05-09 20:53:59 +0900

    fix(vue-app): consider `watchQuery` option in `routerViewKey` (#5516)

--- packages/vue-app/template/components/nuxt.js
+++ packages/vue-app/template/components/nuxt.js
@@ -40,10 +40,30 @@ export default {
       if (typeof this.nuxtChildKey !== 'undefined' || this.$route.matched.length > 1) {
         return this.nuxtChildKey || compile(this.$route.matched[0].path)(this.$route.params)
       }
+
       const Component = this.$route.matched[0] && this.$route.matched[0].components.default
-      if (Component && Component.options && Component.options.key) {
-        return (typeof Component.options.key === 'function' ? Component.options.key(this.$route) : Component.options.key)
+      if (Component && Component.options) {
+        const { key, watchQuery } = Component.options
+
+        if (key) {
+          return (typeof key === 'function' ? key(this.$route) : key)
+        }
+
+        if (watchQuery) {
+          if (watchQuery.length) {
+            const pickedQuery = {}
+            for (const queryKey of watchQuery) {
+              pickedQuery[queryKey] = this.$route.query[queryKey]
+            }
+            return this.$router.resolve({
+              path: this.$route.path,
+              query: pickedQuery
+            }).href
+          }
+          return this.$route.fullPath
+        }
       }
+
       return this.$route.path
     }
   },
---
+++ test/fixtures/basic/pages/scroll-to-top/watch-query-true.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <NuxtLink to="/scroll-to-top/watch-query-true?test=1">
+      go to the same page
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  watchQuery: true
+}
+</script>
+
+<style>
+div {
+  margin-top: 1500px;
+}
+</style>
