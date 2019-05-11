42a470d53b5ba3c7176bdf4273d2df3d3149d10e
Kevin Marrec <kevin@marrec.io>
2019-05-11 15:16:31 +0200

    fix(ts): fix `extendRoutes` method type
    Co-authored-by: René Stalder <renestalder@protonmail.com>

--- packages/config/types/router.d.ts
+++ packages/config/types/router.d.ts
@@ -5,11 +5,15 @@
  *                https://router.vuejs.org/api/#router-construction-options
  */
 
-import { RouterOptions, Route } from 'vue-router'
+import { RouterOptions, RouteConfig } from 'vue-router'
+
+export interface NuxtRouteConfig extends RouteConfig {
+  chunkNames: { [key: string]: string }
+}
 
 export interface NuxtConfigurationRouter extends RouterOptions {
   routeNameSplitter?: string
-  extendRoutes?: (routes: Route[], resolve: (...pathSegments: string[]) => string) => void
+  extendRoutes?(routes: NuxtRouteConfig[], resolve: (...pathSegments: string[]) => string): void
   linkPrefetchedClass?: string
   middleware?: string | string[]
   prefetchLinks?: boolean
