134a72fda61f6d38fd9f0a2a908f202518a657aa
Clark Du <clark.duxin@gmail.com>
2019-05-22 11:46:42 +0100

    disable ssrlog in non-dev mode

--- packages/config/src/options.js
+++ packages/config/src/options.js
@@ -299,9 +299,9 @@ export function getNuxtConfig(_options) {
     options.pageTransition.appear = true
   }
 
-  if (options.render.ssrLog === undefined) {
-    options.render.ssrLog = options.dev
-  }
+  options.render.ssrLog = options.dev
+    ? options.render.ssrLog === undefined || options.render.ssrLog
+    : false
 
   // We assume the SPA fallback path is 404.html (for GitHub Pages, Surge, etc.)
   if (options.generate.fallback === true) {
