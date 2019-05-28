74c4b592e8a4d664b10821684b8f65116ab326e2
Sébastien Chopin <seb@chopin.io>
2019-05-28 10:17:50 +0200

    ui: Use only logo and %

--- packages/vue-app/template/components/nuxt-hmr.vue
+++ packages/vue-app/template/components/nuxt-hmr.vue
@@ -1,7 +1,15 @@
 <template>
   <transition appear>
-    <div class="nuxt__hmr" v-if="building">
-      Building ({{ progress }}%)
+    <div class="nuxt__hmr">
+      <svg viewBox="0 0 96 72" version="1" xmlns="http://www.w3.org/2000/svg">
+        <g fill="none" fill-rule="evenodd">
+          <path d="M6 66h23l1-3 21-37L40 6 6 66zM79 66h11L62 17l-5 9 22 37v3zM54 31L35 66h38z"/>
+          <path d="M29 69v-1-2H6L40 6l11 20 3-6L44 3s-2-3-4-3-3 1-5 3L1 63c0 1-2 3 0 6 0 1 2 2 5 2h28c-3 0-4-1-5-2z" fill="#00C58E"/>
+          <path d="M95 63L67 14c0-1-2-3-5-3-1 0-3 0-4 3l-4 6 3 6 5-9 28 49H79a5 5 0 0 1 0 3c-2 2-5 2-5 2h16c1 0 4 0 5-2 1-1 2-3 0-6z" fill="#00C58E"/>
+          <path d="M79 69v-1-2-3L57 26l-3-6-3 6-21 37-1 3a5 5 0 0 0 0 3c1 1 2 2 5 2h40s3 0 5-2zM54 31l19 35H35l19-35z" fill="#FFF" fill-rule="nonzero"/>
+        </g>
+      </svg>
+      {{ progress }}%
     </div>
   </transition>
 </template>
@@ -92,11 +100,11 @@ export default {
 .nuxt__hmr {
   position: absolute;
   font-family: monospace;
-  bottom: 0px;
-  right: 0px;
+  bottom: 20px;
+  right: 20px;
   background-color: #2E495E;
   padding: 5px 10px;
-  border-top-left-radius: 2px;
+  border-radius: 2px;
   box-shadow: 1px 1px 2px 0px rgba(0,0,0,0.2);
   color: #00C48D;
 }
@@ -107,6 +115,11 @@ export default {
 }
 .v-leave-to {
   opacity: 0;
-  transform: translateY(10px);
+  transform: translateY(20px);
+}
+svg {
+  width: 1.1em;
+  position: relative;
+  top: 1px;
 }
 </style>
