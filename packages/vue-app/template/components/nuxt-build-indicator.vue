0d5b49dcbcefb1bc5d0e11a3fcfc475e1ac3c12d
Sébastien Chopin <seb@chopin.io>
2019-05-30 13:27:41 +0200

    fix(vue-app): update build indicator style (#5834)

--- packages/vue-app/template/components/nuxt-build-indicator.vue
+++ packages/vue-app/template/components/nuxt-build-indicator.vue
@@ -123,18 +123,20 @@ export default {
 }
 </script>
 
-<style scopped>
+<style scoped>
 .nuxt__build_indicator {
+  box-sizing: border-box;
   position: absolute;
   font-family: monospace;
   bottom: 20px;
   right: 20px;
   background-color: #2E495E;
   padding: 5px 10px;
-  border-radius: 2px;
+  border-radius: 5px;
   box-shadow: 1px 1px 2px 0px rgba(0,0,0,0.2);
   color: #00C48D;
-  width: 54px;
+  width: 84px;
+  z-index: 2147483647;
 }
 .v-enter-active, .v-leave-active {
   transition-delay: 0.2s;
