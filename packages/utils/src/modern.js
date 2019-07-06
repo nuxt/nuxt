a1124d2fff253ea85c46786fcdd2a5acc17c29d0
Xin Du (Clark) <clark.duxin@gmail.com>
2019-07-06 16:22:50 +0100

    fix(renderer): correctly load nomodule scripts in safari 10 (#6033)

--- packages/utils/src/modern.js
+++ packages/utils/src/modern.js
@@ -46,3 +46,6 @@ export const isModernRequest = (req, modernMode = false) => {
 
   return socket._modern
 }
+
+// https://gist.github.com/samthor/64b114e4a4f539915a95b91ffd340acc
+export const safariNoModuleFix = `!function(){var e=document,t=e.createElement("script");if(!("noModule"in t)&&"onbeforeload"in t){var n=!1;e.addEventListener("beforeload",function(e){if(e.target===t)n=!0;else if(!e.target.hasAttribute("nomodule")||!n)return;e.preventDefault()},!0),t.type="module",t.src=".",e.head.appendChild(t),t.remove()}}();`
