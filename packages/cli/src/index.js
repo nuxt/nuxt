4d2f8be5a6e6d5ad7445f4b6954977c96509be58
Kevin Marrec <kevin@marrec.io>
2019-08-09 15:05:39 +0200

    feat: export cli options

--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -5,6 +5,7 @@ export const commands = _commands
 export const imports = _imports
 
 export { default as NuxtCommand } from './command'
+export { default as options } from './options'
 export { default as setup } from './setup'
 export { default as run } from './run'
 export { loadNuxtConfig } from './utils/config'
