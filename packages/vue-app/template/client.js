d7fa8b81c7ae249f39a308bd0044cbb7764576e8
Clark Du <clark.duxin@gmail.com>
2019-08-12 21:08:14 +0100

    fix: typeof

--- packages/vue-app/template/client.js
+++ packages/vue-app/template/client.js
@@ -150,7 +150,7 @@ async function loadAsyncComponents(to, from, next) {
           return true
         } else if (Array.isArray(watchQuery)) {
           return watchQuery.some(key => this._diffQuery[key])
-        } else if (typeof watchQuery) {
+        } else if (typeof watchQuery === 'function') {
           return watchQuery(from.query, to.query)
         }
         return false
