27c16f82d0ba0906e4b56d5b9f28b715d136625b
pooya parsa <pooya@pi0.ir>
2019-08-21 23:16:46 +0430

    feat(cli): setup hook

--- packages/cli/src/run.js
+++ packages/cli/src/run.js
@@ -14,7 +14,7 @@ function packageExists (name) {
   }
 }
 
-export default async function run (_argv) {
+export default async function run (_argv, hooks = {}) {
   // Check for not installing both nuxt and nuxt-edge
   const dupPkg = '@nuxt/' + (pkgName === '@nuxt/cli-edge' ? 'cli' : 'cli-edge')
   if (packageExists(dupPkg)) {
@@ -33,8 +33,16 @@ export default async function run (_argv) {
     cmd = await getCommand('dev')
   }
 
+  // Check for dev
+  const dev = argv[0] === 'dev'
+
+  // Call setup hook
+  if (typeof hooks.setup === 'function') {
+    await hooks.setup({ cmd, dev, argv })
+  }
+
   // Setup env
-  setup({ dev: argv[0] === 'dev' })
+  setup({ dev })
 
   // Try internal command
   if (cmd) {
