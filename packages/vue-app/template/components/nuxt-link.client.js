f0557173978819fb1ece9f013d98855b947799d2
pimlie <pimlie@hotmail.com>
2019-09-17 18:52:16 +0200

    fix(vue-app): clear idle callback

--- packages/vue-app/template/components/nuxt-link.client.js
+++ packages/vue-app/template/components/nuxt-link.client.js
@@ -10,6 +10,11 @@ const requestIdleCallback = window.requestIdleCallback ||
       })
     }, 1)
   }
+
+const cancelIdleCallback = window.cancelIdleCallback || function (id) {
+  clearTimeout(id)
+}
+
 const observer = window.IntersectionObserver && new window.IntersectionObserver((entries) => {
   entries.forEach(({ intersectionRatio, target: link }) => {
     if (intersectionRatio <= 0) {
@@ -35,10 +40,12 @@ export default {
   },
   mounted () {
     if (!this.noPrefetch) {
-      requestIdleCallback(this.observe, { timeout: 2e3 })
+      this.handleId = requestIdleCallback(this.observe, { timeout: 2e3 })
     }
   },
   beforeDestroy () {
+    cancelIdleCallback(this.handleId)
+
     if (this.__observed) {
       observer.unobserve(this.$el)
       delete this.$el.__prefetch
