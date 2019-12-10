b91c303edcfd51c9a81966fdc58f2a0b31c49922
Sébastien Chopin <seb@nuxtjs.com>
2019-12-10 11:35:37 +0100

    wip

--- packages/vue-app/template/client.js
+++ packages/vue-app/template/client.js
@@ -323,7 +323,6 @@ async function render (to, from, next) {
     <% } %>
 
     <% if (features.middleware) { %>
-    <% if (isFullStatic) { %>await fullStaticMiddleware(this, Components, app.context)<% } %>
     await callMiddleware.call(this, Components, app.context, layout)
     if (nextCalled) {
       return
@@ -465,6 +464,12 @@ async function render (to, from, next) {
       <% if (features.asyncData) { %>
       // Call asyncData(context)
       if (hasAsyncData) {
+        <% if (isFullStatic) { %>
+          const path = to.path.replace(/\/$/, '')
+          const payloadPath = (`${window.<%= globals.context %>.payloadPath}/${path}/payload.json`).replace(/\/+/g, '/')
+
+          console.log('Fake async data', payloadPath)
+        <% } else { %>
         const promise = promisify(Component.options.asyncData, app.context)
           .then((asyncDataResult) => {
             applyAsyncData(Component, asyncDataResult)
@@ -474,6 +479,7 @@ async function render (to, from, next) {
             }
             <% } %>
           })
+        <% } %>
         promises.push(promise)
       }
       <% } %>
