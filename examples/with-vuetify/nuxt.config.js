212050e7db7a84b9b5aaca48428fcf38737d89e7
Bruno Costa <brunocgmxus@gmail.com>
2019-12-19 14:23:05 -0300

    Add vuetify to optionsPath

--- examples/with-vuetify/nuxt.config.js
+++ examples/with-vuetify/nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   mode: 'universal',
   /*
@@ -47,21 +45,7 @@ export default {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+    optionsPath: './vuetify.options.js'
   },
   /*
    ** Build configuration
---
+++ examples/with-vuetify/vuetify.options.js
@@ -0,0 +1,19 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  customVariables: ['~/assets/variables.scss'],
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: colors.blue.darken2,
+        accent: colors.grey.darken3,
+        secondary: colors.amber.darken3,
+        info: colors.teal.lighten1,
+        warning: colors.amber.base,
+        error: colors.deepOrange.accent4,
+        success: colors.green.accent3
+      }
+    }
+  }
+}
