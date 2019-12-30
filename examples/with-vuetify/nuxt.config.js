3706f7de9f8f35510d81cebd86753b5faff6bb59
Bruno Costa <brunocgmxus@gmail.com>
2019-12-30 02:03:10 -0300

    move vuetify options to nuxt config file

--- examples/with-vuetify/nuxt.config.js
+++ examples/with-vuetify/nuxt.config.js
@@ -1,3 +1,5 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   mode: 'universal',
   /*
@@ -49,7 +51,22 @@ export default {
    ** https://github.com/nuxt-community/vuetify-module
    */
   vuetify: {
-    optionsPath: './vuetify.options.js'
+    optionsPath: './vuetify.options.js',
+    theme: {
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3
+        }
+      }
+    },
+
   },
   /*
    ** Build configuration
