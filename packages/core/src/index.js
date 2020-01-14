b8ec9adaff54194b89536b0d6a8a2d7eb62a23fe
Pooya Parsa <pyapar@gmail.com>
2020-01-14 16:10:30 +0100

    feat(core): expose `getNuxt` utility

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -1,3 +1,11 @@
+import { loadNuxtConfig } from '@nuxt/config'
+import Nuxt from './nuxt'
+
 export { default as Module } from './module'
 export { default as Nuxt } from './nuxt'
 export { default as Resolver } from './resolver'
+
+export function getNuxt (configOptions, autoReady = true) {
+  const nuxt = new Nuxt(loadNuxtConfig(configOptions), autoReady)
+  return autoReady ? nuxt.ready() : Promise.resolve(nuxt)
+}
