9228187bf4dd24f3bbd8b9e844d8a801fee26ecc
Pooya Parsa <pyapar@gmail.com>
2020-01-14 16:16:43 +0100

    feat: expose loadNuxtConfig from core

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -4,6 +4,7 @@ import Nuxt from './nuxt'
 export { default as Module } from './module'
 export { default as Nuxt } from './nuxt'
 export { default as Resolver } from './resolver'
+export { loadNuxtConfig } from '@nuxt/config'
 
 export function getNuxt (configOptions, autoReady = true) {
   const nuxt = new Nuxt(loadNuxtConfig(configOptions), autoReady)
