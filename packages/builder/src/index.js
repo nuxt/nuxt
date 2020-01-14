a3cddf376c4e528c83fdcd3c0bd82acf2e1b7e85
Pooya Parsa <pyapar@gmail.com>
2020-01-14 23:59:28 +0100

    expose build helper

--- packages/builder/src/index.js
+++ packages/builder/src/index.js
@@ -4,3 +4,7 @@ export { default as Builder } from './builder'
 export function getBuilder (nuxt) {
   return new Builder(nuxt)
 }
+
+export function build (nuxt) {
+  return getBuilder(nuxt).build()
+}
