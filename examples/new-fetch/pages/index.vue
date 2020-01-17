c3cde5747cb5bb120c630d896fc0ff8131f24927
Sébastien Chopin <seb@nuxtjs.com>
2020-01-17 13:15:18 +0100

    fix: remove Promised

--- examples/new-fetch/pages/index.vue
+++ examples/new-fetch/pages/index.vue
@@ -1,26 +1,24 @@
 <template>
   <div>
     <h1>Blog posts</h1>
-    <Promised :promise="$fetchPromise">
-      <content-placeholders v-if="$isFetching">
-        <content-placeholders-text :lines="20" />
-      </content-placeholders>
-      <p v-else-if="$fetchError">
-        Error while fetching posts: {{ $fetchError.message }}
-      </p>
-      <ul v-else>
-        <li v-for="post of posts" :key="post.id">
-          <n-link :to="`/posts/${post.id}`">
-            {{ post.title }}
-          </n-link>
-        </li>
-        <li>
-          <n-link to="/posts/not-found">
-            404 post
-          </n-link>
-        </li>
-      </ul>
-    </Promised>
+    <content-placeholders v-if="$isFetching">
+      <content-placeholders-text :lines="20" />
+    </content-placeholders>
+    <p v-else-if="$fetchError">
+      Error while fetching posts: {{ $fetchError.message }}
+    </p>
+    <ul v-else>
+      <li v-for="post of posts" :key="post.id">
+        <n-link :to="`/posts/${post.id}`">
+          {{ post.title }}
+        </n-link>
+      </li>
+      <li>
+        <n-link to="/posts/404">
+          404 post
+        </n-link>
+      </li>
+    </ul>
   </div>
 </template>
 
