aa80cc9b52556e0b443e3e4083600477a74d3fab
pooya parsa <pooya@pi0.ir>
2020-02-26 14:06:56 +0100

    upate async-config.size-limit test

--- test/dev/async-config.size-limit.test.js
+++ test/dev/async-config.size-limit.test.js
@@ -20,26 +20,35 @@ describe('nuxt basic resources size limit', () => {
   it('should stay within the size limit range in legacy mode', async () => {
     const legacyResourcesSize = await getResourcesSize(distDir, 'client', { gzip: true, brotli: true })
 
-    const LEGACY_JS_RESOURCES_KB_SIZE = 198
+    const LEGACY_JS_RESOURCES_KB_SIZE = 209
     expect(legacyResourcesSize.uncompressed).toBeWithinSize(LEGACY_JS_RESOURCES_KB_SIZE)
 
-    const LEGACY_JS_RESOURCES_GZIP_KB_SIZE = 68
+    const LEGACY_JS_RESOURCES_GZIP_KB_SIZE = 72
     expect(legacyResourcesSize.gzip).toBeWithinSize(LEGACY_JS_RESOURCES_GZIP_KB_SIZE)
 
-    const LEGACY_JS_RESOURCES_BROTLI_KB_SIZE = 61
+    const LEGACY_JS_RESOURCES_BROTLI_KB_SIZE = 63
     expect(legacyResourcesSize.brotli).toBeWithinSize(LEGACY_JS_RESOURCES_BROTLI_KB_SIZE)
   })
 
   it('should stay within the size limit range in modern mode', async () => {
     const modernResourcesSize = await getResourcesSize(distDir, 'modern', { gzip: true, brotli: true })
 
-    const MODERN_JS_RESOURCES_KB_SIZE = 177
+    const MODERN_JS_RESOURCES_KB_SIZE = 186
     expect(modernResourcesSize.uncompressed).toBeWithinSize(MODERN_JS_RESOURCES_KB_SIZE)
 
-    const MODERN_JS_RESOURCES_GZIP_KB_SIZE = 62
+    const MODERN_JS_RESOURCES_GZIP_KB_SIZE = 64
     expect(modernResourcesSize.gzip).toBeWithinSize(MODERN_JS_RESOURCES_GZIP_KB_SIZE)
 
-    const MODERN_JS_RESOURCES_BROTLI_KB_SIZE = 55
+    const MODERN_JS_RESOURCES_BROTLI_KB_SIZE = 57
     expect(modernResourcesSize.brotli).toBeWithinSize(MODERN_JS_RESOURCES_BROTLI_KB_SIZE)
+  })
+
+  it('modern should be smaller than legacy', async () => {
+    const legacyResourcesSize = await getResourcesSize(distDir, 'client', { gzip: true, brotli: true })
+    const modernResourcesSize = await getResourcesSize(distDir, 'modern', { gzip: true, brotli: true })
+
+    expect(modernResourcesSize.uncompressed < legacyResourcesSize.uncompressed)
+    expect(modernResourcesSize.gzip < legacyResourcesSize.gzip)
+    expect(modernResourcesSize.brotli < legacyResourcesSize.brotli)
   })
 })
