d175c8693b1664db46400bc635ddced3c41a6c3f
pooya parsa <pooya@pi0.ir>
2020-02-29 11:58:01 +0100

    feat: --depth and --no-colors

--- packages/cli/src/commands/webpack.js
+++ packages/cli/src/commands/webpack.js
@@ -14,11 +14,22 @@ export default {
       type: 'string',
       default: 'client',
       description: 'Webpack bundle name. Can be one of: Client, Server and Modern'
+    },
+    depth: {
+      alias: 'd',
+      type: 'string',
+      default: 4,
+      description: 'Inspection Depth'
+    },
+    colors: {
+      type: 'boolean',
+      default: process.stdout.isTTY,
+      description: 'Output with Ansi Colors'
     }
   },
   async run (cmd) {
     const { name } = cmd.argv
-    const [path, query] = cmd._argv
+    const [path, query] = cmd.argv._
 
     const config = await cmd.getNuxtConfig({ dev: false, server: false, _build: true })
     const nuxt = await cmd.getNuxt(config)
@@ -31,7 +42,10 @@ export default {
       throw new Error(`No match in webpack config for path:${path}${query ? ` query:${query}` : ''}. Try inspecting without query and path.`)
     }
 
-    consola.log(formatObj(match) + '\n')
+    consola.log(formatObj(match, {
+      depth: parseInt(cmd.argv.depth),
+      colors: cmd.argv.colors
+    }) + '\n')
   }
 }
 
@@ -78,12 +92,9 @@ function advancedGet (obj = {}, path = '', query = '') {
   return result.length ? result : undefined
 }
 
-function formatObj (obj) {
+function formatObj (obj, formatOptions) {
   if (!util.formatWithOptions) {
     return util.format(obj)
   }
-  return util.formatWithOptions({
-    colors: process.stdout.isTTY,
-    depth: Infinity
-  }, obj)
+  return util.formatWithOptions(formatOptions, obj)
 }
