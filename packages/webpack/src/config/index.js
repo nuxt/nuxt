903c3de5b65edb1c3eed763371a72c1b534e22f7
Pooya Parsa <pyapar@gmail.com>
2020-03-02 19:15:00 +0100

    feat: expose webpack config (#7029)

--- packages/webpack/src/config/index.js
+++ packages/webpack/src/config/index.js
@@ -1,3 +1,3 @@
-export { default as Client } from './client'
-export { default as Modern } from './modern'
-export { default as Server } from './server'
+export { default as client } from './client'
+export { default as modern } from './modern'
+export { default as server } from './server'
