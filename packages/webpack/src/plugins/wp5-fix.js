71e15ab336c83ebc2b9e22abd1d8b4e412b786e7
pooya parsa <pooya@pi0.ir>
2020-03-23 14:08:28 +0100

    chore: update html-webpack-plugin to stable and comment out wp5-fix

--- packages/webpack/src/plugins/wp5-fix.js
+++ packages/webpack/src/plugins/wp5-fix.js
@@ -1,13 +1,12 @@
 module.exports = class WP5Fix {
   apply (compiler) {
-    // Fix for html-webpack-lugin!
-    compiler.hooks.thisCompilation.tap('HtmlWebpackPlugin', (compilation) => {
-      compilation.compilationDependencies = {
-        add () {
-          // eslint-disable-next-line no-console
-          // console.error(new Error('compilation.compilationDependencies is removed!'))
-        }
-      }
-    })
+    // compiler.hooks.thisCompilation.tap('HtmlWebpackPlugin', (compilation) => {
+    //   compilation.compilationDependencies = {
+    //     add () {
+    //       // eslint-disable-next-line no-console
+    //       // console.error(new Error('compilation.compilationDependencies is removed!'))
+    //     }
+    //   }
+    // })
   }
 }
