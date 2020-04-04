9b26f4eeff7c00569a9e544ad25db71513165cee
John.R <jonathan_renard@hotmail.fr>
2020-04-04 14:44:24 +0200

    feat: add check branch name script
    this script verify branch name, and return a non-zero exit code if branch name doesn't respect standard convention

--- scripts/hooks/check-branch-name.js
+++ scripts/hooks/check-branch-name.js
@@ -1,19 +1,25 @@
 const exec = require('child_process').exec
 
-exec('git rev-parse --abbrev-ref HEAD', (error, stdout) => {
-  if (error) {
-    throw error
-  }
-
+const checkBranchName = (name) => {
   // correspond to abc/abcd-efghij-klmnopqr
   const regex = new RegExp('^[a-z-]{3,}/[a-z-]{3,}$')
 
   // remove extra break-line
-  const branchName = stdout.replace(/(\r\n|\n|\r)/gm, '')
+  const branchName = name.replace(/(\r\n|\n|\r)/gm, '')
 
   if (!regex.test(branchName)) {
     // eslint-disable-next-line no-console
     console.error('your branch name does\'nt respect conventionnal-commit', '\n', 'more infos: https://www.conventionalcommits.org/en/v1.0.0/#summary')
     process.exit(1)
+  } else {
+    process.exit(0)
   }
+}
+
+exec('git rev-parse --abbrev-ref HEAD', (error, stdout) => {
+  if (error) {
+    throw error
+  }
+
+  checkBranchName(stdout)
 })
