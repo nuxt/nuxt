d9faa2c6cae1f1c9d5abacd92a59e85753c8ebef
Sébastien Chopin <seb@nuxtjs.com>
2020-05-08 16:10:45 +0200

    chore(cli): no need _build flag

--- packages/cli/src/commands/export.js
+++ packages/cli/src/commands/export.js
@@ -21,7 +21,6 @@ export default {
     const config = await cmd.getNuxtConfig({
       dev: false,
       target: TARGETS.static,
-      _build: false,
       _export: true
     })
     const nuxt = await cmd.getNuxt(config)
