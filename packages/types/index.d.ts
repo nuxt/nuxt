a8c9beb7648f625b762981c9a961269a501401b9
pooya parsa <pooya@pi0.ir>
2020-05-28 18:45:19 +0200

    refactor: export *

--- packages/types/index.d.ts
+++ packages/types/index.d.ts
@@ -1,4 +1,4 @@
 import './process'
 
-export { Context, Middleware, NuxtAppOptions, NuxtError, Plugin, Transition } from './app'
-export { Configuration, Module, ServerMiddleware } from './config'
+export * from './app'
+export * from './config'
