0ad2dc21ac8d0cdbb9fe56ddced7841d9cd0b36c
Jonas Galvez <jonasgalvez@gmail.com>
2020-05-31 12:04:29 -0300

    fix(vue-app): prevent double layout execution (#5703)

--- packages/vue-app/template/client.js
+++ packages/vue-app/template/client.js
@@ -595,13 +595,8 @@ function normalizeComponents (to, ___) {
   })
 }
 
-function showNextPage (to) {
-  // Hide error component if no error
-  if (this._hadError && this._dateLastError === this.$options.nuxt.dateErr) {
-    this.error()
-  }
-
-  <% if (features.layouts) { %>
+<% if (features.layouts) { %>
+function setLayoutForNextPage (to, from, next) {
   // Set layout
   let layout = this.$options.nuxt.err
     ? (NuxtError.options || NuxtError).layout
@@ -611,7 +606,15 @@ function showNextPage (to) {
     layout = layout(app.context)
   }
   this.setLayout(layout)
-  <% } %>
+  next()
+}
+<% } %>
+
+function checkForErrors (to) {
+  // Hide error component if no error
+  if (this._hadError && this._dateLastError === this.$options.nuxt.dateErr) {
+    this.error()
+  }
 }
 
 // When navigating on a different route but the same component is used, Vue.js
@@ -647,7 +650,7 @@ function fixPrepatch (to, ___) {
         })
       }
     })
-    showNextPage.call(this, to)
+    checkForErrors.call(this)
     <% if (isDev) { %>
     // Hot reloading
     setTimeout(() => hotReloadAPI(this), 100)
@@ -826,6 +829,9 @@ async function mountApp (__app) {
 
     // Add afterEach router hooks
     router.afterEach(normalizeComponents)
+    <% if (features.layouts) { %>
+    router.beforeResolve(setLayoutForNextPage.bind(_app))
+    <% } %>
     router.afterEach(fixPrepatch.bind(_app))
 
     // Listen for first Vue update
@@ -874,7 +880,8 @@ async function mountApp (__app) {
   // First render on client-side
   const clientFirstMount = () => {
     normalizeComponents(router.currentRoute, router.currentRoute)
-    showNextPage.call(_app, router.currentRoute)
+    setLayoutForNextPage.call(_app, router.currentRoute)
+    checkForErrors.call(_app)
     // Don't call fixPrepatch.call(_app, router.currentRoute, router.currentRoute) since it's first render
     mount()
   }
