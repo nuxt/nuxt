d178d654fdbc1f8e048720d393f9735ade93a63b
Pooya Parsa <pyapar@gmail.com>
2020-06-16 17:21:42 +0200

    test: update test

--- packages/generator/test/generator.gen.test.js
+++ packages/generator/test/generator.gen.test.js
@@ -95,12 +95,9 @@ describe('generator: generate routes', () => {
     const generator = new Generator(nuxt)
 
     generator.generateRoute = jest.fn()
-    jest.spyOn(routes, 'splice')
 
     const errors = await generator.generateRoutes(routes)
 
-    expect(routes.splice).toBeCalledTimes(3)
-    expect(routes.splice).toBeCalledWith(0, 2)
     expect(waitFor).toBeCalledTimes(5)
     expect(waitFor).nthCalledWith(1, 0)
     expect(waitFor).nthCalledWith(2, 100)
