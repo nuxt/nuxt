9c59482c810a87a4324a5aa78448cf2e98129ad9
Simon Uhrmann <suhrmann@posteo.de>
2020-06-24 16:57:12 +0200

    initial commit

--- examples/meta-info/pages/index.vue
+++ examples/meta-info/pages/index.vue
@@ -1,30 +1,32 @@
 <template>
   <div class="container">
     <h1>Home page 🚀</h1>
-    <NuxtLink to="/about">
-      About page
-    </NuxtLink>
+    <NuxtLink to="/about">About page</NuxtLink>
   </div>
 </template>
 
 <script>
 export default {
-  head: {
-    title: 'Home page 🚀',
-    meta: [
-      { hid: 'description', name: 'description', content: 'Home page description' }
-    ],
-    noscript: [
-      { innerHTML: 'Body No Scripts', body: true }
-    ],
-    script: [
-      { src: '/head.js' },
-      // Supported since 1.0
-      { src: '/body.js', body: true },
-      { src: '/defer.js', defer: '' }
-    ]
+  head() {
+    return {
+      title: "Home page 🚀",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Home page description"
+        }
+      ],
+      noscript: [{ innerHTML: "Body No Scripts", body: true }],
+      script: [
+        { src: "/head.js" },
+        // Supported since 1.0
+        { src: "/body.js", body: true },
+        { src: "/defer.js", defer: "" }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
