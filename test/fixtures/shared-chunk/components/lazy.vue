fe3adb4290ad567f2595ab083d61f44b75cf7bf5
pooya parsa <pyapar@gmail.com>
2020-07-04 14:01:51 +0200

    test: add dynamic import to shared-chunk fixture

---
+++ test/fixtures/shared-chunk/components/lazy.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    Lazy Components
+  </div>
+</template>
--- test/fixtures/shared-chunk/pages/index.vue
+++ test/fixtures/shared-chunk/pages/index.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+    <MyLazyComponent />
     <SharedVendor />
   </div>
 </template>
+
+<script>
+export default {
+  components: {
+    MyLazyComponent: () => import('~/components/lazy.vue')
+  }
+}
+</script>
