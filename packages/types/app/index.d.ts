edc74aaacb740a0b5969014d24239411a54ce51e
hjdtl <873804682@qq.com>
2020-07-07 16:36:52 +0800

    fix nuxt build error
    ERROR in D:/lib/nuxtapp/node_modules/@nuxt/types/app/index.d.ts
    ERROR in D:/lib/nuxtapp/node_modules/@nuxt/types/app/index.d.ts(119,18):
    119:18 Interface 'NuxtApp' incorrectly extends interface 'Vue'.
      Types of property '$loading' are incompatible.
        Type 'NuxtLoading' is not assignable to type '(options: LoadingServiceOptions) => ElLoadingComponent'.
          Type 'DefaultNuxtLoading' is not assignable to type '(options: LoadingServiceOptions) => ElLoadingComponent'.
            Type 'DefaultNuxtLoading' provides no match for the signature '(options: LoadingServiceOptions): ElLoadingComponent'.
        117 | }
      > 119 | export interface NuxtApp extends Vue {
            |                  ^
        120 |   $options: NuxtAppOptions
        121 |   $loading: NuxtLoading
        122 |   context: Context
    
     FATAL  Nuxt build error                                                                                                                                                                                                                      16:20:04  
    
      at WebpackBundler.webpackCompile (node_modules\@nuxt\webpack\dist\webpack.js:5472:21)
      at processTicksAndRejections (internal/process/task_queues.js:93:5)
      at WebpackBundler.build (node_modules\@nuxt\webpack\dist\webpack.js:5421:5)
      at Builder.build (node_modules\@nuxt\builder\dist\builder.js:5629:5)
      at Object.run (node_modules\@nuxt\cli\dist\cli-build.js:107:7)
      at NuxtCommand.run (node_modules\@nuxt\cli\dist\cli-index.js:2798:7)
    
    
       ╭─────────────────────────────╮
       │                             │
       │   ✖ Nuxt Fatal Error        │
       │                             │
       │   Error: Nuxt build error   │
       │                             │
       ╰─────────────────────────────╯
    
    error Command failed with exit code 1.
    info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.

--- packages/types/app/index.d.ts
+++ packages/types/app/index.d.ts
@@ -121,7 +121,7 @@ export interface NuxtAppOptions extends ComponentOptions<Vue> {
 
 export interface NuxtApp extends Vue {
   $options: NuxtAppOptions
-  $loading: NuxtLoading
+  $loading: any // TBD
   context: Context
   error(params: NuxtError): void
   isOffline: boolean
