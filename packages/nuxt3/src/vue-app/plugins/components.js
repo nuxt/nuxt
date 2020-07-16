00e13c3e41275caf21496a2e9c2c8667ca68fd65
pooya parsa <pyapar@gmail.com>
2020-07-16 11:40:48 +0200

    fix: RouterLink import

--- packages/nuxt3/src/vue-app/plugins/components.js
+++ packages/nuxt3/src/vue-app/plugins/components.js
@@ -1,11 +1,11 @@
 // import { h, defineComponent } from 'vue'
-import { Link } from 'vue-router'
+import { RouterLink } from 'vue-router'
 
 // const NuxtLink = defineComponent({
 //   extends: Link
 // })
 
 export default function components ({ app }) {
-  app.component('NuxtLink', Link)
-  app.component('NLink', Link) // TODO: deprecate
+  app.component('NuxtLink', RouterLink)
+  app.component('NLink', RouterLink) // TODO: deprecate
 }
