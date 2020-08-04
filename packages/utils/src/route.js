2eb42876a28d009ccd0b673a9457ef19a3e68423
Ahad Birang <farnabaz@gmail.com>
2020-08-04 17:33:00 +0430

    fix(utils): remove optional params in routes ending with `index` (#7845)

--- packages/utils/src/route.js
+++ packages/utils/src/route.js
@@ -54,6 +54,9 @@ function cleanChildrenRoutes (routes, isChild = false, routeNameSplitter = '-', 
   routes.forEach((route) => {
     route.path = isChild ? route.path.replace('/', '') : route.path
     if (route.path.includes('?')) {
+      if (route.name.endsWith(`${routeNameSplitter}index`)) {
+        route.path = route.path.replace(/\?$/, '')
+      }
       const names = route.name.replace(regExpParentRouteName, '').split(routeNameSplitter)
       const paths = route.path.split('/')
       if (!isChild) {
