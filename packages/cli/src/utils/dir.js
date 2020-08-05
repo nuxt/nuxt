ace29148c17841b821382be1ada6edaf921bcccb
Farnabaz <farnabaz@gmail.com>
2020-08-05 23:40:43 +0430

    use `path.join`

--- packages/cli/src/utils/dir.js
+++ packages/cli/src/utils/dir.js
@@ -1,10 +1,10 @@
 import fs from 'fs'
 import path from 'path'
 
-export function isNuxtDir (dir) {
-  if (fs.existsSync(`${dir}${path.sep}nuxt.config.js`) ||
-    fs.existsSync(`${dir}${path.sep}pages`) ||
-    fs.existsSync(`${dir}${path.sep}nuxt.config.ts`)) {
+export function isNuxtDir (rootDir) {
+  if (fs.existsSync(path.join(rootDir, 'nuxt.config.js')) ||
+    fs.existsSync(path.join(rootDir, 'pages')) ||
+    fs.existsSync(path.join(rootDir, 'nuxt.config.ts'))) {
     return true
   }
   return false
