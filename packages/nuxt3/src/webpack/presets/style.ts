232d3298b443581dc193f3b1e7dd8f4260443720
Xin Du (Clark) <clark.duxin@gmail.com>
2020-09-21 10:37:13 +0100

    feat(style): add style loaders (#50)
    * feat(style): add style loaders
    
    * refactor: merge utils/style-loader to style preset
    
    * refactor style.ts
    
    Co-authored-by: Pooya Parsa <pyapar@gmail.com>

--- packages/nuxt3/src/webpack/presets/style.ts
+++ packages/nuxt3/src/webpack/presets/style.ts
@@ -1,62 +1,136 @@
-// import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-// import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
-// import StyleLoader from '../utils/style-loader'
+import path from 'path'
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin'
+import { wrapArray } from 'src/utils'
+import { fileName, WebpackConfigContext, applyPresets } from '../utils/config'
 
-import { WebpackConfigContext } from '../utils/config'
+export function style (ctx: WebpackConfigContext) {
+  applyPresets(ctx, [
+    loaders,
+    extractCSS,
+    minimizer
+  ])
+}
 
-export function style (_ctx: WebpackConfigContext) {
-  // // CSS extraction)
-  // if (options.build.extractCSS) {
-  //   plugins.push(new MiniCssExtractPlugin(Object.assign({
-  //     filename: fileName(ctx, 'css'),
-  //     chunkFilename: fileName(ctx, 'css')
-  //   }, options.build.extractCSS)))
-  // }
+function minimizer (ctx: WebpackConfigContext) {
+  const { options, config } = ctx
+
+  if (options.build.optimizeCSS && Array.isArray(config.optimization.minimizer)) {
+    config.optimization.minimizer.push(new CssMinimizerPlugin({
+      ...options.build.optimizeCSS
+    }))
+  }
+}
+
+function extractCSS (ctx: WebpackConfigContext) {
+  const { options, config } = ctx
+
   // CSS extraction
-  // if (options.build.extractCSS) {
-  //   plugins.push(new MiniCssExtractPlugin(Object.assign({
-  //     filename: fileName(ctx, 'css'),
-  //     chunkFilename: fileName(ctx, 'css'),
-  //     // TODO: https://github.com/faceyspacey/extract-css-chunks-webpack-plugin/issues/132
-  //     reloadAll: true
-  //   }, options.build.extractCSS)))
-  // }
+  if (options.build.extractCSS) {
+    config.plugins.push(new MiniCssExtractPlugin({
+      filename: fileName(ctx, 'css'),
+      chunkFilename: fileName(ctx, 'css'),
+      ...(options.build.extractCSS as object)
+    }))
+  }
+}
+
+function loaders (ctx: WebpackConfigContext) {
+  const { config, options } = ctx
+
+  // CSS
+  config.module.rules.push(createdStyleRule('css', /\.css$/i, null, ctx))
+
+  //  Postcss
+  config.module.rules.push(createdStyleRule('postcss', /\.p(ost)?css$/i, null, ctx))
+
+  // Less
+  const lessLoader = { loader: 'less-loader', options: options.build.loaders.less }
+  config.module.rules.push(createdStyleRule('less', /\.less$/i, lessLoader, ctx))
+
+  // Sass (TODO: optional dependency)
+  const sassLoader = { loader: 'sass-loader', options: options.build.loaders.sass }
+  config.module.rules.push(createdStyleRule('sass', /\.sass$/i, sassLoader, ctx))
+
+  const scssLoader = { loader: 'sass-loader', options: options.build.loaders.scss }
+  config.module.rules.push(createdStyleRule('scss', /\.scss$/i, scssLoader, ctx))
+
+  // Stylus
+  const stylusLoader = { loader: 'stylus-loader', options: options.build.loaders.stylus }
+  config.module.rules.push(createdStyleRule('stylus', /\.styl(us)?$/i, stylusLoader, ctx))
+}
+
+function createdStyleRule (lang: string, test: RegExp, processorLoader, ctx: WebpackConfigContext) {
+  const { options } = ctx
+
+  const styleLoaders = [
+    processorLoader,
+    createStyleResourcesLoaderRule(lang, options.build.styleResources, options.rootDir)
+  ].filter(Boolean)
+
+  options.build.loaders.css.importLoaders =
+    options.build.loaders.cssModules.importLoaders =
+    styleLoaders.length
+
+  const cssLoaders = createCssLoadersRule(ctx, options.build.loaders.css)
+  const cssModuleLoaders = createCssLoadersRule(ctx, options.build.loaders.cssModules)
+
+  return {
+    test,
+    oneOf: [
+      // This matches <style module>
+      {
+        resourceQuery: /module/,
+        use: cssModuleLoaders.concat(styleLoaders)
+      },
+      // This matches plain <style> or <style scoped>
+      {
+        use: cssLoaders.concat(styleLoaders)
+      }
+    ]
+  }
+}
+
+function createCssLoadersRule (ctx: WebpackConfigContext, cssLoaderOptions) {
+  const { options } = ctx
+
+  const cssLoader = { loader: 'css-loader', options: cssLoaderOptions }
+
+  if (options.build.extractCSS) {
+    if (ctx.isServer) {
+      return [cssLoader]
+    }
+
+    return [
+      {
+        loader: MiniCssExtractPlugin.loader,
+        options: { reloadAll: ctx.isDev, hot: ctx.isDev }
+      },
+      cssLoader
+    ]
+  }
+
   return [
-    // {
-    //   test: /\.css$/i,
-    //   oneOf: styleLoader.apply('css')
-    // },
-    // {
-    //   test: /\.p(ost)?css$/i,
-    //   oneOf: styleLoader.apply('postcss')
-    // },
-    // {
-    //   test: /\.less$/i,
-    //   oneOf: styleLoader.apply('less', {
-    //     loader: 'less-loader',
-    //     options: loaders.less
-    //   })
-    // },
-    // {
-    //   test: /\.sass$/i,
-    //   oneOf: styleLoader.apply('sass', {
-    //     loader: 'sass-loader',
-    //     options: loaders.sass
-    //   })
-    // },
-    // {
-    //   test: /\.scss$/i,
-    //   oneOf: styleLoader.apply('scss', {
-    //     loader: 'sass-loader',
-    //     options: loaders.scss
-    //   })
-    // },
-    // {
-    //   test: /\.styl(us)?$/i,
-    //   oneOf: styleLoader.apply('stylus', {
-    //     loader: 'stylus-loader',
-    //     options: loaders.stylus
-    //   })
-    // }
+    {
+      loader: 'vue-style-loader',
+      options: options.build.loaders.vueStyle
+    },
+    cssLoader
   ]
+}
+
+function createStyleResourcesLoaderRule (styleLang, styleResources, rootDir) {
+  // style-resources-loader
+  // https://github.com/yenshih/style-resources-loader
+  if (!styleResources[styleLang]) {
+    return
+  }
+
+  return {
+    loader: 'style-resources-loader',
+    options: {
+      patterns: wrapArray(styleResources[styleLang]).map(p => path.resolve(rootDir, p)),
+      ...styleResources.options
+    }
+  }
 }
