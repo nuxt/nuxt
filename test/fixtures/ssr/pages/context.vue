9c4dac3c611382a6a580350e43b6b4fb2d4ccc88
Daniel Roe <daniel@roe.dev>
2020-10-13 11:19:08 +0100

    fix(vue-app): use getter to provide `this.$nuxt` (#8170)

---
+++ test/fixtures/ssr/pages/context.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <section data-test="parent">
+      <pre>vm: {{ $route.fullPath }}</pre>
+      <pre>context: {{ $nuxt.context.route.fullPath }}</pre>
+    </section>
+
+    <NuxtChild />
+  </div>
+</template>
