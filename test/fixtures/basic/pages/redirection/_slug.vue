e14b7201b4c646d17d4ba355c17e03e06b45c34f
Daniel Roe <daniel@roe.dev>
2020-11-04 13:35:12 +0000

    fix(vue-app): prevent redirection loop with uri encoded path (#8280)

---
+++ test/fixtures/basic/pages/redirection/_slug.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>
+    Redirected page
+  </h1>
+</template>
+
+<script>
+export default {
+  asyncData ({ redirect, route }) {
+    if (process.server) {
+      // Redirect to the same route
+      redirect(route)
+    }
+
+    return {}
+  }
+}
+</script>
