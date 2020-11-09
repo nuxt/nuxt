1df092d08e062947b356224fdc80f591f7c9ca07
Pooya Parsa <pyapar@gmail.com>
2020-11-09 19:49:58 +0100

    fix(vercel): add api prefix

--- packages/nitro/src/targets/vercel.ts
+++ packages/nitro/src/targets/vercel.ts
@@ -3,7 +3,7 @@ import { SLSTarget } from '../config'
 import { node } from './node'
 
 export const vercel: SLSTarget = extendTarget(node, {
-  targetDir: '{{ rootDir }}/.vercel_build_output/functions/node/_nuxt',
+  targetDir: '{{ rootDir }}/.vercel_build_output/functions/node/api/_nuxt',
   outName: 'index.js',
   inlineChunks: false,
   generateIgnore: [
