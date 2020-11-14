97dcc4a791803fb2a5cb4ef3f42efa79d6583e89
Clark Du <clark.duxin@gmail.com>
2020-11-14 21:19:59 +0000

    fix css-loader require

--- packages/config/src/config/build.js
+++ packages/config/src/config/build.js
@@ -34,8 +34,9 @@ export default () => ({
         embed: 'src'
       }
     },
-    css: {},
+    css: { esModule: false },
     cssModules: {
+      esModule: false,
       modules: {
         localIdentName: '[local]_[hash:base64:5]'
       }
