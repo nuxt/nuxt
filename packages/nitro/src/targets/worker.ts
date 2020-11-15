047761f8b7719d6aac8e318022b4d04dfe479a0f
Pooya Parsa <pyapar@gmail.com>
2020-11-15 03:43:43 +0100

    fix: move hrtime polyfill to timing plugin

--- packages/nitro/src/targets/worker.ts
+++ packages/nitro/src/targets/worker.ts
@@ -1,22 +1,11 @@
 import { SLSTarget } from '../config'
 
-// https://gist.github.com/pi0/1476085924f8a2eb1df85929c20cb43f
-
-const polyfill = `
-const exports = {};
-const module = { exports };
-const global = typeof globalThis !== 'undefined' ? globalThis : "undefined" !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
-global.process = global.process || {};
-(function(){const o=Date.now(),t=()=>Date.now()-o;global.process.hrtime=o=>{const e=Math.floor(.001*(Date.now()-t())),n=.001*t();let a=Math.floor(n)+e,r=Math.floor(n%1*1e9);return o&&(a-=o[0],r-=o[1],r<0&&(a--,r+=1e9)),[a,r]};})();
-`
-
 export const worker: SLSTarget = {
   entry: null, // Abstract
   node: false,
   minify: true,
   hooks: {
     'rollup:before' ({ rollupConfig }) {
-      rollupConfig.output.intro = polyfill + rollupConfig.output.intro
       rollupConfig.output.format = 'iife'
     }
   }
