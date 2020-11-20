6794b01404d99e58b9bbab677a1c2fa79bcefb05
Pooya Parsa <pyapar@gmail.com>
2020-11-20 22:09:53 +0100

    chore: hide automock warn

--- packages/nitro/src/rollup/automock.ts
+++ packages/nitro/src/rollup/automock.ts
@@ -1,11 +1,8 @@
-import consola from 'consola'
-
 export function autoMock () {
   return {
     name: 'auto-mock',
     resolveId (src: string) {
       if (src && !src.startsWith('.') && !src.includes('?') && !src.includes('.js')) {
-        consola.info('Automatically mocking', src)
         return {
           id: require.resolve('@nuxt/un/runtime/mock/proxy')
         }
