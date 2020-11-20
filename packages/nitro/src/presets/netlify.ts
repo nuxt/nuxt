f638a445687af08da8b281c1efbd153856696873
Pooya Parsa <pyapar@gmail.com>
2020-11-20 02:49:43 +0100

    fix: use dist for netlify as default

--- packages/nitro/src/presets/netlify.ts
+++ packages/nitro/src/presets/netlify.ts
@@ -3,6 +3,10 @@ import { SigmaPreset } from '../context'
 import { lambda } from './lambda'
 
 export const netlify: SigmaPreset = extendPreset(lambda, {
+  // @ts-ignore
+  output: {
+    publicDir: '{{ _nuxt.rootDir }}/dist'
+  },
   ignore: [
     'netlify.toml',
     '_redirects'
