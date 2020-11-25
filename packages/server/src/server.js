62bc35920d835cd8d9f44895eeb9d88a1898bdb2
Daniel Roe <daniel@roe.dev>
2020-11-25 23:18:31 +0000

    fix(cli, server): respect `router.trailingSlash` when serving static files
    https://github.com/expressjs/serve-static#redirect
    
    closes #8350

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -92,7 +92,10 @@ export default class Server {
     // For serving static/ files to /
     const staticMiddleware = serveStatic(
       path.resolve(this.options.srcDir, this.options.dir.static),
-      this.options.render.static
+      {
+        redirect: !!this.options.router.trailingSlash,
+        ...this.options.render.static
+      }
     )
     staticMiddleware.prefix = this.options.render.static.prefix
     this.useMiddleware(staticMiddleware)
