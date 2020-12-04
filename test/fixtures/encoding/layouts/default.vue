1c63f05452a52e2cc45ad8a865d3c9f5ef757415
Pooya Parsa <pyapar@gmail.com>
2020-12-04 12:47:02 +0100

    update fixture

--- test/fixtures/encoding/layouts/default.vue
+++ test/fixtures/encoding/layouts/default.vue
@@ -1,37 +1,37 @@
 <template>
   <div>
     <div>
-      <NLink to="/тест">
-        unicode
-      </NLink>
-      <br>
-      <NLink :to="encodeURI('/тест')">
-        unicode (encoded)
-      </NLink>
-      <br>
-      <NLink to="/тест?spa">
-        unicode (SPA)
-      </NLink>
-      <br>
-      <NLink :to="encodeURI('/тест?spa')">
-        unicode (SPA encoded)
-      </NLink>
-      <br>
-      <NLink to="/dynamic/سلام چطوری?q=cofee,food,دسر">
-        dynamic
-      </NLink>
-      <br>
-      <NLink :to="encodeURI('/dynamic/سلام چطوری?q=cofee,food,دسر')">
-        dynamic (encodeURI)
-      </NLink>
-      <br>
-      <NLink to="/dynamic/%D8%B3%D9%84%D8%A7%D9%85%20%DA%86%D8%B7%D9%88%D8%B1%DB%8C?q=cofee%2Cfood%2C%D8%AF%D8%B3%D8%B1">
-        dynamic (encodeURIComponent)
-      </NLink>
+      <ul>
+        <li v-for="link in links" :key="link">
+          <NLink :to="link">
+            {{ link }}
+          </NLink>
+          <a :href="link">(direct)</a>
+        </li>
+      </ul>
     </div>
     <Nuxt />
   </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    links () {
+      return [
+        '/тест',
+        encodeURI('/тест'),
+        '/тест?spa',
+        encodeURI('/тест?spa'),
+        '/dynamic/سلام چطوری?q=cofee,food,دسر',
+        encodeURI('/dynamic/سلام چطوری?q=cofee,food,دسر'),
+        // Using encodeURIComponent on each segment
+        '/dynamic/%D8%B3%D9%84%D8%A7%D9%85%20%DA%86%D8%B7%D9%88%D8%B1%DB%8C?q=cofee%2Cfood%2C%D8%AF%D8%B3%D8%B1'
+      ]
+    }
+  }
+}
+</script>
 
 <style scoped>
 a {
