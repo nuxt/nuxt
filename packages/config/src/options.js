6fdec2d0272a18981ea64257739dd979f2592277
Pooya Parsa <pyapar@gmail.com>
2020-12-06 17:05:11 +0100

    revert formatting change

--- packages/config/src/options.js
+++ packages/config/src/options.js
@@ -6,10 +6,7 @@ import pick from 'lodash/pick'
 import uniq from 'lodash/uniq'
 import consola from 'consola'
 import destr from 'destr'
-import {
-  TARGETS, MODES, guardDir, isNonEmptyString,
-  isPureObject, isUrl, getMainModule, urlJoin, getPKG
-} from '@nuxt/utils'
+import { TARGETS, MODES, guardDir, isNonEmptyString, isPureObject, isUrl, getMainModule, urlJoin, getPKG } from '@nuxt/utils'
 import { normalizeURL } from '@nuxt/ufo'
 import { defaultNuxtConfigFile, getDefaultNuxtConfig } from './config'
 
