0199610834e603e052db5767a1fca36c7ce47d34
Pooya Parsa <pyapar@gmail.com>
2020-12-07 13:53:32 +0100

    chore: enable back automock plugin

--- packages/nitro/src/rollup/plugins/automock.ts
+++ packages/nitro/src/rollup/plugins/automock.ts
@@ -1,11 +1,8 @@
-import consola from 'consola'
-
 export function autoMock () {
   return {
     name: 'auto-mock',
     resolveId (src: string) {
       if (src && !src.startsWith('.') && !src.includes('?') && !src.includes('.js')) {
-        consola.warn('Auto mock external ', src)
         return {
           id: require.resolve('@nuxt/un/runtime/mock/proxy')
         }
