bf8a629a5cbbc80687844adfd8b2965e4038c82a
Daniel Roe <daniel@roe.dev>
2020-12-10 15:05:41 +0000

    fix: use `options.debug` to guard check for duplicate item

--- packages/vue-app/template/mixins/fetch.server.js
+++ packages/vue-app/template/mixins/fetch.server.js
@@ -26,9 +26,11 @@ async function serverPrefetch() {
   attrs['data-fetch-key'] = this._fetchKey
 
   // Add to ssrContext for window.__NUXT__.fetch
-  if (process.env.NODE_ENV === 'development' && this.$ssrContext.nuxt.fetch[this._fetchKey] !== undefined) {
+  <% if (debug) { %>
+  if (this.$ssrContext.nuxt.fetch[this._fetchKey] !== undefined) {
     console.warn(`Duplicate fetch key detected (${this._fetchKey}). This may lead to unexpected results.`)
   }
+  <% } %>
   this.$ssrContext.nuxt.fetch[this._fetchKey] =
     this.$fetchState.error ? { _error: this.$fetchState.error } : purifyData(this._data)
 }
