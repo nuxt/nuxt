e226ea58d50566bd415b37237826baeb42066d85
Alexander Lichter <github@lichter.io>
2021-01-12 21:34:47 +0100

    types: create a basic linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -72,5 +72,24 @@ module.exports = {
     rules: {
       'standard/no-callback-literal': 'off'
     }
+  }, {
+    files: ['**/*.ts'],
+    env: { browser: true, es6: true, node: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended'
+    ],
+    globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaFeatures: { jsx: true },
+      ecmaVersion: 2018,
+      sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 0
+    }
   }]
 }
