776e12f362273435ab0be01f8cf0c734dc3164d3
Pooya Parsa <pyapar@gmail.com>
2021-01-20 21:02:43 +0100

    chore: fix lint issues

--- packages/nuxt3/src/utils/route.ts
+++ packages/nuxt3/src/utils/route.ts
@@ -2,27 +2,6 @@ import path from 'path'
 import get from 'lodash/get'
 import consola from 'consola'
 
-import type { Component } from 'vue'
-import type { _RouteRecordBase } from 'vue-router'
-
-import { r } from './resolve'
-
-interface CreateRouteOptions {
-  files: string[]
-  srcDir: string
-  pagesDir?: string
-  routeNameSplitter?: string
-  supportedExtensions?: string[]
-  trailingSlash: boolean
-}
-
-interface NuxtRouteConfig extends Omit<_RouteRecordBase, 'children'> {
-  component?: Component | string
-  chunkName?: string
-  pathToRegexpOptions?: any
-  children?: NuxtRouteConfig[]
-}
-
 // Guard dir1 from dir2 which can be indiscriminately removed
 export const guardDir = function guardDir (options: Record<string, any>, key1: string, key2: string) {
   const dir1 = get(options, key1, false) as string
