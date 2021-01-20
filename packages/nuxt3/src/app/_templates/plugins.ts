3f99bb7878a3df176b8115004acae7b90182c6d2
Daniel Roe <daniel@roe.dev>
2021-01-20 19:17:36 +0000

    feat: add support for `useHead` (#122)
    * feat: add support for `useHead`
    
    * chore: use sigma@75
    
    Co-authored-by: Pooya Parsa <pyapar@gmail.com>

--- packages/nuxt3/src/app/_templates/plugins.ts
+++ packages/nuxt3/src/app/_templates/plugins.ts
@@ -1,8 +1,10 @@
+import head from 'nuxt/app/plugins/head'
 import router from 'nuxt/app/plugins/router'
 import vuex from 'nuxt/app/plugins/vuex'
 import legacy from 'nuxt/app/plugins/legacy'
 
 export default [
+  head,
   router,
   vuex,
   legacy
