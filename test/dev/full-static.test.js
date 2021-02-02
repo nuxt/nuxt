6a8339e4c4f3f5f05671ff92a1833a10bb5e8784
Daniel Roe <daniel@roe.dev>
2021-02-02 12:38:54 +0000

    fix: encoding issues with payload paths (#8738)
    - fix(generator): decode full static filesystem paths
    - fix(vue-app): check decoded path against manifest
    - fix(vue-app): prevent double encoding for urls
    
    Co-authored-by: Pooya Parsa <pyapar@gmail.com>

--- test/dev/full-static.test.js
+++ test/dev/full-static.test.js
@@ -1,7 +1,8 @@
 import http from 'http'
-import { resolve } from 'path'
+import { join, resolve } from 'path'
 import serveStatic from 'serve-static'
 import finalhandler from 'finalhandler'
+import glob from 'glob'
 import { Builder, Generator, getPort, loadFixture, Nuxt, rp } from '../utils'
 
 let port
@@ -43,7 +44,21 @@ describe('full-static', () => {
     const { body: html } = await rp(url('/payload'))
 
     expect(html).toContain('<script src="https://cdn.nuxtjs.org/test/')
-    expect(html).toContain('<link rel="preload" href="https://cdn.nuxtjs.org/test/_nuxt/static/')
+    expect(html).toContain(
+      '<link rel="preload" href="https://cdn.nuxtjs.org/test/_nuxt/static/'
+    )
+  })
+
+  test('/encoding/中文', async () => {
+    const { body: html } = await rp(url('/encoding/中文'))
+
+    const paths = ['encoding/中文/state.js', 'encoding/中文/payload.js']
+
+    paths.forEach((path) => {
+      const files = glob.sync(join(distDir, '**', path))
+      expect(html).toContain(encodeURI(path))
+      expect(files).toContainEqual(expect.stringContaining(path))
+    })
   })
 
   // Close server and ask nuxt to stop listening to file changes
