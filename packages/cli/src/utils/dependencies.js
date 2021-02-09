d7d03e0394d7b675f4256ea519e75cbb9f68d968
Pooya Parsa <pyapar@gmail.com>
2021-02-09 18:30:10 +0100

    chore: simplify dependencies.js

--- packages/cli/src/utils/dependencies.js
+++ packages/cli/src/utils/dependencies.js
@@ -1,50 +1,35 @@
 import consola from 'consola'
 import { satisfies } from 'semver'
-
 import { getPKG } from '@nuxt/utils'
 
+const dependencies = {
+  postcss: '^7.0.32',
+  webpack: '^4.46.0',
+  'sass-loader': '^10.1.1'
+}
+
+const nodeVersion = '>=12.0.0'
+
+function getInstalledVersion (name) {
+  try {
+    return getPKG(name).version
+  } catch { }
+}
+
 export function checkDependencies () {
-  const constraints = {
-    dependencies: {
-      postcss: {
-        range: '^7.0.32'
-      },
-      webpack: { range: '^4.46.0' }
-    },
-    optionalDependencies: {
-      'sass-loader': { range: '^10.1.1' }
-    },
-    engines: {
-      node: {
-        range: '>=12.0.0',
-        message:
-          'You are using an unsupported version of Node.js (x.y.z) and Nuxt is likely to be broken. It is recommended to use the latest LTS version (https://nodejs.org/en/about/releases/)'
-      }
+  for (const name in dependencies) {
+    const installedVersion = getInstalledVersion(name)
+    if (!installedVersion) {
+      return // Ignore to avoid false-positive warnings
+    }
+    const expectedRange = dependencies[name]
+    if (!satisfies(installedVersion, expectedRange)) {
+      consola.warn(`${name}@${installedVersion} is installed but ${expectedRange} is expected`)
     }
   }
-
-  const packageVersions = {
-    ...constraints.dependencies,
-    ...constraints.optionalDependencies
-  }
-
-  Object.entries(packageVersions).forEach(([name, { range: requiredVersion, message }]) => {
-    try {
-      const installedVersion = getPKG(name).version
-      if (!satisfies(installedVersion, requiredVersion)) {
-        consola.error(message ||
-          `Required version of ${name} (${requiredVersion}) not installed. (${installedVersion} was detected.)`
-        )
-      }
-    } catch {
-      if (!Object.keys(constraints.optionalDependencies).includes(name)) {
-        consola.warn(message || `Expected ${name}@${requiredVersion} to be installed.`)
-      }
-    }
-  })
 
   // Check Node versions
-  if (!satisfies(process.version, constraints.engines.node.range)) {
-    consola.warn(constraints.engines.node.message.replace('x.y.z', process.version))
+  if (!satisfies(process.version, nodeVersion)) {
+    consola.warn(`You are using an unsupported version of Node.js (${process.version}). It is recommended to use the latest LTS version (https://nodejs.org/en/about/releases)`)
   }
 }
