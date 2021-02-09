e5d202badbe65434e762892fc93fdc3142205ab8
Daniel Roe <daniel@roe.dev>
2021-02-09 17:35:22 +0000

    feat(cli): warn if incompatible node/package versions detected (#8792)
    Co-authored-by: Pooya Parsa <pyapar@gmail.com>

---
+++ packages/cli/test/unit/dependencies.test.js
@@ -0,0 +1,25 @@
+import { consola } from '../utils'
+import { checkDependencies } from '../../src/utils/dependencies'
+
+jest.mock('webpack/package.json', () => ({
+  version: '5.0.0'
+}))
+
+describe('cli/utils', () => {
+  afterEach(() => jest.resetAllMocks())
+
+  test('checkDependencies', () => {
+    checkDependencies()
+    expect(consola.warn).toHaveBeenCalledWith(
+      expect.stringMatching(
+        /webpack@.+ is installed but .+ is expected/
+      )
+    )
+    expect(consola.warn).toHaveBeenCalledTimes(1)
+    if (process.version.startsWith('v10')) {
+      expect(consola.warn).toHaveBeenCalledTimes(2)
+    } else {
+      expect(consola.warn).toHaveBeenCalledTimes(1)
+    }
+  })
+})
