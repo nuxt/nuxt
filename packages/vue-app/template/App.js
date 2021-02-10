5d1a5fd7ca1881196c9dce3174def8ee7d1902d7
Daniel Roe <daniel@parentscheme.com>
2021-02-10 11:58:58 +0000

    fix: use ufo without need to add back leading slash

--- packages/vue-app/template/App.js
+++ packages/vue-app/template/App.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { decode, parsePath, withLeadingSlash, withoutBase, withoutTrailingSlash, normalizeURL } from 'ufo'
+import { decode, parsePath, withoutBase, withoutTrailingSlash, normalizeURL } from 'ufo'
 <% utilsImports = [
   ...(features.asyncData || features.fetch) ? [
     'getMatchedComponentsInstances',
@@ -301,7 +301,7 @@ export default {
     },
     getRoutePath(route = '/') {
       const base = this.getRouterBase()
-      return withLeadingSlash(withoutTrailingSlash(withoutBase(parsePath(route).pathname, base)))
+      return withoutTrailingSlash(withoutBase(parsePath(route).pathname, base))
     },
     getStaticAssetsPath(route = '/') {
       const { staticAssetsBase } = window.<%= globals.context %>
