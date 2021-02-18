2548e753c0de4ef706cb163562f8c1ef6dbdd8c8
Sébastien Chopin <seb@nuxtjs.com>
2021-02-18 12:17:25 +0100

    docs: wip welcome page

--- packages/nuxt3/src/app/app.tutorial.vue
+++ packages/nuxt3/src/app/app.tutorial.vue
@@ -1,6 +1,73 @@
 <template>
-  <div class="welcome">
-    <h1>Welcome to Nuxt 3</h1>
-    <p>You can start adding a <code>pages/index.vue</code> or <code>app.vue</code></p>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+      <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+        <svg width="292" height="55" viewBox="0 0 292 55" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M101.929 12.5009H110.507L128.686 42.1505V12.5009H136.605V54.5131H128.085L109.847 24.925V54.5131H101.929V12.5009Z" fill="#003C3C" />
+          <path d="M172 54.5131H164.621V49.8203C162.941 53.1229 159.523 54.9824 155.204 54.9824C148.364 54.9824 143.923 50.0608 143.923 42.9776V24.2622H151.302V41.429C151.302 45.5704 153.823 48.4506 157.602 48.4506C161.742 48.4506 164.621 45.2097 164.621 40.7691V24.2622H172V54.5131Z" fill="#003C3C" />
+          <path d="M197.679 54.5131L190.599 44.4295L183.519 54.5131H175.659L186.579 38.7277L176.5 24.2622H184.539L190.599 32.9673L196.597 24.2622H204.697L194.557 38.7277L205.477 54.5131H197.679Z" fill="#003C3C" />
+          <path d="M220.475 15.1405V24.2621H228.874V30.4449H220.475V45.4618C220.475 45.9662 220.675 46.4499 221.031 46.8068C221.388 47.1637 221.871 47.3646 222.375 47.3654H228.874V54.5013H224.014C217.414 54.5013 213.096 50.6591 213.096 43.9426V30.4508H207.156V24.2621H210.882C212.621 24.2621 213.632 23.2268 213.632 21.5139V15.1405H220.475Z" fill="#003C3C" />
+          <path d="M247.476 44.9985V12.5009H255.392V42.5084C255.392 50.6122 251.493 54.5131 244.894 54.5131H235.679V47.3771H245.099C245.729 47.3771 246.334 47.1265 246.78 46.6804C247.226 46.2344 247.476 45.6293 247.476 44.9985Z" fill="#003C3C" />
+          <path d="M260.134 40.4084H267.756C268.178 45.1012 271.298 48.0899 276.337 48.0899C280.838 48.0899 283.96 46.2919 283.96 42.57C283.96 33.5657 261.386 39.8071 261.386 23.7225C261.391 16.8212 267.331 12.0257 275.865 12.0257C284.746 12.0257 290.744 17.1262 291.11 24.9896H283.529C283.171 21.3292 280.304 18.8684 275.851 18.8684C271.711 18.8684 269.131 20.7895 269.131 23.7284C269.131 33.0319 291.999 26.0748 291.999 42.4556C291.999 50.2574 285.578 55 276.34 55C266.801 55 260.562 49.2367 260.143 40.4142" fill="#003C3C" />
+          <path d="M45.3984 54.4574H75.4809C76.4364 54.4575 77.375 54.2059 78.2025 53.7279C79.0299 53.2499 79.717 52.5624 80.1945 51.7344C80.6721 50.9064 80.9232 49.9673 80.9228 49.0114C80.9224 48.0555 80.6704 47.1165 80.1921 46.289L59.9896 11.2807C59.5121 10.453 58.8252 9.76557 57.9979 9.28764C57.1707 8.80971 56.2322 8.5581 55.2769 8.5581C54.3216 8.5581 53.3831 8.80971 52.5559 9.28764C51.7286 9.76557 51.0416 10.453 50.5641 11.2807L45.3984 20.2381L35.2986 2.72223C34.8207 1.89453 34.1335 1.20724 33.306 0.729397C32.4785 0.251557 31.5399 0 30.5844 0C29.629 0 28.6904 0.251557 27.8629 0.729397C27.0354 1.20724 26.3481 1.89453 25.8702 2.72223L0.730687 46.289C0.252428 47.1165 0.000416245 48.0555 5.15189e-07 49.0114C-0.000415214 49.9673 0.250779 50.9064 0.728319 51.7344C1.20586 52.5624 1.8929 53.2499 2.72035 53.7279C3.5478 54.2059 4.48648 54.4575 5.44197 54.4574H24.3252C31.807 54.4574 37.3245 51.1695 41.1211 44.755L50.3384 28.7849L55.2754 20.2381L70.0924 45.9106H50.3384L45.3984 54.4574ZM24.0174 45.9018L10.8393 45.8989L30.5932 11.6708L40.4497 28.7849L33.8504 40.2236C31.3291 44.3855 28.4648 45.9018 24.0174 45.9018Z" fill="#00DC82" />
+        </svg>
+      </div>
+      <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
+        <div class="grid grid-cols-1 md:grid-cols-2">
+          <div class="p-6">
+            <div class="flex items-center">
+              <svg
+                class="w-6 h-6 text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" /><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" /></svg>
+              <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://nuxtjs.com" class="hover:underline text-gray-900 dark:text-white">Documentation</a></div>
+            </div>
+            <div class="ml-10">
+              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                We highly recommend to take a look at the NuxtJS documentation, wether you are new or have previous expererience with the framework.
+              </div>
+            </div>
+          </div>
+          <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
+            <div class="flex items-center">
+              <svg
+                class="w-6 h-6 text-gray-500"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              ><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" /></svg>
+              <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://twitter.com/nuxt_js" class="hover:underline text-gray-900 dark:text-white">Twitter</a></div>
+            </div>
+            <div class="ml-10">
+              <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
+                Follow the NuxtJS Twitter account to get latest news about releases, new modules, tutorials and tips.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="mt-4 bg-white border-l-8 border-green-400 dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg p-6 pl-4">
+        <p>To remove this welcome page, <strong class="font-bold">you have 2 options</strong>:</p>
+        <ul class="list-inside list-disc pt-1 pl-2">
+          <li>Create an <code class="text-green-600">app.vue</code></li>
+          <li>Create a <code class="text-green-600">pages/index.vue</code></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
