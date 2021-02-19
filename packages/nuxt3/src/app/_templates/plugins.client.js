a1ac066cb51fa99861d52799a11ff4bb1780316c
Pooya Parsa <pyapar@gmail.com>
2021-02-19 14:59:47 +0100

    fix: polyfill $fetch on globalThis
    TODO: nitro client plugin needs rewrite

--- packages/nuxt3/src/app/_templates/plugins.client.js
+++ packages/nuxt3/src/app/_templates/plugins.client.js
@@ -1,8 +1,11 @@
-import '@nuxt/nitro/dist/runtime/app/nitro.client'
+import { $fetch } from 'ohmyfetch'
+import _global from '@nuxt/un/runtime/global'
 import logs from 'nuxt/app/plugins/logs.client.dev'
 import progress from 'nuxt/app/plugins/progress.client'
 <% const plugins = app.plugins.filter(p => p.mode === 'client').map(p => p.src) %>
 <%= nxt.importSources(plugins) %>
+
+_global.$fetch = $fetch
 
 const plugins = [
   progress,
