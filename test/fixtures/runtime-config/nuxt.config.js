16a76bfa9609671240b83a2ce1c9a025c0cb9554
Pooya Parsa <pyapar@gmail.com>
2021-02-19 17:17:14 +0100

    fix(renderer): deep merge runtime config for server

--- test/fixtures/runtime-config/nuxt.config.js
+++ test/fixtures/runtime-config/nuxt.config.js
@@ -2,12 +2,19 @@ export default {
   publicRuntimeConfig: {
     baseURL: process.env.BASE_URL,
     isFixture: true,
-    TOKEN: 'default'
+    TOKEN: 'default',
+    nested: {
+      priv: 0,
+      pub: 1
+    }
   },
   privateRuntimeConfig: {
     baseURL: '${PUBLIC_URL}${BASE_URL}',
     API_SECRET: '',
-    FOO: '123/${FOO}'
+    FOO: '123/${FOO}',
+    nested: {
+      priv: 1
+    }
   },
   serverMiddleware: [
     (req, _, next) => {
