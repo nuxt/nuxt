c9e2f55f50e8d805af7f3b411cf6fa2171add0ec
Daniel Roe <daniel@roe.dev>
2021-03-09 19:30:33 +0000

    test: simplify test behaviour

--- test/dev/basic.relative.test.js
+++ test/dev/basic.relative.test.js
@@ -1,52 +1,41 @@
-import { loadFixture, getPort, Nuxt, rp, Builder } from '../utils'
+import { loadFixture, getPort, Nuxt, rp } from '../utils'
 
 let port
 let nuxt
 const url = route => 'http://localhost:' + port + route
 
-async function expectedBehavior () {
-  const { html } = await nuxt.server.renderRoute('/')
-
-  expect(html).toContain('<img src="./_nuxt/img')
-  const { 1: imageSrc } = html.match(/<img src="\.(\/_nuxt\/img[^"]*)"/)
-
-  const { statusCode } = await rp(url(imageSrc))
-  expect(statusCode).toBe(200)
-}
+const tests = [
+  ['relative publicPath can be used in dev ssr', {
+    build: {
+      publicPath: './_nuxt/'
+    }
+  }],
+  ['relative publicPath can be used in production ssr', {
+    dev: false,
+    build: {
+      publicPath: './_nuxt/'
+    }
+  }]
+]
 
 describe('basic ssr with relative path', () => {
-  test('relative publicPath can be used in dev ssr', async () => {
-    const config = await loadFixture('basic', {
-      build: {
-        publicPath: './_nuxt/'
-      }
-    })
+  tests.forEach(([name, options]) => test(name, async () => {
+    const config = await loadFixture('basic', options)
     nuxt = new Nuxt(config)
     await nuxt.ready()
 
     port = await getPort()
     await nuxt.server.listen(port, 'localhost')
 
-    await expectedBehavior()
+    const { html } = await nuxt.server.renderRoute('/')
+
+    expect(html).toContain('<img src="./_nuxt/img')
+    const { 1: imageSrc } = html.match(/<img src="\.(\/_nuxt\/img[^"]*)"/)
+
+    const { statusCode } = await rp(url(imageSrc))
+    expect(statusCode).toBe(200)
 
     await nuxt.close()
-  })
-
-  test('relative publicPath can be used in production ssr', async () => {
-    const config = await loadFixture('basic', {
-      dev: false,
-      build: {
-        publicPath: './_nuxt/'
-      }
-    })
-    nuxt = new Nuxt(config)
-    await nuxt.ready()
-
-    port = await getPort()
-    await nuxt.server.listen(port, 'localhost')
-
-    await expectedBehavior()
-
-    await nuxt.close()
-  })
+  }
+  ))
 })
