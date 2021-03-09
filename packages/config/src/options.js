8d7a10633c9c18654d11836e859d20de8191c4fc
Daniel Roe <daniel@roe.dev>
2021-03-09 19:25:18 +0000

    fix: improve clarity of `_publicPath` use

--- packages/config/src/options.js
+++ packages/config/src/options.js
@@ -213,8 +213,12 @@ export function getNuxtConfig (_options) {
     options.appTemplatePath = path.resolve(options.srcDir, options.appTemplatePath)
   }
 
-  options.build.publicPath = options.build.publicPath.replace(/([^/])$/, '$1/')
-  options.build._publicPath = options.build._publicPath.replace(/([^/])$/, '$1/').replace(/^\.\//, '/')
+  options.build.publicPath = withTrailingSlash(options.build.publicPath)
+  options.build._publicPath = withTrailingSlash(options.build._publicPath)
+  // Ensure that _publicPath is absolute (for use in server/webpack)
+  if (isRelative(options.build._publicPath)) {
+    options.build._publicPath = options.build._publicPath.replace(/^\.+\//, '/')
+  }
 
   // Ignore publicPath on dev
   if (options.dev && isUrl(options.build.publicPath)) {
