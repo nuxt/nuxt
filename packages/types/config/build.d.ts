88c23be4a6339de34548173d761459421f974d2d
Xin Du (Clark) <clark.duxin@gmail.com>
2021-03-30 23:09:29 +0100

    fix(types): add build-in terser types

--- packages/types/config/build.d.ts
+++ packages/types/config/build.d.ts
@@ -12,7 +12,6 @@ import { Options as SassOptions } from 'sass'
 import { Options as OptimizeCssAssetsWebpackPluginOptions } from 'optimize-css-assets-webpack-plugin'
 import { Plugin as PostcssPlugin } from 'postcss'
 import { Options as PugOptions } from 'pug'
-import { TerserPluginOptions } from 'terser-webpack-plugin'
 import { VueLoaderOptions } from 'vue-loader'
 import {
   Configuration as WebpackConfiguration,
@@ -24,6 +23,8 @@ import {
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 import { Options as WebpackDevMiddlewareOptions } from 'webpack-dev-middleware'
 import { MiddlewareOptions as WebpackHotMiddlewareOptions, ClientOptions as WebpackHotMiddlewareClientOptions } from 'webpack-hot-middleware'
+
+import { TerserPluginOptions } from '../terser-webpack-plugins'
 
 type CssLoaderUrlFunction = (url: string, resourcePath: string) => boolean
 type CssLoaderImportFunction = (url: string, media: string, resourcePath: string) => boolean
