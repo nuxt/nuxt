325c6b8feaaba6346c05812876143cecd9699d14
Pooya Parsa <pyapar@gmail.com>
2021-04-28 17:37:35 +0200

    chore(nitro): avoid optional chaining due to wrangler issue
    https://github.com/nuxt/framework/issues/97

--- packages/nitro/src/runtime/app/vue2.ts
+++ packages/nitro/src/runtime/app/vue2.ts
@@ -8,7 +8,7 @@ const __VUE_SSR_CONTEXT__ = globalThis.__VUE_SSR_CONTEXT__ = {}
 export function renderToString (component, context) {
   return new Promise((resolve, reject) => {
     _renderer.renderToString(component, context, (err, result) => {
-      const styles = [__VUE_SSR_CONTEXT__, context].map(c => c?._styles?.default).filter(Boolean)
+      const styles = [__VUE_SSR_CONTEXT__, context].map(c => c && c._styles && c._styles.default).filter(Boolean)
       if (!context._styles) { context._styles = {} }
       context._styles.default = {
         ids: [...styles.map(s => s.ids)],
