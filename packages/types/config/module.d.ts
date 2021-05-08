1190e7b6851381cafe185a951f00b82bc4a1e64a
Pooya Parsa <pyapar@gmail.com>
2021-05-08 22:35:27 +0200

    hotfix: revert feat additions
    - 380ffb6481ad9c4f68b7d0a4927bd61aaf27a9e7
    - 4d8077f8fee5b1ded6cb9eb557274580a3d8932d
    - 2ec62617ced873fef97c73a6d7aa1271911ccfd

--- packages/types/config/module.d.ts
+++ packages/types/config/module.d.ts
@@ -6,8 +6,6 @@
 
 import { Configuration as WebpackConfiguration } from 'webpack'
 import { NuxtOptionsLoaders } from './build'
-import { NuxtRouteConfig } from './router'
-import { NuxtOptionsServerMiddleware } from './server-middleware'
 import { NuxtOptions } from '.'
 
 interface ExtendFunctionContext {
@@ -19,43 +17,10 @@ interface ExtendFunctionContext {
   loaders: NuxtOptionsLoaders
 }
 
-export interface ModuleTemplateConfig {
-  src: string,
-  fileName?: string
-  filename?: string
-  options?: Record<string, any>
-  [key: string]: any
-}
-
-export interface ModulePluginTemplateConfig extends ModuleTemplateConfig {
-  ssr?: boolean
-  mode?: 'all' | 'client' | 'server'
-}
-
-export interface ModuleTemplateDest {
-  src: string
-  dst: string
-  options?: Record<string, any>
-}
-
-export type ModuleTemplate = ModuleTemplateConfig | string
-export type ModulePluginTemplate = ModulePluginTemplateConfig | string
-
-type ExtendRoutesFunction = (routes: NuxtRouteConfig[], resolve: (...pathSegments: string[]) => string) => void
-type ExtendBuildFunction = (config: WebpackConfiguration, ctx: ExtendFunctionContext) => void
+type ExtendFunction = (config: WebpackConfiguration, ctx: ExtendFunctionContext) => void
 
 interface ModuleThis {
-  addTemplate(template: ModuleTemplate): ModuleTemplateDest
-  addPlugin(template: ModulePluginTemplate): void
-  addLayout(template: ModuleTemplate, name?: string): void
-  addErrorLayout (dst: string): void
-  addServerMiddleware (middleware: NuxtOptionsServerMiddleware): void
-  extendBuild(fn: ExtendBuildFunction): void
-  extendRoutes(fn: ExtendRoutesFunction): void
-  // eslint-disable-next-line no-use-before-define
-  requireModule (moduleOpts: NuxtOptionsModule, paths?: string[]): Promise<any>
-  // eslint-disable-next-line no-use-before-define
-  addModule (moduleOpts: NuxtOptionsModule, paths?: string[]): Promise<any>
+  extendBuild(fn: ExtendFunction): void
   options: NuxtOptions
   nuxt: any // TBD
   [key: string]: any // TBD
