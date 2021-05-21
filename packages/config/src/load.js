nuxt/nuxt

commit 186df335d926b56f8eec5249d6330363622390b3

feat(config): support `nuxtrc` in dist directory (#9280)

packages/config/src/load.js
@@ -88,10 +88,13 @@ export async function loadNuxtConfig ({
   }
 
   // Load Combine configs
-  // Priority: configOverrides > nuxtConfig > .nuxtrc > .nuxtrc (global)
+  // Priority: configOverrides > nuxtConfig > .nuxt/dist/nuxtrc > .nuxtrc > .nuxtrc (global)
+  const dev = configOverrides.dev ?? options.dev ?? configContext.dev
+  const buildDir = configOverrides.buildDir ?? options.buildDir ?? configContext.buildDir ?? '.nuxt'
   options = defu(
     configOverrides,
     options,
+    !dev ? rc.read({ name: 'nuxtrc', dir: path.resolve(buildDir, 'dist') }) : {},
     rc.read({ name: '.nuxtrc', dir: options.rootDir }),
     rc.readUser('.nuxtrc')
   )

commit 24c79a1b21a78ebdc4d4c146ee03f21ffe108d4e

fix(config): remove unsupported nullish coalescing
* issue introduced in https://github.com/nuxt/nuxt.js/pull/9280

closes #9321

packages/config/src/load.js
@@ -8,6 +8,14 @@ import destr from 'destr'
 import * as rc from 'rc9'
 import { defaultNuxtConfigFile } from './config'
 
+function coalesce(...values) {
+  const value = values.shift()
+  if (value !== null && value !== undefined) {
+    return value
+  }
+  return coalesce(...values)
+}
+
 export async function loadNuxtConfig ({
   rootDir = '.',
   envConfig = {},
@@ -89,8 +97,8 @@ export async function loadNuxtConfig ({
 
   // Load Combine configs
   // Priority: configOverrides > nuxtConfig > .nuxt/dist/nuxtrc > .nuxtrc > .nuxtrc (global)
-  const dev = configOverrides.dev ?? options.dev ?? configContext.dev
-  const buildDir = configOverrides.buildDir ?? options.buildDir ?? configContext.buildDir ?? '.nuxt'
+  const dev = coalesce(configOverrides.dev, options.dev, configContext.dev)
+  const buildDir = coalesce(configOverrides.buildDir, options.buildDir, configContext.buildDir, '.nuxt')
   options = defu(
     configOverrides,
     options,