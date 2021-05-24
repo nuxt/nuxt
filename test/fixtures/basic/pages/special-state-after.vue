2a9bc14f35a9f10ca0fd3884d51f4cbce0a101e1
David Ovčačík <david.ovcacik@gmail.com>
2021-05-24 20:55:05 +0200

    feat(ssr): Implement afterNuxtRender context method

---
+++ test/fixtures/basic/pages/special-state-after.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Special state in `window.__NUXT__`</h1>
+</template>
+
+<script>
+export default {
+  middleware ({ afterNuxtRender }) {
+    if (process.server) {
+      afterNuxtRender(({ nuxtState }) => {
+        nuxtState.testAfter = true
+      })
+    }
+  }
+}
+</script>
