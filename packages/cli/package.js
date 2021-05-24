23d9775097f01e60156be19f3826067cb06bf8f7
Pooya Parsa <pyapar@gmail.com>
2021-05-24 19:16:10 +0200

    chore(pkg): build for node12

--- packages/cli/package.js
+++ packages/cli/package.js
@@ -1,5 +1,8 @@
 export default {
   build: true,
-  ignoreUnused: ['crc'],
+  ignoreUnused: [
+    'crc', 'compression', 'connect', 'destr', 'fs-extra',
+    'globby', 'opener', 'pretty-bytes', 'serve-static', 'upath', 'lodash'
+  ],
   externals: ['crc/lib/crc32']
 }
