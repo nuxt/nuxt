e597aaa622184d818cce8e8fa40f93e83997e9be
William L'Archeveque <william.larcheveque@gmail.com>
2021-05-26 03:05:11 -0400

    feat(generator): add ignoreEnv generate option during ensureBuild(cmd) (#8955)
    [release]

---
+++ test/fixtures/full-static-with-ignore-env/pages/store.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>Welcome {{ $store.state.auth.user.name }}!</h3>
+    <br>
+    You visited this page <strong>{{ $store.state.counter }}</strong> times.
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData ({ store }) {
+    await store.dispatch('auth/FETCH_USER')
+  },
+  fetch () {
+    this.$store.commit('COUNT')
+  }
+}
+</script>
