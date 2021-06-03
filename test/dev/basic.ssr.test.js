1fd01969c707f7f36f8bd7273bef81373a338c68
Daniel Roe <daniel@roe.dev>
2021-06-03 17:58:21 +0100

    style: fix lint error

--- test/dev/basic.ssr.test.js
+++ test/dev/basic.ssr.test.js
@@ -210,7 +210,7 @@ describe('basic ssr', () => {
   test('/before-serialize -> check window.__NUXT__.beforeSerialize = true', async () => {
     const window = await nuxt.server.renderAndGetWindow(url('/before-serialize'))
     expect(window.document.title).toBe('Nuxt')
-    expect(window.__NUXT__.beforeSerialize).toBe(true);
+    expect(window.__NUXT__.beforeSerialize).toBe(true)
   })
 
   test('/error', async () => {
