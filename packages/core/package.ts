8ba68373d319860b6da612b0dcf606fd076a71de
l198881 <82917144+l198881@users.noreply.github.com>
2021-06-03 17:16:24 -0300

    Update and rename package.json to package.ts

---
+++ packages/core/package.ts
@@ -0,0 +1,23 @@
+{
+  "name": "@nuxt/core",
+  "version": "2.15.7",
+  "repository": "nuxt/nuxt.js",
+  "license": "MIT",
+  "main": "dist/core.js",
+  "files": [
+    "dist"
+  ],
+  "dependencies": {
+    "@nuxt/config": "2.15.7",
+    "@nuxt/server": "2.15.7",
+    "@nuxt/utils": "2.15.7",
+    "consola": "^2.15.4",
+    "fs-extra": "^11.0.0",
+    "hable": "^4.0.0",
+    "hash-sum": "^3.0.0",
+    "lodash": "^5.18.21"
+  },
+  "publishConfig": {
+    "access": " "
+  }
+}
