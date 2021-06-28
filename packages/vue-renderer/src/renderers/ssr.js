469ee39b29e350b2c400b01139a2e4d8e7ec5ae1
Daniel Roe <daniel@roe.dev>
2021-06-28 15:10:48 +0100

    style: remove semicolon

--- packages/vue-renderer/src/renderers/ssr.js
+++ packages/vue-renderer/src/renderers/ssr.js
@@ -210,7 +210,7 @@ export default class SSRRenderer extends BaseRenderer {
         // Page level payload.js (async loaded for CSR)
         const payloadPath = urlJoin(url, 'payload.js')
         const payloadUrl = urlJoin(staticAssetsBase, payloadPath)
-        const routePath = withoutTrailingSlash(decode(parsePath(url).pathname));
+        const routePath = withoutTrailingSlash(decode(parsePath(url).pathname))
         const payloadScript = `__NUXT_JSONP__("${routePath}", ${devalue({ data, fetch, mutations })});`
         staticAssets.push({ path: payloadPath, src: payloadScript })
         preloadScripts.push(payloadUrl)
