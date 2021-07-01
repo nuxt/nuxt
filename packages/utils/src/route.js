267373f910f1e7800747fbc18acb2d72749241a7
Mohammad Saleh Fadaei <ms.fadaei1997@gmail.com>
2021-07-01 16:23:49 +0430

    fix(utils): Router config trailingSlash causes error with dynamic nuxt-child routes

--- packages/utils/src/route.js
+++ packages/utils/src/route.js
@@ -56,7 +56,11 @@ function cleanChildrenRoutes (routes, isChild = false, routeNameSplitter = '-', 
     route.path = isChild ? route.path.replace('/', '') : route.path
     if (route.path.includes('?')) {
       if (route.name.endsWith(`${routeNameSplitter}index`)) {
-        route.path = route.path.replace(/\?$/, '')
+        if (trailingSlash) {
+          route.path = route.path.replace(/\?\/$/, '/')
+        } else {
+          route.path = route.path.replace(/\?$/, '')
+        }
       }
       const names = route.name.replace(regExpParentRouteName, '').split(routeNameSplitter)
       const paths = route.path.split('/')
