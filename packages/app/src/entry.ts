1bd0b7429953fae3cc86a9183dffdcc913a0767c
Daniel Roe <daniel@roe.dev>
2021-07-12 11:19:34 +0100

    fix: use esm syntax for webpack module hot (#316)

--- packages/app/src/entry.ts
+++ packages/app/src/entry.ts
@@ -26,8 +26,9 @@ if (process.server) {
 if (process.client) {
   // TODO: temporary webpack 5 HMR fix
   // https://github.com/webpack-contrib/webpack-hot-middleware/issues/390
-  if (process.dev && module.hot) {
-    module.hot.accept()
+  // @ts-ignore
+  if (process.dev && import.meta.webpackHot) {
+    import.meta.webpackHot.accept()
   }
 
   entry = async function initApp () {
