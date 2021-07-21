77874bf4d2df4b6c18ada81936dfcae0b8d63e2b
Daniel Roe <daniel@roe.dev>
2021-07-21 21:06:05 +0100

    fix(app): finish hydration when the page is loaded (#346)

--- packages/app/src/entry.ts
+++ packages/app/src/entry.ts
@@ -43,11 +43,14 @@ if (process.client) {
     await nuxt.hooks.callHook('app:created', app)
     await nuxt.hooks.callHook('app:beforeMount', app)
 
+    nuxt.hooks.hookOnce('page:finished', () => {
+      nuxt.isHydrating = false
+    })
+
     app.mount('#__nuxt')
 
     await nuxt.hooks.callHook('app:mounted', app)
     await nextTick()
-    nuxt.isHydrating = false
   }
 
   entry().catch((error) => {
