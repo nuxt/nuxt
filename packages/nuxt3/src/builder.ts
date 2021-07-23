5d78c14ed66a54fc36e328278d6d6387f3c5a344
Daniel Roe <daniel@roe.dev>
2021-07-23 15:42:49 +0100

    fix(nuxt3): reresolve app entrypoint when changes to `app.vue` are detected (#363)

--- packages/nuxt3/src/builder.ts
+++ packages/nuxt3/src/builder.ts
@@ -14,6 +14,9 @@ export async function build (nuxt: Nuxt) {
     watch(nuxt)
     nuxt.hook('builder:watch', async (event, path) => {
       if (event !== 'change' && /app|plugins/i.test(path)) {
+        if (path.match(/app/i)) {
+          app.main = null
+        }
         await generateApp(nuxt, app)
       }
     })
