ee8eaef7ab2aed87eff55b54861e90b4df4ec7fb
Pooya Parsa <pyapar@gmail.com>
2021-07-29 16:13:10 +0200

    chore: update release-edge script

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -xe
+
 # Restore all git changes
 git restore -s@ -SW  -- packages examples
 
@@ -7,13 +9,14 @@ git restore -s@ -SW  -- packages examples
 yarn jiti ./scripts/bump-edge
 
 # Resolve yarn
-yarn
+YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
 
 # Update token
 if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
   echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
   echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
   echo "always-auth=true" >> ~/.npmrc
+  echo "npmAuthToken: ${NODE_AUTH_TOKEN}" >> ~/.yarnrc.yml
   npm whoami
 fi
 
@@ -21,6 +24,6 @@ fi
 for p in packages/* ; do
   pushd $p
   echo "Publishing $p"
-  npm publish -q --access public  # --dry-run
+  yarn npm publish --access public --tolerate-republish
   popd
 done
