369f7c006d45c9f77bcbdb5febff2b2bdeac8df3
mrazauskas <72159681+mrazauskas@users.noreply.github.com>
2021-08-12 08:50:08 +0300

    fix: allow passing builder to getGenerator

--- packages/generator/src/index.js
+++ packages/generator/src/index.js
@@ -1,9 +1,6 @@
-import { Builder } from '@nuxt/builder'
-
 import Generator from './generator'
 export { default as Generator } from './generator'
 
-export function getGenerator (nuxt) {
-  const builder = new Builder(nuxt)
+export function getGenerator (nuxt, builder) {
   return new Generator(nuxt, builder)
 }
