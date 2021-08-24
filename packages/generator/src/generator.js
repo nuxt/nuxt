0aad7b863b735aa199996fc999f8e95525806ff4
aewshyae <1528058+aewshyae@users.noreply.github.com>
2021-08-25 08:01:00 +0900

    add '--fail-on-error' as failOnError to Generator.generate to fail when it encounters the first errors

--- packages/generator/src/generator.js
+++ packages/generator/src/generator.js
@@ -47,7 +47,7 @@ export default class Generator {
     }
   }
 
-  async generate ({ build = true, init = true } = {}) {
+  async generate ({ build = true, init = true, failOnError = false } = {}) {
     consola.debug('Initializing generator...')
     await this.initiate({ build, init })
 
@@ -55,7 +55,7 @@ export default class Generator {
     const routes = await this.initRoutes()
 
     consola.info('Generating pages' + (this.isFullStatic ? ' with full static mode' : ''))
-    const errors = await this.generateRoutes(routes)
+    const errors = await this.generateRoutes(routes, failOnError)
 
     await this.afterGenerate()
 
@@ -168,7 +168,7 @@ or disable the build step: \`generate({ build: false })\``)
     return requireModule(path.join(this.options.buildDir, 'routes.json'))
   }
 
-  async generateRoutes (routes) {
+  async generateRoutes (routes, failOnError = false) {
     const errors = []
 
     this.routes = []
@@ -183,6 +183,9 @@ or disable the build step: \`generate({ build: false })\``)
 
     // Start generate process
     while (this.routes.length) {
+      if (errors.length && failOnError) {
+        return errors
+      }
       let n = 0
       await Promise.all(
         this.routes
