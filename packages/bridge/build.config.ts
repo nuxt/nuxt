9c14b886e8c28945611d730135ee7121c15e584c
Pooya Parsa <pyapar@gmail.com>
2021-09-06 00:01:33 +0200

    fix(bridge): generate `dist/runtime`

--- packages/bridge/build.config.ts
+++ packages/bridge/build.config.ts
@@ -2,7 +2,8 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
-    'src/module'
+    'src/module',
+    { input: 'src/runtime/', outDir: 'dist/runtime', format: 'esm' }
   ],
   externals: [
     'webpack'
