133c03ae2f056f4760d7d9843215ce7ab619ae87
Daniel Roe <daniel@roe.dev>
2021-09-08 10:54:17 +0100

    chore(nitro): avoid optional chaining in nitro render fn (#493)

--- packages/nitro/src/runtime/app/render.ts
+++ packages/nitro/src/runtime/app/render.ts
@@ -110,7 +110,7 @@ async function renderHTML (payload, rendered, ssrContext) {
   const html = rendered.html
 
   if ('renderMeta' in ssrContext) {
-    rendered.meta = await ssrContext.renderMeta?.()
+    rendered.meta = await ssrContext.renderMeta()
   }
 
   const {
