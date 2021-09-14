64a426527fba0c8ccf65dbaf6c4d829487aa64f3
Ahad Birang <farnabaz@gmail.com>
2021-09-14 23:38:17 +0430

    fix(nitro): pass query params to localCall in service worker (#530)

--- packages/nitro/src/runtime/entries/service-worker.ts
+++ packages/nitro/src/runtime/entries/service-worker.ts
@@ -21,7 +21,7 @@ async function handleEvent (url, event) {
   }
   const r = await localCall({
     event,
-    url: url.pathname,
+    url: url.pathname + url.search,
     host: url.hostname,
     protocol: url.protocol,
     headers: event.request.headers,
