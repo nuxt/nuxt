3e0e24f80d0f9643175376d35accc16b28f32a0e
Ahad Birang <farnabaz@gmail.com>
2021-09-20 13:50:55 +0430

    fix(nitro): disable externals in worker preset (#535)

--- packages/nitro/src/presets/worker.ts
+++ packages/nitro/src/presets/worker.ts
@@ -4,5 +4,6 @@ export const worker: NitroPreset = {
   entry: null, // Abstract
   node: false,
   minify: true,
+  externals: false,
   inlineDynamicImports: true // iffe does not support code-splitting
 }
