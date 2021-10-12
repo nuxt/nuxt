ad24965bbc4383d93557f6722a232d304ecbc478
Anthony Fu <anthonyfu117@hotmail.com>
2021-10-12 21:49:21 +0800

    chore: update playground style (#755)

--- playground/app.vue
+++ playground/app.vue
@@ -1,12 +1,30 @@
 <template>
-  <div>
-    <img src="~/assets/logo.svg">
-    <br>
-    Hello Nuxt 3
+  <div class="app">
+    <img src="~/assets/logo.svg" height="40">
+    <h1 class="greeting">
+      {{ hello }}, <br>Nuxt 3!
+    </h1>
   </div>
 </template>
 
-<script>
-export default defineNuxtComponent({
-})
+<script setup>
+const greetings = [
+  'Hello',
+  'Hi',
+  'Salut',
+  'こんにちは',
+  '你好'
+]
+const hello = greetings[Math.random() * greetings.length | 0]
 </script>
+
+<style scoped>
+.app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  padding: 1rem 1.5rem;
+}
+.greeting {
+  font-size: 4em;
+  font-weight: 400;
+}
+</style>
