4b04cdfdf10fa4aaa82c8ee03c9fcaaac802e041
Anthony Fu <anthonyfu117@hotmail.com>
2021-10-12 22:59:19 +0800

    docs: init function for `useState` (#780)

--- packages/nuxt3/src/app/composables/state.ts
+++ packages/nuxt3/src/app/composables/state.ts
@@ -4,8 +4,8 @@ import { useNuxtApp } from '#app'
 /**
  * Create a global reactive ref that will be hydrated but not shared across ssr requests
  *
- * @param key a unique key to identify the data in the Nuxt payload
- * @param init a function that provides initial value for state if it's not initiated
+ * @param key a unique key ensuring that data fetching can be properly de-duplicated across requests
+ * @param init a function that provides initial value for the state when it's not initiated
  */
 export const useState = <T> (key: string, init?: (() => T)): Ref<T> => {
   const nuxt = useNuxtApp()
