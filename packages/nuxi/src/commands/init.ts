870a62855ce8ef09793e853955c7e433dfab7d38
Abderrahim SOUBAI-ELIDRISI <gx.coder@gmail.com>
2021-10-18 11:04:06 +0100

    chore(deps): replace degit by tiged (#1052)

--- packages/nuxi/src/commands/init.ts
+++ packages/nuxi/src/commands/init.ts
@@ -1,5 +1,5 @@
 import { existsSync, readdirSync } from 'fs'
-import createDegit from 'degit'
+import createTiged from 'tiged'
 import { relative, resolve } from 'pathe'
 import superb from 'superb'
 import consola from 'consola'
@@ -24,16 +24,16 @@ export default defineNuxtCommand({
     const t = args.template || args.t
     const src = knownTemplates[t] || t || 'nuxt/starter#v3'
     const dstDir = resolve(process.cwd(), args._[0] || 'nuxt-app')
-    const degit = createDegit(src, { cache: false /* TODO: buggy */, verbose: (args.verbose || args.v) })
+    const tiged = createTiged(src, { cache: false /* TODO: buggy */, verbose: (args.verbose || args.v) })
     if (existsSync(dstDir) && readdirSync(dstDir).length) {
       consola.error(`Directory ${dstDir} is not empty. Please pick another name or remove it first. Aborting.`)
       process.exit(1)
     }
     const formatArgs = msg => msg.replace('options.', '--')
-    degit.on('warn', event => consola.warn(formatArgs(event.message)))
-    degit.on('info', event => consola.info(formatArgs(event.message)))
+    tiged.on('warn', event => consola.warn(formatArgs(event.message)))
+    tiged.on('info', event => consola.info(formatArgs(event.message)))
     try {
-      await degit.clone(dstDir)
+      await tiged.clone(dstDir)
     } catch (e) {
       if (e.toString().includes('could not find commit hash')) {
         consola.warn('Make sure you have installed `git` correctly')
