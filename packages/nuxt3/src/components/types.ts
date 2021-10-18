185366ec2d23c82d4ddf5db271e5fa4fcbc83e67
Xin Du (Clark) <clark.duxin@gmail.com>
2021-10-18 10:54:29 +0100

    fix(nuxt3): correct compoennts hooks parameters types (#1157)

--- packages/nuxt3/src/components/types.ts
+++ packages/nuxt3/src/components/types.ts
@@ -39,13 +39,13 @@ export interface ComponentsDir extends ScanDir {
   transpile?: 'auto' | boolean
 }
 
-type componentsDirHook = (dirs: ComponentsDir[]) => void | Promise<void>
-type componentsExtendHook = (components: (ComponentsDir | ScanDir)[]) => void | Promise<void>
-
 export interface Options {
   dirs: (string | ComponentsDir)[]
   loader: Boolean
 }
+
+type componentsDirHook = (dirs: Options['dirs']) => void | Promise<void>
+type componentsExtendHook = (components: (Component | ComponentsDir | ScanDir)[]) => void | Promise<void>
 
 declare module '@nuxt/kit' {
   interface NuxtOptions {
