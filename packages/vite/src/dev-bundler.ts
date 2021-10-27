f78ec933fe755d7fe3de9b6b72fdfafdd08c54ac
Daniel Roe <daniel@roe.dev>
2021-10-27 15:41:52 +0100

    fix(vite): remove aliases from inline list (#1520)

--- packages/vite/src/dev-bundler.ts
+++ packages/vite/src/dev-bundler.ts
@@ -25,19 +25,10 @@ function isExternal (opts: TransformOptions, id: string) {
   // Externals
   const ssrConfig = (opts.viteServer.config as any).ssr
 
-  // Vite's alias have two possible formats
-  // https://vitejs.dev/config/#resolve-alias
-  const alias = opts.viteServer.config.resolve.alias || {}
-  const aliasKeys = Array.isArray(alias)
-    ? alias.map(i => i.find).filter(Boolean)
-    : Object.keys(alias)
-
   const externalOpts: ExternalsOptions = {
     inline: [
       /virtual:/,
       /\.ts$/,
-      // Things like '~', '@', etc.
-      ...aliasKeys,
       ...ExternalsDefaults.inline,
       ...ssrConfig.noExternal
     ],
