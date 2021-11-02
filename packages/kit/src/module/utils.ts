52cce01962cd6ba2ba89a0e0b122dca13e7c4f2a
Xin Du (Clark) <clark.duxin@gmail.com>
2021-11-02 11:38:12 +0000

    chore: remove duplicate camelCase import (#1637)

--- packages/kit/src/module/utils.ts
+++ packages/kit/src/module/utils.ts
@@ -5,7 +5,6 @@ import hash from 'hash-sum'
 import { pascalCase, camelCase, kebabCase } from 'scule'
 import type { WebpackPluginInstance, Configuration as WebpackConfig } from 'webpack'
 import type { Plugin as VitePlugin, UserConfig as ViteConfig } from 'vite'
-import { camelCase } from 'scule'
 import satisfies from 'semver/functions/satisfies.js' // npm/node-semver#381
 import { NuxtCompatibilityConstraints, NuxtCompatibilityIssues } from '../types/module'
 import { Nuxt } from '../types/nuxt'
