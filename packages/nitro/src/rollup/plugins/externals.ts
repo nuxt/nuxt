aba2d224d0a009c00946301e1bab4c30ef805082
Anthony Fu <anthonyfu117@hotmail.com>
2021-11-02 20:26:43 +0800

    fix(nitro): skip copying symlinks and directories (#1510)

--- packages/nitro/src/rollup/plugins/externals.ts
+++ packages/nitro/src/rollup/plugins/externals.ts
@@ -86,6 +86,10 @@ export function externals (opts: NodeExternalsOptions): Plugin {
         }
 
         const writeFile = async (file) => {
+          // Skip symlinks that are included in fileList
+          if (await fse.stat(file).then(i => i.isDirectory())) {
+            return
+          }
           const src = resolve(opts.traceOptions.base, file)
           const dst = resolve(opts.outDir, 'node_modules', file.split('node_modules/').pop())
           await fse.mkdirp(dirname(dst))
