348c82d55a6d9c0f58acdaffd7e0d47a64e10941
Daniel Roe <daniel@roe.dev>
2021-11-02 09:31:35 +0000

    fix(nuxi): generate types with `nuxi generate` (#1625)

--- packages/nuxi/src/commands/generate.ts
+++ packages/nuxi/src/commands/generate.ts
@@ -3,6 +3,7 @@ import { resolve } from 'pathe'
 import { isNuxt3 } from '@nuxt/kit'
 
 import { loadKit } from '../utils/kit'
+import { writeTypes } from '../utils/prepare'
 import { defineNuxtCommand } from './index'
 
 export default defineNuxtCommand({
@@ -16,13 +17,16 @@ export default defineNuxtCommand({
     const rootDir = resolve(args._[0] || '.')
 
     const { loadNuxt } = await loadKit(rootDir)
-    const nuxt = await loadNuxt({ rootDir })
+    const nuxt = await loadNuxt({ rootDir, config: { _export: true } })
 
     if (isNuxt3(nuxt)) {
       throw new Error('`nuxt generate` is not supported in Nuxt 3. Please follow this RFC: https://git.io/JKfvx')
     } else {
+      // Generate types and close nuxt instance
+      await writeTypes(nuxt)
+      await nuxt.close()
       // Forwards argv to `nuxt generate`
-      await execa('npx nuxt', process.argv.slice(2), { stdio: 'inherit' })
+      await execa('npx', ['nuxt', ...process.argv.slice(2)], { stdio: 'inherit' })
     }
   }
 })
