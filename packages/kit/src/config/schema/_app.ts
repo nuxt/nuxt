9b9843b501e7172686f83b15b0de40dd481d14f4
fgiraud <hello@florent.dev>
2021-11-10 15:16:20 -0500

    docs: update doc meta (#1790)
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- packages/kit/src/config/schema/_app.ts
+++ packages/kit/src/config/schema/_app.ts
@@ -111,6 +111,28 @@ export default {
   /**
    * Set default configuration for `<head>` on every page.
    *
+   * @example
+   * ```js
+   * meta: {
+   *  meta: [
+   *    // <meta name="viewport" content="width=device-width, initial-scale=1">
+   *    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+   *  ],
+   *  script: [
+   *    // <script src="https://myawesome-lib.js"></script>
+   *    { src: 'https://awesome-lib.js' }
+   *  ],
+   *  link: [
+   *    // <link rel="stylesheet" href="https://myawesome-lib.css">
+   *    { rel: 'stylesheet', href: 'https://awesome-lib.css' }
+   *  ],
+   *  // please note that this is an area that is likely to change
+   *  style: [
+   *    // <style type="text/css">:root { color: red }</style>
+   *    { children: ':root { color: red }', type: 'text/css' }
+   *  ]
+   * }
+   * ```
    * @version 3
    */
   meta: {
