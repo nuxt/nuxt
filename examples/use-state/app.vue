bf0933a2f0da518dea824508f3e75c6120df5863
Sébastien Chopin <seb@nuxtjs.com>
2021-11-16 14:17:52 +0100

    docs: improve useState docs and add example (#1961)
    Co-authored-by: pooya parsa <pyapar@gmail.com>

--- examples/use-state/app.vue
+++ examples/use-state/app.vue
@@ -1,15 +1,31 @@
+<script setup>
+const locale = useLocale()
+const locales = [
+  'en-US',
+  'en-GB',
+  'ko-KR',
+  'ar-EG',
+  'fa-IR',
+  'ja-JP-u-ca-japanese'
+]
+if (!locales.includes(locale.value)) {
+  locales.unshift(locale.value)
+}
+
+// Using Intl.DateTimeFormat for language-sensitive date and time formatting
+// Learn more: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat
+const NUXT_BIRTHDAY = new Date('2016-10-26')
+const date = computed(() => new Intl.DateTimeFormat(locale.value).format(NUXT_BIRTHDAY))
+</script>
+
 <template>
   <div>
-    Locale: {{ locale }}
-    <button @click="updateLocale">
-      Set to Klington
-    </button>
+    <h2>Nuxt birthday: {{ date }}</h2>
+    <label for="locale-chooser">Locale: </label>
+    <select id="locale-chooser" v-model="locale">
+      <option v-for="l of locales" :key="l" :value="l">
+        {{ l }}
+      </option>
+    </select>
   </div>
 </template>
-
-<script setup>
-// Defined in composables/stats.ts
-// same as useState('locale')
-const locale = useLocale()
-const updateLocale = () => { locale.value = 'tlh-klingon' }
-</script>
