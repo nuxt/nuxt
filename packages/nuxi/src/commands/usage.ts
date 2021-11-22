2a29c6aa1a9abc2bac7db77b9cf2adf37a2af489
Daniel Roe <daniel@roe.dev>
2021-11-22 17:19:34 +0000

    fix(cli): enable `usage` command (#2079)

--- packages/nuxi/src/commands/usage.ts
+++ packages/nuxi/src/commands/usage.ts
@@ -1,4 +1,5 @@
 import { cyan } from 'colorette'
+import { showHelp } from '../utils/help'
 import { commands, defineNuxtCommand } from './index'
 
 export default defineNuxtCommand({
@@ -13,5 +14,8 @@ export default defineNuxtCommand({
     sections.push(`Usage: ${cyan(`npx nuxi ${Object.keys(commands).join('|')} [args]`)}`)
 
     console.log(sections.join('\n\n') + '\n')
+
+    // Reuse the same wording as in `-h` commands
+    showHelp({})
   }
 })
--- packages/nuxi/src/index.ts
+++ packages/nuxi/src/index.ts
@@ -1,5 +1,5 @@
 import mri from 'mri'
-import { red, cyan } from 'colorette'
+import { red } from 'colorette'
 import consola from 'consola'
 import { checkEngines } from './utils/engines'
 import { commands, Command, NuxtCommand } from './commands'
@@ -14,22 +14,19 @@ async function _main () {
     ]
   })
   // @ts-ignore
-  let command = args._.shift() || 'usage'
+  const command = args._.shift() || 'usage'
 
   showBanner(command === 'dev' && args.clear !== false)
 
   if (!(command in commands)) {
     console.log('\n' + red('Invalid command ' + command))
-    command = 'usage'
+
+    await commands.usage().then(r => r.invoke())
+    process.exit(1)
   }
 
   // Check Node.js version in background
   setTimeout(() => { checkEngines() }, 1000)
-
-  if (command === 'usage') {
-    console.log(`\nUsage: ${cyan(`npx nuxi ${Object.keys(commands).join('|')} [args]`)}\n`)
-    process.exit(1)
-  }
 
   try {
     // @ts-ignore default.default is hotfix for #621
