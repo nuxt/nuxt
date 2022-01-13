baa0853afa230ca9c4bdabb7db2b98275f59fcc8
Anthony Fu <anthonyfu117@hotmail.com>
2022-01-13 19:59:26 +0800

    feat(bridge): upgrade unplugin-vue2-script-setup (#2687)

--- packages/bridge/src/setup.ts
+++ packages/bridge/src/setup.ts
@@ -1,17 +1,16 @@
-import { addVitePlugin, addWebpackPlugin, useNuxt } from '@nuxt/kit'
-import scriptSetupPlugin from 'unplugin-vue2-script-setup'
+import { useNuxt, installModule } from '@nuxt/kit'
+import scriptSetupPlugin from 'unplugin-vue2-script-setup/nuxt'
 import type { ScriptSetupOptions } from '../types'
 
-export const setupScriptSetup = (options: ScriptSetupOptions) => {
+export const setupScriptSetup = async (options: ScriptSetupOptions) => {
   const nuxt = useNuxt()
   const config = options === true ? {} : options
-
-  addVitePlugin(scriptSetupPlugin.vite(config))
-  addWebpackPlugin(scriptSetupPlugin.webpack(config))
 
   nuxt.hook('prepare:types', ({ references }) => {
     references.push({
       types: 'unplugin-vue2-script-setup/types'
     })
   })
+
+  await installModule(scriptSetupPlugin, config)
 }
