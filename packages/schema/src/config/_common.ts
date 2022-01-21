ed2d26f1b51158ae90b8a6d7f6dca1f24ea38f67
Daniel Roe <daniel@roe.dev>
2022-01-21 11:47:13 +0000

    fix(schema): support inline module options (#2852)

--- packages/schema/src/config/_common.ts
+++ packages/schema/src/config/_common.ts
@@ -225,7 +225,7 @@ export default {
    *   function () {}
    * ]
    * ```
-   * @type {(typeof import('../src/types/module').NuxtModule | string)[]}
+   * @type {(typeof import('../src/types/module').NuxtModule | string | [typeof import('../src/types/module').NuxtModule | string, Record<string, any>])[]}
    * @version 2
    * @version 3
    */
@@ -262,7 +262,7 @@ export default {
    * decreases the size of `node_modules` in production deployments. Please refer to each
    * module's documentation to see if it is recommended to use `modules` or `buildModules`.
    *
-   * @type {(typeof import('../src/types/module').NuxtModule | string)[]}
+   * @type {(typeof import('../src/types/module').NuxtModule | string | [typeof import('../src/types/module').NuxtModule | string, Record<string, any>])[]}
    * @version 2
    * @version 3
    */
