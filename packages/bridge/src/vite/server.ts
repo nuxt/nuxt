2b86345e9e4bc4dbac88a8001eac73f4a73e50a9
blazmrak <blaz.mrak13@gmail.com>
2022-01-24 12:40:49 +0100

    fix(bridge): build fails with SSR turned off (#2708)

--- packages/bridge/src/vite/server.ts
+++ packages/bridge/src/vite/server.ts
@@ -51,7 +51,7 @@ export async function buildServer (ctx: ViteBuildContext) {
     },
     build: {
       outDir: resolve(ctx.nuxt.options.buildDir, 'dist/server'),
-      ssr: true,
+      ssr: ctx.nuxt.options.ssr ?? true,
       ssrManifest: true,
       rollupOptions: {
         input: resolve(ctx.nuxt.options.buildDir, 'server.js'),
