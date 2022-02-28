39f7eb2cd21d6e3a5642eca877d5f4fc6d28f840
pooya parsa <pyapar@gmail.com>
2022-02-28 22:41:44 +0100

    fix(test-utils): improve random port allocation (#3398)

--- packages/test-utils/src/server.ts
+++ packages/test-utils/src/server.ts
@@ -1,28 +1,12 @@
 import { resolve } from 'path'
-import { createServer, AddressInfo } from 'net'
 import { execa } from 'execa'
-import { getPort } from 'get-port-please'
+import { getRandomPort, waitForPort } from 'get-port-please'
 import { fetch as _fetch, $fetch as _$fetch, FetchOptions } from 'ohmyfetch'
 import { useTestContext } from './context'
 
-// TODO: use the export from `get-port-please`
-function checkPort (port: number, host: string): Promise<number|false> {
-  return new Promise((resolve) => {
-    const server = createServer()
-    server.unref()
-    server.on('error', () => { resolve(false) })
-    server.listen(port, host, () => {
-      const { port } = server.address() as AddressInfo
-      server.close(() => { resolve(port) })
-    })
-  })
-}
-
 export async function listen () {
   const ctx = useTestContext()
-  const host = process.env.HOST || '0.0.0.0'
-  const port = await getPort({ host, random: true })
-
+  const port = await getRandomPort()
   ctx.url = 'http://localhost:' + port
   ctx.serverProcess = execa('node', [
     // @ts-ignore
@@ -33,17 +17,7 @@ export async function listen () {
       NODE_ENV: 'test'
     }
   })
-
-  const TRIES = 50
-  const DELAY = 100
-
-  for (let i = TRIES; i; i--) {
-    await new Promise(resolve => setTimeout(resolve, DELAY))
-    // wait until port is in used
-    if (await checkPort(port, host) === false) {
-      return
-    }
-  }
+  await waitForPort(port, { retries: 8 })
 }
 
 export function fetch (path: string, options?: any) {
