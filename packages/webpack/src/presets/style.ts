c942465f79b3ebdd784b79ee431d302e078ebd9e
yoho <907415276@qq.com>
2022-03-11 16:07:26 +0800

    chore: remove extra space (#3597)

--- packages/webpack/src/presets/style.ts
+++ packages/webpack/src/presets/style.ts
@@ -40,7 +40,7 @@ function loaders (ctx: WebpackConfigContext) {
   // CSS
   config.module.rules.push(createdStyleRule('css', /\.css$/i, null, ctx))
 
-  //  Postcss
+  // Postcss
   config.module.rules.push(createdStyleRule('postcss', /\.p(ost)?css$/i, null, ctx))
 
   // Less
