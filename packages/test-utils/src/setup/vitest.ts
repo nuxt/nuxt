e67ed55de38de67324876f505d265bdaa0b15ec5
Anthony Fu <anthonyfu117@hotmail.com>
2022-03-16 18:47:02 +0800

    fix(test-utils): use `beforeAll` for setup (#3696)

--- packages/test-utils/src/setup/vitest.ts
+++ packages/test-utils/src/setup/vitest.ts
@@ -2,7 +2,7 @@ import type { TestHooks } from '../types'
 
 export default async function setupVitest (hooks: TestHooks) {
   const vitest = await import('vitest')
-  vitest.test('setup nuxt', hooks.setup, 60000)
+  vitest.beforeAll(hooks.setup, 60000)
   vitest.beforeEach(hooks.beforeEach)
   vitest.afterEach(hooks.afterEach)
   vitest.afterAll(hooks.afterAll)
