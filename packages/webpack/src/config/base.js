4f87ed0ca1d57edd9b6882ce824f799f31128255
Rafal Chlodnicki <rchl2k@gmail.com>
2022-03-19 18:02:27 +0100

    fix: allow files with .cjs extension to be transpiled
    Fixes #10339

--- packages/webpack/src/config/base.js
+++ packages/webpack/src/config/base.js
@@ -329,7 +329,7 @@ export default class WebpackBaseConfig {
         ]
       },
       {
-        test: /\.m?jsx?$/i,
+        test: /\.(c|m)?jsx?$/i,
         type: 'javascript/auto',
         exclude: (file) => {
           file = file.split(/node_modules(.*)/)[1]
