7c1a8ae0d94458a70bfe230953de295124359403
Daniel Roe <daniel@roe.dev>
2022-03-25 12:51:07 +0000

    fix(bridge): define mixin outside plugin, to avoid memory leak (#3900)

--- packages/nuxt3/src/meta/runtime/plugin.ts
+++ packages/nuxt3/src/meta/runtime/plugin.ts
@@ -1,7 +1,7 @@
 import { computed, getCurrentInstance } from 'vue'
 import * as Components from './components'
 import { useMeta } from './composables'
-import { defineNuxtPlugin } from '#app'
+import { defineNuxtPlugin, useNuxtApp } from '#app'
 // @ts-ignore
 import metaConfig from '#build/meta.config.mjs'
 
@@ -10,22 +10,25 @@ declare module 'vue' {
   export interface GlobalComponents extends MetaComponents {}
 }
 
+const metaMixin = {
+  [metaConfig.mixinKey] () {
+    const instance = getCurrentInstance()
+    const options = instance?.type || /* nuxt2 */ instance?.proxy?.$options
+    const nuxtApp = useNuxtApp()
+    if (!options || !('head' in options)) { return }
+
+    const source = typeof options.head === 'function'
+      ? computed(() => options.head(nuxtApp))
+      : options.head
+
+    useMeta(source)
+  }
+}
+
 export default defineNuxtPlugin((nuxtApp) => {
   useMeta(metaConfig.globalMeta)
 
-  nuxtApp.vueApp.mixin({
-    [metaConfig.mixinKey] () {
-      const instance = getCurrentInstance()
-      const options = instance?.type || /* nuxt2 */ instance?.proxy?.$options
-      if (!options || !('head' in options)) { return }
-
-      const source = typeof options.head === 'function'
-        ? computed(() => options.head(nuxtApp))
-        : options.head
-
-      useMeta(source)
-    }
-  })
+  nuxtApp.vueApp.mixin(metaMixin)
 
   for (const name in Components) {
     // eslint-disable-next-line import/namespace
