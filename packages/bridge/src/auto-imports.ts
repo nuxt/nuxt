6276580aa4198ac3e738e1f6053df443b0c52e0f
Daniel Roe <daniel@roe.dev>
2022-04-07 12:35:11 +0100

    fix(bridge): exclude unsupported imports via `autoImports:sources` (#4127)

--- packages/bridge/src/auto-imports.ts
+++ packages/bridge/src/auto-imports.ts
@@ -10,27 +10,25 @@ const CapiHelpers = new Set(Object.keys(CompositionApi))
 export function setupAutoImports () {
   const nuxt = useNuxt()
 
-  const bridgePresets: Preset[] = [
-    {
-      from: '@vue/composition-api',
-      imports: vuePreset.imports.filter(i => CapiHelpers.has(i as string))
-    },
-    {
-      from: '#app',
-      imports: ['useNuxt2Meta']
-    }
-  ]
+  const bridgePresets: Preset[] = [{
+    from: '@vue/composition-api',
+    imports: vuePreset.imports.filter(i => CapiHelpers.has(i as string))
+  }]
+
   nuxt.hook('autoImports:sources', (presets) => {
     const vuePreset = presets.find(p => p.from === 'vue')
     if (vuePreset) { vuePreset.disabled = true }
-  })
 
-  nuxt.hook('autoImports:extend', (imports) => {
-    for (const i of imports) {
-      if (i.from === '#app' && UnsupportedImports.has(i.name)) {
-        i.disabled = true
+    const appPreset = presets.find(p => p.from === '#app')
+    if (!appPreset) { return }
+
+    for (const [index, i] of Object.entries(appPreset.imports).reverse()) {
+      if (typeof i === 'string' && UnsupportedImports.has(i)) {
+        appPreset.imports.splice(Number(index), 1)
       }
     }
+
+    appPreset.imports.push('useNuxt2Meta')
   })
 
   nuxt.hook('modules:done', () => installModule(autoImports, { presets: bridgePresets }))
