25ced97f160055a045c90ce42cd06b0b051cbd03
Clément Ollivier <clement.o2p@gmail.com>
2022-04-13 19:25:45 +0200

    docs: add server-routes example (#4301)

---
+++ docs/content/4.examples/5.server/routes.md
@@ -0,0 +1,12 @@
+---
+template: Example
+---
+
+# Server Routes
+
+This example shows how to create server routes inside a `server/api` directory.
+
+::ReadMore{link="/guide/features/server-routes"}
+::
+
+::sandbox{repo="nuxt/framework" branch="main" dir="examples/server/routes" file="app.vue"}
---
+++ examples/server/routes/server/api/mountain.js
@@ -0,0 +1,17 @@
+export default defineEventHandler(() => {
+  return {
+    title: 'Mount Everest',
+    description: "Mount Everest is Earth's highest mountain above sea level, located in the Mahalangur Himal sub-range of the Himalayas. The China–Nepal border runs across its summit point",
+    height: '8,848 m',
+    countries: [
+      'China',
+      'Nepal'
+    ],
+    continent: 'Asia',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Everest_kalapatthar.jpg/600px-Everest_kalapatthar.jpg',
+    dir: '/mountains',
+    path: '/mountains/mount-everest',
+    slug: 'mount-everest',
+    updatedAt: '2020-12-11T15:40:35.000Z'
+  }
+})
