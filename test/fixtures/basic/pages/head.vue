3e7d6dafa10b7283393f10476b5fc17a1b850b4c
Daniel Roe <daniel@roe.dev>
2022-04-14 16:27:31 +0100

    test(nuxt3): add test case for computed head (#4361)

--- test/fixtures/basic/pages/head.vue
+++ test/fixtures/basic/pages/head.vue
@@ -1,12 +1,14 @@
 <script setup>
+const a = ref('')
 useHead({
   bodyAttrs: {
     class: 'body-attrs-test'
   },
   meta: [{ name: 'description', content: 'first' }]
 })
-useHead({ charset: 'utf-16', meta: [{ name: 'description', content: 'overriding with an inline useHead call' }] })
+useHead({ charset: 'utf-16', meta: [{ name: 'description', content: computed(() => `${a.value} with an inline useHead call`) }] })
 useMeta({ script: [{ children: 'console.log("works with useMeta too")' }] })
+a.value = 'overriding'
 </script>
 
 <script>
