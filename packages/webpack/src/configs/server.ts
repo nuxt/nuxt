7fb29b3a6e88f97612b0ea2511a7698cde2a6f9f
Daniel Roe <daniel@roe.dev>
2022-04-15 11:58:22 +0100

    fix(webpack): add css, sass, scss to asset patterns to transpile (#4375)

--- packages/webpack/src/configs/server.ts
+++ packages/webpack/src/configs/server.ts
@@ -4,7 +4,7 @@ import { WebpackConfigContext, applyPresets, getWebpackConfig } from '../utils/c
 import { nuxt } from '../presets/nuxt'
 import { node } from '../presets/node'
 
-const assetPattern = /\.(png|jpe?g|gif|svg|woff2?|eot|ttf|otf|webp|webm|mp4|ogv)(\?.*)?$/i
+const assetPattern = /\.(css|s[ca]ss|png|jpe?g|gif|svg|woff2?|eot|ttf|otf|webp|webm|mp4|ogv)(\?.*)?$/i
 
 export function server (ctx: WebpackConfigContext) {
   ctx.name = 'server'
