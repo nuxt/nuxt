975ec0c41796d03196765cdbc106f0eb8936442e
pooya parsa <pyapar@gmail.com>
2022-04-20 21:22:59 +0200

    chore(nuxi): add notice for generate deployment (#4474)

--- packages/nuxi/src/commands/generate.ts
+++ packages/nuxi/src/commands/generate.ts
@@ -1,3 +1,4 @@
+import consola from 'consola'
 import buildCommand from './build'
 import { defineNuxtCommand } from './index'
 
@@ -10,5 +11,6 @@ export default defineNuxtCommand({
   async invoke (args) {
     args.prerender = true
     await buildCommand.invoke(args)
+    consola.success('You can now deploy `.output/public` to any static hosting!')
   }
 })
