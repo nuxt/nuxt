e789a5727b06184fa5aa6b8ed2c78451e53ca06c
pooya parsa <pyapar@gmail.com>
2022-04-26 20:15:41 +0200

    docs: add note and banner about edge channel (#4640)

---
+++ docs/components/atoms/StabilityEdge.vue
@@ -0,0 +1,20 @@
+<template>
+  <Alert icon="🧪">
+    {{ title }} is available on edge channel. Check out the <Link to="/guide/going-further/edge-channel">
+      Edge Channel Documentation
+    </Link> to beta test.
+  </Alert>
+</template>
+
+<script>
+import { defineComponent } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      default: 'This feature'
+    }
+  }
+})
+</script>
