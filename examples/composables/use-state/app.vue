173fbfd06ea6e7c78ab88fc5de15a2e92c72a79d
HG <husayt@gmail.com>
2022-05-20 10:12:35 +0100

    docs: demonstrate the multiple `useState` with same key (#5059)
    * Better demonstrate the UseState concept
    
    Add another counter instance with the same id and show that they change in parallel
    
    * Apply suggestions from code review
    
    Co-authored-by: pooya parsa <pyapar@gmail.com>

--- examples/composables/use-state/app.vue
+++ examples/composables/use-state/app.vue
@@ -1,10 +1,12 @@
 <script setup>
 const counter = useState('counter', () => Math.round(Math.random() * 1000))
+const sameCounter = useState('counter');
 </script>
 
 <template>
   <NuxtExampleLayout example="composables/use-state">
     <div>Counter: {{ counter }}</div>
+    <div>Same Counter: {{ sameCounter }}</div>
     <div>
       <NButton class="font-mono" @click="counter++">
         +
