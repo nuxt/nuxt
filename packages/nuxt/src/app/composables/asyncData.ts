eafd92c4576174a5c0f85cb64eacc072e66ca2c6
Anthony Fu <anthonyfu117@hotmail.com>
2022-06-09 03:45:12 +0800

    fix(types): tidy types for `useAsyncData` (#5340)

--- packages/nuxt/src/app/composables/asyncData.ts
+++ packages/nuxt/src/app/composables/asyncData.ts
@@ -5,7 +5,14 @@ import { NuxtApp, useNuxtApp } from '#app'
 
 export type _Transform<Input = any, Output = any> = (input: Input) => Output
 
-export type PickFrom<T, K extends Array<string>> = T extends Array<any> ? T : T extends Record<string, any> ? Pick<T, K[number]> : T
+export type PickFrom<T, K extends Array<string>> = T extends Array<any>
+  ? T
+  : T extends Record<string, any>
+  ? keyof T extends K[number]
+    ? T // Exact same keys as the target, skip Pick
+    : Pick<T, K[number]>
+  : T
+
 export type KeysOf<T> = Array<keyof T extends string ? keyof T : string>
 export type KeyOfRes<Transform extends _Transform> = KeysOf<ReturnType<Transform>>
 
