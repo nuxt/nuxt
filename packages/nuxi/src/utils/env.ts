9d28c0adbb7336d34eac345535ba9a39ffa79ea7
Ennio Visconti <ennio.visconti@gmail.com>
2022-06-13 00:13:59 +0200

    refactor(nuxi): improve env update message (#5430)

--- packages/nuxi/src/utils/env.ts
+++ packages/nuxi/src/utils/env.ts
@@ -1,7 +1,7 @@
 export const overrideEnv = (targetEnv: string) => {
   const currentEnv = process.env.NODE_ENV
   if (currentEnv && currentEnv !== targetEnv) {
-    console.warn(`Changing \`NODE_ENV\` from \`${currentEnv}\` to \`${targetEnv}\`.`)
+    console.warn(`Changing \`NODE_ENV\` from \`${currentEnv}\` to \`${targetEnv}\`, to avoid unintended behavior.`)
   }
 
   process.env.NODE_ENV = targetEnv
