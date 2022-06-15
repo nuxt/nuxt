cf255259caee205b37beaebaccfd5fc2de8ecb3f
Daniel Roe <daniel@roe.dev>
2022-06-15 16:18:45 +0100

    fix(vite): add typecheck to vite plugins (#5464)

--- packages/vite/src/server.ts
+++ packages/vite/src/server.ts
@@ -88,7 +88,7 @@ export async function buildServer (ctx: ViteBuildContext) {
   // Add type-checking
   if (ctx.nuxt.options.typescript.typeCheck === true || (ctx.nuxt.options.typescript.typeCheck === 'build' && !ctx.nuxt.options.dev)) {
     const checker = await import('vite-plugin-checker').then(r => r.default)
-    ctx.config.plugins.push(checker({ typescript: true }))
+    serverConfig.plugins.push(checker({ typescript: true }))
   }
 
   await ctx.nuxt.callHook('vite:extendConfig', serverConfig, { isClient: false, isServer: true })
