885672157caec98896b31131c8f0ff3b7b08eecb
pooya parsa <pyapar@gmail.com>
2022-06-24 01:41:24 +0200

    Update nuxt.d.ts

--- packages/types/nuxt.d.ts
+++ packages/types/nuxt.d.ts
@@ -1,6 +1,16 @@
-import { NuxtOptions } from './config'
+import type { NuxtOptions } from './config'
+import type { RequestListener } from 'http'
 
 export interface Nuxt {
   options: NuxtOptions,
+  server: { app: RequestListener },
+  ready: () => any
+  close: (callback?: Function) => any
+  resolver: any
+  moduleContainer: any
+  resolveAlias(path: string): string
+  resolvePath(path: string, opts?: any): string
+  renderRoute(...args: any[]): any
+  renderAndGetWindow(url: string, opts?: any, config?: any): any
   [key: string]: any
 }
