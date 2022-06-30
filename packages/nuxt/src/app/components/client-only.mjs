fe508f5d200db93244f30ba31f94b1678b668f5c
Daniel Roe <daniel@roe.dev>
2022-06-30 15:08:05 +0100

    fix(nuxt): pass attrs and props to client-only components (#5668)

--- packages/nuxt/src/app/components/client-only.mjs
+++ packages/nuxt/src/app/components/client-only.mjs
@@ -21,14 +21,15 @@ export default defineComponent({
 export function createClientOnly (component) {
   return defineComponent({
     name: 'ClientOnlyWrapper',
-    setup (props, { attrs, slots }) {
+    inheritAttrs: false,
+    setup (_props, { attrs, slots }) {
       const mounted = ref(false)
       onMounted(() => { mounted.value = true })
       return () => {
         if (mounted.value) {
-          return h(component, { props, attrs }, slots)
+          return h(component, attrs, slots)
         }
-        return h('div')
+        return h('div', { class: attrs.class, style: attrs.style })
       }
     }
   })
