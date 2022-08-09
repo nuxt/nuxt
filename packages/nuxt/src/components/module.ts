0b2207940961f2b00eef3113f221e3d7b439dbb0
Gregor Becker <gregor@codedredd.de>
2022-08-09 11:13:54 +0200

    fix(nuxt): ensure component dirs in `node_modules` have lower scanning priority (#6382)

--- packages/nuxt/src/components/module.ts
+++ packages/nuxt/src/components/module.ts
@@ -102,6 +102,11 @@ export default defineNuxtModule<ComponentsOptions>({
         }
       }).filter(d => d.enabled)
 
+      componentDirs = [
+        ...componentDirs.filter(dir => !dir.path.includes('node_modules')),
+        ...componentDirs.filter(dir => dir.path.includes('node_modules'))
+      ]
+
       nuxt.options.build!.transpile!.push(...componentDirs.filter(dir => dir.transpile).map(dir => dir.path))
     })
 
