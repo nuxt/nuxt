0640a6c7a4053d7450cd528f9db30d7f0d25e639
Daniel Roe <daniel@roe.dev>
2022-09-21 11:20:29 +0100

    fix(test-utils): support vitest v0.20.x (#7712)

--- packages/test-utils/src/run.ts
+++ packages/test-utils/src/run.ts
@@ -21,14 +21,20 @@ export async function runTests (opts: RunTestOptions) {
     process.env.NUXT_TEST_DEV = 'true'
   }
 
+  // TODO: add `as typeof import('vitest/dist/node')` and remove workaround
+  // when we upgrade vitest: see https://github.com/nuxt/framework/issues/6297
+
   // @ts-ignore missing types
   const { startVitest } = await import('vitest/dist/node.mjs')
-  const succeeded = await startVitest(
+  const args: any[] = [
     [] /* argv */,
     // Vitest options
     {
       root: opts.rootDir,
-      run: !opts.watch
+      run: !opts.watch,
+      deps: {
+        inline: [/@nuxt\/test-utils/]
+      }
     },
     // Vite options
     {
@@ -36,7 +42,9 @@ export async function runTests (opts: RunTestOptions) {
         tsconfigRaw: '{}'
       }
     }
-  )
+  ]
+  if (startVitest.length >= 4) { args.unshift('test') }
+  const succeeded = await startVitest(...args)
 
   if (!succeeded) {
     process.exit(1)
