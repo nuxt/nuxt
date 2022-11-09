0cf0f59bc5d9be82448622ac53aa1141563b7bb6
Alexander Lichter <github@lichter.io>
2022-11-09 10:04:19 +0100

    fix(ssr): ensure useRequestHeaders are case-insensitive (#8805)
    Co-authored-by: Haruaki OTAKE <aaharu@hotmail.com>

--- packages/nuxt/src/app/composables/ssr.ts
+++ packages/nuxt/src/app/composables/ssr.ts
@@ -2,13 +2,13 @@
 import type { H3Event } from 'h3'
 import { useNuxtApp, NuxtApp } from '../nuxt'
 
-export function useRequestHeaders<K extends string = string> (include: K[]): Record<K, string | undefined>
+export function useRequestHeaders<K extends string = string> (include: K[]): Record<Lowercase<K>, string | undefined>
 export function useRequestHeaders (): Readonly<Record<string, string | undefined>>
 export function useRequestHeaders (include?: any[]) {
   if (process.client) { return {} }
   const headers = useNuxtApp().ssrContext?.event.req.headers ?? {}
   if (!include) { return headers }
-  return Object.fromEntries(include.filter(key => headers[key]).map(key => [key, headers[key]]))
+  return Object.fromEntries(include.map(key => key.toLowerCase()).filter(key => headers[key]).map(key => [key, headers[key]]))
 }
 
 export function useRequestEvent (nuxtApp: NuxtApp = useNuxtApp()): H3Event {
