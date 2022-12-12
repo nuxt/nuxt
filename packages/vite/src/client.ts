6d0c49101fe43def1faef2e7d2f94cf9b9141a17
Daniel Roe <daniel@roe.dev>
2022-12-12 12:28:49 +0000

    style: split out type imports

--- packages/vite/src/client.ts
+++ packages/vite/src/client.ts
@@ -1,4 +1,4 @@
-import { IncomingMessage, ServerResponse } from 'node:http'
+import type { IncomingMessage, ServerResponse } from 'node:http'
 import { join, relative, resolve } from 'pathe'
 import * as vite from 'vite'
 import vuePlugin from '@vitejs/plugin-vue'
