9de94c21cf7fa7008c75f859e0299e3f1c0b445c
Alex Liu <39984251+Mini-ghost@users.noreply.github.com>
2023-01-10 22:33:21 +0800

    refactor: improve internal type definitions of `<NuxtLink>` (#9869)

--- packages/nuxt/src/app/components/nuxt-link.ts
+++ packages/nuxt/src/app/components/nuxt-link.ts
@@ -180,7 +180,7 @@ export function defineNuxtLink (options: NuxtLinkOptions) {
         if (shouldPrefetch) {
           const nuxtApp = useNuxtApp()
           let idleId: number
-          let unobserve: Function | null = null
+          let unobserve: (() => void)| null = null
           onMounted(() => {
             const observer = useObserver()
             onNuxtReady(() => {
@@ -268,8 +268,10 @@ export function defineNuxtLink (options: NuxtLinkOptions) {
 export default defineNuxtLink({ componentName: 'NuxtLink' })
 
 // --- Prefetching utils ---
+type CallbackFn = () => void
+type ObserveFn = (element: Element, callback: CallbackFn) => () => void
 
-function useObserver () {
+function useObserver (): { observe: ObserveFn } | undefined {
   if (process.server) { return }
 
   const nuxtApp = useNuxtApp()
@@ -278,10 +280,10 @@ function useObserver () {
   }
 
   let observer: IntersectionObserver | null = null
-  type CallbackFn = () => void
+
   const callbacks = new Map<Element, CallbackFn>()
 
-  const observe = (element: Element, callback: CallbackFn) => {
+  const observe: ObserveFn = (element, callback) => {
     if (!observer) {
       observer = new IntersectionObserver((entries) => {
         for (const entry of entries) {
