585e8be2e0f7415119b016f5857fab565c8ee06b
Mehdi HosseinZade <arshen@outlook.com>
2023-01-21 20:24:02 +0330

    feat(kit): add `extendRouteRules` method (#9771)

--- packages/kit/src/pages.ts
+++ packages/kit/src/pages.ts
@@ -1,4 +1,6 @@
 import type { NuxtHooks } from '@nuxt/schema'
+import type { NitroRouteConfig } from 'nitropack'
+import { defu } from 'defu'
 import { useNuxt } from './context'
 import { isNuxt2 } from './compatibility'
 
@@ -9,5 +11,26 @@ export function extendPages (cb: NuxtHooks['pages:extend']) {
     nuxt.hook('build:extendRoutes', cb)
   } else {
     nuxt.hook('pages:extend', cb)
+  }
+}
+
+export interface ExtendRouteRulesOptions {
+  /**
+   * Override route rule config
+   *
+   * @default false
+   */
+  override?: boolean
+}
+
+export function extendRouteRules (route: string, rule: NitroRouteConfig, options: ExtendRouteRulesOptions = {}) {
+  const nuxt = useNuxt()
+  for (const opts of [nuxt.options, nuxt.options.nitro]) {
+    if (!opts.routeRules) {
+      opts.routeRules = {}
+    }
+    opts.routeRules[route] = options.override
+      ? defu(rule, opts.routeRules[route])
+      : defu(opts.routeRules[route], rule)
   }
 }
