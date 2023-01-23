01e932df76db3688c480356fdfd0cb385bb892c7
Daniel Roe <daniel@roe.dev>
2023-01-23 09:43:59 +0000

    test: add type test and simplify typing

--- packages/nuxt/src/app/nuxt.ts
+++ packages/nuxt/src/app/nuxt.ts
@@ -283,10 +283,10 @@ export function isNuxtPlugin (plugin: unknown) {
  * @param nuxt A Nuxt instance
  * @param setup The function to call
  */
-export function callWithNuxt<T extends (...args: any[]) => any> (nuxt: NuxtApp | _NuxtApp, setup: T, args?: Parameters<T>): Promise<ReturnType<T>> | ReturnType<T> {
-  const fn = () => args ? setup(...args as Parameters<T>) : setup()
+export function callWithNuxt<T extends (...args: any[]) => any> (nuxt: NuxtApp | _NuxtApp, setup: T, args?: Parameters<T>) {
+  const fn: () => ReturnType<T> = () => args ? setup(...args as Parameters<T>) : setup()
   if (process.server) {
-    return nuxtAppCtx.callAsync<ReturnType<T>>(nuxt, fn)
+    return nuxtAppCtx.callAsync(nuxt, fn)
   } else {
     // In client side we could assume nuxt app is singleton
     nuxtAppCtx.set(nuxt)
