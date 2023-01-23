83a8e1378e565784f3ae18026ca7c36751f384e9
Daniel Roe <daniel@roe.dev>
2023-01-23 10:32:02 +0000

    fix(nuxt): let vue router handle internal redirects within middleware

--- packages/nuxt/src/app/composables/router.ts
+++ packages/nuxt/src/app/composables/router.ts
@@ -6,6 +6,7 @@ import { useNuxtApp, useRuntimeConfig } from '../nuxt'
 import type { NuxtError } from './error'
 import { createError } from './error'
 import { useState } from './state'
+import { setResponseStatus } from './ssr'
 
 export const useRouter = () => {
   return useNuxtApp()?.$router as Router
@@ -97,8 +98,15 @@ export const navigateTo = (to: RouteLocationRaw | undefined | null, options?: Na
   if (process.server) {
     const nuxtApp = useNuxtApp()
     if (nuxtApp.ssrContext && nuxtApp.ssrContext.event) {
+      // Let vue-router handle internal redirects within middleware
+      // to prevent the navigation happening after response is sent
+      if (isProcessingMiddleware() && !isExternal) {
+        setResponseStatus(options?.redirectCode || 302)
+        return to
+      }
       const redirectLocation = isExternal ? toPath : joinURL(useRuntimeConfig().app.baseURL, router.resolve(to).fullPath || '/')
-      return nuxtApp.callHook('app:redirected').then(() => sendRedirect(nuxtApp.ssrContext!.event, redirectLocation, options?.redirectCode || 302))
+      return nuxtApp.callHook('app:redirected')
+        .then(() => sendRedirect(nuxtApp.ssrContext!.event, redirectLocation, options?.redirectCode || 302))
     }
   }
 
