33e9cb6009bb19e96e60e04d245eba1fceab6815
noriyuki.shimizu0612@gmail.com <shiminori0612>
2023-01-27 12:23:30 +0900

    fix validate

--- packages/nuxt/src/pages/runtime/validate.ts
+++ packages/nuxt/src/pages/runtime/validate.ts
@@ -1,11 +1,9 @@
-import { defineNuxtRouteMiddleware } from '#app'
-
+import { showError, defineNuxtRouteMiddleware } from '#app'
 export default defineNuxtRouteMiddleware(async (to) => {
-  if (!to.meta?.validate) { return }
-
-  const result = await Promise.resolve(to.meta.validate(to))
-  if (result === true) {
-    return
+  if (to.meta?.validate) {
+    const result = await Promise.resolve(to.meta.validate(to))
+    if (result === false) {
+      return showError({ statusCode: 404, message: 'This page could not be found' })
+    }
   }
-  return result
 })
