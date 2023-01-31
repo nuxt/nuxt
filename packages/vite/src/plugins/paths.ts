0ddab77543aebb83195b702e0009b11d62cafd98
Daniel Roe <daniel@roe.dev>
2023-01-31 16:19:36 +0000

    fix(vite): don't add import statement to assets

--- packages/vite/src/plugins/paths.ts
+++ packages/vite/src/plugins/paths.ts
@@ -1,4 +1,6 @@
+import { pathToFileURL } from 'node:url'
 import MagicString from 'magic-string'
+import { parseQuery, parseURL } from 'ufo'
 import type { Plugin } from 'vite'
 
 export interface RuntimePathsOptions {
@@ -6,12 +8,22 @@ export interface RuntimePathsOptions {
 }
 
 const VITE_ASSET_RE = /__VITE_ASSET__|__VITE_PUBLIC_ASSET__/
+const JS_RE = /\.((c|m)?j|t)sx?$/
 
 export function runtimePathsPlugin (options: RuntimePathsOptions): Plugin {
   return {
     name: 'nuxt:runtime-paths-dep',
     enforce: 'post',
     transform (code, id) {
+      const { pathname, search } = parseURL(decodeURIComponent(pathToFileURL(id).href))
+      if (pathname.endsWith('.vue')) {
+        // vue files
+        if (search && parseQuery(search).type !== 'script') {
+          return
+        }
+      } else if (!JS_RE.test(pathname)) {
+        return
+      }
       if (VITE_ASSET_RE.test(code)) {
         const s = new MagicString(code)
         // Register dependency on paths.mjs, which sets globalThis.__publicAssetsURL
