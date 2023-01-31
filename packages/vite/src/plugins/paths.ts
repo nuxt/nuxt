cea4b6424ef8a872a8bf52a55dc0afb5fe2984d3
Daniel Roe <daniel@roe.dev>
2023-01-31 16:13:54 +0000

    perf(vite): use compiled regexp for test

--- packages/vite/src/plugins/paths.ts
+++ packages/vite/src/plugins/paths.ts
@@ -5,12 +5,14 @@ export interface RuntimePathsOptions {
   sourcemap?: boolean
 }
 
+const VITE_ASSET_RE = /__VITE_ASSET__|__VITE_PUBLIC_ASSET__/
+
 export function runtimePathsPlugin (options: RuntimePathsOptions): Plugin {
   return {
     name: 'nuxt:runtime-paths-dep',
     enforce: 'post',
     transform (code, id) {
-      if (code.includes('__VITE_ASSET__') || code.includes('__VITE_PUBLIC_ASSET__')) {
+      if (VITE_ASSET_RE.test(code)) {
         const s = new MagicString(code)
         // Register dependency on paths.mjs, which sets globalThis.__publicAssetsURL
         s.prepend('import "#build/paths.mjs";')
