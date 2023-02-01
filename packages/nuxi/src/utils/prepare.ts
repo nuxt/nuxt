727cf7958fa8c8fc1bf0c6ce72ac1fe80c8d2138
Daniel Roe <daniel@roe.dev>
2023-02-01 18:38:15 +0000

    chore: update `2.x` branch name

--- packages/nuxi/src/utils/prepare.ts
+++ packages/nuxi/src/utils/prepare.ts
@@ -102,7 +102,7 @@ export const writeTypes = async (nuxt: Nuxt) => {
   }
 
   // This is needed for Nuxt 2 which clears the build directory again before building
-  // https://github.com/nuxt/nuxt/blob/2.x-dev/packages/builder/src/builder.js#L144
+  // https://github.com/nuxt/nuxt/blob/2.x/packages/builder/src/builder.js#L144
   // @ts-expect-error
   nuxt.hook('builder:prepared', writeFile)
 
