06ed20997e356e86228d510d08600c7145a25f46
Anthony Fu <anthonyfu117@hotmail.com>
2023-02-03 12:11:15 +0100

    perf: only enable subprocess in dev command

--- packages/nuxi/src/cli-wrapper.ts
+++ packages/nuxi/src/cli-wrapper.ts
@@ -5,14 +5,14 @@ import { fileURLToPath } from 'node:url'
 import { execa } from 'execa'
 import { EXIT_CODE_RESTART } from './constants'
 
-const ENTRY = new URL('../dist/cli-run.mjs', import.meta.url)
+const cliEntry = fileURLToPath(new URL('../dist/cli-run.mjs', import.meta.url))
 
-async function start (preArgs: string[], postArgs: string[]) {
+async function startSubprocess (preArgs: string[], postArgs: string[]) {
   const child = await execa(
     'node',
     [
       ...preArgs,
-      fileURLToPath(ENTRY),
+      cliEntry,
       ...postArgs
     ],
     {
@@ -25,10 +25,16 @@ async function start (preArgs: string[], postArgs: string[]) {
     }
   )
   if (child.exitCode === EXIT_CODE_RESTART) {
-    await start(preArgs, postArgs)
+    await startSubprocess(preArgs, postArgs)
   } else {
     process.exit(child.exitCode)
   }
 }
 
-start([], process.argv.slice(2))
+const args = process.argv.slice(2)
+// only enable wrapper in dev command
+if (args[0] === 'dev') {
+  startSubprocess([], args)
+} else {
+  import(cliEntry)
+}
