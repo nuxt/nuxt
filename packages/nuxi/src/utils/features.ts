aeff68794e5169e4f86bee8c6344354fb8c555dc
Daniel Roe <daniel@roe.dev>
2023-02-07 16:25:59 +0100

    refactor: use `devtools` as command rather than `enable`

--- packages/nuxi/src/utils/features.ts
+++
@@ -1,7 +0,0 @@
-export const featureCLIs = {
-  devtools: '@nuxt/devtools@latest'
-} as const
-
-export function isFeature (feature: string): feature is keyof typeof featureCLIs {
-  return feature in featureCLIs
-}
