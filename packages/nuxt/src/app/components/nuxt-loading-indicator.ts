681cfa83fde4baf1d66a51e30113cc635df86210
Nathan Chase <nathan@nathanchase.com>
2023-02-16 08:04:09 -0500

    perf(nuxt): animate transform rather than width (#19073)

--- packages/nuxt/src/app/components/nuxt-loading-indicator.ts
+++ packages/nuxt/src/app/components/nuxt-loading-indicator.ts
@@ -42,12 +42,14 @@ export default defineComponent({
         right: 0,
         left: 0,
         pointerEvents: 'none',
-        width: `${indicator.progress.value}%`,
+        width: 'auto',
         height: `${props.height}px`,
         opacity: indicator.isLoading.value ? 1 : 0,
         background: props.color || undefined,
         backgroundSize: `${(100 / indicator.progress.value) * 100}% auto`,
-        transition: 'width 0.1s, height 0.4s, opacity 0.4s',
+        transform: `scaleX(${indicator.progress.value}%)`,
+        transformOrigin: 'left',
+        transition: 'transform 0.1s, height 0.4s, opacity 0.4s',
         zIndex: 999999
       }
     }, slots)
