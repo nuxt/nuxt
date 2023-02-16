387d124b05bc90287949756f1cd08da270cf8a96
Daniel Roe <daniel@roe.dev>
2023-02-16 16:51:51 +0000

    test: add fixture to prevent regression

--- test/fixtures/basic/middleware/redirect.global.ts
+++ test/fixtures/basic/middleware/redirect.global.ts
@@ -1,5 +1,10 @@
+import { withoutTrailingSlash } from 'ufo'
+
 export default defineNuxtRouteMiddleware(async (to) => {
   const nuxtApp = useNuxtApp()
+  if (useRequestHeaders(['trailing-slash'])['trailing-slash'] && to.fullPath.endsWith('/')) {
+    return navigateTo(withoutTrailingSlash(to.fullPath), { redirectCode: 307 })
+  }
   if (to.path.startsWith('/redirect/')) {
     await new Promise(resolve => setTimeout(resolve, 100))
     return navigateTo(to.path.slice('/redirect/'.length - 1))
