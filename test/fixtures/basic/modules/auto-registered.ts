3ac7f732b6bd106c85b4fcc38ab2d22ea2133d90
Sébastien Chopin <seb@nuxt.com>
2023-03-02 16:52:31 +0100

    test: use addServerHandler for best practices

--- test/fixtures/basic/modules/auto-registered.ts
+++ test/fixtures/basic/modules/auto-registered.ts
@@ -1,15 +1,13 @@
-import { createResolver, defineNuxtModule, useNuxt } from '@nuxt/kit'
+import { createResolver, defineNuxtModule, addServerHandler } from '@nuxt/kit'
 
 export default defineNuxtModule({
   meta: {
     name: 'auto-registered-module'
   },
   setup () {
-    const nuxt = useNuxt()
     const resolver = createResolver(import.meta.url)
 
-    nuxt.options.nitro.handlers = nuxt.options.nitro.handlers || []
-    nuxt.options.nitro.handlers.push({
+    addServerHandler({
       handler: resolver.resolve('./runtime/handler'),
       route: '/auto-registered-module'
     })
