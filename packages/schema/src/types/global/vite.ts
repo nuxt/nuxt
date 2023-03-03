515d1dd972ee3fbc62ffa1485fc35c08e9c0964e
Daniel Roe <daniel@roe.dev>
2023-03-03 10:05:58 +0000

    style: reduce diff

--- packages/schema/src/types/global/vite.ts
+++ packages/schema/src/types/global/vite.ts
@@ -2,37 +2,37 @@
  * Reference: https://github.com/vitejs/vite/blob/main/packages/vite/types/importMeta.d.ts
  */
 export type ModuleNamespace = Record<string, any> & {
-  [Symbol.toStringTag]: "Module";
-};
+  [Symbol.toStringTag]: 'Module'
+}
 
-export interface ViteHotContext {
-  readonly data: any;
+export interface ViteHot {
+  readonly data: any
 
-  accept(): void;
-  accept(cb: (mod: ModuleNamespace | undefined) => void): void;
-  accept(dep: string, cb: (mod: ModuleNamespace | undefined) => void): void;
+  accept(): void
+  accept(cb: (mod: ModuleNamespace | undefined) => void): void
+  accept(dep: string, cb: (mod: ModuleNamespace | undefined) => void): void
   accept(
     deps: readonly string[],
     cb: (mods: Array<ModuleNamespace | undefined>) => void
-  ): void;
+  ): void
 
   acceptExports(
     exportNames: string | readonly string[],
     cb?: (mod: ModuleNamespace | undefined) => void
-  ): void;
+  ): void
 
-  dispose(cb: (data: any) => void): void;
-  prune(cb: (data: any) => void): void;
-  invalidate(message?: string): void;
+  dispose(cb: (data: any) => void): void
+  prune(cb: (data: any) => void): void
+  invalidate(message?: string): void
 
-  on(event: any, cb: (payload: any) => void): void;
-  send(event: any, data?: any): void;
+  on(event: any, cb: (payload: any) => void): void
+  send(event: any, data?: any): void
 }
 
 export interface KnownAsTypeMap {
-  raw: string;
-  url: string;
-  worker: Worker;
+  raw: string
+  url: string
+  worker: Worker
 }
 
 export interface ImportGlobOptions<
@@ -42,27 +42,27 @@ export interface ImportGlobOptions<
   /**
    * Import type for the import url.
    */
-  as?: AsType;
+  as?: AsType
   /**
    * Import as static or dynamic
    *
    * @default false
    */
-  eager?: Eager;
+  eager?: Eager
   /**
    * Import only the specific named export. Set to `default` to import the default export.
    */
-  import?: string;
+  import?: string
   /**
    * Custom queries
    */
-  query?: string | Record<string, string | number | boolean>;
+  query?: string | Record<string, string | number | boolean>
   /**
    * Search files also inside `node_modules/` and hidden directories (e.g. `.git/`). This might have impact on performance.
    *
    * @default false
    */
-  exhaustive?: boolean;
+  exhaustive?: boolean
 }
 
 export interface ImportGlobFunction {
@@ -82,7 +82,7 @@ export interface ImportGlobFunction {
   ? true
   : false) extends true
     ? Record<string, T>
-    : Record<string, () => Promise<T>>;
+    : Record<string, () => Promise<T>>
   /**
    * Import a list of files with a glob pattern.
    *
@@ -91,7 +91,7 @@ export interface ImportGlobFunction {
   <M>(
     glob: string | string[],
     options?: ImportGlobOptions<false, string>
-  ): Record<string, () => Promise<M>>;
+  ): Record<string, () => Promise<M>>
   /**
    * Import a list of files with a glob pattern.
    *
@@ -100,7 +100,7 @@ export interface ImportGlobFunction {
   <M>(
     glob: string | string[],
     options: ImportGlobOptions<true, string>
-  ): Record<string, M>;
+  ): Record<string, M>
 }
 
 export interface ImportGlobEagerFunction {
@@ -114,8 +114,8 @@ export interface ImportGlobEagerFunction {
     T = As extends keyof KnownAsTypeMap ? KnownAsTypeMap[As] : unknown
   >(
     glob: string | string[],
-    options?: Omit<ImportGlobOptions<boolean, As>, "eager">
-  ): Record<string, T>;
+    options?: Omit<ImportGlobOptions<boolean, As>, 'eager'>
+  ): Record<string, T>
   /**
    * Eagerly import a list of files with a glob pattern.
    *
@@ -123,19 +123,19 @@ export interface ImportGlobEagerFunction {
    */
   <M>(
     glob: string | string[],
-    options?: Omit<ImportGlobOptions<boolean, string>, "eager">
-  ): Record<string, M>;
+    options?: Omit<ImportGlobOptions<boolean, string>, 'eager'>
+  ): Record<string, M>
 }
 
 export interface ViteImportMeta {
   /** Vite client HMR API - see https://vitejs.dev/guide/api-hmr.html */
-  readonly hot?: ViteHotContext;
+  readonly hot?: ViteHot
 
   /** vite glob import utility - https://vitejs.dev/guide/features.html#glob-import */
-  glob: ImportGlobFunction;
+  glob: ImportGlobFunction
 
   /**
    * @deprecated Use `import.meta.glob('*', { eager: true })` instead
    */
-  globEager: ImportGlobEagerFunction;
+  globEager: ImportGlobEagerFunction
 }
