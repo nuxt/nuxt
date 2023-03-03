1761f368174eb0a116a085d8d640cfe516eb8021
Stefan Heim <stefan.heim@tip.co.at>
2023-03-03 09:45:56 +0100

    fix(nuxt): pass component transform options to loaderPlugin

--- packages/nuxt/src/components/module.ts
+++ packages/nuxt/src/components/module.ts
@@ -202,6 +202,7 @@ export default defineNuxtModule<ComponentsOptions>({
         sourcemap: nuxt.options.sourcemap[mode],
         getComponents,
         mode,
+        transform: nuxt.options.components?.transform,
         experimentalComponentIslands: nuxt.options.experimental.componentIslands
       }))
     })
@@ -219,6 +220,7 @@ export default defineNuxtModule<ComponentsOptions>({
           sourcemap: nuxt.options.sourcemap[mode],
           getComponents,
           mode,
+          transform: nuxt.options.components?.transform,
           experimentalComponentIslands: nuxt.options.experimental.componentIslands
         }))
       })
