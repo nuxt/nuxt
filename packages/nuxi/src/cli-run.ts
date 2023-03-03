3637514e808cb17a908e412729af4b63563df6b4
Pooya Parsa <pooya@pi0.io>
2023-03-03 15:27:05 +0100

    fix(cli): hard restart with communication channel

--- packages/nuxi/src/cli-run.ts
+++ packages/nuxi/src/cli-run.ts
@@ -1,4 +1,5 @@
 // @ts-ignore
 process._startTime = Date.now()
+
 // @ts-ignore
 import('./cli').then(r => (r.default || r).main())
--- packages/nuxi/src/cli-wrapper.ts
+++ packages/nuxi/src/cli-wrapper.ts
@@ -2,39 +2,32 @@
  * This file is used to wrap the CLI entrypoint in a restartable process.
  */
 import { fileURLToPath } from 'node:url'
-import { execa } from 'execa'
-import { EXIT_CODE_RESTART } from './constants'
+import { fork } from 'node:child_process'
+import type { ChildProcess } from 'node:child_process'
 
 const cliEntry = fileURLToPath(new URL('../dist/cli-run.mjs', import.meta.url))
 
-async function startSubprocess (preArgs: string[], postArgs: string[]) {
-  const child = await execa(
-    'node',
-    [
-      ...preArgs,
-      cliEntry,
-      ...postArgs
-    ],
-    {
-      reject: false,
-      stdio: 'inherit',
-      env: {
-        ...process.env,
-        NUXI_CLI_WRAPPER: 'true'
-      }
-    }
-  )
-  if (child.exitCode === EXIT_CODE_RESTART) {
-    await startSubprocess(preArgs, postArgs)
-  } else {
-    process.exit(child.exitCode)
-  }
-}
-
-const args = process.argv.slice(2)
-// only enable wrapper in dev command
-if (args[0] === 'dev') {
-  startSubprocess([], args)
+// Only enable wrapper For nuxt dev command
+if (process.argv[2] === 'dev') {
+  process.env.__CLI_ARGV__ = JSON.stringify(process.argv)
+  startSubprocess()
 } else {
   import(cliEntry)
+}
+
+function startSubprocess () {
+  let childProc: ChildProcess
+  start()
+  function start () {
+    if (childProc) {
+      childProc.kill()
+    }
+    childProc = fork(cliEntry)
+    childProc.on('close', (code) => { process.exit(code ?? 1) })
+    childProc.on('message', (message) => {
+      if ((message as { type: string })?.type === 'nuxt:restart') {
+        startSubprocess()
+      }
+    })
+  }
 }
