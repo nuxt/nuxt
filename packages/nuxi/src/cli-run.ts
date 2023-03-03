db5ea91916b9d6bdbf56043e428fc6096a243f13
Anthony Fu <anthonyfu117@hotmail.com>
2023-03-03 14:45:38 +0100

    feat(nuxi): cli wrapper for self restart (#18641)

---
+++ packages/nuxi/src/cli-run.ts
@@ -0,0 +1,4 @@
+// @ts-ignore
+process._startTime = Date.now()
+// @ts-ignore
+import('./cli').then(r => (r.default || r).main())
---
+++ packages/nuxi/src/cli-wrapper.ts
@@ -0,0 +1,40 @@
+/**
+ * This file is used to wrap the CLI entrypoint in a restartable process.
+ */
+import { fileURLToPath } from 'node:url'
+import { execa } from 'execa'
+import { EXIT_CODE_RESTART } from './constants'
+
+const cliEntry = fileURLToPath(new URL('../dist/cli-run.mjs', import.meta.url))
+
+async function startSubprocess (preArgs: string[], postArgs: string[]) {
+  const child = await execa(
+    'node',
+    [
+      ...preArgs,
+      cliEntry,
+      ...postArgs
+    ],
+    {
+      reject: false,
+      stdio: 'inherit',
+      env: {
+        ...process.env,
+        NUXI_CLI_WRAPPER: 'true'
+      }
+    }
+  )
+  if (child.exitCode === EXIT_CODE_RESTART) {
+    await startSubprocess(preArgs, postArgs)
+  } else {
+    process.exit(child.exitCode)
+  }
+}
+
+const args = process.argv.slice(2)
+// only enable wrapper in dev command
+if (args[0] === 'dev') {
+  await startSubprocess([], args)
+} else {
+  await import(cliEntry)
+}
