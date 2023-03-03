ddc6b36941cd9f46cbfddc1caa34fa3525cca970
Sebastian Landwehr <info@sebastianlandwehr.com>
2023-03-03 16:14:08 +0100

    fix: fix

--- packages/nuxi/src/run.ts
+++ packages/nuxi/src/run.ts
@@ -2,7 +2,7 @@ import mri from 'mri'
 import type { Command, NuxtCommand } from './commands'
 import { commands } from './commands'
 
-export async function runCommand (command: string, argv = process.argv.slice(2), options: any) {
+export async function runCommand (command: string, argv = process.argv.slice(2), options: any = {}) {
   const args = mri(argv)
   args.clear = false // used by dev
   args.config = options.config
