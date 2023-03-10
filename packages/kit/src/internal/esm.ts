384e38388d9a6c43e177035abc91384a9f32bb20
Daniel Roe <daniel@roe.dev>
2023-03-10 13:10:33 +0000

    fix: remove unneeded normalisation

--- packages/kit/src/internal/esm.ts
+++ packages/kit/src/internal/esm.ts
@@ -8,26 +8,17 @@ import { interopDefault, resolvePath } from 'mlly'
  * @internal
  */
 export async function tryResolveModule (id: string, url = import.meta.url) {
-  if (!url.startsWith('file:')) {
-    url = pathToFileURL(url).href
-  }
   try {
     return await resolvePath(id, { url })
   } catch { }
 }
 
 export async function importModule (id: string, url = import.meta.url) {
-  if (!url.startsWith('file:')) {
-    url = pathToFileURL(url).href
-  }
   const resolvedPath = await resolvePath(id, { url })
   return import(pathToFileURL(resolvedPath).href).then(interopDefault)
 }
 
 export function tryImportModule (id: string, url = import.meta.url) {
-  if (!url.startsWith('file:')) {
-    url = pathToFileURL(url).href
-  }
   try {
     return importModule(id, url).catch(() => undefined)
   } catch { }
--- packages/nuxi/src/utils/esm.ts
+++ packages/nuxi/src/utils/esm.ts
@@ -2,26 +2,17 @@ import { pathToFileURL } from 'node:url'
 import { interopDefault, resolvePath } from 'mlly'
 
 export async function tryResolveModule (id: string, url = import.meta.url) {
-  if (!url.startsWith('file:')) {
-    url = pathToFileURL(url).href
-  }
   try {
     return await resolvePath(id, { url })
   } catch { }
 }
 
 export async function importModule (id: string, url = import.meta.url) {
-  if (!url.startsWith('file:')) {
-    url = pathToFileURL(url).href
-  }
   const resolvedPath = await resolvePath(id, { url })
   return import(pathToFileURL(resolvedPath).href).then(interopDefault)
 }
 
 export function tryImportModule (id: string, url = import.meta.url) {
-  if (!url.startsWith('file:')) {
-    url = pathToFileURL(url).href
-  }
   try {
     return importModule(id, url).catch(() => undefined)
   } catch { }
