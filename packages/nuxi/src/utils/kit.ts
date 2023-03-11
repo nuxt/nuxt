c7b971b02d8ccb35e3c3d48a1e78fd6ab00a3050
Daniel Roe <daniel@roe.dev>
2023-03-11 14:07:14 +0000

    fix: update wording

--- packages/nuxi/src/utils/kit.ts
+++ packages/nuxi/src/utils/kit.ts
@@ -9,7 +9,7 @@ export const loadKit = async (rootDir: string): Promise<typeof import('@nuxt/kit
     return await importModule('@nuxt/kit', rootURL) as typeof import('@nuxt/kit')
   } catch (e: any) {
     if (e.toString().includes("Cannot find module '@nuxt/kit'")) {
-      throw new Error('nuxi requires `@nuxt/kit` to be installed in your project. Try installing `nuxt3` or `@nuxt/bridge` first.')
+      throw new Error('nuxi requires `@nuxt/kit` to be installed in your project. Try installing `nuxt` v3 or `@nuxt/bridge` first.')
     }
     throw e
   }
