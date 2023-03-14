01ee5bd7c59b49f6ef485491d16af85354929e62
Daniel Roe <daniel@roe.dev>
2023-03-14 14:06:48 +0000

    fix(nuxi): use file url to start nuxi (#19676)

--- packages/nuxi/src/cli-wrapper.ts
+++ packages/nuxi/src/cli-wrapper.ts
@@ -5,14 +5,14 @@ import { fileURLToPath } from 'node:url'
 import { fork } from 'node:child_process'
 import type { ChildProcess } from 'node:child_process'
 
-const cliEntry = fileURLToPath(new URL('../dist/cli-run.mjs', import.meta.url))
+const cliEntry = new URL('../dist/cli-run.mjs', import.meta.url)
 
 // Only enable wrapper for nuxt dev command
 if (process.argv[2] === 'dev') {
   process.env.__CLI_ARGV__ = JSON.stringify(process.argv)
   startSubprocess()
 } else {
-  import(cliEntry)
+  import(cliEntry.href)
 }
 
 function startSubprocess () {
@@ -33,7 +33,7 @@ function startSubprocess () {
   start()
 
   function start () {
-    childProc = fork(cliEntry)
+    childProc = fork(fileURLToPath(cliEntry))
     childProc.on('close', (code) => { if (code) { process.exit(code) } })
     childProc.on('message', (message) => {
       if ((message as { type: string })?.type === 'nuxt:restart') {
