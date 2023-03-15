886cca19ea5128c260fbd027616e61912f9dc4e6
Daniel Roe <daniel@roe.dev>
2023-03-15 11:26:01 +0000

    fix(kit): provide name to `performance.mark()` (#19687)

--- packages/kit/src/module/define.ts
+++ packages/kit/src/module/define.ts
@@ -68,9 +68,10 @@ export function defineNuxtModule<OptionsT extends ModuleOptions> (definition: Mo
     }
 
     // Call setup
-    const mark = performance.mark()
+    const key = `nuxt:module:${uniqueKey || (Math.round(Math.random() * 10000))}`
+    const mark = performance.mark(key)
     const res = await definition.setup?.call(null as any, _options, nuxt) ?? {}
-    const perf = performance.measure(`nuxt:module:${uniqueKey || (Math.round(Math.random() * 10000))}`, mark.name)
+    const perf = performance.measure(key, mark.name)
     const setupTime = Math.round((perf.duration * 100)) / 100
 
     // Measure setup time
