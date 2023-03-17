1ae8a201793dbf6332b7a5fc7fbe6e25865799e5
Daniel Roe <daniel@roe.dev>
2023-03-17 15:19:35 +0000

    fix(webpack): transpile app directory

--- packages/webpack/src/presets/base.ts
+++ packages/webpack/src/presets/base.ts
@@ -158,7 +158,8 @@ export function baseTranspile (ctx: WebpackConfigContext) {
   const transpile = [
     /\.vue\.js/i, // include SFCs in node_modules
     /consola\/src/,
-    /vue-demi/
+    /vue-demi/,
+    /(^|\/)nuxt\/(dist\/)?app($|\/)/
   ]
 
   for (let pattern of options.build.transpile) {
