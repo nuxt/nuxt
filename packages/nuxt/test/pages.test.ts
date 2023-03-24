3e4ffe06620ce7c7cbab836adf662a0b8638df0f
Daniel Roe <daniel@roe.dev>
2023-03-24 16:14:48 +0100

    test: update snapshots in pages test

--- packages/nuxt/test/pages.test.ts
+++ packages/nuxt/test/pages.test.ts
@@ -64,13 +64,13 @@ describe('pages:generateRoutesFromFiles', () => {
       output: [
         {
           name: 'parent-child',
-          path: '/parent/:child',
+          path: '/parent/:child()',
           file: `${pagesDir}/parent/[child].vue`,
           children: []
         },
         {
           name: 'parent-child',
-          path: '/parent-:child',
+          path: '/parent-:child()',
           file: `${pagesDir}/parent-[child].vue`,
           children: []
         }
@@ -91,7 +91,7 @@ describe('pages:generateRoutesFromFiles', () => {
         },
         {
           name: 'stories-id',
-          path: '/stories/:id',
+          path: '/stories/:id()',
           file: `${pagesDir}/stories/[id].vue`,
           children: []
         }
@@ -106,7 +106,7 @@ describe('pages:generateRoutesFromFiles', () => {
       output: [
         {
           name: 'stories-id',
-          path: '/stories/:id',
+          path: '/stories/:id()',
           file: `${pagesDir}/stories/[id].vue`,
           children: []
         },
@@ -167,7 +167,7 @@ describe('pages:generateRoutesFromFiles', () => {
           children: [],
           name: 'slug',
           file: 'pages/[slug].vue',
-          path: '/:slug'
+          path: '/:slug()'
         },
         {
           children: [
@@ -186,11 +186,11 @@ describe('pages:generateRoutesFromFiles', () => {
           children: [],
           name: 'bar',
           file: 'pages/[bar]/index.vue',
-          path: '/:bar'
+          path: '/:bar()'
         },
         {
           name: 'nonopt-slug',
-          path: '/nonopt/:slug',
+          path: '/nonopt/:slug()',
           file: `${pagesDir}/nonopt/[slug].vue`,
           children: []
         },
@@ -202,7 +202,7 @@ describe('pages:generateRoutesFromFiles', () => {
         },
         {
           name: 'sub-route-slug',
-          path: '/:sub?/route-:slug',
+          path: '/:sub?/route-:slug()',
           file: `${pagesDir}/[[sub]]/route-[slug].vue`,
           children: []
         }
@@ -243,20 +243,27 @@ describe('pages:generateRoutesFromFiles', () => {
       files: [
         `${pagesDir}/[a1_1a].vue`,
         `${pagesDir}/[b2.2b].vue`,
+        `${pagesDir}/[b2]_[2b].vue`,
         `${pagesDir}/[[c3@3c]].vue`,
         `${pagesDir}/[[d4-4d]].vue`
       ],
       output: [
         {
           name: 'a1_1a',
-          path: '/:a1_1a',
+          path: '/:a1_1a()',
           file: `${pagesDir}/[a1_1a].vue`,
           children: []
         },
         {
           name: 'b2.2b',
-          path: '/:b2.2b',
+          path: '/:b2.2b()',
           file: `${pagesDir}/[b2.2b].vue`,
+          children: []
+        },
+        {
+          name: 'b2_2b',
+          path: '/:b2()_:2b()',
+          file: `${pagesDir}/[b2]_[2b].vue`,
           children: []
         },
         {
