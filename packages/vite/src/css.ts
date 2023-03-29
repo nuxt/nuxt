ae82d7089598bd1a4e5a8f88cd9edf7a1f12d7c7
Daniel Roe <daniel@roe.dev>
2023-03-29 11:59:57 +0100

    fix(vite): allow extending vue config per-environment (#19968)

--- packages/vite/src/css.ts
+++ packages/vite/src/css.ts
@@ -1,10 +1,10 @@
 import { requireModule } from '@nuxt/kit'
 import type { Nuxt } from '@nuxt/schema'
-import type { ViteOptions } from './vite'
+import type { InlineConfig as ViteConfig } from 'vite'
 import { distDir } from './dirs'
 
-export function resolveCSSOptions (nuxt: Nuxt): ViteOptions['css'] {
-  const css: ViteOptions['css'] & { postcss: NonNullable<Exclude<NonNullable<ViteOptions['css']>['postcss'], string>> } = {
+export function resolveCSSOptions (nuxt: Nuxt): ViteConfig['css'] {
+  const css: ViteConfig['css'] & { postcss: NonNullable<Exclude<NonNullable<ViteConfig['css']>['postcss'], string>> } = {
     postcss: {
       plugins: []
     }
