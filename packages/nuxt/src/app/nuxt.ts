4ed5d2f87226332a13db31dee931e1fe42b1fc8c
Daniel Roe <daniel@roe.dev>
2023-03-31 10:17:10 +0100

    fix(nuxt): suppress handled errors (#20002)

--- packages/nuxt/src/app/nuxt.ts
+++ packages/nuxt/src/app/nuxt.ts
@@ -238,8 +238,9 @@ export function createNuxtApp (options: CreateOptions) {
     })
 
     // Log errors captured when running plugins, in the `app:created` and `app:beforeMount` hooks
-    // as well as when mounting the app and in the `app:mounted` hook
-    nuxtApp.hook('app:error', (...args) => { console.error('[nuxt] error caught during app initialization', ...args) })
+    // as well as when mounting the app.
+    const unreg = nuxtApp.hook('app:error', (...args) => { console.error('[nuxt] error caught during app initialization', ...args) })
+    nuxtApp.hook('app:mounted', unreg)
   }
 
   // Expose runtime config
