c56fe08cdbda9d84a8869aeed758e7f147d499ad
Daniel Roe <daniel@roe.dev>
2023-04-01 13:10:51 +0100

    style: lint handler

--- packages/nuxt/src/core/runtime/nitro/no-ssr.ts
+++ packages/nuxt/src/core/runtime/nitro/no-ssr.ts
@@ -1,6 +1,6 @@
 import { defineEventHandler, getRequestHeader } from 'h3'
 
-export default defineEventHandler(async event => {
+export default defineEventHandler((event) => {
   if (getRequestHeader(event, 'x-nuxt-no-ssr') === 'true') {
     event.context.nuxt = event.context.nuxt || {}
     event.context.nuxt.noSSR = true
