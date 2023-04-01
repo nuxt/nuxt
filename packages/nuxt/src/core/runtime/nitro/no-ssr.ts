560c17b3686e473cd30868d8750a9f5a3870173d
Daniel Roe <daniel@roe.dev>
2023-04-01 22:20:56 +0100

    fix: test for truthy header only

--- packages/nuxt/src/core/runtime/nitro/no-ssr.ts
+++ packages/nuxt/src/core/runtime/nitro/no-ssr.ts
@@ -1,7 +1,7 @@
 import { defineEventHandler, getRequestHeader } from 'h3'
 
 export default defineEventHandler((event) => {
-  if (getRequestHeader(event, 'x-nuxt-no-ssr') === 'true') {
+  if (getRequestHeader(event, 'x-nuxt-no-ssr')) {
     event.context.nuxt = event.context.nuxt || {}
     event.context.nuxt.noSSR = true
   }
