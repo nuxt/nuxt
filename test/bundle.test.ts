5785908ec62305e2415dee4f627944afef46b54b
Daniel Roe <daniel@roe.dev>
2023-04-01 10:11:48 +0100

    test: bump bundle size test by 100 bytes

--- test/bundle.test.ts
+++ test/bundle.test.ts
@@ -40,7 +40,7 @@ describe.skipIf(isWindows || process.env.ECOSYSTEM_CI)('minimal nuxt application
 
   it('default server bundle size', async () => {
     stats.server = await analyzeSizes(['**/*.mjs', '!node_modules'], serverDir)
-    expect(stats.server.totalBytes).toBeLessThan(93200)
+    expect(stats.server.totalBytes).toBeLessThan(93300)
 
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
     expect(modules.totalBytes).toBeLessThan(2694900)
