bc60b2c643a70417b2cabf0c1ede9ce98a35c81b
Daniel Roe <daniel@roe.dev>
2023-04-01 12:36:29 +0100

    chore: revert whitespace change

--- packages/nuxt/src/core/runtime/nitro/renderer.ts
+++ packages/nuxt/src/core/runtime/nitro/renderer.ts
@@ -295,9 +295,9 @@ export default defineRenderHandler(async (event) => {
       process.env.NUXT_NO_SCRIPTS
         ? undefined
         : (_PAYLOAD_EXTRACTION
-          ? `<script type="module">import p from "${payloadURL}";window.__NUXT__={...p,...(${devalue(splitPayload(ssrContext).initial)})}</script>`
-          : `<script>window.__NUXT__=${devalue(ssrContext.payload)}</script>`
-        ),
+            ? `<script type="module">import p from "${payloadURL}";window.__NUXT__={...p,...(${devalue(splitPayload(ssrContext).initial)})}</script>`
+            : `<script>window.__NUXT__=${devalue(ssrContext.payload)}</script>`
+          ),
       _rendered.renderScripts(),
       // Note: bodyScripts may contain tags other than <script>
       renderedMeta.bodyScripts
