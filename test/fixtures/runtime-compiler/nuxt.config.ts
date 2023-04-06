00580bdc1e3169b84d70503814060bf77fa17b55
Daniel Roe <daniel@roe.dev>
2023-04-06 14:13:53 +0100

    test: move to new path

--- test/fixtures/runtime-compiler/nuxt.config.ts
+++ test/fixtures/runtime-compiler/nuxt.config.ts
@@ -1,8 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   experimental: {
-    runtimeVueCompiler: true,
-    externalVue: false
+    runtimeVueCompiler: true
+  },
+  vue: {
+    externalLib: false
   },
   builder: process.env.TEST_BUILDER as 'webpack' | 'vite' ?? 'vite'
 })
