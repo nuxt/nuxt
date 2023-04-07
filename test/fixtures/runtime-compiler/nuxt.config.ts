83d816be8cd30115c709d0a842c97a66a91c0ec3
Daniel Roe <daniel@roe.dev>
2023-04-07 10:54:50 +0100

    chore: undo changes to externalVue
    This reverts commit 00580bdc1e3169b84d70503814060bf77fa17b55.

--- test/fixtures/runtime-compiler/nuxt.config.ts
+++ test/fixtures/runtime-compiler/nuxt.config.ts
@@ -1,10 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   experimental: {
-    runtimeVueCompiler: true
-  },
-  vue: {
-    externalLib: false
+    runtimeVueCompiler: true,
+    externalVue: false
   },
   builder: process.env.TEST_BUILDER as 'webpack' | 'vite' ?? 'vite'
 })
