89a6390001b30dd3e073c0f336713fbc8410913d
Daniel Roe <daniel@roe.dev>
2023-04-14 15:08:51 +0100

    refactor: rearrange file for clarity

--- packages/nuxt/src/app/config.ts
+++ packages/nuxt/src/app/config.ts
@@ -3,19 +3,18 @@ import type { AppConfig } from 'nuxt/schema'
 // @ts-expect-error virtual file
 import __appConfig from '#build/app.config.mjs'
 
+const appConfig = process.server ? __appConfig : reactive(__appConfig)
+export const useAppConfig = () => appConfig
+
+/**
+ * Deep assign the current appConfig with the new one.
+ *
+ * Will preserve existing properties.
+ */
 type DeepPartial<T> = T extends Function ? T : T extends Record<string, any> ? { [P in keyof T]?: DeepPartial<T[P]> } : T
-
-function deepDelete (obj: any, newObj: any) {
-  for (const key in obj) {
-    const val = newObj[key]
-    if (!(key in newObj)) {
-      delete (obj as any)[key]
-    }
-
-    if (val !== null && typeof val === 'object') {
-      deepDelete(obj[key], newObj[key])
-    }
-  }
+export function updateAppConfig (appConfig: DeepPartial<AppConfig>) {
+  const _appConfig = useAppConfig()
+  deepAssign(_appConfig, appConfig)
 }
 
 function deepAssign (obj: any, newObj: any) {
@@ -30,21 +29,21 @@ function deepAssign (obj: any, newObj: any) {
   }
 }
 
-const appConfig = process.server ? __appConfig : reactive(__appConfig)
-export const useAppConfig = () => appConfig
-
-/**
- * Deep assign the current appConfig with the new one.
- *
- * Will preserve existing properties.
- */
-export function updateAppConfig (appConfig: DeepPartial<AppConfig>) {
-  const _appConfig = useAppConfig()
-  deepAssign(_appConfig, appConfig)
-}
-
 // HMR Support
 if (process.dev) {
+  function deepDelete (obj: any, newObj: any) {
+    for (const key in obj) {
+      const val = newObj[key]
+      if (!(key in newObj)) {
+        delete (obj as any)[key]
+      }
+
+      if (val !== null && typeof val === 'object') {
+        deepDelete(obj[key], newObj[key])
+      }
+    }
+  }
+
   function applyHMR (newConfig: AppConfig) {
     const appConfig = useAppConfig()
     if (newConfig && appConfig) {
