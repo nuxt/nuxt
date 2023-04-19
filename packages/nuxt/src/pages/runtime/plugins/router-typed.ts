ada04edb75ffae5f5d9a744731f9189d832ba7d3
Daniel Roe <daniel@roe.dev>
2023-04-19 14:23:14 +0100

    refactor: merge back into single plugin

--- packages/nuxt/src/pages/runtime/plugins/router-typed.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter } from 'vue-router/auto'
-import { createRouterPlugin } from './router-base'
-
-export default createRouterPlugin((history, routerOptions) => {
-  return createRouter({
-    ...routerOptions,
-    history
-  })
-})
