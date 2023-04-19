c7be5b4ec6a3b129c68e892c9f3dad816446bb10
Daniel Roe <daniel@roe.dev>
2023-04-19 19:17:36 +0100

    refactor(nuxt): rework and use `isJS` and `isVue` utilities consistently (#20344)

--- packages/nuxt/src/components/loader.ts
+++ packages/nuxt/src/components/loader.ts
@@ -6,7 +6,7 @@ import { resolve } from 'pathe'
 import type { Component, ComponentsOptions } from 'nuxt/schema'
 
 import { distDir } from '../dirs'
-import { isVueTemplate } from './helpers'
+import { isVue } from '../core/utils'
 
 interface LoaderOptions {
   getComponents (): Component[]
@@ -31,7 +31,7 @@ export const loaderPlugin = createUnplugin((options: LoaderOptions) => {
       if (include.some(pattern => id.match(pattern))) {
         return true
       }
-      return isVueTemplate(id)
+      return isVue(id, { type: ['template', 'script'] })
     },
     transform (code) {
       const components = options.getComponents()
