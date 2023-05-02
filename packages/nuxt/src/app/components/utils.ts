194fd0eb8363a1e02a07a289048e3dd58ac51a10
julien huang <julien.huang@outlook.fr>
2023-05-02 23:06:13 +0200

    lint

--- packages/nuxt/src/app/components/utils.ts
+++ packages/nuxt/src/app/components/utils.ts
@@ -1,4 +1,4 @@
-import { defineComponent, h } from 'vue'
+import { h } from 'vue'
 import type { Component, RendererNode } from 'vue'
 // eslint-disable-next-line
 import { isString, isPromise, isArray, isObject } from '@vue/shared'
