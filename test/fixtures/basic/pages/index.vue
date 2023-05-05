9d44f0ef9f2d03e4f3d352340494739201f378bc
Rafal Chlodnicki <rchl2k@gmail.com>
2023-05-06 01:51:21 +0200

    fix(vue-app): interrupt page render early on error or navigating away

--- test/fixtures/basic/pages/index.vue
+++ test/fixtures/basic/pages/index.vue
@@ -2,5 +2,8 @@
   <div>
     <h1>Index page</h1>
     <img src="~/assets/test.svg">
+    <nuxt-link to="navigation">
+      Continue to /navigation
+    </nuxt-link>
   </div>
 </template>
---
+++ test/fixtures/basic/pages/navigation/asyncData-error.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Async Data Error</h1>
+</template>
+<script>
+export default {
+  async asyncData ({ error }) {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+    error({ statusCode: 401, message: 'Not allowed here' })
+  }
+}
+</script>
