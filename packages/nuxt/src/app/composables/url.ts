b5aa94c07a359c8bf40cf2f110a2e4f555a657a1
Sébastien Chopin <seb@nuxt.com>
2023-05-10 19:02:08 +0200

    chore: remove baseURL on client-side

--- packages/nuxt/src/app/composables/url.ts
+++ packages/nuxt/src/app/composables/url.ts
@@ -1,9 +1,11 @@
 import { getRequestURL as getServerRequestURL } from 'h3'
 import { useRequestEvent } from './ssr'
+import { useRuntimeConfig } from '#app'
 
 export function getRequestURL () {
   if (process.server) {
     return getServerRequestURL(useRequestEvent())
   }
-  return new URL(window.location.href)
+  const { baseURL } = useRuntimeConfig().app
+  return new URL(window.location.href.replace(baseURL, '/'))
 }
