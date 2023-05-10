2dcae18121e7649b07619def8e1adfc1f0076435
Inesh Bose <2504266b@student.gla.ac.uk>
2023-05-10 17:33:38 +0100

    feat: module as function

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -131,10 +131,6 @@ export default defineNuxtConfig({
         internalParent!.children = newPages
       })
     },
-    function (_, nuxt) {
-      nuxt.options.optimization.treeShake.composables.server[nuxt.options.rootDir] = ['useClientOnlyComposable', 'setTitleToPink']
-      nuxt.options.optimization.treeShake.composables.client[nuxt.options.rootDir] = ['useServerOnlyComposable']
-    },
     // To test falsy module values
     undefined
   ],
