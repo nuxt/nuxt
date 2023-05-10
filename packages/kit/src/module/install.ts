acae1c52fd841effe112da1096a6d98d18aea42a
Daniel Roe <daniel@roe.dev>
2023-05-10 12:54:20 +0100

    fix(kit): prefer esm resolution for modules to install

--- packages/kit/src/module/install.ts
+++ packages/kit/src/module/install.ts
@@ -3,7 +3,7 @@ import type { Nuxt, NuxtModule } from '@nuxt/schema'
 import { dirname, isAbsolute } from 'pathe'
 import { isNuxt2 } from '../compatibility'
 import { useNuxt } from '../context'
-import { requireModule, resolveModule } from '../internal/cjs'
+import { requireModule } from '../internal/cjs'
 import { importModule } from '../internal/esm'
 import { resolveAlias } from '../resolve'
 
@@ -53,12 +53,9 @@ async function normalizeModule (nuxtModule: string | NuxtModule, inlineOptions?:
 
   // Import if input is string
   if (typeof nuxtModule === 'string') {
-    const _src = resolveModule(resolveAlias(nuxtModule), { paths: nuxt.options.modulesDir })
-    // TODO: also check with type: 'module' in closest `package.json`
-    const isESM = _src.endsWith('.mjs')
-
     try {
-      nuxtModule = isESM ? await importModule(_src, nuxt.options.rootDir) : requireModule(_src)
+      // Prefer ESM resolution if possible
+      nuxtModule = await importModule(nuxtModule, nuxt.options.modulesDir).catch(() => null) ?? requireModule(resolveAlias(nuxtModule), { paths: nuxt.options.modulesDir })
     } catch (error: unknown) {
       console.error(`Error while requiring module \`${nuxtModule}\`: ${error}`)
       throw error
