5e74fe4e0b661745647e5f25449ca7c27a2a476d
Daniel Roe <daniel@roe.dev>
2023-05-11 18:57:18 +0100

    fix(nuxt): enable suspensible behaviour for nested pages (#20777)

---
+++ test/fixtures/basic/pages/suspense.vue
@@ -0,0 +1,22 @@
+<script setup>
+definePageMeta({
+  // Nested <Suspense> + <Transition> is still buggy
+  pageTransition: false,
+  layoutTransition: false
+})
+const links = ['sync', 'async'].flatMap(parent => [1, 2].flatMap(p => ['sync', 'async'].flatMap(child => [1, 2].map(c => `/suspense/${parent}-${p}/${child}-${c}/`))))
+</script>
+
+<template>
+  <div>
+    This exists to test synchronous transitions between nested Suspense components.
+    <hr>
+    <NuxtLink v-for="link in links" :key="link" :to="link" style="display: block;">
+      {{ link }}
+    </NuxtLink>
+    <hr>
+    <div>
+      <NuxtPage />
+    </div>
+  </div>
+</template>
