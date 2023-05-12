1bda655b9a6e0c3b4dd8c19d78ee7d4ac8d8c40d
Miachel Brevard <yonshi29@gmail.com>
2023-05-12 18:46:00 +0300

    Update adhoc.ts

--- packages/schema/src/config/adhoc.ts
+++ packages/schema/src/config/adhoc.ts
@@ -29,9 +29,9 @@ export default defineUntypedSchema({
    * @type {typeof import('../src/types/imports').ImportsOptions}
    */
   imports: {
-    /**
-     * Toggle auto-imports for all composables, components, directories, and all other parts of your application that Nuxt usually handles.
-     */
+     /**
+      * Toggle auto-imports for all composables, components, directories, and all other parts of your application that Nuxt usually handles.
+      */
     autoImport: false,
 
     global: false,
