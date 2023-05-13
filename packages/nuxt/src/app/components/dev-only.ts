5f9b25ae0ad2afcd030c8881eb027867e7894d69
Daniel Roe <daniel@roe.dev>
2023-05-13 22:50:17 +0100

    fix: invoke slot

--- packages/nuxt/src/app/components/dev-only.ts
+++ packages/nuxt/src/app/components/dev-only.ts
@@ -6,6 +6,6 @@ export default defineComponent({
     if (process.dev) {
       return () => props.slots.default?.()
     }
-    return () => props.slots.fallback
+    return () => props.slots.fallback?.()
   }
 })
