1f30cf18e35fe9864677281d6588235500e35bb7
Inesh Bose <56732164+ineshbose@users.noreply.github.com>
2023-05-15 13:33:46 +0100

    fix(nuxt): get fallback for `<DevOnly>` from parsed html (#20840)

--- packages/nuxt/src/core/plugins/dev-only.ts
+++ packages/nuxt/src/core/plugins/dev-only.ts
@@ -3,13 +3,14 @@ import { stripLiteral } from 'strip-literal'
 import { parseQuery, parseURL } from 'ufo'
 import MagicString from 'magic-string'
 import { createUnplugin } from 'unplugin'
+import { type Node, parse } from 'ultrahtml'
 
 interface DevOnlyPluginOptions {
   sourcemap?: boolean
 }
 
 export const DevOnlyPlugin = createUnplugin((options: DevOnlyPluginOptions) => {
-  const DEVONLY_COMP_RE = /<(?:dev-only|DevOnly)>[^<]*(?:<template\s+#fallback>(?<fallback>[\s\S]*?)<\/template>)?[\s\S]*?<\/(?:dev-only|DevOnly)>/g
+  const DEVONLY_COMP_RE = /<(?:dev-only|DevOnly)>[\s\S]*?<\/(?:dev-only|DevOnly)>/g
 
   return {
     name: 'nuxt:server-devonly:transform',
@@ -29,7 +30,11 @@ export const DevOnlyPlugin = createUnplugin((options: DevOnlyPluginOptions) => {
       const s = new MagicString(code)
       const strippedCode = stripLiteral(code)
       for (const match of strippedCode.matchAll(DEVONLY_COMP_RE) || []) {
-        s.overwrite(match.index!, match.index! + match[0].length, match.groups?.fallback || '')
+        const ast: Node = parse(match[0]).children[0]
+        const fallback: Node | undefined = ast.children?.find((n: Node) => n.name === 'template' && Object.values(n.attributes).includes('#fallback'))
+        const replacement = fallback ? match[0].slice(fallback.loc[0].end, fallback.loc[fallback.loc.length - 1].start) : ''
+
+        s.overwrite(match.index!, match.index! + match[0].length, replacement)
       }
 
       if (s.hasChanged()) {
