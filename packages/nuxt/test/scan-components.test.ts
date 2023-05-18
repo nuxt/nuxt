a9fc5ec2e6b200ce64e51e654369850019dfe386
Daniel Roe <daniel@roe.dev>
2023-05-18 21:32:10 +0100

    fix(nuxt): don't match partial component names with prefix

--- packages/nuxt/test/scan-components.test.ts
+++ packages/nuxt/test/scan-components.test.ts
@@ -257,7 +257,9 @@ const tests: Array<[string, string[], string]> = [
   ['ThingItemTest', ['Item', 'Thing', 'Foo'], 'ItemThingFooThingItemTest'],
   ['ItemIn', ['Item', 'Holder', 'Item', 'In'], 'ItemHolderItemIn'],
   ['Item', ['Item', 'Holder', 'Test'], 'ItemHolderTestItem'],
-  ['ItemHolderItem', ['Item', 'Holder', 'Item', 'Holder'], 'ItemHolderItemHolderItem']
+  ['ItemHolderItem', ['Item', 'Holder', 'Item', 'Holder'], 'ItemHolderItemHolderItem'],
+  ['Icones', ['Icon'], 'IconIcones'],
+  ['Icon', ['Icones'], 'IconesIcon']
 ]
 
 describe('components:resolveComponentName', () => {
