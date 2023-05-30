be9426d560c0e2516fd17daadfdd2296903ab559
Harlan Wilton <harlan@harlanzw.com>
2023-05-30 09:25:18 +0800

    chore: fix tests

--- packages/kit/src/module/compatibility.test.ts
+++ packages/kit/src/module/compatibility.test.ts
@@ -1,11 +1,26 @@
 import { describe, expect, it } from 'vitest'
 import { loadNuxt } from '../loader/nuxt'
-import { hasNuxtModuleCompatibility } from './compatibility'
+import { hasNuxtModule, hasNuxtModuleCompatibility } from './compatibility'
 import { defineNuxtModule } from './define'
 
 describe('nuxt module compatibility', () => {
+  it('check module installed', async () => {
+    const nuxt = await loadNuxt({
+      overrides: {
+        modules: [
+          defineNuxtModule({
+            meta: {
+              name: 'nuxt-module-foo'
+            }
+          }),
+        ]
+      }
+    })
+    expect(hasNuxtModule('nuxt-module-foo', nuxt)).toStrictEqual(true)
+    await nuxt.close()
+  })
   it('detects module instance versions', async () => {
-    const nuxt = loadNuxt({})
+    const nuxt = await loadNuxt({})
     const module = defineNuxtModule({
       meta: {
         name: 'nuxt-module-foo',
@@ -14,5 +29,6 @@ describe('nuxt module compatibility', () => {
     })
     expect(await hasNuxtModuleCompatibility(module, '^1.0.0', nuxt)).toStrictEqual(true)
     expect(await hasNuxtModuleCompatibility(module, '^2.0.0', nuxt)).toStrictEqual(false)
+    await nuxt.close()
   })
 })
