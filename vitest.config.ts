5614ef9b8f8a415f9f5d7e92afd24671cba27d56
Daniel Roe <daniel@roe.dev>
2023-06-02 12:12:33 +0100

    chore: avoid inlining deps in vitest (#21316)

--- vitest.config.ts
+++ vitest.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
   test: {
     globalSetup: 'test/setup.ts',
     testTimeout: isWindows ? 60000 : 10000,
-    deps: { inline: ['@vitejs/plugin-vue'] },
     // Excluded plugin because it should throw an error when accidentally loaded via Nuxt
     exclude: [...configDefaults.exclude, '**/this-should-not-load.spec.js'],
     maxThreads: process.env.TEST_ENV === 'dev' ? 1 : undefined,
