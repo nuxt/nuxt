d18f7988da880cdfea3eec856bab8de9dda65a64
Daniel Roe <daniel@roe.dev>
2023-06-05 12:42:54 +0100

    perf: more narrowly restrict plugin to entrypoint

--- packages/vite/src/plugins/type-check.ts
+++ packages/vite/src/plugins/type-check.ts
@@ -2,10 +2,18 @@ import MagicString from 'magic-string'
 import type { Plugin } from 'vite'
 
 export function typeCheckPlugin (): Plugin {
+  let entry: string
   return {
     name: 'nuxt:type-check',
+    configResolved (config) {
+      const input = config.build.rollupOptions.input
+      console.log(input)
+      if (input && typeof input !== 'string' && !Array.isArray(input)) {
+        entry = input.entry
+      }
+    },
     transform (code, id) {
-      if (!id.includes('entry.ts')) { return }
+      if (id !== entry) { return }
 
       const s = new MagicString(code)
 
