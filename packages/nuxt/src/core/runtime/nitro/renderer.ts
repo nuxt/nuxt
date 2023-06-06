5f447b5149a019fb547ec664e4c2ce71ed94aecc
Daniel Roe <daniel@roe.dev>
2023-06-06 22:12:30 +0100

    fix: exchange a steely-eyed glance with the error and ask: 'are you the one?'

--- packages/nuxt/src/core/runtime/nitro/renderer.ts
+++ packages/nuxt/src/core/runtime/nitro/renderer.ts
@@ -247,7 +247,7 @@ export default defineRenderHandler(async (event): Promise<Partial<RenderResponse
 
   const _rendered = await renderer.renderToString(ssrContext).catch(async (error) => {
     // We use error to bypass full render if we have an early response we can make
-    if (ssrContext._renderResponse) { return {} as ReturnType<typeof renderer['renderToString']> }
+    if (ssrContext._renderResponse && error.message === 'skipping render') { return {} as ReturnType<typeof renderer['renderToString']> }
 
     // Use explicitly thrown error in preference to subsequent rendering errors
     const _err = (!ssrError && ssrContext.payload?.error) || error
