95dbf21c3478b49d8e36697ae54806a17f8786e8
Daniel Roe <daniel@roe.dev>
2023-06-12 16:36:41 +0100

    fix: add types for `findPackage`

--- packages/nuxi/src/commands/info.ts
+++ packages/nuxi/src/commands/info.ts
@@ -4,6 +4,7 @@ import { createRequire } from 'node:module'
 import { resolve } from 'pathe'
 import jiti from 'jiti'
 import destr from 'destr'
+import type { PackageJson } from 'pkg-types'
 import { splitByCase } from 'scule'
 import clipboardy from 'clipboardy'
 import type { NuxtModule } from '@nuxt/schema'
@@ -145,7 +146,7 @@ function findPackage (rootDir: string) {
   return findup(rootDir, (dir) => {
     const p = resolve(dir, 'package.json')
     if (existsSync(p)) {
-      return readJSONSync(p)
+      return readJSONSync(p) as PackageJson
     }
   }) || {}
 }
