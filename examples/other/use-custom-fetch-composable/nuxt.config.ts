444d4e1ca7e5924ebfdaaaddad98a7bb6b5fb390
Daniel Roe <daniel@roe.dev>
2023-06-19 18:51:11 +0100

    docs: remove `examples/` from repository (#21538)
    Co-authored-by: Sébastien Chopin <seb@nuxt.com>

--- examples/other/use-custom-fetch-composable/nuxt.config.ts
+++
@@ -1,6 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  modules: [
-    '@nuxt/ui'
-  ]
-})
--- examples/routing/layouts/pages/default.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <p>Content inside <code>default</code> layout</p>
-    <br>
-    <NuxtLink to="/">
-      Back to home
-    </NuxtLink>
-  </div>
-</template>
