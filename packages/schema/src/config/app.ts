44867a8ac64208ed39f6a1c2eb83295e0d5539b0
Daniel Roe <daniel@roe.dev>
2023-06-20 19:09:25 +0100

    docs: add a link and example loading indicator

--- packages/schema/src/config/app.ts
+++ packages/schema/src/config/app.ts
@@ -180,11 +180,57 @@ export default defineUntypedSchema({
 
   /** A path to an HTML file, the contents of which will be inserted into any HTML page
    * rendered with `ssr: false`.
-   * 
+   *
+   * By default Nuxt will look in `~/app/spa-loading-template.html` for this file.
+   *
    * You can set this to `false` to disable any loading indicator.
-   * 
+   *
    * Some good sources for spinners are [SpinKit](https://github.com/tobiasahlin/SpinKit) or [SVG Spinners](https://icones.js.org/collection/svg-spinners).
-   * 
+   *
+   * @example ~/app/spa-loading-template.html
+   * ```html
+   * <!-- https://github.com/barelyhuman/snips/blob/dev/pages/css-loader.md -->
+   * <div class="loader"></div>
+   * <style>
+   * .loader {
+   *   display: block;
+   *   position: fixed;
+   *   z-index: 1031;
+   *   top: 50%;
+   *   left: 50%;
+   *   transform: translate(-50%, -50%);
+   *   width: 18px;
+   *   height: 18px;
+   *   box-sizing: border-box;
+   *   border: solid 2px transparent;
+   *   border-top-color: #000;
+   *   border-left-color: #000;
+   *   border-bottom-color: #efefef;
+   *   border-right-color: #efefef;
+   *   border-radius: 50%;
+   *   -webkit-animation: loader 400ms linear infinite;
+   *   animation: loader 400ms linear infinite;
+   * }
+   *
+   * \@-webkit-keyframes loader {
+   *   0% {
+   *     -webkit-transform: rotate(0deg);
+   *   }
+   *   100% {
+   *     -webkit-transform: rotate(360deg);
+   *   }
+   * }
+   * \@keyframes loader {
+   *   0% {
+   *     transform: rotate(0deg);
+   *   }
+   *   100% {
+   *     transform: rotate(360deg);
+   *   }
+   * }
+   * </style>
+   * ```
+   *
    * @type {string | false}
    */
   spaLoadingTemplate: {
