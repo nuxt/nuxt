4906fe135984150de054467816a2e17c87d98ac9
Bogdan Kostyuk <contact@bogdankostyuk.xyz>
2023-06-22 17:47:42 +0300

    feat: create `usePreviewMode` composable

---
+++ packages/nuxt/src/app/composables/preview.ts
@@ -0,0 +1,57 @@
+import { computed, ref } from 'vue'
+import type { Ref } from 'vue'
+
+import { refreshNuxtData, useRoute, useRouter } from '#app'
+
+export interface PreviewOptions {
+  controls: boolean
+  tokenQueryName: string
+}
+
+const previewQueryName = 'preview'
+const previewDefaultOptions: PreviewOptions = {
+  controls: false,
+  tokenQueryName: 'token'
+}
+
+const tokensMap = new Map<string, string | null>()
+
+let _isPreview: Ref<boolean>
+let addedAfterNavigationCallback = false
+
+export function usePreviewMode (options?: PreviewOptions) {
+  options = { ...previewDefaultOptions, ...(options || {}) }
+
+  const router = useRouter()
+  const route = useRoute()
+
+  const previewParam = route.query[previewQueryName]
+
+  const isPreview = _isPreview || ref<boolean>(!!previewParam && previewParam === 'true')
+  _isPreview = isPreview
+
+  const tokenFromMap = tokensMap.get(options!.tokenQueryName)
+
+  if (!isPreview.value || !tokenFromMap) {
+    const query = route.query[options!.tokenQueryName]
+    const token = Array.isArray(query) ? query[0] : query
+
+    tokensMap.set(options!.tokenQueryName, token)
+  }
+  
+  const token = ref<string | null>(tokenFromMap || null)
+
+  const refreshData = () => {
+    addedAfterNavigationCallback = true
+    refreshNuxtData()
+  }
+
+  if (isPreview.value && !addedAfterNavigationCallback && process.client) {
+    refreshData()
+    router.afterEach(refreshData)
+  }
+
+  if (options.controls) { return { isPreview, token } }
+
+  return isPreview
+}
