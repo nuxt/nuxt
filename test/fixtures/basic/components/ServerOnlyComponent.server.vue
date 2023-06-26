4fb263007f5139eb105622ccf010443ee4371f87
Daniel Roe <daniel@roe.dev>
2023-06-26 15:21:36 +0000

    test: add regression test

--- test/fixtures/basic/components/ServerOnlyComponent.server.vue
+++ test/fixtures/basic/components/ServerOnlyComponent.server.vue
@@ -1,14 +1,14 @@
-<template>
-  <div>
-    server-only component
-  </div>
-</template>
-
 <script setup>
 import { appendResponseHeader } from 'h3'
 
 appendResponseHeader(useRequestEvent(), 'x-nitro-prerender', '/some/url/from/server-only/component')
 </script>
+
+<template>
+  <div>
+    server-only component
+  </div>
+</template>
 
 <style>
 :root {
