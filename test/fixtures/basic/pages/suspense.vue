0a551859a8e2734a800b1038dbc48b80b561d275
Daniel Roe <daniel@roe.dev>
2023-07-04 14:01:47 +0200

    test: remove duplicate `definePageMeta`

--- test/fixtures/basic/pages/suspense.vue
+++ test/fixtures/basic/pages/suspense.vue
@@ -1,12 +1,10 @@
 <script setup>
-definePageMeta({
-  // Nested <Suspense> + <Transition> is still buggy
-  pageTransition: false,
-  layoutTransition: false
-})
 const links = new Set(['sync', 'async'].flatMap(parent => [1, 2].flatMap(p => ['sync', 'async'].flatMap(child => [null, 1, 2].map(c => !c ? `/suspense/${parent}-${p}/` : `/suspense/${parent}-${p}/${child}-${c}/`)))))
 
 definePageMeta({
+// Nested <Suspense> + <Transition> is still buggy
+  pageTransition: false,
+  layoutTransition: false,
   middleware: (to) => {
     if ('layout' in to.query) {
       if (to.query.layout === 'false') {
