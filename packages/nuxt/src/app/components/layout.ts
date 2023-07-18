a6865bca02ef151e8ff8b050042fce94f42c9ee7
Daniel Roe <daniel@roe.dev>
2023-07-18 15:25:16 +0100

    fix(nuxt): ensure we only increment hydrating count once

--- packages/nuxt/src/app/components/layout.ts
+++ packages/nuxt/src/app/components/layout.ts
@@ -33,8 +33,9 @@ export default defineComponent({
     const layoutRef = ref()
     context.expose({ layoutRef })
 
+    const done = nuxtApp.deferHydration()
+
     return () => {
-      const done = nuxtApp.deferHydration()
       const hasLayout = layout.value && layout.value in layouts
       if (process.dev && layout.value && !hasLayout && layout.value !== 'default') {
         console.warn(`Invalid layout \`${layout.value}\` selected.`)
