fe103eb98797eeb4343ac49128d3b60c90430613
Daniel Roe <daniel@roe.dev>
2023-07-30 23:50:33 +0100

    style: revert formatting changes

--- packages/nuxt/src/app/components/nuxt-layout.ts
+++ packages/nuxt/src/app/components/nuxt-layout.ts
@@ -34,13 +34,7 @@ export default defineComponent({
   inheritAttrs: false,
   props: {
     name: {
-      type: [
-        String,
-        Boolean,
-        Object
-      ] as unknown as () => unknown extends PageMeta['layout']
-        ? MaybeRef<string | false>
-        : PageMeta['layout'] | MaybeRef<false>,
+      type: [String, Boolean, Object] as unknown as () => unknown extends PageMeta['layout'] ? MaybeRef<string | false> : PageMeta['layout'] | MaybeRef<false>,
       default: null
     }
   },
@@ -48,12 +42,9 @@ export default defineComponent({
     const nuxtApp = useNuxtApp()
     // Need to ensure (if we are not a child of `<NuxtPage>`) that we use synchronous route (not deferred)
     const injectedRoute = inject(PageRouteSymbol)
-    const route =
-      injectedRoute === useRoute() ? useVueRouterRoute() : injectedRoute
+    const route = injectedRoute === useRoute() ? useVueRouterRoute() : injectedRoute
 
-    const layout = computed(
-      () => unref(props.name) ?? (route.meta.layout as string) ?? 'default'
-    )
+    const layout = computed(() => unref(props.name) ?? route.meta.layout as string ?? 'default')
 
     const layoutRef = ref()
     context.expose({ layoutRef })
@@ -62,45 +53,26 @@ export default defineComponent({
 
     return () => {
       const hasLayout = layout.value && layout.value in layouts
-      if (
-        process.dev &&
-        layout.value &&
-        !hasLayout &&
-        layout.value !== 'default'
-      ) {
+      if (process.dev && layout.value && !hasLayout && layout.value !== 'default') {
         console.warn(`Invalid layout \`${layout.value}\` selected.`)
       }
 
-      const transitionProps =
-        route.meta.layoutTransition ?? defaultLayoutTransition
+      const transitionProps = route.meta.layoutTransition ?? defaultLayoutTransition
 
       // We avoid rendering layout transition if there is no layout to render
       return _wrapIf(Transition, hasLayout && transitionProps, {
-        default: () =>
-          h(
-            Suspense,
+        default: () => h(Suspense, { suspensible: true, onResolve: () => { nextTick(done) } }, {
+          default: () => h(
+            // @ts-expect-error seems to be an issue in vue types
+            LayoutProvider,
             {
-              suspensible: true,
-              onResolve: () => {
-                nextTick(done)
-              }
-            },
-            {
-              default: () =>
-                h(
-                  // @ts-expect-error seems to be an issue in vue types
-                  LayoutProvider,
-                  {
-                    layoutProps: mergeProps(context.attrs, { ref: layoutRef }),
-                    key: layout.value,
-                    name: layout.value,
-                    shouldProvide: !props.name,
-                    hasTransition: !!transitionProps
-                  },
-                  context.slots
-                )
-            }
-          )
+              layoutProps: mergeProps(context.attrs, { ref: layoutRef }),
+              key: layout.value,
+              name: layout.value,
+              shouldProvide: !props.name,
+              hasTransition: !!transitionProps
+            }, context.slots)
+        })
       }).default()
     }
   }
