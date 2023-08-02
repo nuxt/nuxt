2501227c411d4f13c1d19f87b673666fdcb3148a
Harlan Wilton <harlan@harlanzw.com>
2023-08-03 00:51:09 +0300

    fix: enable capo tracking

--- packages/nuxt/src/head/runtime/plugins/capo.ts
+++ packages/nuxt/src/head/runtime/plugins/capo.ts
@@ -4,6 +4,6 @@ import { defineNuxtPlugin } from '#app/nuxt'
 export default defineNuxtPlugin({
   name: 'nuxt:head:capo',
   setup (nuxtApp) {
-    nuxtApp.vueApp._context.provides.usehead.use(CapoPlugin())
+    nuxtApp.vueApp._context.provides.usehead.use(CapoPlugin({ track: true }))
   }
 })
