9ee7aa67a835df48ed5cf3937a8b3cfe26b277d9
Bogdan Kostyuk <bogdankostyuk12@gmail.com>
2023-08-02 22:49:26 +0300

    refactor: remove `previewMode` from variable names

--- packages/nuxt/src/app/composables/preview.ts
+++ packages/nuxt/src/app/composables/preview.ts
@@ -3,77 +3,77 @@ import { defu } from 'defu';
 
 import { refreshNuxtData, useRoute, useRouter, useState } from '#app'
 
-interface PreviewState {
+interface Preview {
   enabled: boolean
   state: Record<any, unknown>
   addedAfterNavigationCallback: boolean
 }
 
-type GetPreviewStateFunc = (currentState: PreviewState['state']) => Record<any, unknown> | null | undefined | void;
-type ShouldEnablePreviewMode = (currentState: PreviewState['state']) => boolean | null | undefined | void;
+type GetStateFunc = (currentState: Preview['state']) => Record<any, unknown> | null | undefined | void;
+type ShouldEnableFunc = (currentState: Preview['state']) => boolean | null | undefined | void;
 
-let shouldEnablePreviewMode: ShouldEnablePreviewMode = () => {
+let shouldEnablePreviewMode: ShouldEnableFunc = () => {
   const route = useRoute();
   const previewQueryName = 'preview'
 
   return route.query[previewQueryName] === 'true'
 }
 
-export function usePreviewMode<Controls extends boolean = false, GetPreviewState extends GetPreviewStateFunc = GetPreviewStateFunc> (options?: {
+export function usePreviewMode<Controls extends boolean = false, GetPreviewState extends GetStateFunc = GetStateFunc> (options?: {
   controls?: Controls,
-  getPreviewState?: GetPreviewState
-  shouldEnablePreviewMode?: ShouldEnablePreviewMode;
+  getState?: GetPreviewState
+  shouldEnable?: ShouldEnableFunc;
 }) {
   const normalizedOptions = defu(options, {
     controls: false,
-    getPreviewState: (state: PreviewState['state']) => {
+    getState: (state: Preview['state']) => {
       const route = useRoute()
       const token = state.token 
         ?? (Array.isArray(route.query.token) ? route.query.token[0] : route.query.token);
 
-      return { token: token as string } as PreviewState['state']
+      return { token: token as string } as Preview['state']
     },
   })
 
   const router = useRouter()
 
-  const previewState = useState('_preview-composable', () => shallowReactive<PreviewState>({
+  const preview = useState('_preview-composable', () => shallowReactive<Preview>({
     enabled: false,
     state: reactive({}),
     addedAfterNavigationCallback: false,
   }))
 
-  if (normalizedOptions.shouldEnablePreviewMode) {
-    shouldEnablePreviewMode = normalizedOptions.shouldEnablePreviewMode
+  if (normalizedOptions.shouldEnable) {
+    shouldEnablePreviewMode = normalizedOptions.shouldEnable
   }
   
-  if (!previewState.value.enabled) {
-    const result = shouldEnablePreviewMode(previewState.value.state);
+  if (!preview.value.enabled) {
+    const result = shouldEnablePreviewMode(preview.value.state);
 
     if (typeof result === 'boolean')
-      previewState.value.enabled = result
+      preview.value.enabled = result
   }
 
-  if (previewState.value.enabled) {
-    const newState = normalizedOptions.getPreviewState(previewState.value.state)
+  if (preview.value.enabled) {
+    const newState = normalizedOptions.getState(preview.value.state)
 
     if (newState) {
-      Object.assign(previewState.value.state, newState)
+      Object.assign(preview.value.state, newState)
     }
   }
 
   const refreshData = () => {
-    previewState.value.addedAfterNavigationCallback = true
+    preview.value.addedAfterNavigationCallback = true
     refreshNuxtData()
   }
 
-  if (previewState.value.enabled && !previewState.value.addedAfterNavigationCallback && process.client) {
+  if (preview.value.enabled && !preview.value.addedAfterNavigationCallback && process.client) {
     refreshData()
     router.afterEach(refreshData)
   }
 
-  const enabled = readonly(toRef(previewState.value, 'enabled'))
-  const state = previewState.value.state as NonNullable<ReturnType<GetPreviewState>>
+  const enabled = readonly(toRef(preview.value, 'enabled'))
+  const state = preview.value.state as NonNullable<ReturnType<GetPreviewState>>
 
   return (normalizedOptions.controls
     ? { enabled, state }
