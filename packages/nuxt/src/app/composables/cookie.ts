66c29da81a0c85ddb87c26ab71d2df2ba8b27639
Daniel Roe <daniel@roe.dev>
2023-08-03 17:10:30 +0100

    chore: remove args

--- packages/nuxt/src/app/composables/cookie.ts
+++ packages/nuxt/src/app/composables/cookie.ts
@@ -53,7 +53,7 @@ export function useCookie<T = string | null | undefined> (name: string, _opts?: 
     }
 
     if (opts.watch) {
-      watch(cookie, (newVal, oldVal) => {
+      watch(cookie, () => {
         if (watchPaused) { return }
         callback()
       },
