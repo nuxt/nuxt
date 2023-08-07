01f0984d701bb8d84ea6fb095bfbdfd4d2af5c67
Lehoczky Zoltán <zoltan.lehoczky@betssongroup.com>
2023-08-07 16:09:41 +0200

    feat: check for additional external config files

--- packages/nuxt/src/core/builder.ts
+++ packages/nuxt/src/core/builder.ts
@@ -7,8 +7,8 @@ import { interopDefault } from 'mlly'
 import { debounce } from 'perfect-debounce'
 import { normalize, relative, resolve } from 'pathe'
 import type { Nuxt } from 'nuxt/schema'
-
 import { generateApp as _generateApp, createApp } from './app'
+import { checkForExternalConfigurationFiles } from './external-config-files'
 
 export async function build (nuxt: Nuxt) {
   const app = createApp(nuxt)
@@ -43,7 +43,7 @@ export async function build (nuxt: Nuxt) {
 
   await nuxt.callHook('build:before')
   if (!nuxt.options._prepare) {
-    await bundle(nuxt)
+    await Promise.all([checkForExternalConfigurationFiles(), bundle(nuxt)])
     await nuxt.callHook('build:done')
   }
 
