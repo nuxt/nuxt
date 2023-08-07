3c8a3517202c598764b1c65e56cf62e31a32bb8b
Daniel Roe <daniel@roe.dev>
2023-08-07 14:38:16 +0000

    fix: resolve aliases and skip `/__skip_vite`

--- packages/nuxt/src/core/plugins/resolve-deep-imports.ts
+++ packages/nuxt/src/core/plugins/resolve-deep-imports.ts
@@ -1,7 +1,9 @@
 import { parseNodeModulePath, resolvePath } from 'mlly'
 import { isAbsolute } from 'pathe'
 import type { Plugin } from 'vite'
+import { resolveAlias } from '@nuxt/kit'
 import type { Nuxt } from '@nuxt/schema'
+
 import { pkgDir } from '../../dirs'
 
 export function resolveDeepImportsPlugin (nuxt: Nuxt): Plugin {
@@ -9,9 +11,10 @@ export function resolveDeepImportsPlugin (nuxt: Nuxt): Plugin {
     name: 'nuxt:resolve-bare-imports',
     enforce: 'post',
     async resolveId (id, importer, options) {
-      if (!importer || isAbsolute(id) || !isAbsolute(importer) || id.startsWith('virtual:')) {
+      if (!importer || isAbsolute(id) || !isAbsolute(importer) || id.startsWith('virtual:') || id.startsWith('/__skip_vite')) {
         return
       }
+      id = resolveAlias(id, nuxt.options.alias)
       const { dir } = parseNodeModulePath(importer)
       return await this.resolve?.(id, dir || pkgDir, { skipSelf: true }) ?? await resolvePath(id, {
         url: [dir || pkgDir, ...nuxt.options.modulesDir],
