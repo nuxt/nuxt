4565828275cb2a44bb9db0e64fff2dc40f78501b
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-08-08 22:15:18 +0200

    chore: change indentation from 4 spaces to 2

--- packages/schema/src/types/nuxt.ts
+++ packages/schema/src/types/nuxt.ts
@@ -5,54 +5,54 @@ import type { Component } from './components'
 import type { NuxtOptions } from './config'
 
 export interface NuxtPlugin {
-    /** @deprecated use mode */
-    ssr?: boolean
-    src: string
-    mode?: 'all' | 'server' | 'client'
-    /**
-     * This allows more granular control over plugin order and should only be used by advanced users.
-     * Lower numbers run first, and user plugins default to `0`.
-     *
-     * Default Nuxt priorities can be seen at [here](https://github.com/nuxt/nuxt/blob/9904849bc87c53dfbd3ea3528140a5684c63c8d8/packages/nuxt/src/core/plugins/plugin-metadata.ts#L15-L34).
-     */
-    order?: number
+  /** @deprecated use mode */
+  ssr?: boolean
+  src: string
+  mode?: 'all' | 'server' | 'client'
+  /**
+   * This allows more granular control over plugin order and should only be used by advanced users.
+   * Lower numbers run first, and user plugins default to `0`.
+   *
+   * Default Nuxt priorities can be seen at [here](https://github.com/nuxt/nuxt/blob/9904849bc87c53dfbd3ea3528140a5684c63c8d8/packages/nuxt/src/core/plugins/plugin-metadata.ts#L15-L34).
+   */
+  order?: number
 }
 
 export interface NuxtTemplate<Options = Record<string, any>> {
-    /** resolved output file path (generated) */
-    dst?: string
-    /** The target filename once the template is copied into the Nuxt buildDir */
-    filename?: string
-    /** An options object that will be accessible within the template via `<% options %>` */
-    options?: Options
-    /** The resolved path to the source file to be template */
-    src?: string
-    /** Provided compile option instead of src */
-    getContents?: (data: Options) => string | Promise<string>
-    /** Write to filesystem */
-    write?: boolean
+  /** resolved output file path (generated) */
+  dst?: string
+  /** The target filename once the template is copied into the Nuxt buildDir */
+  filename?: string
+  /** An options object that will be accessible within the template via `<% options %>` */
+  options?: Options
+  /** The resolved path to the source file to be template */
+  src?: string
+  /** Provided compile option instead of src */
+  getContents?: (data: Options) => string | Promise<string>
+  /** Write to filesystem */
+  write?: boolean
 }
 
 export interface ResolvedNuxtTemplate<Options = Record<string, any>> extends NuxtTemplate<Options> {
-    filename: string
-    dst: string
+  filename: string
+  dst: string
 }
 
 type _TemplatePlugin<Options> = Omit<NuxtPlugin, 'src'> & NuxtTemplate<Options>
 export interface NuxtPluginTemplate<Options = Record<string, any>> extends _TemplatePlugin<Options> { }
 
 export interface NuxtApp {
-    mainComponent?: string | null
-    rootComponent?: string | null
-    errorComponent?: string | null
-    dir: string
-    extensions: string[]
-    plugins: NuxtPlugin[]
-    components: Component[]
-    layouts: Record<string, NuxtLayout>
-    middleware: NuxtMiddleware[]
-    templates: NuxtTemplate[]
-    configs: string[]
+  mainComponent?: string | null
+  rootComponent?: string | null
+  errorComponent?: string | null
+  dir: string
+  extensions: string[]
+  plugins: NuxtPlugin[]
+  components: Component[]
+  layouts: Record<string, NuxtLayout>
+  middleware: NuxtMiddleware[]
+  templates: NuxtTemplate[]
+  configs: string[]
 }
 
 export interface Nuxt {
