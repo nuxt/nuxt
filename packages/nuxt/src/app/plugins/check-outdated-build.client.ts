198daf68a7b55edb6cbe4002b13fd753b8b28353
Daniel Roe <daniel@roe.dev>
2023-08-18 15:30:54 +0200

    fix: update url

--- packages/nuxt/src/app/plugins/check-outdated-build.client.ts
+++ packages/nuxt/src/app/plugins/check-outdated-build.client.ts
@@ -10,7 +10,7 @@ export default defineNuxtPlugin((nuxtApp) => {
     const currentManifest = await getAppManifest()
     if (timeout) { clearTimeout(timeout) }
     timeout = setTimeout(getLatestManifest, 1000 * 60 * 60)
-    const meta = await $fetch<NuxtAppManifestMeta>(joinURL(config.app.cdnURL || config.app.baseURL, '_builds/latest.json'))
+    const meta = await $fetch<NuxtAppManifestMeta>(joinURL(config.app.cdnURL || config.app.baseURL, config.app.buildAssetsDir, 'builds/latest.json'))
     if (meta.id !== currentManifest.id) {
       // There is a newer build which we will let the user handle
       nuxtApp.hooks.callHook('app:manifest:update', meta)
