716b291421dc637be1f7f8eceb714517858227e9
julien huang <julien.huang@outlook.fr>
2023-08-19 14:24:31 +0200

    test: remove .only

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1650,7 +1650,7 @@ describe('app config', () => {
   })
 })
 
-describe.only('component islands', () => {
+describe('component islands', () => {
   it('renders components with route', async () => {
     const result: NuxtIslandResponse = await $fetch('/__nuxt_island/RouteComponent?url=/foo')
 
