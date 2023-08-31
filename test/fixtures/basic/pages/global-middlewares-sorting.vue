9e88b29bacd750d1d02f89789e203df242e23a07
userquin <userquin@gmail.com>
2023-08-31 15:04:56 +0200

    chore: update length error message

--- test/fixtures/basic/pages/global-middlewares-sorting.vue
+++ test/fixtures/basic/pages/global-middlewares-sorting.vue
@@ -2,11 +2,17 @@
 definePageMeta({
   middleware: [defineNuxtRouteMiddleware((to) => {
     const globalMiddleware: string[] | undefined = to.meta.globalMiddleware
-    if (!globalMiddleware?.length) { return createError('Missing to.meta.globalMiddleware') }
+    if (!globalMiddleware?.length) {
+      return createError('Missing to.meta.globalMiddleware')
+    }
 
-    if (globalMiddleware.length !== 4) { return createError('Missing to.meta.globalMiddleware') }
+    if (globalMiddleware.length !== 4) {
+      return createError(`Missmatch to.meta.globalMiddleware length, expected 4 entries and found ${globalMiddleware.length}`)
+    }
 
-    if (!globalMiddleware.every((m, idx) => (idx === 0 && m === '01.a.global') || (idx === 1 && m === '01.b.global') || (idx === 2 && m === 'a.global') || (idx === 3 && m === 'b.global'))) { return createError('Missmatch to.meta.globalMiddleware sorting order') }
+    if (!globalMiddleware.every((m, idx) => (idx === 0 && m === '01.a.global') || (idx === 1 && m === '01.b.global') || (idx === 2 && m === 'a.global') || (idx === 3 && m === 'b.global'))) {
+      return createError('Missmatch to.meta.globalMiddleware sorting order')
+    }
   })]
 })
 </script>
