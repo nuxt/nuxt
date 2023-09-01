05f8a290600be42c58c332b86d925012b4e8b0a2
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-01 16:53:35 +0200

    chore: improve prepare script

--- test/prepare.ts
+++ test/prepare.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import fg from 'fast-glob'
+import fs from 'fs-extra'
 import { execa } from 'execa'
 
 const dirs = await fg('*', {
@@ -7,6 +8,13 @@ const dirs = await fg('*', {
   cwd: fileURLToPath(new URL('./fixtures', import.meta.url)),
   absolute: true
 })
+
+await Promise.all([
+  // clear nuxt build files
+  ...dirs.map(dir => fs.remove(`${dir}/.nuxt`)),
+  // clear vite cache
+  ...dirs.map(dir => fs.remove(`${dir}/node_modules/.vite`), { force: true })
+])
 
 await Promise.all(
   dirs.map(dir => execa('pnpm', ['nuxi', 'prepare'], { cwd: dir }))
