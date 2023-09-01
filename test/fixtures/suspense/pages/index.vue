4a915ef64f2ee2b111383825239cde8b00c4b7ea
Anthony Fu <anthonyfu117@hotmail.com>
2023-09-01 15:50:16 +0200

    chore: patch

--- test/fixtures/suspense/pages/index.vue
+++ test/fixtures/suspense/pages/index.vue
@@ -7,5 +7,16 @@
     <NuxtLink id="btn-b" to="/target?b">
       Target B
     </NuxtLink>
+
+    <button @click="trigger">
+      Trigger (for manual testing)
+    </button>
   </div>
 </template>
+
+<script setup>
+function trigger () {
+  document.getElementById('btn-a').click()
+  document.getElementById('btn-b').click()
+}
+</script>
