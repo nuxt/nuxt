9ce720af062312fe53a71c90f6f955ee350e9f2f
julien huang <julien.huang@outlook.fr>
2023-09-08 23:13:06 +0200

    refactor: move to NuxtIsland

--- packages/nuxt/src/components/runtime/server-component.ts
+++ packages/nuxt/src/components/runtime/server-component.ts
@@ -7,12 +7,10 @@ export const createServerComponent = (name: string) => {
     inheritAttrs: false,
     props: { lazy: Boolean },
     setup (props, { attrs, slots }) {
-      // #23051
-      const attrsWithoutVueDataAttr = Object.entries(attrs).reduce<Record<string, string>>((acc, [key, value]) => key.startsWith('data-v-') ? acc : Object.assign(acc, { [key]: value }), {})
       return () => h(NuxtIsland, {
         name,
         lazy: props.lazy,
-        props: attrsWithoutVueDataAttr
+        props: attrs
       }, slots)
     }
   })
