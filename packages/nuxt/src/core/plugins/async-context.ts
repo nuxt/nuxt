df401d01c53ee804bcd650ba5a6d0c7bc14779de
Pooya Parsa <pooya@pi0.io>
2023-10-04 16:54:27 +0200

    refactor: avoid vfs due to unplugin/webpack issues

--- packages/nuxt/src/core/plugins/async-context.ts
+++ packages/nuxt/src/core/plugins/async-context.ts
@@ -4,37 +4,17 @@ import type { Nuxt } from '@nuxt/schema'
 import { isVue } from '../utils'
 
 export const AsyncContextInjectionPlugin = (nuxt: Nuxt) => createUnplugin(() => {
-  const virtualFileId = '\0vue-async-context'
   return {
     name: 'nuxt:vue-async-context',
     transformInclude (id) {
       return isVue(id, { type: ['template', 'script'] })
-    },
-    resolveId (id) {
-      if (id === virtualFileId) {
-        return id
-      }
-    },
-    load (id) {
-      if (id !== virtualFileId) {
-        return
-      }
-      return `
-        import { withAsyncContext as withVueAsyncContext, getCurrentInstance } from 'vue'
-        export function withAsyncContext(fn) {
-          return withVueAsyncContext(() => {
-            const nuxtApp = getCurrentInstance()?.appContext.app.$nuxt
-            return nuxtApp ? nuxtApp.runWithContext(fn) : fn()
-          })
-        }
-      `.trim()
     },
     transform (code) {
       if (!code.includes('_withAsyncContext')) {
         return
       }
       const s = new MagicString(code)
-      s.prepend(`import { withAsyncContext as _withAsyncContext } from "${virtualFileId}";\n`)
+      s.prepend('import { withAsyncContext as _withAsyncContext } from "#app/composables/asyncContext";\n')
       s.replace(/withAsyncContext as _withAsyncContext,?/, '')
       if (s.hasChanged()) {
         return {
