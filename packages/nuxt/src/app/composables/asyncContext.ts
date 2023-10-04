318e2cb59ca2f6ce32223c7b2078064514b965b5
Pooya Parsa <pooya@pi0.io>
2023-10-04 14:12:14 +0200

    refactor: self-contained impl

--- packages/nuxt/src/app/composables/asyncContext.ts
+++
@@ -1,8 +0,0 @@
-// @ts-expect-error Private Vue API
-import { withAsyncContext as withVueAsyncContext } from 'vue'
-
-import { useNuxtApp } from '#app/nuxt'
-
-export function withAsyncContext (fn: () => PromiseLike<unknown>) {
-  return withVueAsyncContext(() => useNuxtApp().runWithContext(fn))
-}
--- packages/nuxt/src/core/plugins/async-context.ts
+++ packages/nuxt/src/core/plugins/async-context.ts
@@ -3,19 +3,34 @@ import type { Nuxt } from '@nuxt/schema'
 import { isVue } from '../utils'
 
 export const AsyncContextInjectionPlugin = (_nuxt: Nuxt) => createUnplugin(() => {
+  const virtualFileId = '\0vue-async-context'
   return {
     name: 'nuxt:vue-async-context',
     transformInclude (id) {
       return isVue(id, { type: ['template', 'script'] })
     },
-    transform (code) {
-      if (!code.includes('_withAsyncContext')) {
+    resolveId (id) {
+      if (id === virtualFileId) {
+        return id
+      }
+    },
+    load (id) {
+      if (id !== virtualFileId) {
         return
       }
-      // TODO: Use parser
+      return `
+        import { withAsyncContext as withVueAsyncContext } from 'vue'
+        import { useNuxtApp } from '#app'
+        export function withAsyncContext(fn) {
+          return withVueAsyncContext(() => useNuxtApp().runWithContext(fn))
+        }
+      `.trim()
+    },
+    transform (code) {
+      // TODO
       const newCode = code.replace(
-        'import { withAsyncContext as _withAsyncContext } from \'vue\'',
-        'import { withAsyncContext as _withAsyncContext } from \'#app\''
+        "import { withAsyncContext as _withAsyncContext } from 'vue'",
+        `import { withAsyncContext as _withAsyncContext } from "${virtualFileId}"`
       )
       if (code === newCode) {
         return
