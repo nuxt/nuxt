de32acef6976e0bee683cd0fb7c79e338370e68c
Pooya Parsa <pooya@pi0.io>
2023-10-04 14:51:04 +0200

    safer impl

--- packages/nuxt/src/core/plugins/async-context.ts
+++ packages/nuxt/src/core/plugins/async-context.ts
@@ -22,21 +22,22 @@ export const AsyncContextInjectionPlugin = (_nuxt: Nuxt) => createUnplugin(() =>
         import { withAsyncContext as withVueAsyncContext } from 'vue'
         import { useNuxtApp } from '#app'
         export function withAsyncContext(fn) {
-          return withVueAsyncContext(() => useNuxtApp().runWithContext(fn))
+          return withVueAsyncContext(() => {
+            let nuxtApp
+            try { nuxtApp = useNuxtApp() } catch {}
+            return nuxtApp ? nuxtApp.runWithContext(fn) : fn()
+          })
         }
       `.trim()
     },
     transform (code) {
-      // TODO
-      const newCode = code.replace(
-        "import { withAsyncContext as _withAsyncContext } from 'vue'",
-        `import { withAsyncContext as _withAsyncContext } from "${virtualFileId}"`
-      )
-      if (code === newCode) {
+      if (!code.includes('_withAsyncContext')) {
         return
       }
+      // TODO
+      code = `import { withAsyncContext as _withAsyncContext } from "${virtualFileId}";${code.replace(/withAsyncContext as _withAsyncContext,?/g, '')}`
       return {
-        code: newCode,
+        code,
         map: null
       }
     }
