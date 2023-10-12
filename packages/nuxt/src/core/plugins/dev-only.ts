b011d8cb1578a2387d58a35dc0508653994db91b
Daniel Roe <daniel@roe.dev>
2023-10-12 13:09:26 +0100

    fix: correctly handle macro queries in `isVue` utils

--- packages/nuxt/src/core/plugins/dev-only.ts
+++ packages/nuxt/src/core/plugins/dev-only.ts
@@ -1,29 +1,22 @@
-import { pathToFileURL } from 'node:url'
 import { stripLiteral } from 'strip-literal'
-import { parseQuery, parseURL } from 'ufo'
 import MagicString from 'magic-string'
 import { createUnplugin } from 'unplugin'
 import { type Node, parse } from 'ultrahtml'
+import { isVue } from '../utils'
 
 interface DevOnlyPluginOptions {
   sourcemap?: boolean
 }
 
-const DEVONLY_COMP_SINGLE_RE = /<(?:dev-only|DevOnly)>[\s\S]*?<\/(?:dev-only|DevOnly)>/
+const DEVONLY_COMP_SINGLE_RE = /<(?:dev-only|DevOnly|lazy-dev-only|LazyDevOnly)>[\s\S]*?<\/(?:dev-only|DevOnly|lazy-dev-only|LazyDevOnly)>/
 const DEVONLY_COMP_RE = /<(?:dev-only|DevOnly|lazy-dev-only|LazyDevOnly)>[\s\S]*?<\/(?:dev-only|DevOnly|lazy-dev-only|LazyDevOnly)>/g
 
-export const DevOnlyPlugin = createUnplugin((options: DevOnlyPluginOptions, meta) => {
+export const DevOnlyPlugin = createUnplugin((options: DevOnlyPluginOptions) => {
   return {
     name: 'nuxt:server-devonly:transform',
     enforce: 'pre',
     transformInclude (id) {
-      const { pathname, search } = parseURL(decodeURIComponent(pathToFileURL(id).href))
-      const { type } = parseQuery(search)
-
-      // vue files
-      if (pathname.endsWith('.vue') && (meta.framework === 'webpack' || type === 'template' || !search)) {
-        return true
-      }
+      return isVue(id, { type: ['template'] })
     },
     transform (code) {
       if (!DEVONLY_COMP_SINGLE_RE.test(code)) { return }
@@ -33,7 +26,7 @@ export const DevOnlyPlugin = createUnplugin((options: DevOnlyPluginOptions, meta
       for (const match of strippedCode.matchAll(DEVONLY_COMP_RE) || []) {
         const ast: Node = parse(match[0]).children[0]
         const fallback: Node | undefined = ast.children?.find((n: Node) => n.name === 'template' && Object.values(n.attributes).includes('#fallback'))
-        const replacement = fallback ? match[0].slice(fallback.loc[0].end, fallback.loc[fallback.loc.length - 1].start).trim() : ''
+        const replacement = fallback ? match[0].slice(fallback.loc[0].end, fallback.loc[fallback.loc.length - 1].start) : ''
 
         s.overwrite(match.index!, match.index! + match[0].length, replacement)
       }
