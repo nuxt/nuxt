1b647c4ee619797a18dde7c101cd8cc8efd84784
Daniel Roe <daniel@roe.dev>
2023-10-20 23:45:49 +0100

    refactor(nuxt): use paths helpers to resolve manifest url

--- packages/nuxt/src/app/composables/manifest.ts
+++ packages/nuxt/src/app/composables/manifest.ts
@@ -1,10 +1,11 @@
-import { joinURL } from 'ufo'
 import type { MatcherExport, RouteMatcher } from 'radix3'
 import { createMatcherFromExport } from 'radix3'
 import { defu } from 'defu'
-import { useAppConfig, useRuntimeConfig } from '#app'
+import { useAppConfig } from '#app'
 // @ts-expect-error virtual file
 import { appManifest as isAppManifestEnabled } from '#build/nuxt.config.mjs'
+// @ts-expect-error virtual file
+import { buildAssetsURL } from '#build/paths.mjs'
 
 export interface NuxtAppManifestMeta {
   id: string
@@ -23,10 +24,9 @@ function fetchManifest () {
   if (!isAppManifestEnabled) {
     throw new Error('[nuxt] app manifest should be enabled with `experimental.appManifest`')
   }
-  const config = useRuntimeConfig()
   // @ts-expect-error private property
   const buildId = useAppConfig().nuxt?.buildId
-  manifest = $fetch<NuxtAppManifest>(joinURL(config.app.cdnURL || config.app.baseURL, config.app.buildAssetsDir, `builds/meta/${buildId}.json`))
+  manifest = $fetch<NuxtAppManifest>(buildAssetsURL(`builds/meta/${buildId}.json`))
   manifest.then((m) => {
     matcher = createMatcherFromExport(m.matcher)
   })
