2b17095f239201f3c3c107d954f07ead5b4bf46c
Daniel Roe <daniel@roe.dev>
2023-10-20 16:58:02 +0100

    fix(nuxt): use `.json` extension for server components (#23802)

--- packages/nuxt/src/core/runtime/nitro/renderer.ts
+++ packages/nuxt/src/core/runtime/nitro/renderer.ts
@@ -176,7 +176,7 @@ async function getIslandContext (event: H3Event): Promise<NuxtIslandContext> {
     url = await islandPropCache!.getItem(event.path) as string
   }
   url = url.substring('/__nuxt_island'.length + 1) || ''
-  const [componentName, hashId] = url.split('?')[0].split('_')
+  const [componentName, hashId] = url.split('?')[0].replace(/\.json$/, '').split('_')
 
   // TODO: Validate context
   const context = event.method === 'GET' ? getQuery(event) : await readBody(event)
@@ -445,8 +445,8 @@ export default defineRenderHandler(async (event): Promise<Partial<RenderResponse
       }
     } satisfies RenderResponse
     if (import.meta.prerender) {
-      await islandCache!.setItem(`/__nuxt_island/${islandContext!.name}_${islandContext!.id}`, response)
-      await islandPropCache!.setItem(`/__nuxt_island/${islandContext!.name}_${islandContext!.id}`, event.path)
+      await islandCache!.setItem(`/__nuxt_island/${islandContext!.name}_${islandContext!.id}.json`, response)
+      await islandPropCache!.setItem(`/__nuxt_island/${islandContext!.name}_${islandContext!.id}.json`, event.path)
     }
     return response
   }
