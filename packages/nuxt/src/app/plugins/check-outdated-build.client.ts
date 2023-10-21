cfe13a2c9cea633aff1fb4b64c15555ba4fc9103
Daniel Roe <daniel@roe.dev>
2023-10-21 19:17:53 +0100

    fix(nuxt): handle relative baseURL in nitro runtime config (#23841)

--- packages/nuxt/src/app/plugins/check-outdated-build.client.ts
+++ packages/nuxt/src/app/plugins/check-outdated-build.client.ts
@@ -1,18 +1,18 @@
-import { joinURL } from 'ufo'
 import type { NuxtAppManifestMeta } from '#app'
-import { defineNuxtPlugin, getAppManifest, onNuxtReady, useRuntimeConfig } from '#app'
+import { defineNuxtPlugin, getAppManifest, onNuxtReady } from '#app'
+// @ts-expect-error virtual file
+import { buildAssetsURL } from '#build/paths.mjs'
 
 export default defineNuxtPlugin((nuxtApp) => {
   if (import.meta.test) { return }
 
   let timeout: NodeJS.Timeout
-  const config = useRuntimeConfig()
 
   async function getLatestManifest () {
     const currentManifest = await getAppManifest()
     if (timeout) { clearTimeout(timeout) }
     timeout = setTimeout(getLatestManifest, 1000 * 60 * 60)
-    const meta = await $fetch<NuxtAppManifestMeta>(joinURL(config.app.cdnURL || config.app.baseURL, config.app.buildAssetsDir, 'builds/latest.json'))
+    const meta = await $fetch<NuxtAppManifestMeta>(buildAssetsURL('builds/latest.json'))
     if (meta.id !== currentManifest.id) {
       // There is a newer build which we will let the user handle
       nuxtApp.hooks.callHook('app:manifest:update', meta)
