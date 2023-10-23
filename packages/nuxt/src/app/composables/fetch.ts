ad439832a11e22038b7d7efcaa3c0e6f0ccf8d95
Daniel Roe <daniel@roe.dev>
2023-10-23 21:01:54 +0900

    refactor: make code more readable

--- packages/nuxt/src/app/composables/fetch.ts
+++ packages/nuxt/src/app/composables/fetch.ts
@@ -140,9 +140,15 @@ export function useFetch<
     controller?.abort?.()
     controller = typeof AbortController !== 'undefined' ? new AbortController() : {} as AbortController
 
-    const isLocalFetch = typeof _request.value === 'string' && _request.value.startsWith('/') && (!unref(opts.baseURL) || unref(opts.baseURL)!.startsWith('/'))
+    let _$fetch = opts.$fetch || globalThis.$fetch
+
     // Use fetch with request context and headers for server direct API calls
-    const _$fetch = opts.$fetch || ((import.meta.server && isLocalFetch) ? useRequestFetch() : globalThis.$fetch)
+    if (import.meta.server && !opts.$fetch) {
+      const isLocalFetch = typeof _request.value === 'string' && _request.value.startsWith('/') && (!unref(opts.baseURL) || unref(opts.baseURL)!.startsWith('/'))
+      if (isLocalFetch) {
+        _$fetch = useRequestFetch()
+      }
+    }
 
     return _$fetch(_request.value, { signal: controller.signal, ..._fetchOptions } as any) as Promise<_ResT>
   }, _asyncDataOptions)
