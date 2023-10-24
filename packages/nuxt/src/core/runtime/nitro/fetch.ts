f31d5eb0b45de36d5d5442e71d3019a05e0b377f
Anthony Fu <anthonyfu117@hotmail.com>
2023-10-24 11:40:55 +0800

    chore: update

--- packages/nuxt/src/core/runtime/nitro/fetch.ts
+++
@@ -1,9 +0,0 @@
-import { $fetch } from 'ofetch'
-import type { $Fetch, NitroFetchRequest } from 'nitropack'
-import { baseURL } from './paths'
-
-if (!globalThis.$fetch) {
-  globalThis.$fetch = $fetch.create({
-    baseURL: baseURL()
-  }) as $Fetch<unknown, NitroFetchRequest>
-}
