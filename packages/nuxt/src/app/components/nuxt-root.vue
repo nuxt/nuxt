27791f4c3350e2baa1a55c7be71bfbc02a7662b5
Daniel Roe <daniel@roe.dev>
2023-10-30 22:04:45 +0100

    perf(nuxt): remove pure annotations plugin (#24033)

--- packages/nuxt/src/app/components/nuxt-root.vue
+++ packages/nuxt/src/app/components/nuxt-root.vue
@@ -24,7 +24,7 @@ const nuxtApp = useNuxtApp()
 const onResolve = nuxtApp.deferHydration()
 
 const url = import.meta.server ? nuxtApp.ssrContext.url : window.location.pathname
-const SingleRenderer = import.meta.test && import.meta.dev && import.meta.server && url.startsWith('/__nuxt_component_test__/') && /* #__PURE__ */ defineAsyncComponent(() => import('#build/test-component-wrapper.mjs')
+const SingleRenderer = import.meta.test && import.meta.dev && import.meta.server && url.startsWith('/__nuxt_component_test__/') && defineAsyncComponent(() => import('#build/test-component-wrapper.mjs')
   .then(r => r.default(import.meta.server ? url : window.location.href)))
 
 // Inject default route (outside of pages) as active route
