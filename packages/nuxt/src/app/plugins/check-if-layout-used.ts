61433b55d619b4e63c5b2799808ace5a389bba94
Luke Nelson <luke@nelson.zone>
2023-11-04 17:02:17 +0000

    dx: check for number of layouts in src directory

--- packages/nuxt/src/app/plugins/check-if-layout-used.ts
+++ packages/nuxt/src/app/plugins/check-if-layout-used.ts
@@ -1,13 +1,21 @@
 import { defineNuxtPlugin } from '../nuxt'
 import { onNuxtReady } from '../composables/ready'
 
+import layouts from '#build/layouts'
+
 export default defineNuxtPlugin({
   name: 'nuxt:checkIfLayoutUsed',
   enforce: 'post',
   setup (nuxtApp) {
+    const projectHasLayouts:()=>boolean = () => {
+      const numLayouts = Object.keys(layouts).length
+      // TODO: Also check layers
+      return numLayouts > 0
+    }
+
     onNuxtReady(() => {
-      const hasLayouts = false // TODO:
-      if (!nuxtApp.payload.isNuxtLayoutUsed && hasLayouts) {
+      if (!nuxtApp.payload.isNuxtLayoutUsed && projectHasLayouts()) {
+        // TODO: Use logger
         console.log('Your project has layouts but the <NuxtLayout /> component has not been used.')
       }
     })
