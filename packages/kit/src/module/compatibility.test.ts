fcb12afd5911dc01b3731bd7a9d6c4b86fdb3a82
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 23:13:17 +0100

    refactor: update `packages/kit/src/module/compatibility.test.ts`

--- packages/kit/src/module/compatibility.test.ts
+++ packages/kit/src/module/compatibility.test.ts
@@ -26,31 +26,43 @@ describe('nuxt module compatibility', () => {
         ]
       }
     })
+
     expect(hasNuxtModule('nuxt-module-foo', nuxt)).toStrictEqual(true)
+
     expect(hasNuxtModule('module-instance-with-options', nuxt)).toStrictEqual(true)
+
     await nuxt.close()
   })
+
   it('can retrieve module version from module instance', async () => {
     const nuxt = await loadNuxt({})
+
     const module = defineNuxtModule({
       meta: {
         name: 'nuxt-module-foo',
         version: '1.0.0'
       }
     })
+
     expect(await getNuxtModuleVersion(module, nuxt)).toEqual('1.0.0')
+
     await nuxt.close()
   })
+
   it('check module instance version compatibility', async () => {
     const nuxt = await loadNuxt({})
+
     const module = defineNuxtModule({
       meta: {
         name: 'nuxt-module-foo',
         version: '1.0.0'
       }
     })
+
     expect(await hasNuxtModuleCompatibility(module, '^1.0.0', nuxt)).toStrictEqual(true)
+
     expect(await hasNuxtModuleCompatibility(module, '^2.0.0', nuxt)).toStrictEqual(false)
+
     await nuxt.close()
   })
 })
