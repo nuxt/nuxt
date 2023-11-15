2b94257ee3b31ef24973e2893d2961fdbc831d64
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 23:12:04 +0100

    refactor: update `packages/kit/src/internal/template.ts`

--- packages/kit/src/internal/template.ts
+++ packages/kit/src/internal/template.ts
@@ -1,43 +1,62 @@
 import { promises as fsp } from 'node:fs'
+
 // TODO: swap out when https://github.com/lodash/lodash/pull/5649 is merged
 import { template as lodashTemplate } from 'lodash-es'
 import { genDynamicImport, genImport, genSafeVariableName } from 'knitwork'
 
-import type { NuxtTemplate } from '@nuxt/schema'
+import { type NuxtTemplate } from '@nuxt/schema'
 import { logger } from '../logger'
 
 /** @deprecated */
 // TODO: Remove support for compiling ejs templates in v4
-export async function compileTemplate (template: NuxtTemplate, ctx: any) {
-  const data = { ...ctx, options: template.options }
+// eslint-disable-next-line ts/no-explicit-any
+export async function compileTemplate(template: NuxtTemplate, context: any) {
+  // eslint-disable-next-line ts/no-unsafe-assignment
+  const data = { ...context, options: template.options }
+
   if (template.src) {
     try {
-      const srcContents = await fsp.readFile(template.src, 'utf-8')
-      return lodashTemplate(srcContents, {})(data)
-    } catch (err) {
+      const sourceContents = await fsp.readFile(template.src, 'utf8')
+
+      // eslint-disable-next-line ts/no-unsafe-argument
+      return lodashTemplate(sourceContents, {})(data)
+    } catch (error) {
       logger.error('Error compiling template: ', template)
-      throw err
+
+      throw error
     }
   }
+
   if (template.getContents) {
+    // eslint-disable-next-line ts/no-unsafe-argument
     return template.getContents(data)
   }
   throw new Error('Invalid template: ' + JSON.stringify(template))
 }
 
 /** @deprecated */
-const serialize = (data: any) => JSON.stringify(data, null, 2).replace(/"{(.+)}"(?=,?$)/gm, r => JSON.parse(r).replace(/^{(.*)}$/, '$1'))
+// eslint-disable-next-line ts/no-explicit-any
+const serialize = (data: any) => JSON
+  .stringify(data, undefined, 2)
+  .replaceAll(
+    /"{(.+)}"(?=,?$)/gm,
+    // eslint-disable-next-line style/max-len
+    // eslint-disable-next-line ts/no-unsafe-return, ts/no-unsafe-call, ts/no-unsafe-member-access
+    (r) => JSON.parse(r).replace(/^{(.*)}$/, '$1')
+  )
 
 /** @deprecated */
 const importSources = (sources: string | string[], { lazy = false } = {}) => {
   if (!Array.isArray(sources)) {
     sources = [sources]
   }
-  return sources.map((src) => {
+
+  return sources.map((source) => {
     if (lazy) {
-      return `const ${genSafeVariableName(src)} = ${genDynamicImport(src, { comment: `webpackChunkName: ${JSON.stringify(src)}` })}`
+      return `const ${genSafeVariableName(source)} = ${genDynamicImport(source, { comment: `webpackChunkName: ${JSON.stringify(source)}` })}`
     }
-    return genImport(src, genSafeVariableName(src))
+
+    return genImport(source, genSafeVariableName(source))
   }).join('\n')
 }
 
