c7488f4999e640351bda8088384fec9beea84503
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 23:12:32 +0100

    refactor: update `packages/kit/src/loader/config.ts`

--- packages/kit/src/loader/config.ts
+++ packages/kit/src/loader/config.ts
@@ -1,15 +1,20 @@
 import { resolve } from 'pathe'
-import type { JSValue } from 'untyped'
+import { type JSValue } from 'untyped'
 import { applyDefaults } from 'untyped'
-import type { LoadConfigOptions } from 'c12'
+import { type LoadConfigOptions } from 'c12'
 import { loadConfig } from 'c12'
-import type { NuxtConfig, NuxtOptions } from '@nuxt/schema'
+import { type NuxtConfig, type NuxtOptions } from '@nuxt/schema'
 import { NuxtConfigSchema } from '@nuxt/schema'
 
 export interface LoadNuxtConfigOptions extends LoadConfigOptions<NuxtConfig> {}
 
-export async function loadNuxtConfig (opts: LoadNuxtConfigOptions): Promise<NuxtOptions> {
-  (globalThis as any).defineNuxtConfig = (c: any) => c
+export async function loadNuxtConfig(
+  options: LoadNuxtConfigOptions
+): Promise<NuxtOptions> {
+  // eslint-disable-next-line style/max-len
+  // eslint-disable-next-line ts/no-unsafe-return, ts/no-explicit-any, ts/no-unsafe-member-access
+  (globalThis as any).defineNuxtConfig = (config: any) => config
+
   const result = await loadConfig<NuxtConfig>({
     name: 'nuxt',
     configFile: 'nuxt.config',
@@ -17,30 +22,41 @@ export async function loadNuxtConfig (opts: LoadNuxtConfigOptions): Promise<Nuxt
     extend: { extendKey: ['theme', 'extends'] },
     dotenv: true,
     globalRc: true,
-    ...opts
+    ...options
   })
+
+  // eslint-disable-next-line ts/no-unsafe-member-access, ts/no-explicit-any
   delete (globalThis as any).defineNuxtConfig
-  const { configFile, layers = [], cwd } = result
-  const nuxtConfig = result.config!
+
+  const { configFile, layers = [], cwd, config } = result
+  // eslint-disable-next-line ts/no-non-null-assertion
+  const nuxtConfig = config!
 
   // Fill config
   nuxtConfig.rootDir = nuxtConfig.rootDir || cwd
+
   nuxtConfig._nuxtConfigFile = configFile
+
   nuxtConfig._nuxtConfigFiles = [configFile]
 
   // Resolve `rootDir` & `srcDir` of layers
   for (const layer of layers) {
     layer.config = layer.config || {}
+
     layer.config.rootDir = layer.config.rootDir ?? layer.cwd
+
+    // eslint-disable-next-line ts/no-non-null-assertion
     layer.config.srcDir = resolve(layer.config.rootDir!, layer.config.srcDir!)
   }
 
   // Filter layers
-  const _layers = layers.filter(layer => layer.configFile && !layer.configFile.endsWith('.nuxtrc'))
-  ;(nuxtConfig as any)._layers = _layers
+  const _layers = layers.filter((layer) => layer.configFile && !layer.configFile.endsWith('.nuxtrc'));
+
+  // eslint-disable-next-line ts/no-unsafe-member-access, ts/no-explicit-any
+  (nuxtConfig as any)._layers = _layers
 
   // Ensure at least one layer remains (without nuxt.config)
-  if (!_layers.length) {
+  if (_layers.length === 0) {
     _layers.push({
       cwd,
       config: {
@@ -51,5 +67,7 @@ export async function loadNuxtConfig (opts: LoadNuxtConfigOptions): Promise<Nuxt
   }
 
   // Resolve and apply defaults
-  return await applyDefaults(NuxtConfigSchema, nuxtConfig as NuxtConfig & Record<string, JSValue>) as unknown as NuxtOptions
+  return await applyDefaults(
+    NuxtConfigSchema, nuxtConfig as NuxtConfig & Record<string, JSValue>
+  ) as unknown as NuxtOptions
 }
