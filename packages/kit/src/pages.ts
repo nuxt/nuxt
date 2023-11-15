981f572b04626d1cd4c246ad91fb36ab4798b717
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 21:16:01 +0100

    refactor: update `packages/kit/src/pages.ts`

--- packages/kit/src/pages.ts
+++ packages/kit/src/pages.ts
@@ -1,17 +1,19 @@
-import type { NuxtHooks, NuxtMiddleware } from '@nuxt/schema'
-import type { NitroRouteConfig } from 'nitropack'
+import { type NuxtHooks, type NuxtMiddleware } from '@nuxt/schema'
+import { type NitroRouteConfig } from 'nitropack'
 import { defu } from 'defu'
 import { useNuxt } from './context'
 import { isNuxt2 } from './compatibility'
 import { logger } from './logger'
+import { toArray } from './utils'
 
-export function extendPages (cb: NuxtHooks['pages:extend']) {
+export function extendPages(callback: NuxtHooks['pages:extend']) {
   const nuxt = useNuxt()
+
   if (isNuxt2(nuxt)) {
     // @ts-expect-error TODO: Nuxt 2 hook
-    nuxt.hook('build:extendRoutes', cb)
+    nuxt.hook('build:extendRoutes', callback)
   } else {
-    nuxt.hook('pages:extend', cb)
+    nuxt.hook('pages:extend', callback)
   }
 }
 
@@ -23,15 +25,21 @@ export interface ExtendRouteRulesOptions {
   override?: boolean
 }
 
-export function extendRouteRules (route: string, rule: NitroRouteConfig, options: ExtendRouteRulesOptions = {}) {
+export function extendRouteRules(
+  route: string,
+  rule: NitroRouteConfig,
+  options: ExtendRouteRulesOptions = {}
+) {
   const nuxt = useNuxt()
-  for (const opts of [nuxt.options, nuxt.options.nitro]) {
-    if (!opts.routeRules) {
-      opts.routeRules = {}
+
+  for (const options_ of [nuxt.options, nuxt.options.nitro]) {
+    if (!options_.routeRules) {
+      options_.routeRules = {}
     }
-    opts.routeRules[route] = options.override
-      ? defu(rule, opts.routeRules[route])
-      : defu(opts.routeRules[route], rule)
+
+    options_.routeRules[route] = options.override
+      ? defu(rule, options_.routeRules[route])
+      : defu(options_.routeRules[route], rule)
   }
 }
 
@@ -43,12 +51,19 @@ export interface AddRouteMiddlewareOptions {
   override?: boolean
 }
 
-export function addRouteMiddleware (input: NuxtMiddleware | NuxtMiddleware[], options: AddRouteMiddlewareOptions = {}) {
+export function addRouteMiddleware(
+  input: NuxtMiddleware | NuxtMiddleware[],
+  options: AddRouteMiddlewareOptions = {}
+) {
   const nuxt = useNuxt()
-  const middlewares = Array.isArray(input) ? input : [input]
+  const middlewares = toArray(input)
+
   nuxt.hook('app:resolve', (app) => {
     for (const middleware of middlewares) {
-      const find = app.middleware.findIndex(item => item.name === middleware.name)
+      const find = app.middleware.findIndex(
+        (item) => item.name === middleware.name
+      )
+
       if (find >= 0) {
         if (options.override === true) {
           app.middleware[find] = middleware
