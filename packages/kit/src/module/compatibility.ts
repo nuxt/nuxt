d33b16dbf7f8b8d1c69c06993c167408b8c4244a
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 23:13:28 +0100

    refactor: update `packages/kit/src/module/compatibility.ts`

--- packages/kit/src/module/compatibility.ts
+++ packages/kit/src/module/compatibility.ts
@@ -1,40 +1,60 @@
 import satisfies from 'semver/functions/satisfies.js' // npm/node-semver#381
-import type { Nuxt, NuxtModule, NuxtOptions } from '@nuxt/schema'
+import { type Nuxt, type NuxtModule, type NuxtOptions } from '@nuxt/schema'
 import { useNuxt } from '../context'
 import { normalizeSemanticVersion } from '../compatibility'
 import { loadNuxtModuleInstance } from './install'
 
-function resolveNuxtModuleEntryName (m: NuxtOptions['modules'][number]): string | false {
-  if (typeof m === 'object' && !Array.isArray(m)) {
-    return (m as any as NuxtModule).name
+function resolveNuxtModuleEntryName(
+  module: NuxtOptions['modules'][number]
+): string | false {
+  if (typeof module === 'object' && !Array.isArray(module)) {
+    // eslint-disable-next-line ts/no-explicit-any
+    return (module as any as NuxtModule).name
   }
-  if (Array.isArray(m)) {
-    return resolveNuxtModuleEntryName(m[0])
+
+  if (Array.isArray(module)) {
+    return resolveNuxtModuleEntryName(module[0])
   }
-  return m as string || false
+
+  return module as string || false
 }
 
 /**
  * Check if a Nuxt module is installed by name.
  *
- * This will check both the installed modules and the modules to be installed. Note
- * that it cannot detect if a module is _going to be_ installed programmatically by another module.
+ * This will check both the installed modules and the modules to be installed.
+ * Note that it cannot detect if a module is _going to be_ installed
+ * programmatically by another module.
  */
-export function hasNuxtModule (moduleName: string, nuxt: Nuxt = useNuxt()) : boolean {
-  // check installed modules
-  return nuxt.options._installedModules.some(({ meta }) => meta.name === moduleName) ||
-    // check modules to be installed
-    nuxt.options.modules.some(m => moduleName === resolveNuxtModuleEntryName(m))
+export function hasNuxtModule(
+  moduleName: string, nuxt: Nuxt = useNuxt()
+): boolean {
+  const hasInstalledModules = nuxt.options._installedModules.some(
+    // eslint-disable-next-line ts/no-unsafe-member-access
+    (module) => module.meta.name === moduleName
+  )
+
+  const hasModulesToBeInstalled = nuxt.options.modules.some(
+    (module) => moduleName === resolveNuxtModuleEntryName(module)
+  )
+
+  return hasInstalledModules || hasModulesToBeInstalled
 }
 
 /**
  * Checks if a Nuxt Module is compatible with a given semver version.
  */
-export async function hasNuxtModuleCompatibility (module: string | NuxtModule, semverVersion: string, nuxt: Nuxt = useNuxt()): Promise<boolean> {
+export async function hasNuxtModuleCompatibility(
+  module: string | NuxtModule,
+  semverVersion: string,
+  nuxt: Nuxt = useNuxt()
+): Promise<boolean> {
   const version = await getNuxtModuleVersion(module, nuxt)
+
   if (!version) {
     return false
   }
+
   return satisfies(normalizeSemanticVersion(version), semverVersion, {
     includePrerelease: true
   })
@@ -43,24 +63,56 @@ export async function hasNuxtModuleCompatibility (module: string | NuxtModule, s
 /**
  * Get the version of a Nuxt module.
  *
- * Scans installed modules for the version, if it's not found it will attempt to load the module instance and get the version from there.
+ * Scans installed modules for the version.
+ * If it's not found it will attempt to load the module instance
+ * and get the version from there.
  */
-export async function getNuxtModuleVersion (module: string | NuxtModule, nuxt: Nuxt | any = useNuxt()): Promise<string | false> {
+export async function getNuxtModuleVersion(
+  module: string | NuxtModule, nuxt: Nuxt | object = useNuxt()
+): Promise<string | false> {
   const moduleMeta = (typeof module === 'string' ? { name: module } : await module.getMeta?.()) || {}
-  if (moduleMeta.version) { return moduleMeta.version }
+
+  if (moduleMeta.version) {
+    return moduleMeta.version
+  }
+
   // need a name from here
-  if (!moduleMeta.name) { return false }
+  if (!moduleMeta.name) {
+    return false
+  }
+
   // maybe the version got attached within the installed module instance?
+  // @ts-expect-error _installedModules is not typed
+  // eslint-disable-next-line style/max-len
+  // eslint-disable-next-line ts/no-unsafe-assignment, ts/no-unsafe-member-access, ts/no-unsafe-call
   const version = nuxt.options._installedModules
-    // @ts-expect-error _installedModules is not typed
-    .filter(m => m.meta.name === moduleMeta.name).map(m => m.meta.version)?.[0]
+    .filter(
+
+      // @ts-expect-error _installedModules is not typed
+      // eslint-disable-next-line ts/no-unsafe-member-access
+      (module) => module.meta.name === moduleMeta.name
+
+      // @ts-expect-error _installedModules is not typed
+    // eslint-disable-next-line ts/no-unsafe-member-access, ts/no-unsafe-return
+    ).map((module) => module.meta.version)?.[0]
+
   if (version) {
+    // eslint-disable-next-line ts/no-unsafe-return
     return version
   }
-  // it's possible that the module will be installed, it just hasn't been done yet, preemptively load the instance
+
+  // it's possible that the module will be installed,
+  // it just hasn't been done yet, preemptively load the instance
   if (hasNuxtModule(moduleMeta.name)) {
-    const { buildTimeModuleMeta } = await loadNuxtModuleInstance(moduleMeta.name, nuxt)
+    const { buildTimeModuleMeta } = await loadNuxtModuleInstance(
+      moduleMeta.name,
+
+      // @ts-expect-error Nuxt might be an object
+      nuxt
+    )
+
     return buildTimeModuleMeta.version || false
   }
+
   return false
 }
