3d012ae99759140940d7d1aeee4d703ac8cbf0bf
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 19:20:35 +0100

    refactor: update `packages/kit/src/compatibility.ts`

--- packages/kit/src/compatibility.ts
+++ packages/kit/src/compatibility.ts
@@ -1,38 +1,52 @@
 import satisfies from 'semver/functions/satisfies.js' // npm/node-semver#381
-import type { Nuxt, NuxtCompatibility, NuxtCompatibilityIssues } from '@nuxt/schema'
+import { type Nuxt, type NuxtCompatibility, type NuxtCompatibilityIssues } from '@nuxt/schema'
 import { useNuxt } from './context'
 
-export function normalizeSemanticVersion (version: string) {
-  return version.replace(/-[0-9]+\.[0-9a-f]+/, '') // Remove edge prefix
+export function normalizeSemanticVersion(version: string) {
+  return version.replace(/-\d+\.[\da-f]+/, '') // Remove edge prefix
 }
 
 /**
  * Check version constraints and return incompatibility issues as an array
  */
-export async function checkNuxtCompatibility (constraints: NuxtCompatibility, nuxt: Nuxt = useNuxt()): Promise<NuxtCompatibilityIssues> {
+export async function checkNuxtCompatibility(
+  constraints: NuxtCompatibility,
+  nuxt: Nuxt = useNuxt()
+): Promise<NuxtCompatibilityIssues> {
   const issues: NuxtCompatibilityIssues = []
 
   // Nuxt version check
   if (constraints.nuxt) {
     const nuxtVersion = getNuxtVersion(nuxt)
-    if (!satisfies(normalizeSemanticVersion(nuxtVersion), constraints.nuxt, { includePrerelease: true })) {
+
+    if (
+      !satisfies(
+        normalizeSemanticVersion(nuxtVersion),
+        constraints.nuxt,
+        { includePrerelease: true }
+      )
+    ) {
       issues.push({
         name: 'nuxt',
-        message: `Nuxt version \`${constraints.nuxt}\` is required but currently using \`${nuxtVersion}\``
+        message: `Nuxt version \`${
+            constraints.nuxt
+        }\` is required but currently using \`${nuxtVersion}\``
       })
     }
   }
 
   // Bridge compatibility check
   if (isNuxt2(nuxt)) {
-    const bridgeRequirement = constraints.bridge
-    const hasBridge = !!(nuxt.options as any).bridge
-    if (bridgeRequirement === true && !hasBridge) {
+    const isBridgeRequired = !!constraints.bridge
+
+    const hasBridge = 'bridge' in nuxt.options && !!nuxt.options['bridge']
+
+    if (isBridgeRequired && !hasBridge) {
       issues.push({
         name: 'bridge',
         message: 'Nuxt bridge is required'
       })
-    } else if (bridgeRequirement === false && hasBridge) {
+    } else if (isBridgeRequired && hasBridge) {
       issues.push({
         name: 'bridge',
         message: 'Nuxt bridge is not supported'
@@ -44,52 +58,75 @@ export async function checkNuxtCompatibility (constraints: NuxtCompatibility, nu
   await nuxt.callHook('kit:compatibility', constraints, issues)
 
   // Issues formatter
-  issues.toString = () =>
-    issues.map(issue => ` - [${issue.name}] ${issue.message}`).join('\n')
+  issues.toString = () => issues.map((issue) => ` - [${issue.name}] ${issue.message}`).join('\n')
 
   return issues
 }
 
 /**
- * Check version constraints and throw a detailed error if has any, otherwise returns true
+ * Check version constraints and throw a detailed error if has any,
+ * otherwise returns true
  */
-export async function assertNuxtCompatibility (constraints: NuxtCompatibility, nuxt: Nuxt = useNuxt()): Promise<true> {
+export async function assertNuxtCompatibility(
+  constraints: NuxtCompatibility,
+  nuxt: Nuxt = useNuxt()
+): Promise<true> {
   const issues = await checkNuxtCompatibility(constraints, nuxt)
-  if (issues.length) {
+
+  if (issues.length > 0) {
     throw new Error('Nuxt compatibility issues found:\n' + issues.toString())
   }
+
   return true
 }
 
 /**
  * Check version constraints and return true if passed, otherwise returns false
  */
-export async function hasNuxtCompatibility (constraints: NuxtCompatibility, nuxt: Nuxt = useNuxt()): Promise<boolean> {
+export async function hasNuxtCompatibility(
+  constraints: NuxtCompatibility,
+  nuxt: Nuxt = useNuxt()
+): Promise<boolean> {
   const issues = await checkNuxtCompatibility(constraints, nuxt)
-  return !issues.length
+
+  return issues.length === 0
 }
 
 /**
- * Check if current nuxt instance is version 2 legacy
+ * Check if current Nuxt instance is version 2 legacy
  */
-export function isNuxt2 (nuxt: Nuxt = useNuxt()) {
+export function isNuxt2(nuxt: Nuxt = useNuxt()) {
   return getNuxtVersion(nuxt).startsWith('2.')
 }
 
 /**
- * Check if current nuxt instance is version 3
+ * Check if current Nuxt instance is version 3
  */
-export function isNuxt3 (nuxt: Nuxt = useNuxt()) {
+export function isNuxt3(nuxt: Nuxt = useNuxt()) {
   return getNuxtVersion(nuxt).startsWith('3.')
 }
 
 /**
- * Get nuxt version
+ * Get Nuxt version
  */
-export function getNuxtVersion (nuxt: Nuxt | any = useNuxt() /* TODO: LegacyNuxt */) {
-  const version = (nuxt?._version || nuxt?.version || nuxt?.constructor?.version || '').replace(/^v/g, '')
-  if (!version) {
-    throw new Error('Cannot determine nuxt version! Is current instance passed?')
+export function getNuxtVersion(
+  nuxt: Nuxt | object = useNuxt() /* TODO: LegacyNuxt */
+) {
+  let version = ''
+
+  if ('_version' in nuxt) {
+    version = nuxt._version
+  } else if ('version' in nuxt && typeof nuxt.version === 'string') {
+    version = nuxt.version
+  } else if ('version' in nuxt.constructor && typeof nuxt.constructor.version === 'string') {
+    version = nuxt.constructor.version
   }
+
+  version = version.replaceAll(/^v/g, '')
+
+  if (!version) {
+    throw new Error('Cannot determine Nuxt version! Is current instance passed?')
+  }
+
   return version
 }
