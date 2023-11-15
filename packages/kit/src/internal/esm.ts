d2aa6ea393cc11eb5468505de639839007cbcd36
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 23:11:55 +0100

    refactor: update `packages/kit/src/internal/esm.ts`

--- packages/kit/src/internal/esm.ts
+++ packages/kit/src/internal/esm.ts
@@ -7,19 +7,26 @@ import { interopDefault, resolvePath } from 'mlly'
  *
  * @internal
  */
-export async function tryResolveModule (id: string, url: string | string[] = import.meta.url) {
+export async function tryResolveModule(
+  id: string,
+  url: string | string[] = import.meta.url
+) {
   try {
     return await resolvePath(id, { url })
-  } catch { }
+  } catch {}
 }
 
-export async function importModule (id: string, url: string | string[] = import.meta.url) {
+export async function importModule(
+  id: string,
+  url: string | string[] = import.meta.url
+) {
   const resolvedPath = await resolvePath(id, { url })
+
   return import(pathToFileURL(resolvedPath).href).then(interopDefault)
 }
 
-export function tryImportModule (id: string, url = import.meta.url) {
+export function tryImportModule(id: string, url = import.meta.url) {
   try {
-    return importModule(id, url).catch(() => undefined)
-  } catch { }
+    return importModule(id, url).catch(() => {})
+  } catch {}
 }
