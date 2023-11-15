e63ad5157b9b7d1766d68c179a32622d3fc2f72d
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 20:44:55 +0100

    refactor: update `packages/kit/src/imports.ts`

--- packages/kit/src/imports.ts
+++ packages/kit/src/imports.ts
@@ -1,30 +1,37 @@
-import type { Import } from 'unimport'
-import type { ImportPresetWithDeprecation } from '@nuxt/schema'
+import { type Import } from 'unimport'
+import { type ImportPresetWithDeprecation } from '@nuxt/schema'
 import { useNuxt } from './context'
 import { assertNuxtCompatibility } from './compatibility'
+import { toArray } from './utils'
 
-export function addImports (imports: Import | Import[]) {
-  assertNuxtCompatibility({ bridge: true })
+export async function addImports(imports: Import | Import[]) {
+  await assertNuxtCompatibility({ bridge: true })
 
   useNuxt().hook('imports:extend', (_imports) => {
-    _imports.push(...(Array.isArray(imports) ? imports : [imports]))
+    _imports.push(...toArray(imports))
   })
 }
 
-export function addImportsDir (dirs: string | string[], opts: { prepend?: boolean } = {}) {
-  assertNuxtCompatibility({ bridge: true })
+export async function addImportsDir(
+  directories: string | string[],
+  options: { prepend?: boolean } = {}
+) {
+  await assertNuxtCompatibility({ bridge: true })
 
-  useNuxt().hook('imports:dirs', (_dirs: string[]) => {
-    for (const dir of (Array.isArray(dirs) ? dirs : [dirs])) {
-      _dirs[opts.prepend ? 'unshift' : 'push'](dir)
+  useNuxt().hook('imports:dirs', (_directories: string[]) => {
+    for (const directory of toArray(directories)) {
+      _directories[options.prepend ? 'unshift' : 'push'](directory)
     }
   })
 }
-export function addImportsSources (presets: ImportPresetWithDeprecation | ImportPresetWithDeprecation[]) {
-  assertNuxtCompatibility({ bridge: true })
+
+export async function addImportsSources(
+  presets: ImportPresetWithDeprecation | ImportPresetWithDeprecation[]
+) {
+  await assertNuxtCompatibility({ bridge: true })
 
   useNuxt().hook('imports:sources', (_presets: ImportPresetWithDeprecation[]) => {
-    for (const preset of (Array.isArray(presets) ? presets : [presets])) {
+    for (const preset of toArray(presets)) {
       _presets.push(preset)
     }
   })
