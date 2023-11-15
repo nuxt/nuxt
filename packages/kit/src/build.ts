e790ed9088fa3d06de5a20d002e25ecefac1e78b
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 18:51:18 +0100

    refactor: update `packages/kit/src/build.ts`

--- packages/kit/src/build.ts
+++ packages/kit/src/build.ts
@@ -1,68 +1,74 @@
-import type { Configuration as WebpackConfig, WebpackPluginInstance } from 'webpack'
-import type { UserConfig as ViteConfig, Plugin as VitePlugin } from 'vite'
+import { type Configuration as WebpackConfig, type WebpackPluginInstance } from 'webpack'
+import { type UserConfig as ViteConfig, type Plugin as VitePlugin } from 'vite'
 import { useNuxt } from './context'
+import { toArray } from './utils'
 
 export interface ExtendConfigOptions {
   /**
-   * Install plugin on dev
-   * @default true
-   */
+     * Install plugin on dev
+     * @default true
+     */
   dev?: boolean
+
   /**
-   * Install plugin on build
-   * @default true
-   */
+     * Install plugin on build
+     * @default true
+     */
   build?: boolean
+
   /**
-   * Install plugin on server side
-   * @default true
-   */
+     * Install plugin on server side
+     * @default true
+     */
   server?: boolean
+
   /**
-   * Install plugin on client side
-   * @default true
-   */
+     * Install plugin on client side
+     * @default true
+     */
   client?: boolean
+
   /**
-   * Prepends the plugin to the array with `unshift()` instead of `push()`.
-   */
+     * Prepends the plugin to the array with `unshift()` instead of `push()`.
+     */
   prepend?: boolean
 }
 
-export interface ExtendWebpackConfigOptions extends ExtendConfigOptions {}
+export interface ExtendWebpackConfigOptions extends ExtendConfigOptions { }
 
-export interface ExtendViteConfigOptions extends ExtendConfigOptions {}
+export interface ExtendViteConfigOptions extends ExtendConfigOptions { }
 
 /**
  * Extend webpack config
  *
- * The fallback function might be called multiple times
+ * The callback function might be called multiple times
  * when applying to both client and server builds.
  */
-export function extendWebpackConfig (
-  fn: ((config: WebpackConfig) => void),
+export function extendWebpackConfig(
+  callbackFunction: ((config: WebpackConfig) => void),
   options: ExtendWebpackConfigOptions = {}
 ) {
   const nuxt = useNuxt()
 
-  if (options.dev === false && nuxt.options.dev) {
-    return
-  }
-  if (options.build === false && nuxt.options.build) {
+  if (
+    (options.dev === false && nuxt.options.dev)
+    || (options.build === false && !nuxt.options.dev)
+  ) {
     return
   }
 
   nuxt.hook('webpack:config', (configs: WebpackConfig[]) => {
     if (options.server !== false) {
-      const config = configs.find(i => i.name === 'server')
+      const config = configs.find((index) => index.name === 'server')
+
       if (config) {
-        fn(config)
+        callbackFunction(config)
       }
-    }
-    if (options.client !== false) {
-      const config = configs.find(i => i.name === 'client')
+    } else if (options.client !== false) {
+      const config = configs.find((index) => index.name === 'client')
+
       if (config) {
-        fn(config)
+        callbackFunction(config)
       }
     }
   })
@@ -71,30 +77,32 @@ export function extendWebpackConfig (
 /**
  * Extend Vite config
  */
-export function extendViteConfig (
-  fn: ((config: ViteConfig) => void),
+export function extendViteConfig(
+  callbackFunction: ((config: ViteConfig) => void),
   options: ExtendViteConfigOptions = {}
 ) {
   const nuxt = useNuxt()
 
-  if (options.dev === false && nuxt.options.dev) {
-    return
-  }
-  if (options.build === false && nuxt.options.build) {
+  if (
+    (options.dev === false && nuxt.options.dev)
+    || (options.build === false && !nuxt.options.dev)
+  ) {
     return
   }
 
   if (options.server !== false && options.client !== false) {
-    // Call fn() only once
-    return nuxt.hook('vite:extend', ({ config }) => fn(config))
+    // Call callbackFunction() only once
+    return nuxt.hook('vite:extend', ({ config }) => {
+      callbackFunction(config)
+    })
   }
 
   nuxt.hook('vite:extendConfig', (config, { isClient, isServer }) => {
-    if (options.server !== false && isServer) {
-      return fn(config)
-    }
-    if (options.client !== false && isClient) {
-      return fn(config)
+    if (
+      (options.server !== false && isServer)
+      || (options.client !== false && isClient)
+    ) {
+      callbackFunction(config)
     }
   })
 }
@@ -102,34 +110,48 @@ export function extendViteConfig (
 /**
  * Append webpack plugin to the config.
  */
-export function addWebpackPlugin (pluginOrGetter: WebpackPluginInstance | WebpackPluginInstance[] | (() => WebpackPluginInstance | WebpackPluginInstance[]), options?: ExtendWebpackConfigOptions) {
+export function addWebpackPlugin(
+  pluginOrGetter:
+  | WebpackPluginInstance
+  | WebpackPluginInstance[]
+  | (() => WebpackPluginInstance | WebpackPluginInstance[]),
+  options?: ExtendWebpackConfigOptions
+) {
   extendWebpackConfig((config) => {
     const method: 'push' | 'unshift' = options?.prepend ? 'unshift' : 'push'
-    const plugin = typeof pluginOrGetter === 'function' ? pluginOrGetter() : pluginOrGetter
+
+    // eslint-disable-next-line ts/no-unsafe-assignment
+    const plugins = toArray(
+      typeof pluginOrGetter === 'function' ? pluginOrGetter() : pluginOrGetter
+    )
 
     config.plugins = config.plugins || []
-    if (Array.isArray(plugin)) {
-      config.plugins[method](...plugin)
-    } else {
-      config.plugins[method](plugin)
-    }
+
+    // eslint-disable-next-line ts/no-unsafe-argument
+    config.plugins[method](...plugins)
   }, options)
 }
 
 /**
  * Append Vite plugin to the config.
  */
-export function addVitePlugin (pluginOrGetter: VitePlugin | VitePlugin[] | (() => VitePlugin | VitePlugin[]), options?: ExtendViteConfigOptions) {
+export function addVitePlugin(
+  pluginOrGetter:
+  | VitePlugin
+  | VitePlugin[]
+  | (() => VitePlugin | VitePlugin[]),
+  options?: ExtendViteConfigOptions
+) {
   extendViteConfig((config) => {
     const method: 'push' | 'unshift' = options?.prepend ? 'unshift' : 'push'
-    const plugin = typeof pluginOrGetter === 'function' ? pluginOrGetter() : pluginOrGetter
+
+    const plugins = toArray(
+      typeof pluginOrGetter === 'function' ? pluginOrGetter() : pluginOrGetter
+    )
 
     config.plugins = config.plugins || []
-    if (Array.isArray(plugin)) {
-      config.plugins[method](...plugin)
-    } else {
-      config.plugins[method](plugin)
-    }
+
+    config.plugins[method](...plugins)
   }, options)
 }
 
@@ -138,7 +160,10 @@ interface AddBuildPluginFactory {
   webpack?: () => WebpackPluginInstance | WebpackPluginInstance[]
 }
 
-export function addBuildPlugin (pluginFactory: AddBuildPluginFactory, options?: ExtendConfigOptions) {
+export function addBuildPlugin(
+  pluginFactory: AddBuildPluginFactory,
+  options?: ExtendConfigOptions
+) {
   if (pluginFactory.vite) {
     addVitePlugin(pluginFactory.vite, options)
   }
