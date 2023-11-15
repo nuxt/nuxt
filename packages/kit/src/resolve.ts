3e6b351f4481df1ba5dfae07ca1a4e1ac4be1be8
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 21:40:15 +0100

    refactor: update `packages/kit/src/resolve.ts`

--- packages/kit/src/resolve.ts
+++ packages/kit/src/resolve.ts
@@ -6,6 +6,7 @@ import { resolvePath as _resolvePath } from 'mlly'
 import { resolveAlias as _resolveAlias } from 'pathe/utils'
 import { tryUseNuxt } from './context'
 import { isIgnored } from './ignore'
+import { toArray } from './utils'
 
 export interface ResolvePathOptions {
   /** Base for resolving paths from. Default is Nuxt rootDir. */
@@ -19,13 +20,18 @@ export interface ResolvePathOptions {
 }
 
 /**
- * Resolve full path to a file or directory respecting Nuxt alias and extensions options
+ * Resolve full path to a file or directory
+ * respecting Nuxt alias and extensions options
  *
  * If path could not be resolved, normalized input path will be returned
  */
-export async function resolvePath (path: string, opts: ResolvePathOptions = {}): Promise<string> {
+export async function resolvePath(
+  path: string,
+  options: ResolvePathOptions = {}
+): Promise<string> {
   // Always normalize input
   const _path = path
+
   path = normalize(path)
 
   // Fast return if the path exists
@@ -35,9 +41,9 @@ export async function resolvePath (path: string, opts: ResolvePathOptions = {}):
 
   // Use current nuxt options
   const nuxt = tryUseNuxt()
-  const cwd = opts.cwd || (nuxt ? nuxt.options.rootDir : process.cwd())
-  const extensions = opts.extensions || (nuxt ? nuxt.options.extensions : ['.ts', '.mjs', '.cjs', '.json'])
-  const modulesDir = nuxt ? nuxt.options.modulesDir : []
+  const cwd = options.cwd || (nuxt ? nuxt.options.rootDir : process.cwd())
+  const extensions = options.extensions || (nuxt ? nuxt.options.extensions : ['.ts', '.mjs', '.cjs', '.json'])
+  const modulesDirectory = nuxt ? nuxt.options.modulesDir : []
 
   // Resolve aliases
   path = resolveAlias(path)
@@ -48,30 +54,38 @@ export async function resolvePath (path: string, opts: ResolvePathOptions = {}):
   }
 
   // Check if resolvedPath is a file
-  let _isDir = false
+  let _isDirectory = false
+
   if (existsSync(path)) {
-    _isDir = await isDirectory(path)
-    if (!_isDir) {
+    _isDirectory = await isDirectory(path)
+
+    if (!_isDirectory) {
       return path
     }
   }
 
   // Check possible extensions
-  for (const ext of extensions) {
+  for (const extension of extensions) {
     // path.[ext]
-    const pathWithExt = path + ext
-    if (existsSync(pathWithExt)) {
-      return pathWithExt
+    const pathWithExtension = path + extension
+
+    if (existsSync(pathWithExtension)) {
+      return pathWithExtension
     }
+
     // path/index.[ext]
-    const pathWithIndex = join(path, 'index' + ext)
-    if (_isDir && existsSync(pathWithIndex)) {
+    const pathWithIndex = join(path, 'index' + extension)
+
+    if (_isDirectory && existsSync(pathWithIndex)) {
       return pathWithIndex
     }
   }
 
   // Try to resolve as module id
-  const resolveModulePath = await _resolvePath(_path, { url: [cwd, ...modulesDir] }).catch(() => null)
+  const resolveModulePath = await _resolvePath(
+    _path, { url: [cwd, ...modulesDirectory] }
+  ).catch(() => {})
+
   if (resolveModulePath) {
     return resolveModulePath
   }
@@ -83,57 +97,69 @@ export async function resolvePath (path: string, opts: ResolvePathOptions = {}):
 /**
  * Try to resolve first existing file in paths
  */
-export async function findPath (paths: string | string[], opts?: ResolvePathOptions, pathType: 'file' | 'dir' = 'file'): Promise<string | null> {
-  if (!Array.isArray(paths)) {
-    paths = [paths]
-  }
-  for (const path of paths) {
-    const rPath = await resolvePath(path, opts)
+export async function findPath(
+  paths: string | string[],
+  options?: ResolvePathOptions,
+  pathType: 'file' | 'dir' = 'file'
+): Promise<string | null> {
+  for (const path of toArray(paths)) {
+    const rPath = await resolvePath(path, options)
+
     if (await existsSensitive(rPath)) {
-      const _isDir = await isDirectory(rPath)
-      if (!pathType || (pathType === 'file' && !_isDir) || (pathType === 'dir' && _isDir)) {
+      const _isDirectory = await isDirectory(rPath)
+
+      if ((pathType === 'file' && !_isDirectory) || (pathType === 'dir' && _isDirectory)) {
         return rPath
       }
     }
   }
+
+  // eslint-disable-next-line unicorn/no-null
   return null
 }
 
 /**
  * Resolve path aliases respecting Nuxt alias options
  */
-export function resolveAlias (path: string, alias?: Record<string, string>): string {
+export function resolveAlias(
+  path: string,
+  alias?: Record<string, string>
+): string {
   if (!alias) {
     alias = tryUseNuxt()?.options.alias || {}
   }
+
   return _resolveAlias(path, alias)
 }
 
 export interface Resolver {
   resolve (...path: string[]): string
-  resolvePath (path: string, opts?: ResolvePathOptions): Promise<string>
+  resolvePath (path: string, options?: ResolvePathOptions): Promise<string>
 }
 
 /**
  * Create a relative resolver
  */
-export function createResolver (base: string | URL): Resolver {
+export function createResolver(base: string | URL): Resolver {
   if (!base) {
     throw new Error('`base` argument is missing for createResolver(base)!')
   }
 
   base = base.toString()
+
   if (base.startsWith('file://')) {
     base = dirname(fileURLToPath(base))
   }
 
   return {
     resolve: (...path) => resolve(base as string, ...path),
-    resolvePath: (path, opts) => resolvePath(path, { cwd: base as string, ...opts })
+    resolvePath: (path, options) => resolvePath(
+      path, { cwd: base as string, ...options }
+    )
   }
 }
 
-export async function resolveNuxtModule (base: string, paths: string[]) {
+export async function resolveNuxtModule(base: string, paths: string[]) {
   const resolved = []
   const resolver = createResolver(base)
 
@@ -142,7 +168,10 @@ export async function resolveNuxtModule (base: string, paths: string[]) {
       resolved.push(path.split('/index.ts')[0])
     } else {
       const resolvedPath = await resolver.resolvePath(path)
-      resolved.push(resolvedPath.slice(0, resolvedPath.lastIndexOf(path) + path.length))
+
+      resolved.push(
+        resolvedPath.slice(0, resolvedPath.lastIndexOf(path) + path.length)
+      )
     }
   }
 
@@ -151,18 +180,36 @@ export async function resolveNuxtModule (base: string, paths: string[]) {
 
 // --- Internal ---
 
-async function existsSensitive (path: string) {
-  if (!existsSync(path)) { return false }
-  const dirFiles = await fsp.readdir(dirname(path))
-  return dirFiles.includes(basename(path))
+async function existsSensitive(path: string) {
+  if (!existsSync(path)) {
+    return false
+  }
+
+  const directoryFiles = await fsp.readdir(dirname(path))
+
+  return directoryFiles.includes(basename(path))
 }
 
 // Usage note: We assume path existence is already ensured
-async function isDirectory (path: string) {
-  return (await fsp.lstat(path)).isDirectory()
+async function isDirectory(path: string) {
+  const pathStats = await fsp.lstat(path)
+
+  return pathStats.isDirectory()
 }
 
-export async function resolveFiles (path: string, pattern: string | string[], opts: { followSymbolicLinks?: boolean } = {}) {
-  const files = await globby(pattern, { cwd: path, followSymbolicLinks: opts.followSymbolicLinks ?? true })
-  return files.map(p => resolve(path, p)).filter(p => !isIgnored(p)).sort()
+export async function resolveFiles(
+  path: string,
+  pattern: string | string[],
+  options: { followSymbolicLinks?: boolean } = {}
+) {
+  const files = await globby(
+    pattern,
+    { cwd: path, followSymbolicLinks: options.followSymbolicLinks ?? true }
+  )
+
+  return files.map(
+    (p) => resolve(path, p)
+  ).filter(
+    (p) => !isIgnored(p)
+  ).sort()
 }
