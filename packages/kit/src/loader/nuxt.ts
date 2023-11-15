adbc8ad353b9428cd8254631aaadccef0eeea690
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-15 23:12:40 +0100

    refactor: update `packages/kit/src/loader/nuxt.ts`

--- packages/kit/src/loader/nuxt.ts
+++ packages/kit/src/loader/nuxt.ts
@@ -1,8 +1,8 @@
 import { pathToFileURL } from 'node:url'
 import { readPackageJSON, resolvePackageJSON } from 'pkg-types'
-import type { Nuxt } from '@nuxt/schema'
+import { type Nuxt } from '@nuxt/schema'
 import { importModule, tryImportModule } from '../internal/esm'
-import type { LoadNuxtConfigOptions } from './config'
+import { type LoadNuxtConfigOptions } from './config'
 
 export interface LoadNuxtOptions extends LoadNuxtConfigOptions {
   /** Load nuxt with development mode */
@@ -18,68 +18,120 @@ export interface LoadNuxtOptions extends LoadNuxtConfigOptions {
   config?: LoadNuxtConfigOptions['overrides']
 }
 
-export async function loadNuxt (opts: LoadNuxtOptions): Promise<Nuxt> {
+export async function loadNuxt(options: LoadNuxtOptions): Promise<Nuxt> {
   // Backward compatibility
-  opts.cwd = opts.cwd || opts.rootDir
-  opts.overrides = opts.overrides || opts.config || {}
+  options.cwd = options.cwd || options.rootDir
+
+  options.overrides = options.overrides || options.config || {}
 
   // Apply dev as config override
-  opts.overrides.dev = !!opts.dev
+  options.overrides.dev = !!options.dev
 
-  const nearestNuxtPkg = await Promise.all(['nuxt-nightly', 'nuxt3', 'nuxt', 'nuxt-edge']
-    .map(pkg => resolvePackageJSON(pkg, { url: opts.cwd }).catch(() => null)))
-    .then(r => (r.filter(Boolean) as string[]).sort((a, b) => b.length - a.length)[0])
-  if (!nearestNuxtPkg) {
-    throw new Error(`Cannot find any nuxt version from ${opts.cwd}`)
+  const nearestNuxtPackage = await Promise.all(['nuxt-nightly', 'nuxt3', 'nuxt', 'nuxt-edge']
+    .map(
+      (package__) => resolvePackageJSON(
+        package__, { url: options.cwd }
+      ).catch(() => {}))
+  )
+    .then(
+      (r) => (
+        r.filter(Boolean) as string[]
+      ).sort((a, b) => b.length - a.length)[0]
+    )
+
+  if (!nearestNuxtPackage) {
+    throw new Error(`Cannot find any nuxt version from ${options.cwd}`)
   }
-  const pkg = await readPackageJSON(nearestNuxtPkg)
-  const majorVersion = parseInt((pkg.version || '').split('.')[0])
 
-  const rootDir = pathToFileURL(opts.cwd || process.cwd()).href
+  const package_ = await readPackageJSON(nearestNuxtPackage)
+  const majorVersion = Number.parseInt((package_.version || '').split('.')[0])
+
+  const rootDirectory = pathToFileURL(options.cwd || process.cwd()).href
 
   // Nuxt 3
   if (majorVersion === 3) {
-    const { loadNuxt } = await importModule((pkg as any)._name || pkg.name, rootDir)
-    const nuxt = await loadNuxt(opts)
+    // eslint-disable-next-line ts/no-unsafe-assignment
+    const { loadNuxt } = await importModule(
+      // eslint-disable-next-line style/max-len
+      // eslint-disable-next-line ts/no-unsafe-argument, ts/no-unsafe-member-access, ts/no-explicit-any
+      (package_ as any)._name || package_.name, rootDirectory
+    )
+
+    // eslint-disable-next-line ts/no-unsafe-call, ts/no-unsafe-assignment
+    const nuxt = await loadNuxt(options)
+
+    // eslint-disable-next-line ts/no-unsafe-return
     return nuxt
   }
 
   // Nuxt 2
-  const { loadNuxt } = await tryImportModule('nuxt-edge', rootDir) || await importModule('nuxt', rootDir)
+  // eslint-disable-next-line ts/no-unsafe-assignment
+  const { loadNuxt } = await tryImportModule('nuxt-edge', rootDirectory) || await importModule('nuxt', rootDirectory)
+
+  // eslint-disable-next-line ts/no-unsafe-assignment, ts/no-unsafe-call
   const nuxt = await loadNuxt({
-    rootDir: opts.cwd,
-    for: opts.dev ? 'dev' : 'build',
-    configOverrides: opts.overrides,
-    ready: opts.ready,
-    envConfig: opts.dotenv // TODO: Backward format conversion
+    rootDir: options.cwd,
+    for: options.dev ? 'dev' : 'build',
+    configOverrides: options.overrides,
+    ready: options.ready,
+    envConfig: options.dotenv // TODO: Backward format conversion
   })
 
   // Mock new hookable methods
+  // eslint-disable-next-line ts/no-unsafe-member-access, ts/no-unsafe-call
   nuxt.removeHook ||= nuxt.clearHook.bind(nuxt)
+
+  // eslint-disable-next-line ts/no-unsafe-member-access, ts/no-unsafe-call
   nuxt.removeAllHooks ||= nuxt.clearHooks.bind(nuxt)
-  nuxt.hookOnce ||= (name: string, fn: (...args: any[]) => any, ...hookArgs: any[]) => {
-    const unsub = nuxt.hook(name, (...args: any[]) => {
+
+  // eslint-disable-next-line ts/no-unsafe-member-access
+  nuxt.hookOnce ||= (
+    name: string,
+    // eslint-disable-next-line ts/no-explicit-any
+    function_: (...arguments_: any[]) => any, ...hookArguments: any[]
+  ) => {
+    // eslint-disable-next-line style/max-len
+    // eslint-disable-next-line ts/no-unsafe-assignment, ts/no-unsafe-member-access, ts/no-unsafe-call, ts/no-explicit-any
+    const unsub = nuxt.hook(name, (...arguments_: any[]) => {
+      // eslint-disable-next-line ts/no-unsafe-call
       unsub()
-      return fn(...args)
-    }, ...hookArgs)
+
+      // eslint-disable-next-line ts/no-unsafe-return, ts/no-unsafe-argument
+      return function_(...arguments_)
+    }, ...hookArguments)
+
+    // eslint-disable-next-line ts/no-unsafe-return
     return unsub
   }
+
+  // eslint-disable-next-line style/max-len
   // https://github.com/nuxt/nuxt/tree/main/packages/kit/src/module/define.ts#L111-L113
+  // eslint-disable-next-line ts/no-unsafe-member-access
   nuxt.hooks ||= nuxt
 
   return nuxt as Nuxt
 }
 
-export async function buildNuxt (nuxt: Nuxt): Promise<any> {
-  const rootDir = pathToFileURL(nuxt.options.rootDir).href
+// eslint-disable-next-line ts/no-explicit-any
+export async function buildNuxt(nuxt: Nuxt): Promise<any> {
+  const rootDirectory = pathToFileURL(nuxt.options.rootDir).href
 
   // Nuxt 3
   if (nuxt.options._majorVersion === 3) {
-    const { build } = await tryImportModule('nuxt-nightly', rootDir) || await tryImportModule('nuxt3', rootDir) || await importModule('nuxt', rootDir)
+    // eslint-disable-next-line ts/no-unsafe-assignment
+    const { build } = await tryImportModule('nuxt-nightly', rootDirectory)
+      || await tryImportModule('nuxt3', rootDirectory)
+      || await importModule('nuxt', rootDirectory)
+
+    // eslint-disable-next-line ts/no-unsafe-return, ts/no-unsafe-call
     return build(nuxt)
   }
 
   // Nuxt 2
-  const { build } = await tryImportModule('nuxt-edge', rootDir) || await importModule('nuxt', rootDir)
+  // eslint-disable-next-line ts/no-unsafe-assignment
+  const { build } = await tryImportModule('nuxt-edge', rootDirectory)
+    || await importModule('nuxt', rootDirectory)
+
+  // eslint-disable-next-line ts/no-unsafe-return, ts/no-unsafe-call
   return build(nuxt)
 }
