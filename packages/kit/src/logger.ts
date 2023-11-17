49e515e8bfbd7148c538c16f4abbd0510569dad6
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-17 18:10:53 +0100

    refactor: lint with JSDoc in `packages/kit/src/logger.ts`

--- packages/kit/src/logger.ts
+++ packages/kit/src/logger.ts
@@ -2,6 +2,12 @@ import { consola } from 'consola'
 
 export const logger = consola
 
+/**
+ * Returns a logger instance. It uses {@link https://github.com/unjs/consola consola} under the hood.
+ * @param tag - A tag to prefix all log messages with.
+ * @returns Consola instance
+ * @see {@link https://nuxt.com/docs/api/kit/logging#uselogger documentation}
+ */
 export function useLogger(tag?: string) {
   return tag ? logger.withTag(tag) : logger
 }
