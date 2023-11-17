31d5ff01ff7e45367e53c247b491b916f166238c
Damian Glowala <damian.glowala.rebkow@gmail.com>
2023-11-17 18:03:41 +0100

    refactor: lint with JSDoc in `packages/kit/src/imports.ts`

--- packages/kit/src/imports.ts
+++ packages/kit/src/imports.ts
@@ -4,6 +4,11 @@ import { useNuxt } from './context'
 import { assertNuxtCompatibility } from './compatibility'
 import { toArray } from './utils'
 
+/**
+ * Add imports to the Nuxt application. It makes your imports available in the Nuxt application without the need to import them manually.
+ * @param imports - An object or an array of objects with the {@link https://nuxt.com/docs/api/kit/autoimports#imports following properties}.
+ * @see {@link https://nuxt.com/docs/api/kit/autoimports#addimports documentation}
+ */
 export async function addImports(imports: Import | Import[]) {
   await assertNuxtCompatibility({ bridge: true })
 
@@ -12,6 +17,13 @@ export async function addImports(imports: Import | Import[]) {
   })
 }
 
+/**
+ * Add imports from a directory to the Nuxt application. It will automatically import all files from the directory and make them available in the Nuxt application without the need to import them manually.
+ * @param directories - A string or an array of strings with the path to the directory to import from.
+ * @param options - Options to pass to the import.
+ * @param options.prepend - If set to `true`, the imports will be prepended to the list of imports.
+ * @see {@link https://nuxt.com/docs/api/kit/autoimports#addimportsdir documentation}
+ */
 export async function addImportsDir(
   directories: string | string[],
   options: { prepend?: boolean } = {}
@@ -25,6 +37,11 @@ export async function addImportsDir(
   })
 }
 
+/**
+ * Add listed imports to the Nuxt application.
+ * @param presets - An object or an array of objects with the {@link https://nuxt.com/docs/api/kit/autoimports#importsources following properties}.
+ * @see {@link https://nuxt.com/docs/api/kit/autoimports#addimportssources documentation}
+ */
 export async function addImportsSources(
   presets: ImportPresetWithDeprecation | ImportPresetWithDeprecation[]
 ) {
