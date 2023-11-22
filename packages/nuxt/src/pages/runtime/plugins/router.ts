17ff26ea931e3b430443ab28cc4ff62da205bb78
Daniel Roe <daniel@roe.dev>
2023-11-22 22:01:00 +0000

    chore: import types from '#app'

--- packages/nuxt/src/pages/runtime/plugins/router.ts
+++ packages/nuxt/src/pages/runtime/plugins/router.ts
@@ -13,8 +13,7 @@ import { isEqual, withoutBase } from 'ufo'
 
 import type { PageMeta } from '../composables'
 
-import type { RouteMiddleware } from '#app/composables/router'
-import type { Plugin } from '#app/nuxt'
+import type { Plugin, RouteMiddleware } from '#app'
 import { defineNuxtPlugin, useRuntimeConfig } from '#app/nuxt'
 import { clearError, showError, useError } from '#app/composables/error'
 import { navigateTo } from '#app/composables/router'
