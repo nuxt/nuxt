f8b5a875a7356456090772a40984f7e63c88be66
Daniel Roe <daniel@roe.dev>
2023-11-23 21:08:28 +0000

    chore: lint

--- packages/kit/src/logger.ts
+++ packages/kit/src/logger.ts
@@ -4,5 +4,5 @@ import type { ConsolaOptions } from 'consola';
 export const logger = consola
 
 export function useLogger (tag?: string, options: Partial<ConsolaOptions> = {}) {
-  return tag ? logger.create(options).withTag(tag) : logger;
+  return tag ? logger.create(options).withTag(tag) : logger
 }
