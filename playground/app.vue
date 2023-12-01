155b7528236677ed10c5b783f969c5672964624f
Eugen Guriev <gurievcreative@gmail.com>
2023-12-01 23:08:18 +0200

    chore: clean up playgroud

--- playground/app.vue
+++ playground/app.vue
@@ -3,7 +3,11 @@
 
 <template>
   <!-- Edit this file to play around with Nuxt but never commit changes! -->
-  <div>Nuxt 3 Playground</div>
+  <div>
+    Nuxt 3 Playground
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
