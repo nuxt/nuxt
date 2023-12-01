4eb5d2ab78645f573b34855d9d3efe90cb079e6c
Eugen Guriev <gurievcreative@gmail.com>
2023-12-01 23:07:12 +0200

    chore: clean up playgroud

--- playground/app.vue
+++ playground/app.vue
@@ -1,81 +1,9 @@
 <script setup lang="ts">
-const { data, pending, error, refresh } = useAsyncData(
-  "test",
-  () =>
-    new Promise((resolve) => {
-      console.log("fetching data...");
-      setTimeout(() => {
-        resolve("Hello World");
-      }, 1000);
-    }), {
-      deduplication: true
-    }
-);
-useAsyncData(
-  "test",
-  () =>
-    new Promise((resolve) => {
-      console.log("fetching data...");
-      setTimeout(() => {
-        resolve("Hello World");
-      }, 1000);
-    }), {
-      deduplication: true
-    }
-);
-useAsyncData(
-  "test",
-  () =>
-    new Promise((resolve) => {
-      console.log("fetching data...");
-      setTimeout(() => {
-        resolve("Hello World");
-      }, 1000);
-    }), {
-      deduplication: true
-    }
-);
-useAsyncData(
-  "test",
-  () =>
-    new Promise((resolve) => {
-      console.log("fetching data...");
-      setTimeout(() => {
-        resolve("Hello World");
-      }, 1000);
-    }), {
-      deduplication: true
-    }
-);
-useAsyncData(
-  "test",
-  () =>
-    new Promise((resolve) => {
-      console.log("fetching data...");
-      setTimeout(() => {
-        resolve("Hello World");
-      }, 1000);
-    }), {
-      deduplication: true
-    }
-);
-useAsyncData(
-  "test",
-  () =>
-    new Promise((resolve) => {
-      console.log("fetching data...");
-      setTimeout(() => {
-        resolve("Hello World");
-      }, 1000);
-    }), {
-      deduplication: true
-    }
-);
 </script>
 
 <template>
   <!-- Edit this file to play around with Nuxt but never commit changes! -->
-  <div>Nuxt 3 Playground {{ data }}</div>
+  <div>Nuxt 3 Playground</div>
 </template>
 
 <style scoped></style>
