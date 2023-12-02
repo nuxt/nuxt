a62fa5eea3744a8f4173939bb72e98f1214c864a
julien huang <julien.huang@outlook.fr>
2023-12-02 14:34:02 +0100

    test(basic): throw error in dependsOnPlugin and make it depend on plugin that don't exist

--- test/fixtures/basic/plugins/dependsOnPlugin.ts
+++ test/fixtures/basic/plugins/dependsOnPlugin.ts
@@ -1,7 +1,11 @@
 export default defineNuxtPlugin({
   name: 'depends-on-plugin',
-  dependsOn: ['async-plugin'],
+  dependsOn: ['async-plugin', 'unexisting-plugin'],
   async setup () {
+    const nuxtApp = useNuxtApp()
+    if (!nuxtApp.$asyncPlugin) {
+      throw new Error('$asyncPlugin is not defined')
+    }
     await new Promise(resolve => setTimeout(resolve, 100))
   },
   parallel: true
