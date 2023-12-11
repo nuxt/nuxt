34570d95699da06472269d2fc9fed44450d52de7
Daniel Roe <daniel@roe.dev>
2023-12-11 18:20:11 +0000

    chore: update vitest dependencies (#24659)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,5 @@
 import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-import { configDefaults, coverageConfigDefaults } from 'vitest/config'
+import { configDefaults, defineConfig } from 'vitest/config'
 import { isWindows } from 'std-env'
 
 export default defineConfig({
@@ -19,13 +18,13 @@ export default defineConfig({
     globalSetup: './test/setup.ts',
     setupFiles: ['./test/setup-env.ts'],
     testTimeout: isWindows ? 60000 : 10000,
-    coverage: {
-      // TODO: remove when we upgrade to vitest 0.34.0: https://github.com/vitest-dev/vitest/pull/3794
-      exclude: [...coverageConfigDefaults.exclude, '**/virtual:nuxt:**'],
-    },
     // Excluded plugin because it should throw an error when accidentally loaded via Nuxt
-    exclude: [...configDefaults.exclude, '**/test/nuxt/**', '**/test.ts', '**/this-should-not-load.spec.js'],
-    maxThreads: process.env.TEST_ENV === 'dev' ? 1 : undefined,
-    minThreads: process.env.TEST_ENV === 'dev' ? 1 : undefined
+    exclude: [...configDefaults.exclude, 'nuxt/**', '**/test.ts', '**/this-should-not-load.spec.js'],
+    poolOptions: {
+      threads: {
+        maxThreads: process.env.TEST_ENV === 'dev' ? 1 : undefined,
+        minThreads: process.env.TEST_ENV === 'dev' ? 1 : undefined
+      }
+    }
   }
 })
