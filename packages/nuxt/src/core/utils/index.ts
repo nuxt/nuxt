01ef2ad4372fa031947d9e06d92ded09356260ca
Michael Brevard <yonshi29@gmail.com>
2023-12-13 00:52:05 +0200

    perf(nuxt): early return for empty directories

--- packages/nuxt/src/core/utils/index.ts
+++ packages/nuxt/src/core/utils/index.ts
@@ -2,6 +2,9 @@ export * from './names'
 export * from './plugins'
 
 export function uniqueBy<T, K extends keyof T> (arr: T[], key: K) {
+  if (arr.length === 0) {
+    return [];
+  }
   const res: T[] = []
   const seen = new Set<T[K]>()
   for (const item of arr) {
