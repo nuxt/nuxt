e4acfb40630bb0d1d77c026efab9173fee61056f
Daniel Roe <daniel@roe.dev>
2023-12-15 12:36:13 +0000

    fix: add brackets

--- packages/schema/src/config/vite.ts
+++ packages/schema/src/config/vite.ts
@@ -39,7 +39,7 @@ export default defineUntypedSchema({
         if (val) {
           consola.warn('Directly configuring the `vite.publicDir` option is not supported. Instead, set `dir.public`. You can read more in `https://nuxt.com/docs/api/nuxt-config#public`.')
         }
-        return val ?? await Promise.all([get('srcDir'), get('dir')]).then([srcDir, dir] => resolve(srcDir, dir.public))
+        return val ?? await Promise.all([get('srcDir'), get('dir')]).then(([srcDir, dir]) => resolve(srcDir, dir.public))
       }
     },
     vue: {
