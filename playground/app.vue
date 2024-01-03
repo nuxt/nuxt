1576b461fae18f65423fa16c59ff7e32e8ca1c46
Pooya Parsa <pooya@pi0.io>
2024-01-03 13:08:25 +0100

    revert playground

--- playground/app.vue
+++ playground/app.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-const base64 = Buffer.from('Nuxt is Awesome!', 'utf8').toString('base64')
 </script>
 
 <template>
   <!-- Edit this file to play around with Nuxt but never commit changes! -->
   <div>
-    {{ base64 }}
+    Nuxt 3 Playground
   </div>
 </template>
 
