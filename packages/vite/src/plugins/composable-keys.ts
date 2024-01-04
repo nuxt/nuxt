00e424844007aa61caf12e0440fe0b30ddab1f27
Michael Brevard <yonshi29@gmail.com>
2024-01-04 20:41:37 +0200

    composable object.fromEntries

--- packages/vite/src/plugins/composable-keys.ts
+++ packages/vite/src/plugins/composable-keys.ts
@@ -22,7 +22,10 @@ const NUXT_LIB_RE = /node_modules\/(nuxt|nuxt3|nuxt-nightly)\//
 const SUPPORTED_EXT_RE = /\.(m?[jt]sx?|vue)/
 
 export const composableKeysPlugin = createUnplugin((options: ComposableKeysOptions) => {
-  const composableMeta = Object.fromEntries(options.composables.map(({ name, ...meta }) => [name, meta]))
+  const composableMeta = options.composables.reduce((acc, { name, ...meta }) => {
+    acc[name] = meta
+    return acc
+  }, Object.create(null))
 
   const maxLength = Math.max(...options.composables.map(({ argumentLength }) => argumentLength))
   const keyedFunctions = new Set(options.composables.map(({ name }) => name))
