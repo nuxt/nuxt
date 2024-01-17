eb23cfbab18b06a56c06cbea1469bc3e249e6782
Daniel Roe <daniel@roe.dev>
2024-01-17 11:13:12 +0000

    chore: remove pages from `basic` fixture

--- test/fixtures/basic/pages/suspense-multiple-target/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-await new Promise(resolve => setTimeout(resolve, 50))
-</script>
-
-<template>
-  <div>
-    Hello {{ Object.keys($route.query).join(',') }}
-  </div>
-</template>
