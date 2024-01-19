0d5a055d47a7792ac9e64e8221809c8994c1dee4
Daniel Roe <daniel@roe.dev>
2024-01-19 20:22:50 +0000

    style: lint

--- test/fixtures/basic/modules/lazy-import-components/runtime/components.ts
+++ test/fixtures/basic/modules/lazy-import-components/runtime/components.ts
@@ -1,7 +1,7 @@
-import { defineComponent, h } from 'vue';
+import { defineComponent, h } from 'vue'
 
 export const NComp = defineComponent({
-    name: 'NComp',
-    props: { message: String },
-    render: (props: any) => h('h1', props.message),
-});
+  name: 'NComp',
+  props: { message: String },
+  render: (props: any) => h('h1', props.message),
+})
