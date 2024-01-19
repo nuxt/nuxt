f7fff635d87baeb278eb73adca9a5d1ee1ad6745
Daniel Roe <daniel@roe.dev>
2024-01-19 22:44:04 +0000

    style: lint

--- test/fixtures/basic/modules/import-components/runtime/components.ts
+++ test/fixtures/basic/modules/import-components/runtime/components.ts
@@ -1,13 +1,13 @@
-import { defineComponent, h } from 'vue';
+import { defineComponent, h } from 'vue'
 
 export default defineComponent({
-    name: 'DComp',
-    props: { message: String },
-    render: (props: any) => h('h1', props.message),
-});
+  name: 'DComp',
+  props: { message: String },
+  render: (props: any) => h('h1', props.message),
+})
 
 export const NComp = defineComponent({
-    name: 'NComp',
-    props: { message: String },
-    render: (props: any) => h('h1', props.message),
-});
+  name: 'NComp',
+  props: { message: String },
+  render: (props: any) => h('h1', props.message),
+})
