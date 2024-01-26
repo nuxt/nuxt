ee969331b5977af359340ba8a87abcb320ebdf36
Daniel Roe <daniel@roe.dev>
2024-01-26 22:15:40 +0000

    test: add regression test for components within islands

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1383,6 +1383,7 @@ describe.skipIf(isDev() || isWebpack)('inlining component styles', () => {
     '{--postcss:"postcss"}', // <style lang=postcss>
     '{--scoped:"scoped"}', // <style lang=css>
     '{--shared-component:"shared-component"}', // styles in a chunk shared between pages
+    '{--server-only-child:"server-only-child"}', // child of a server-only component
     '{--server-only:"server-only"}' // server-only component not in client build
     // TODO: ideally both client/server components would have inlined css when used
     // '{--client-only:"client-only"}', // client-only component not in server build
