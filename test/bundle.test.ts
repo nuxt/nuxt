a8a138e02d3bdb4e0b318efcb0e8115ae3ff89e6
Daniel Roe <daniel@roe.dev>
2024-02-09 12:09:10 +0000

    test: update bundle size snapshot

--- test/bundle.test.ts
+++ test/bundle.test.ts
@@ -35,7 +35,7 @@ describe.skipIf(process.env.SKIP_BUNDLE_SIZE === 'true' || process.env.ECOSYSTEM
     expect.soft(roundToKilobytes(serverStats.totalBytes)).toMatchInlineSnapshot(`"202k"`)
 
     const modules = await analyzeSizes('node_modules/**/*', serverDir)
-    expect.soft(roundToKilobytes(modules.totalBytes)).toMatchInlineSnapshot(`"1333k"`)
+    expect.soft(roundToKilobytes(modules.totalBytes)).toMatchInlineSnapshot(`"1334k"`)
 
     const packages = modules.files
       .filter(m => m.endsWith('package.json'))
