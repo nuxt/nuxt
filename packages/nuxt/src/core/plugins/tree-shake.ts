e3e32450db414e04b9330a20fd15b653ebc5b667
Michael Brevard <yonshi29@gmail.com>
2024-02-09 17:26:07 +0200

    Update tree-shake.ts

--- packages/nuxt/src/core/plugins/tree-shake.ts
+++ packages/nuxt/src/core/plugins/tree-shake.ts
@@ -31,8 +31,7 @@ export const TreeShakeComposablesPlugin = createUnplugin((options: TreeShakeComp
 
       const s = new MagicString(code)
       const strippedCode = stripLiteral(code)
-      const matches = strippedCode.matchAll(COMPOSABLE_RE_GLOBAL)
-      for (const match of matches) {
+      for (const match of strippedCode.matchAll(COMPOSABLE_RE_GLOBAL)) {
         s.overwrite(match.index!, match.index! + match[0].length, `${match[1]} false && /*@__PURE__*/ ${match[2]}`)
       }
 
