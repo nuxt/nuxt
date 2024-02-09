6cbfdac2a1d63365b4e01c62a1fb6febbd677e68
Marc Cremer <marc@cremer.dev>
2024-02-10 00:08:58 +0100

    add appBaseURL to dynamic client import

--- packages/nuxt/src/app/components/nuxt-island.ts
+++ packages/nuxt/src/app/components/nuxt-island.ts
@@ -29,13 +29,13 @@ const getId = import.meta.client ? () => (id++).toString() : randomUUID
 
 const components = import.meta.client ? new Map<string, Component>() : undefined
 
-async function loadComponents (source = '/', paths: NuxtIslandResponse['components']) {
+async function loadComponents (source = '/', paths: NuxtIslandResponse['components'],baseURL:string = "") {
   const promises = []
 
   for (const component in paths) {
     if (!(components!.has(component))) {
       promises.push((async () => {
-        const chunkSource = join(source, paths[component].chunk)
+        const chunkSource = join(source,baseURL, paths[component].chunk)
         const c = await import(/* @vite-ignore */ chunkSource).then(m => m.default || m)
         components!.set(component, c)
       })())
@@ -194,7 +194,7 @@ export default defineComponent({
 
         if (selectiveClient && import.meta.client) {
           if (canLoadClientComponent.value && res.components) {
-            await loadComponents(props.source, res.components)
+            await loadComponents(props.source, res.components,useRuntimeConfig().app.baseURL)
           }
         }
 
