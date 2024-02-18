012fc4e81d13e232489d77072ff95deebbb11169
Michael Brevard <yonshi29@gmail.com>
2024-02-18 23:54:23 +0200

    fix: escapes

--- packages/nuxt/src/core/templates.ts
+++ packages/nuxt/src/core/templates.ts
@@ -17,8 +17,8 @@ export const vueShim: NuxtTemplate = {
       return ''
     }
 
-    return `declare module \'*.vue\' {
-  import { DefineComponent } from \'vue\'
+    return `declare module '*.vue' {
+  import { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component\n}`
   }
