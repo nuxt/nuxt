adb32fcd38e7f6d7b18d828062df7dc11bb664e9
julien huang <julien.huang@outlook.fr>
2024-02-20 20:13:17 +0100

    fix: mark island specific component with @__PURE__

--- packages/nuxt/src/app/components/nuxt-teleport-island-component.ts
+++ packages/nuxt/src/app/components/nuxt-teleport-island-component.ts
@@ -13,6 +13,7 @@ type ExtendedComponent = Component & {
  * component only used with componentsIsland
  * this teleport the component in SSR only if it needs to be hydrated on client
  */
+/* @__PURE__ */
 export default defineComponent({
   name: 'NuxtTeleportIslandComponent',
   props: {
