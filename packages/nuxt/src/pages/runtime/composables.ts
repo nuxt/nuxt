3207fd6d10d35bd58fe8a77c51d4c1e8e4740a88
Daniel Roe <daniel@roe.dev>
2024-02-25 22:50:25 +0000

    fix: allow setting 'all' as a mode

--- packages/nuxt/src/pages/runtime/composables.ts
+++ packages/nuxt/src/pages/runtime/composables.ts
@@ -39,7 +39,7 @@ export interface PageMeta {
   /** Set to `false` to avoid scrolling to top on page navigations */
   scrollToTop?: boolean | ((to: RouteLocationNormalizedLoaded, from: RouteLocationNormalizedLoaded) => boolean)
   /** Set the render mode between client only and island/server only. Currently only server is supported . */
-  mode?: 'server'
+  mode?: 'server' | 'all'
 }
 
 declare module 'vue-router' {
