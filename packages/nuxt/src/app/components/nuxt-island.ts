6ecb6a05728170da9e0da5623ce7c9e5223f2b79
Michael Brevard <yonshi29@gmail.com>
2024-02-27 19:46:00 +0200

    test: testing timeout reason

--- packages/nuxt/src/app/components/nuxt-island.ts
+++ packages/nuxt/src/app/components/nuxt-island.ts
@@ -77,20 +77,7 @@ export default defineComponent({
     const error = ref<unknown>(null)
     const config = useRuntimeConfig()
     const nuxtApp = useNuxtApp()
-    const filteredProps = computed(() => {
-      if (props.props) {
-        const filtered = Object.create(null)
-        for (const key in props.props) {
-          if (!key.startsWith('data-v-')) {
-            filtered[key] = props.props[key]
-          }
-        }
-        return filtered
-      }
-      else {
-        return {}
-      }
-    })
+    const filteredProps = computed(() => props.props ? Object.fromEntries(Object.entries(props.props).filter(([key]) => !key.startsWith('data-v-'))) : {})
     const hashId = computed(() => hash([props.name, filteredProps.value, props.context, props.source]))
     const instance = getCurrentInstance()!
     const event = useRequestEvent()
