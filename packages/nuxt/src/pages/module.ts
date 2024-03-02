f128e0a1290d7a81a19c967a235a7b7272c46691
Michael Brevard <yonshi29@gmail.com>
2024-03-02 18:10:13 +0200

    fix: remove nuxt type import

--- packages/nuxt/src/pages/module.ts
+++ packages/nuxt/src/pages/module.ts
@@ -4,7 +4,7 @@ import { addBuildPlugin, addComponent, addPlugin, addTemplate, addTypeTemplate, 
 import { dirname, join, relative, resolve } from 'pathe'
 import { genImport, genObjectFromRawEntries, genString } from 'knitwork'
 import { joinURL } from 'ufo'
-import type { Nuxt, NuxtApp, NuxtPage } from 'nuxt/schema'
+import type { NuxtApp, NuxtPage } from 'nuxt/schema'
 import { createRoutesContext } from 'unplugin-vue-router'
 import { resolveOptions } from 'unplugin-vue-router/options'
 import type { EditableTreeNode, Options as TypedRouterOptions } from 'unplugin-vue-router'
@@ -459,7 +459,7 @@ export default defineNuxtModule({
     const composablesFile = genString(relative(join(nuxt.options.buildDir, 'types'), resolve(runtimeDir, 'composables')))
     addTemplate({
       filename: 'types/middleware.d.ts',
-      getContents: ({  app }: { app: NuxtApp }) => {
+      getContents: ({ app }: { app: NuxtApp }) => {
         const namedMiddleware = app.middleware.filter(mw => !mw.global)
         let middlewareKey = ''
         if (namedMiddleware.length) {
