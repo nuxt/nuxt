70c00f4a5fb1a387384d264271808a2b80491a99
Michael Brevard <yonshi29@gmail.com>
2024-03-08 15:31:37 +0200

    perf: fetch.ts

--- packages/nuxt/src/app/composables/fetch.ts
+++ packages/nuxt/src/app/composables/fetch.ts
@@ -253,7 +253,8 @@ function generateOptionSegments <_ResT, DataT, DefaultT>(opts: UseFetchOptions<_
     if (!obj) { continue }
 
     const unwrapped: Record<string, string> = {}
-    for (const [key, value] of Object.entries(obj)) {
+    for (const key in obj) {
+      const value = obj[key]
       unwrapped[toValue(key)] = toValue(value)
     }
     segments.push(unwrapped)
