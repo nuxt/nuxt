013d5c71e1d5e39912f69187377834245fe90a78
Daniel Roe <daniel@roe.dev>
2024-03-15 09:22:03 -0700

    chore: reduce diff

--- packages/nuxt/src/app/nuxt.ts
+++ packages/nuxt/src/app/nuxt.ts
@@ -43,11 +43,11 @@ export interface RuntimeNuxtHooks {
   'link:prefetch': (link: string) => HookResult
   'page:start': (Component?: VNode) => HookResult
   'page:finish': (Component?: VNode) => HookResult
-  'page:loading:start': () => HookResult
-  'page:loading:end': () => HookResult
   'page:transition:start': () => HookResult
   'page:transition:finish': (Component?: VNode) => HookResult
   'page:view-transition:start': (transition: ViewTransition) => HookResult
+  'page:loading:start': () => HookResult
+  'page:loading:end': () => HookResult
   'layout:start': (Component?: VNode) => HookResult
   'layout:finish': (Component?: VNode) => HookResult
   'layout:transition:finish': (Component?: VNode) => HookResult
