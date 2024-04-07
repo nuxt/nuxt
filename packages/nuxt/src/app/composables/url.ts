2b48eb01a1195ec3a7fbc3abd2ecaff2e8977561
Damian Glowala <damian.glowala.rebkow@gmail.com>
2024-04-07 18:08:15 +0200

    fix(nuxt): add opts param to `useRequestURL`

--- packages/nuxt/src/app/composables/url.ts
+++ packages/nuxt/src/app/composables/url.ts
@@ -2,9 +2,12 @@ import { getRequestURL } from 'h3'
 import { useRequestEvent } from './ssr'
 
 /** @since 3.5.0 */
-export function useRequestURL () {
+export function useRequestURL (opts?: {
+  xForwardedHost?: boolean
+  xForwardedProto?: boolean
+}) {
   if (import.meta.server) {
-    return getRequestURL(useRequestEvent()!)
+    return getRequestURL(useRequestEvent()!, opts)
   }
   return new URL(window.location.href)
 }
