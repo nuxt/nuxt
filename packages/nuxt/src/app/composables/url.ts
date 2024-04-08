444ac930848721e9e22cf41c983f03c6eda6cb2f
Damian Głowala <damian.glowala.rebkow@gmail.com>
2024-04-08 10:53:30 +0200

    refactor: get param type directly from H3

--- packages/nuxt/src/app/composables/url.ts
+++ packages/nuxt/src/app/composables/url.ts
@@ -2,10 +2,7 @@ import { getRequestURL } from 'h3'
 import { useRequestEvent } from './ssr'
 
 /** @since 3.5.0 */
-export function useRequestURL (opts?: {
-  xForwardedHost?: boolean
-  xForwardedProto?: boolean
-}) {
+export function useRequestURL (opts?: Parameters<typeof getRequestURL>[1]) {
   if (import.meta.server) {
     return getRequestURL(useRequestEvent()!, opts)
   }
