5653d9e255780ae5a8c5cfd6607f361710e85a02
Michael Brevard <yonshi29@gmail.com>
2024-04-08 20:16:18 +0300

    wip: retrying through mouse.wheel

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -2567,9 +2567,7 @@ describe('lazy import components', () => {
     expect(html).not.toContain('This shouldn\'t be visible at first!')
     const { page } = await renderPage('/lazy-import-components')
     expect(await page.locator('body').getByText('This shouldn\'t be visible at first!').all()).toHaveLength(0)
-    await page.evaluate(() => {
-      window.scrollTo(0, document.body.scrollHeight)
-    })
+    await page.mouse.wheel(0,4000)
     expect(await page.locator('body').getByText('This shouldn\'t be visible at first!').all()).toHaveLength(1)
   })
 })
