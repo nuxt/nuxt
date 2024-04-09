3be1cb3399ba8fcc77815c3821197110cd58f953
Daniel Roe <daniel@roe.dev>
2024-04-09 20:36:01 +0200

    refactor: slightly reorganise

--- packages/nuxt/src/pages/runtime/plugins/prerender.ts
+++ packages/nuxt/src/pages/runtime/plugins/prerender.ts
@@ -1,5 +1,6 @@
-import type { RouterConfig } from '@nuxt/schema'
+import type { RouteRecordRaw } from 'vue-router'
 import { joinURL } from 'ufo'
+
 import { defineNuxtPlugin } from '#app/nuxt'
 import { prerenderRoutes } from '#app/composables/ssr'
 // @ts-expect-error virtual file
@@ -7,34 +8,34 @@ import _routes from '#build/routes'
 // @ts-expect-error virtual file
 import routerOptions from '#build/router.options'
 
-const OPTIONAL_PARAM_RE = /^\/?:.*(\?|\(\.\*\)\*)$/
-
 export default defineNuxtPlugin(() => {
   if (!import.meta.server || !import.meta.prerender || routerOptions.hashMode) {
     return
   }
+
   const routes = routerOptions.routes?.(_routes) ?? _routes
-  const routesToPrerender = new Set<string>()
-  const processRoutes = (
-    routes: ReturnType<NonNullable<RouterConfig['routes']>>,
-    currentPath = '/',
-  ) => {
-    for (const route of routes) {
-      // Add root of optional dynamic paths and catchalls
-      if (OPTIONAL_PARAM_RE.test(route.path) && !route.children?.length) {
-        routesToPrerender.add(currentPath)
-      }
-      // Skip dynamic paths
-      if (route.path.includes(':')) {
-        continue
-      }
-      const fullPath = joinURL(currentPath, route.path)
-      routesToPrerender.add(fullPath)
-      if (route.children) {
-        processRoutes(route.children, fullPath)
-      }
+  prerenderRoutes(Array.from(processRoutes(routes)))
+})
+
+// Implementation
+
+const OPTIONAL_PARAM_RE = /^\/?:.*(\?|\(\.\*\)\*)$/
+
+function processRoutes (routes: RouteRecordRaw[], currentPath = '/', routesToPrerender = new Set<string>()) {
+  for (const route of routes) {
+    // Add root of optional dynamic paths and catchalls
+    if (OPTIONAL_PARAM_RE.test(route.path) && !route.children?.length) {
+      routesToPrerender.add(currentPath)
+    }
+    // Skip dynamic paths
+    if (route.path.includes(':')) {
+      continue
+    }
+    const fullPath = joinURL(currentPath, route.path)
+    routesToPrerender.add(fullPath)
+    if (route.children) {
+      processRoutes(route.children, fullPath, routesToPrerender)
     }
   }
-  processRoutes(routes)
-  prerenderRoutes(Array.from(routesToPrerender))
-})
+  return routesToPrerender
+}
