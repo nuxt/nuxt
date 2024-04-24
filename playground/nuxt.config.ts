02bf8031a3a33d66d73d8012b720a898e5974849
Daniel Roe <daniel@roe.dev>
2024-04-24 17:07:37 +0100

    chore: revert playground changes

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,11 +1,3 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  future: {
-    v4: true
-  },
-  modules: [
-    function (_, nuxt) {
-      console.log(nuxt.options.experimental)
-    }
-  ]
 })
