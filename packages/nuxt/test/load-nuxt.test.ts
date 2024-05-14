79d60f2dbe5917e3f6234c100c135c0b45f8bd58
Julien Huang <julien.huang@leetchi.com>
2024-05-14 20:17:57 +0200

    test(nuxt): add unit test

--- packages/nuxt/test/load-nuxt.test.ts
+++ packages/nuxt/test/load-nuxt.test.ts
@@ -1,10 +1,29 @@
 import { fileURLToPath } from 'node:url'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi, afterEach } from 'vitest'
+import { version } from "../package.json"
 import { normalize } from 'pathe'
 import { withoutTrailingSlash } from 'ufo'
 import { loadNuxt } from '../src'
+import { readPackageJSON } from 'pkg-types'
 
 const repoRoot = withoutTrailingSlash(normalize(fileURLToPath(new URL('../../../', import.meta.url))))
+
+vi.stubGlobal('console', {
+  ...console,
+  error: vi.fn(console.error),
+})
+
+vi.mock('pkg-types', async (og) => {
+  const originalPkgTypes = (await og<typeof import('pkg-types')>())
+  return {
+    ...originalPkgTypes,
+    readPackageJSON: vi.fn(originalPkgTypes.readPackageJSON),
+  }
+})
+
+afterEach(() => {
+  vi.clearAllMocks()
+})
 
 describe('loadNuxt', () => {
   it('respects hook overrides', async () => {
@@ -22,5 +41,37 @@ describe('loadNuxt', () => {
     })
     await nuxt.close()
     expect(hookRan).toBe(true)
+  })
+})
+
+describe('dependency mismatch', () => {
+  it('expect mismatched dependency to log a warning',async  () => {
+    vi.mocked(readPackageJSON).mockReturnValue(Promise.resolve({
+      version: '3.0.0'
+    }))
+
+    const nuxt = await loadNuxt({
+      cwd: repoRoot,
+    })
+
+    expect(console.error).toHaveBeenCalledWith(`Version mismatch for @nuxt/kit and nuxt: expected ${version} (nuxt) but got 3.0.0`)
+    expect(console.error).toHaveBeenCalledWith(`Version mismatch for @nuxt/schema and nuxt: expected ${version} (nuxt) but got 3.0.0`)
+    
+    vi.mocked(readPackageJSON).mockRestore()
+    await nuxt.close()
+  })
+  it('expect no warning when dependency version matches',async  () => {
+    vi.mocked(readPackageJSON).mockReturnValue(Promise.resolve({
+      version
+    }))
+
+    const nuxt = await loadNuxt({
+      cwd: repoRoot,
+    })
+
+    expect(console.error).not.toHaveBeenCalled()
+
+    await nuxt.close()
+    vi.mocked(readPackageJSON).mockRestore()
   })
 })
