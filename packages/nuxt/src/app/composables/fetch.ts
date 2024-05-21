b9dbfec1f9aad2e27636bef49689c8db77166100
Daniel Roe <daniel@roe.dev>
2024-05-21 17:32:02 +0100

    chore: move comment

--- packages/nuxt/src/app/composables/fetch.ts
+++ packages/nuxt/src/app/composables/fetch.ts
@@ -4,11 +4,11 @@ import type { MaybeRef, Ref } from 'vue'
 import { computed, reactive, toValue } from 'vue'
 import { hash } from 'ohash'
 
-// TODO: temporary module for backwards compatibility
-
 import { useRequestFetch } from './ssr'
 import type { AsyncData, AsyncDataOptions, KeysOf, MultiWatchSources, PickFrom } from './asyncData'
 import { useAsyncData } from './asyncData'
+
+// TODO: temporary module for backwards compatibility
 import type { DefaultAsyncDataErrorValue, DefaultAsyncDataValue } from '#app/defaults'
 
 // @ts-expect-error virtual file
