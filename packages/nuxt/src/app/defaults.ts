9ee07126e0b5d494d85d3576ba8cf29803102a2e
Daniel Roe <daniel@roe.dev>
2024-05-22 22:00:41 +0100

    fix: use v3 defaults

--- packages/nuxt/src/app/defaults.ts
+++ packages/nuxt/src/app/defaults.ts
@@ -1,7 +1,7 @@
 // TODO: temporary module for backwards compatibility
 
-export type DefaultAsyncDataErrorValue = undefined
-export type DefaultAsyncDataValue = undefined
-export type DefaultErrorValue = undefined
+export type DefaultAsyncDataErrorValue = null
+export type DefaultAsyncDataValue = null
+export type DefaultErrorValue = null
 
 export {}
