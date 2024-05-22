7efa76664b132b0b8ecd5a55cbdc82efe07a4e00
Daniel Roe <daniel@roe.dev>
2024-05-22 21:43:56 +0100

    fix(nuxt): provide default data type values in nuxt/app dir

---
+++ packages/nuxt/src/app/defaults.ts
@@ -0,0 +1,7 @@
+// TODO: temporary module for backwards compatibility
+
+export type DefaultAsyncDataErrorValue = undefined
+export type DefaultAsyncDataValue = undefined
+export type DefaultErrorValue = undefined
+
+export {}
