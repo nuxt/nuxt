04b14e473df5018ad3e2c0c303aa76e8c2c35bf2
Israel Ortuño <ai.ortuno@gmail.com>
2024-05-23 17:07:16 +0200

    fix: use plain boolean / number value

--- packages/nuxt/src/core/templates.ts
+++ packages/nuxt/src/core/templates.ts
@@ -410,7 +410,7 @@ export const nuxtConfigTemplate: NuxtTemplate = {
       `export const vueAppRootContainer = ${ctx.nuxt.options.app.rootId ? `'#${ctx.nuxt.options.app.rootId}'` : `'body > ${ctx.nuxt.options.app.rootTag}'`}`,
       `export const viewTransition = ${ctx.nuxt.options.experimental.viewTransition}`,
       `export const appId = ${JSON.stringify(ctx.nuxt.options.appId)}`,
-      `export const outdatedBuildInterval = ${JSON.stringify(ctx.nuxt.options.experimental.checkOutdatedBuildInterval)}`,
+      `export const outdatedBuildInterval = ${ctx.nuxt.options.experimental.checkOutdatedBuildInterval}`,
     ].join('\n\n')
   },
 }
