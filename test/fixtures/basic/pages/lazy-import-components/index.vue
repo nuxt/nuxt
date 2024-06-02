f4f8ca08dcf9ee6ead5824656d702daba53c6397
Michael Brevard <yonshi29@gmail.com>
2024-06-03 00:02:31 +0300

    chore: add network idle component

--- test/fixtures/basic/pages/lazy-import-components/index.vue
+++ test/fixtures/basic/pages/lazy-import-components/index.vue
@@ -3,9 +3,10 @@
     <LazyNCompAll message="lazy-named-comp-all" />
     <LazyNCompClient message="lazy-named-comp-client" />
     <LazyNCompServer message="lazy-named-comp-server" />
+    <LazyIdleDelayedNetwork />
     <div style="height:3000px">
       This is a very tall div
     </div>
-    <LazyVisibleDelayedHydration />
+    <LazyVisibleDelayedVisible />
   </div>
 </template>
