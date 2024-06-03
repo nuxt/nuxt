4dee10a603605dead805abf1c7aeffda7cca58b1
Nicolas Payot <nicolas.payot@gmail.com>
2024-06-03 17:31:04 +0200

    refactor(nuxt): rename multiApp export in clientConfigTemplate

--- packages/nuxt/src/app/plugins/revive-payload.client.ts
+++ packages/nuxt/src/app/plugins/revive-payload.client.ts
@@ -5,7 +5,7 @@ import { createError } from '../composables/error'
 import { defineNuxtPlugin, useNuxtApp } from '../nuxt'
 
 // @ts-expect-error Virtual file.
-import { appId, componentIslands, multiApp } from '#build/nuxt.config.mjs'
+import { appId, componentIslands, runningMultiApp } from '#build/nuxt.config.mjs'
 
 const revivers: Record<string, (data: any) => any> = {
   NuxtError: data => createError(data),
@@ -50,7 +50,7 @@ export default defineNuxtPlugin({
     }
     Object.assign(nuxtApp.payload, await nuxtApp.runWithContext(getNuxtClientPayload))
     // For backwards compatibility - TODO: remove later
-    if (!multiApp) {
+    if (!runningMultiApp) {
       window.__NUXT__ = nuxtApp.payload
     } else {
       window.__NUXT__ ||= {}
