7f6e0e063b883a694299135dffa9963227dc3051
Michael Brevard <yonshi29@gmail.com>
2024-06-05 18:29:00 +0300

    perf: more templates.ts

--- packages/nuxt/src/components/templates.ts
+++ packages/nuxt/src/components/templates.ts
@@ -84,9 +84,14 @@ export const componentsIslandsTemplate: NuxtTemplate = {
       // .server components without a corresponding .client component will need to be rendered as an island
       (component.mode === 'server' && !components.some(c => c.pascalName === component.pascalName && c.mode === 'client')),
     )
-    const pageExports = pages?.filter(p => (p.mode === 'server' && p.file && p.name)).map((p) => {
-      return `"page:${p.name}": defineAsyncComponent(${genDynamicImport(p.file!)}.then(c => c.default || c))`
-    }) || []
+    const pageExports: string[] = []
+    if (pages?.length) {
+      for (const p of pages) {
+        if (p.mode === 'server' && p.file && p.name) {
+          pageExports.push(`"page:${p.name}": defineAsyncComponent(${genDynamicImport(p.file!)}.then(c => c.default || c))`)
+        }
+      }
+    }
 
     return [
       'import { defineAsyncComponent } from \'vue\'',
@@ -107,13 +112,16 @@ export const componentsTypeTemplate = {
   filename: 'components.d.ts' as const,
   getContents: ({ app, nuxt }) => {
     const buildDir = nuxt.options.buildDir
-    const componentTypes = app.components.filter(c => !c.island).map((c) => {
-      const type = `typeof ${genDynamicImport((isAbsolute(c.filePath) ? relative(buildDir, c.filePath) : c.filePath).replace(/\b\.(?!vue)\w+$/g, ''), { wrapper: false })}['${c.export}']`
-      return [
-        c.pascalName,
-        c.island || c.mode === 'server' ? `IslandComponent<${type}>` : type,
-      ]
-    })
+    const componentTypes: [string, string][] =  []
+    for (const c of app.components) {
+      if (!c.island) {
+        const type = `typeof ${genDynamicImport((isAbsolute(c.filePath) ? relative(buildDir, c.filePath) : c.filePath).replace(/\b\.(?!vue)\w+$/g, ''), { wrapper: false })}['${c.export}']`
+        componentTypes.push([
+          c.pascalName,
+          c.island || c.mode === 'server' ? `IslandComponent<${type}>` : type,
+        ])
+      }
+    }
 
     const islandType = 'type IslandComponent<T extends DefineComponent> = T & DefineComponent<{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, SlotsType<{ fallback: { error: unknown } }>>'
 
