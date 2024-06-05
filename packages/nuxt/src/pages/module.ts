ee62a0bd8b43e1283e185c83de3b344097535c14
Michael Brevard <yonshi29@gmail.com>
2024-06-05 22:13:38 +0300

    fix: more types

--- packages/nuxt/src/pages/module.ts
+++ packages/nuxt/src/pages/module.ts
@@ -436,7 +436,7 @@ export {}`,
         const routerOptionsFiles = await resolveRouterOptions()
         const rawOptions = []
         for (const key in nuxt.options.router.options) {
-          rawOptions.push([key, genString(nuxt.options.router.options[key as keyof typeof nuxt.options.router.options])])
+          rawOptions.push([key, genString(nuxt.options.router.options[key as keyof typeof nuxt.options.router.options] as string)])
         }
         const configRouterOptions = genObjectFromRawEntries(rawOptions)
 
