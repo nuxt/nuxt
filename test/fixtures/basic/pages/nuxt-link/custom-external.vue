45aeee6c1a4c33ed6aa7b684609264c199f3dceb
Daniel Roe <daniel@roe.dev>
2024-06-07 21:06:24 +0100

    fix(nuxt): handle more edge cases with external/custom links

---
+++ test/fixtures/basic/pages/nuxt-link/custom-external.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const MyLink = defineNuxtLink({
+  componentName: 'MyLink',
+  trailingSlash: 'append',
+})
+</script>
+
+<template>
+  <div>
+    <div>
+      <MyLink to="https://thehackernews.com/2024/01/urgent-upgrade-gitlab-critical.html">
+        Trailing slashes should not be applied to implicit external links
+      </MyLink>
+    </div>
+    <div>
+      <NuxtLink
+        :to="{ path: 'https://thehackernews.com/2024/01/urgent-upgrade-gitlab-critical.html' }"
+        external
+      >
+        External links within route objects should be respected
+      </NuxtLink>
+    </div>
+    <div>
+      <MyLink
+        :to="{ path: '/missing-page' }"
+        external
+      >
+        External links within route objects should be respected
+      </MyLink>
+    </div>
+    <div>
+      <MyLink to="/missing-page" external>
+        External links should be respected and not have trailing slashes applied
+      </MyLink>
+    </div>
+    <div>
+      <NuxtLink custom to="https://google.com" external>
+        <template v-slot="{ navigate }">
+          <button @click="navigate()">
+            Using navigate() with external link should work
+          </button>
+        </template>
+      </NuxtLink>
+    </div>
+  </div>
+</template>
