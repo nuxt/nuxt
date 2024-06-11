7f228c6c484551953fd5030b88a7027d77acf699
Michael Brevard <yonshi29@gmail.com>
2024-06-11 19:06:13 +0300

    chore: remove ts-expect-error

--- packages/nuxt/src/core/nitro.ts
+++ packages/nuxt/src/core/nitro.ts
@@ -411,6 +411,7 @@ export async function initNitro (nuxt: Nuxt & { _nitro?: Nitro }) {
   // Init nitro
   const [nitro, spaLoadingTemplateFilePath] = await Promise.all([
     createNitro(nitroConfig, {
+      // @ts-expect-error this will be valid in a future version of Nitro
       compatibilityDate: nuxt.options.compatibilityDate,
     }),
     spaLoadingTemplatePath(nuxt),
