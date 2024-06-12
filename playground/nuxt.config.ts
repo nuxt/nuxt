d3eaf18015e8980f695a2ffa0707432653860b5c
Daniel Roe <daniel@roe.dev>
2024-06-12 17:03:11 +0100

    wip

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,3 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
+  modules: ["nuxt-simple-robots"],
+
+  nitro: {
+    prerender: {
+      // routes: ['/']
+    }
+  },
 })
