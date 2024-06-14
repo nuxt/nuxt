9ba1682eba0526b4815e73dc8957708adcecf366
tbitw2549 <yonshi29@gmail.com>
2024-06-14 23:32:13 +0300

    fix: add space

--- packages/nuxt/src/imports/presets.ts
+++ packages/nuxt/src/imports/presets.ts
@@ -42,7 +42,7 @@ const granularAppPresets: InlinePreset[] = [
     from: '#app/composables/asyncData',
   },
   {
-    imports: ['useHydration','createVisibleLoader', 'createIdleLoader'],
+    imports: ['useHydration', 'createVisibleLoader', 'createIdleLoader'],
     from: '#app/composables/hydrate',
   },
   {
