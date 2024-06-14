e3a02420487940fb943f13871d78d0edca6ed80a
Michael Brevard <yonshi29@gmail.com>
2024-06-15 00:00:51 +0300

    chore: delete unused delayed hydration server plugin

--- test/fixtures/basic/server/plugins/basicRenderDelayedHydration.ts
+++
@@ -1,5 +0,0 @@
-export default defineNitroPlugin((nitroApp) => {
-  nitroApp.hooks.hook('render:html', (html, { event }) => {
-    html.head = html.head.map((headSection: string) => headSection.replace(/<link((?=[^>]+\bhref="\/_nuxt\/DelayedWrapperTestComponent\.([^.]+)\.js")[^>]+>)/, '')) // .replace(/<link rel="preload" href="\/_nuxt\/DelayedWrapperTestComponent\.js" as="script">/, "")
-  })
-})
