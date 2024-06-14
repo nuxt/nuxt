1f663e7f27c600c46a0b4afed4188471c32f6d78
Dmitriy <32272181+Kolobok12309@users.noreply.github.com>
2024-06-14 15:08:41 +0300

    fix(vue-app): prevent double page mount (#10874)

---
+++ test/fixtures/page-mount-with-layouts/pages/page-2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Page 2</h2>
+
+    <NuxtLink
+      id="link"
+      to="/page-1"
+    >
+      To page 1
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'layout-2',
+
+  mounted () {
+    window.mountedCount = (window.mountedCount || 0) + 1
+  }
+}
+</script>
