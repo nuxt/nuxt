e12d4b13ee8c6f20a2579ccce680410a3b8d2fe3
Michael Brevard <yonshi29@gmail.com>
2024-06-15 08:25:03 +0300

    fix: assert bounding box is not null

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -2677,7 +2677,7 @@ describe('lazy import components', () => {
     expect(await page.locator('body').getByText('This should be visible at first with viewport!').all()).toHaveLength(1)
     expect(await page.locator('body').getByText('This should be visible at first with events!').all()).toHaveLength(1)
     const component = await page.locator('#lazyevent');
-    const rect = await component.boundingBox()
+    const rect = (await component.boundingBox())!
     await page.mouse.move(rect.x + rect.width / 2, rect.y + rect.height / 2)
     await page.waitForLoadState('networkidle')
     expect(await page.locator('body').getByText('This shouldn\'t be visible at first with events!').all()).toHaveLength(1)
