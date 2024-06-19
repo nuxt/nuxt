c6183cd1c82fb69f7e8f2ecf68465ca8b3b69830
Daniel Roe <daniel@roe.dev>
2024-06-19 16:55:22 +0100

    chore: remove blank line

--- packages/nuxt/src/app/composables/fetch.ts
+++ packages/nuxt/src/app/composables/fetch.ts
@@ -8,7 +8,6 @@ import { useRequestFetch } from './ssr'
 import type { AsyncData, AsyncDataOptions, KeysOf, MultiWatchSources, PickFrom } from './asyncData'
 import { useAsyncData } from './asyncData'
 
-
 // @ts-expect-error virtual file
 import { fetchDefaults } from '#build/nuxt.config.mjs'
 
