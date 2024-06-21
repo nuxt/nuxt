866a5319a30f8e55c543da7b6a4bb0e9209ec7fe
Daniel Roe <daniel@roe.dev>
2024-06-21 12:12:43 +0100

    fix(nuxt): defer registering inp handler until nuxt is mounted

--- packages/nuxt/src/app/plugins/navigation-repaint.client.ts
+++ packages/nuxt/src/app/plugins/navigation-repaint.client.ts
@@ -1,19 +1,23 @@
 import { defineNuxtPlugin } from '../nuxt'
-import { useRouter } from '../composables'
+import { onNuxtReady } from '../composables/ready'
+import { useRouter } from '../composables/router'
 
 export default defineNuxtPlugin(() => {
-  useRouter().beforeResolve(async () => {
-    /**
-     * This gives an opportunity for the browser to repaint, acknowledging user interaction.
-     * It can reduce INP when navigating on prerendered routes.
-     *
-     * @see https://github.com/nuxt/nuxt/issues/26271#issuecomment-2178582037
-     * @see https://vercel.com/blog/demystifying-inp-new-tools-and-actionable-insights
-     */
-    await new Promise((resolve) => {
-      // Ensure we always resolve, even if the animation frame never fires
-      setTimeout(resolve, 100)
-      requestAnimationFrame(() => { setTimeout(resolve, 0) })
+  const router = useRouter()
+  onNuxtReady(() => {
+    router.beforeResolve(async () => {
+      /**
+       * This gives an opportunity for the browser to repaint, acknowledging user interaction.
+       * It can reduce INP when navigating on prerendered routes.
+       *
+       * @see https://github.com/nuxt/nuxt/issues/26271#issuecomment-2178582037
+       * @see https://vercel.com/blog/demystifying-inp-new-tools-and-actionable-insights
+       */
+      await new Promise((resolve) => {
+        // Ensure we always resolve, even if the animation frame never fires
+        setTimeout(resolve, 100)
+        requestAnimationFrame(() => { setTimeout(resolve, 0) })
+      })
     })
   })
 })
