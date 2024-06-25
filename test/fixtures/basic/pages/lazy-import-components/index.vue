2a53b02710145d207ce8b252866c8ccb71e06a1d
Michael Brevard <yonshi29@gmail.com>
2024-06-25 17:37:41 +0300

    test: use test component

--- test/fixtures/basic/pages/lazy-import-components/index.vue
+++ test/fixtures/basic/pages/lazy-import-components/index.vue
@@ -3,5 +3,6 @@
     <LazyNCompAll message="lazy-named-comp-all" />
     <LazyNCompClient message="lazy-named-comp-client" />
     <LazyNCompServer message="lazy-named-comp-server" />
+    <LazyFake />
   </div>
 </template>
