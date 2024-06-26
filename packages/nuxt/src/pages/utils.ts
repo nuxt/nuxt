9dea20600170bd92ba2e9e52b78031b2de118c2a
Alexander Lichter <github@lichter.io>
2024-06-26 10:40:50 +0200

    fix: check key correctly

--- packages/nuxt/src/pages/utils.ts
+++ packages/nuxt/src/pages/utils.ts
@@ -142,10 +142,11 @@ export function generateRoutesFromFiles (files: ScannedFile[], options: Generate
 
 export async function augmentPages (routes: NuxtPage[], vfs: Record<string, string>, augmentedPagesWithPaths = new Set<string>(), parentPath = '') {
   for (const route of routes) {
-    if (route.file && !augmentedPagesWithPaths.has(route.file)) {
+    const key = `${route.file}_${parentPath}${route.path}`
+    if (route.file && !augmentedPagesWithPaths.has(key)) {
       const fileContent = route.file in vfs ? vfs[route.file] : fs.readFileSync(await resolvePath(route.file), 'utf-8')
       Object.assign(route, await getRouteMeta(fileContent, route.file))
-      augmentedPagesWithPaths.add(`${route.file}_${parentPath}${route.path}`)
+      augmentedPagesWithPaths.add(key)
     }
 
     if (route.children && route.children.length > 0) {
