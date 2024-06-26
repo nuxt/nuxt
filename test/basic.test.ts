55a68d709d5f6f20109246dbad6233d68dd5dc1c
Julien Huang <julien.h.dev@gmail.com>
2024-06-26 12:53:52 +0200

    test: disable page transition

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -522,8 +522,7 @@ describe('pages', () => {
     await clientInitialPage.waitForFunction(() => window.useNuxtApp?.()._route.fullPath === '/client-only-page/normal')
 
     // that page should be client rendered
-    // There's multiple elements due to page transition that don't contain any transition style
-    expect(await clientInitialPage.locator('#server-rendered').first().textContent()).toMatchInlineSnapshot('"false"')
+    expect(await clientInitialPage.locator('#server-rendered').textContent()).toMatchInlineSnapshot('"false"')
     // and not contain any errors or warnings
     expect(errors.length).toBe(0)
 
--- test/fixtures/basic/pages/client-only-page/index.client.vue
+++ test/fixtures/basic/pages/client-only-page/index.client.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+
+definePageMeta({
+  // Disable page transition for this page to avoid having multiple time the same page during transition
+  pageTransition: false
+})
+
 const state = useState('test', () => {
   let hasAccessToWindow = null as null | boolean
 
