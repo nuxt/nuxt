a0510358817bd98ab585d2e933054a2eb630b3e6
Daniel Roe <daniel@roe.dev>
2024-07-16 11:08:21 +0100

    test: add `typeCheck` test in minimal build (#28166)

--- test/fixtures/minimal/nuxt.config.ts
+++ test/fixtures/minimal/nuxt.config.ts
@@ -1,10 +1,17 @@
+import { readFileSync } from 'node:fs'
 import { fileURLToPath } from 'node:url'
 
 const testWithInlineVue = process.env.EXTERNAL_VUE === 'false'
 
+const nuxtEntry = fileURLToPath(new URL('../../../packages/nuxt/dist/index.mjs', import.meta.url))
+const isStubbed = readFileSync(nuxtEntry, 'utf-8').includes('const _module = jiti')
+
 export default defineNuxtConfig({
   future: { compatibilityVersion: process.env.TEST_V4 === 'true' ? 4 : 3 },
   compatibilityDate: '2024-06-28',
+  typescript: {
+    typeCheck: isStubbed ? false : 'build',
+  },
   pages: false,
   experimental: {
     externalVue: !testWithInlineVue,
