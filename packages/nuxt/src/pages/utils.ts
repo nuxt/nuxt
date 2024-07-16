997fada52e5e508810516de22809d80614aed346
xjccc <546534045@qq.com>
2024-07-16 19:27:53 +0800

    fix: test

--- packages/nuxt/src/pages/utils.ts
+++ packages/nuxt/src/pages/utils.ts
@@ -144,8 +144,11 @@ export async function augmentPages (routes: NuxtPage[], vfs: Record<string, stri
   for (const route of routes) {
     if (route.file && !augmentedPages.has(route.file)) {
       const fileContent = route.file in vfs ? vfs[route.file] : fs.readFileSync(await resolvePath(route.file), 'utf-8')
-      const meta = Object.assign(route.meta || {}, (await getRouteMeta(fileContent, route.file)).meta || {})
-      Object.assign(route, { meta })
+      let meta
+      if (route.meta || (await getRouteMeta(fileContent, route.file)).meta) {
+        meta = Object.assign({}, route.meta, (await getRouteMeta(fileContent, route.file)).meta)
+      }
+      Object.assign(route, await getRouteMeta(fileContent, route.file), { meta })
       augmentedPages.add(route.file)
     }
 
