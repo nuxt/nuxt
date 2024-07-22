5d7e296ab32070af726fc09811040f72b7a1b147
Julien Huang <julien.huang@outlook.fr>
2024-07-22 20:15:03 +0200

    Update packages/schema/src/config/common.ts
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- packages/schema/src/config/common.ts
+++ packages/schema/src/config/common.ts
@@ -158,7 +158,7 @@ export default defineUntypedSchema({
     $resolve: async (val: string | undefined, get): Promise<string> => {
       const isV4 = ((await get('future') as Record<string, unknown>).compatibilityVersion === 4)
 
-      return val ? val : resolve(await get('rootDir') as string, isV4 ? 'server' : resolve(await get('srcDir') as string, 'server'))
+      return resolve(isV4 ? await get('rootDir') as string : await get('srcDir') as string, val ?? 'server')
     },
   },
 
