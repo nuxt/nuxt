d073a0bf093ad252ca1a8b552bdd90b69b50b2c7
Horu <73709188+HigherOrderLogic@users.noreply.github.com>
2024-07-23 20:08:50 +0700

    chore: more tests

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -573,7 +573,7 @@ describe('pages', () => {
   })
 
   it('groups routes', async () => {
-    for (const targetRoute of ['/group-page-1', '/group-page-2', '/nested-group/group-page-1', '/nested-group/']) {
+    for (const targetRoute of ['/group-page-1', '/group-page-2', '/nested-group/group-page-1', '/nested-group/group-page-2', '/nested-group']) {
       const { status } = await fetch(targetRoute)
 
       expect(status).toBe(200)
