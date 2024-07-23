21dc80a13e6c62a4de975031a524078b29a1d466
Daniel Roe <daniel@roe.dev>
2024-07-24 00:19:31 +0200

    chore: update to vue v3.5 + native `useId`

--- packages/nuxt/src/imports/presets.ts
+++ packages/nuxt/src/imports/presets.ts
@@ -106,10 +106,6 @@ const granularAppPresets: InlinePreset[] = [
     from: '#app/composables/preview',
   },
   {
-    imports: ['useId'],
-    from: '#app/composables/id',
-  },
-  {
     imports: ['useRouteAnnouncer'],
     from: '#app/composables/route-announcer',
   },
@@ -231,6 +227,7 @@ const vuePreset = defineUnimportPreset({
     'useCssVars',
     'useSlots',
     'useTransitionState',
+    'useId',
   ],
 })
 
