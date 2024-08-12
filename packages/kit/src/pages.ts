9de596d80241befd613b517703d92f3eae488050
Son Tran <sontd.it@gmail.com>
2024-08-13 01:41:01 +0700

    feat(kit): add `prepend` option to `addRouteMiddleware` (#28496)

--- packages/kit/src/pages.ts
+++ packages/kit/src/pages.ts
@@ -42,6 +42,11 @@ export interface AddRouteMiddlewareOptions {
    * @default false
    */
   override?: boolean
+  /**
+   * Prepend middleware to the list
+   * @default false
+   */
+  prepend?: boolean
 }
 
 export function addRouteMiddleware (input: NuxtMiddleware | NuxtMiddleware[], options: AddRouteMiddlewareOptions = {}) {
@@ -58,6 +63,8 @@ export function addRouteMiddleware (input: NuxtMiddleware | NuxtMiddleware[], op
         } else {
           logger.warn(`'${middleware.name}' middleware already exists at '${foundPath}'. You can set \`override: true\` to replace it.`)
         }
+      } else if (options.prepend === true) {
+        app.middleware.unshift({ ...middleware })
       } else {
         app.middleware.push({ ...middleware })
       }
