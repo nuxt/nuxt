2f0805b49ca53b945fa598e9a8eec14958869b08
BracketJohn <njonalik@protonmail.com>
2024-09-04 09:50:20 +0200

    feat: remember to.path and refresh at it, when the chunkError happens during navigation

--- packages/nuxt/src/app/plugins/chunk-reload-immediate.client.ts
+++ packages/nuxt/src/app/plugins/chunk-reload-immediate.client.ts
@@ -1,16 +1,23 @@
 import { defineNuxtPlugin } from '../nuxt'
 import { reloadNuxtApp } from '../composables/chunk'
+import { addRouteMiddleware } from '../../../../kit/src'
 
-const reloadNuxtApp_ = () => { reloadNuxtApp({ persistState: true }) }
+const reloadNuxtApp_ = (path: string) => { reloadNuxtApp({ persistState: true, path }) }
 
 // See https://github.com/nuxt/nuxt/issues/23612 for more context
 export default defineNuxtPlugin({
   name: 'nuxt:chunk-reload-immediate',
   setup (nuxtApp) {
+    // Remember `to.path` when navigating to a new path: A `chunkError` may occur during navigation, we then want to then reload at `to.path`
+    let currentPath: null | string = null
+    addRouteMiddleware((to) => {
+      currentPath = to.path
+    })
+
     // Reload when a `chunkError` is thrown
-    nuxtApp.hook('app:chunkError', reloadNuxtApp_)
+    nuxtApp.hook('app:chunkError', () => reloadNuxtApp_(currentPath ?? nuxtApp._route.path))
 
     // Reload when the app manifest updates
-    nuxtApp.hook('app:manifest:update', reloadNuxtApp_)
+    nuxtApp.hook('app:manifest:update', () => reloadNuxtApp_(nuxtApp._route.path))
   },
 })
