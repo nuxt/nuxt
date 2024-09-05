20aea1fa23fc28819e9a25b1050a75f6b1c1e182
Daniel Roe <daniel@roe.dev>
2024-09-05 16:50:27 +0200

    chore: use consola to log out compatibility info

--- packages/nuxt/src/core/nuxt.ts
+++ packages/nuxt/src/core/nuxt.ts
@@ -104,8 +104,7 @@ async function initNuxt (nuxt: Nuxt) {
 
     const shouldShowPrompt = nuxt.options.dev && hasTTY && !isCI
     if (!shouldShowPrompt) {
-      // eslint-disable-next-line no-console
-      console.log(`Using \`${fallbackCompatibilityDate}\` as fallback compatibility date.`)
+      logger.info(`Using \`${fallbackCompatibilityDate}\` as fallback compatibility date.`)
     }
 
     async function promptAndUpdate () {
@@ -114,8 +113,7 @@ async function initNuxt (nuxt: Nuxt) {
         default: true,
       })
       if (result !== true) {
-        // eslint-disable-next-line no-console
-        console.log(`Using \`${fallbackCompatibilityDate}\` as fallback compatibility date.`)
+        logger.info(`Using \`${fallbackCompatibilityDate}\` as fallback compatibility date.`)
         return
       }
 
@@ -149,8 +147,7 @@ async function initNuxt (nuxt: Nuxt) {
         consola.error(`Failed to update config: ${message}`)
       }
 
-      // eslint-disable-next-line no-console
-      console.log(`Using \`${fallbackCompatibilityDate}\` as fallback compatibility date.`)
+      logger.info(`Using \`${fallbackCompatibilityDate}\` as fallback compatibility date.`)
     }
 
     nuxt.hooks.hookOnce('nitro:init', (nitro) => {
@@ -159,8 +156,7 @@ async function initNuxt (nuxt: Nuxt) {
       nitro.hooks.hookOnce('compiled', () => {
         warnedAboutCompatDate = true
         // Print warning
-        // eslint-disable-next-line no-console
-        console.info(`Nuxt now supports pinning the behavior of provider and deployment presets with a compatibility date. We recommend you specify a \`compatibilityDate\` in your \`nuxt.config\` file, or set an environment variable, such as \`COMPATIBILITY_DATE=${todaysDate}\`.`)
+        logger.info(`Nuxt now supports pinning the behavior of provider and deployment presets with a compatibility date. We recommend you specify a \`compatibilityDate\` in your \`nuxt.config\` file, or set an environment variable, such as \`COMPATIBILITY_DATE=${todaysDate}\`.`)
         if (shouldShowPrompt) { promptAndUpdate() }
       })
     })
@@ -669,8 +665,7 @@ async function initNuxt (nuxt: Nuxt) {
   // Show compatibility version banner when Nuxt is running with a compatibility version
   // that is different from the current major version
   if (!(satisfies(nuxt._version, nuxt.options.future.compatibilityVersion + '.x'))) {
-    // eslint-disable-next-line no-console
-    console.info(`Running with compatibility version \`${nuxt.options.future.compatibilityVersion}\``)
+    logger.info(`Running with compatibility version \`${nuxt.options.future.compatibilityVersion}\``)
   }
 
   await nuxt.callHook('ready', nuxt)
