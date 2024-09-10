05d4b9b33ac29812648e6ca4a97ff281be2f3b25
Julien Huang <julien.h.dev@gmail.com>
2024-09-10 23:27:08 +0200

    fix(nuxt): strongly type runWithContext arg

--- packages/nuxt/src/app/nuxt.ts
+++ packages/nuxt/src/app/nuxt.ts
@@ -281,7 +281,7 @@ export function createNuxtApp (options: CreateOptions) {
     static: {
       data: {},
     },
-    runWithContext (fn: any) {
+    runWithContext (fn: () => (any | Promise<any>)) {
       if (nuxtApp._scope.active && !getCurrentScope()) {
         return nuxtApp._scope.run(() => callWithNuxt(nuxtApp, fn))
       }
