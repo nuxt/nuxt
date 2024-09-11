a0ea790b80848d6249fa227c23fff40ccae1c9b7
Daniel Roe <daniel@roe.dev>
2024-09-11 11:02:22 +0100

    fix(kit,nuxt,schema)!: remove other support for nuxt2/bridge (#28936)

--- packages/nuxt/test/plugin-metadata.test.ts
+++ packages/nuxt/test/plugin-metadata.test.ts
@@ -40,7 +40,6 @@ describe('plugin-metadata', () => {
   it('should overwrite invalid plugins', () => {
     const invalidPlugins = [
       'export const plugin = {}',
-      'export default function (ctx, inject) {}',
     ]
     for (const plugin of invalidPlugins) {
       expect(transformPlugin.transform.call({ parse }, plugin, 'my-plugin.mjs').code).toBe('export default () => {}')
