a947a31300a96185d11c53a327508abc61d996f1
Daniel Roe <daniel@roe.dev>
2024-09-15 21:30:46 +0100

    fix: don't use `as`

--- packages/nuxt/src/app/composables/error.ts
+++ packages/nuxt/src/app/composables/error.ts
@@ -9,7 +9,7 @@ import { useRouter } from './router'
 export const NUXT_ERROR_SIGNATURE = '__nuxt_error'
 
 /** @since 3.0.0 */
-export const useError = () => toRef(useNuxtApp().payload, 'error') as Ref<NuxtPayload['error']>
+export const useError = (): Ref<NuxtPayload['error']> => toRef(useNuxtApp().payload, 'error')
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface NuxtError<DataT = unknown> extends H3Error<DataT> {}
