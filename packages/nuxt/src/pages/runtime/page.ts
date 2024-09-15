d5cf7080417deacdc17dbd62eb12b532fb8be45e
Peter Buglavecz <buggio.dev@gmail.com>
2024-09-15 16:45:03 +0200

    fix(nuxt):  hook called twice during navigation

--- packages/nuxt/src/pages/runtime/page.ts
+++ packages/nuxt/src/pages/runtime/page.ts
@@ -41,7 +41,6 @@ export default defineComponent({
     const nuxtApp = useNuxtApp()
     const pageRef = ref()
     const forkRoute = inject(PageRouteSymbol, null)
-    let previousPageKey: string | undefined | false
 
     expose({ pageRef })
 
@@ -97,10 +96,6 @@ export default defineComponent({
           }
 
           const key = generateRouteKey(routeProps, props.pageKey)
-          if (!nuxtApp.isHydrating && !hasChildrenRoutes(forkRoute, routeProps.route, routeProps.Component) && previousPageKey === key) {
-            nuxtApp.callHook('page:loading:end')
-          }
-          previousPageKey = key
 
           const hasTransition = !!(props.transition ?? routeProps.route.meta.pageTransition ?? defaultPageTransition)
           const transitionProps = hasTransition && _mergeTransitionProps([
@@ -160,11 +155,4 @@ function haveParentRoutesRendered (fork: RouteLocationNormalizedLoaded | null, n
     .some(
       (c, i) => c.components?.default !== fork.matched[i]?.components?.default) ||
     (Component && generateRouteKey({ route: newRoute, Component }) !== generateRouteKey({ route: fork, Component }))
-}
-
-function hasChildrenRoutes (fork: RouteLocationNormalizedLoaded | null, newRoute: RouteLocationNormalizedLoaded, Component?: VNode) {
-  if (!fork) { return false }
-
-  const index = newRoute.matched.findIndex(m => m.components?.default === Component?.type)
-  return index < newRoute.matched.length - 1
 }
