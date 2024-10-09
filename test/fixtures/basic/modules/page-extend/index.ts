671c56b6691d16df786e715e3dbc876c65b2c6ec
Daniel Roe <daniel@roe.dev>
2024-10-09 17:15:41 +0200

    test: update fixture

--- test/fixtures/basic/modules/page-extend/index.ts
+++ test/fixtures/basic/modules/page-extend/index.ts
@@ -13,13 +13,18 @@ export default defineNuxtModule({
         name: 'page-extend',
         path: '/page-extend',
         file: resolver.resolve('../runtime/page.vue'),
-      }, {
+      })
+    })
+
+    nuxt.hook('pages:resolved', (pages) => {
+      pages.push({
         path: '/big-page-1',
         file: resolver.resolve('./pages/big-page.vue'),
         meta: {
           layout: false,
         },
-      }, {
+      },
+      {
         path: '/big-page-2',
         file: resolver.resolve('./pages/big-page.vue'),
         meta: {
