0a0ec349a1ccfb15987802e672bd67cd2a6ae225
tbitw2549 <yonshi29@gmail.com>
2024-10-24 20:41:56 +0300

    perf: more regex (and related areas) deallocation

---
+++ playground/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div><button @click="reloadNuxtApp()">EEE</button><C /> <NestedD /> <SuperNestedE /><div style="height: 3000px"/> <NuxtLink to="/second">EEEE</NuxtLink><NuxtLink to="/third">BBBB</NuxtLink></div>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  layout: false
+})</script>
---
+++ playground/pages/second.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Second</div>
+</template>
