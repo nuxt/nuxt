4301555b9d364330a7c612b673bfed295bb78c81
Daniel Roe <daniel@roe.dev>
2024-11-02 09:11:16 +0000

    chore: update types for nitro

--- packages/nuxt/src/core/nitro.ts
+++ packages/nuxt/src/core/nitro.ts
@@ -399,7 +399,6 @@ export async function initNitro (nuxt: Nuxt & { _nitro?: Nitro }) {
 
   // Init nitro
   const nitro = await createNitro(nitroConfig, {
-    // @ts-expect-error this will be valid in a future version of Nitro
     compatibilityDate: nuxt.options.compatibilityDate,
   })
 
