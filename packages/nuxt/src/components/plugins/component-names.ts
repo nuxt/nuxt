a71c82f2a7a03f366a430433711a87aa37c0c713
Daniel Roe <daniel@roe.dev>
2024-11-11 13:14:29 +0000

    fix: use component regexp and provide `__name`

--- packages/nuxt/src/components/plugins/component-names.ts
+++ packages/nuxt/src/components/plugins/component-names.ts
@@ -31,18 +31,17 @@ export const ComponentNamePlugin = (options: NameDevPluginOptions) => createUnpl
         return
       }
 
+      const NAME_RE = new RegExp(`__name:\\s*['"]${filename}['"]`)
       const s = new MagicString(code)
+      s.replace(NAME_RE, `__name: ${JSON.stringify(component.pascalName)}`)
 
-      if (code.includes('__name')) {
-        const NAME_RE = new RegExp(`__name:\\s*['"]${filename}['"]`)
-
-        s.replace(NAME_RE, `__name: ${JSON.stringify(component.pascalName)}`)
-      } else {
+      // Without setup function, vue compiler does not generate __name
+      if (!s.hasChanged()) {
         const ast = this.parse(code) as Program
         const exportDefault = ast.body.find(node => node.type === 'ExportDefaultDeclaration')
         if (exportDefault) {
           const { start, end } = exportDefault.declaration
-          s.overwrite(start, end, `Object.assign(${code.slice(start, end)}, { name: ${JSON.stringify(component.pascalName)} })`)
+          s.overwrite(start, end, `Object.assign(${code.slice(start, end)}, { __name: ${JSON.stringify(component.pascalName)} })`)
         }
       }
 
