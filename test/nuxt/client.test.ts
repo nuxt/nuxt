df8dd106f35a9de312a1315c315afbe2b75a91f0
Julien Huang <julien.h.dev@gmail.com>
2024-12-06 16:08:55 +0100

    test: add runtime test

--- test/nuxt/client.test.ts
+++ test/nuxt/client.test.ts
@@ -3,7 +3,9 @@ import type { ComponentOptions } from 'vue'
 import { defineComponent, h, toDisplayString, useAttrs } from 'vue'
 import { mountSuspended } from '@nuxt/test-utils/runtime'
 import { createClientOnly } from '../../packages/nuxt/src/app/components/client-only'
-
+ import { flushPromises, mount} from "@vue/test-utils"
+ import { Suspense } from 'vue'
+import { createClientPage } from '../../packages/nuxt/dist/components/runtime/client-component'
 const Client = defineComponent({
   name: 'TestClient',
   setup () {
@@ -11,7 +13,7 @@ const Client = defineComponent({
     return () => h('div', {}, toDisplayString(attrs))
   },
 })
-
+ 
 describe('createClient attribute inheritance', () => {
   it('should retrieve attributes with useAttrs()', async () => {
     const wrapper = await mountSuspended(createClientOnly(Client as ComponentOptions), {
@@ -25,5 +27,40 @@ describe('createClient attribute inheritance', () => {
         "id": "client"
         }</div>"
     `)
+  })
+})
+
+
+describe('client page', () => {
+  it('Should be suspensed when out of hydration',async  () =>{
+    let resolve
+    const promise = new Promise((_resolve) => {
+      resolve = _resolve
+    })
+  
+    const comp = defineComponent({
+      async setup() {
+        await promise
+        return () => h('div', {id: 'async'}, 'async resolved')
+      }
+    })
+ 
+    const wrapper = mount({
+      setup() {
+        return () => h('div', {}, [
+          h(Suspense, {}, {
+            default: () => h(createClientPage(() => Promise.resolve(comp)), {}),
+            fallback: () => h('div', {id: 'fallback'}, 'loading')
+          })
+        ])
+      }
+    })
+
+    await flushPromises()
+    expect(wrapper.find('#fallback').exists()).toBe(true)
+
+    resolve!()
+    await flushPromises()
+    expect(wrapper.find('#async').exists()).toBe(true)
   })
 })
