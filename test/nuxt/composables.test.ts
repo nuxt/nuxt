2b6875d3b2ccbea16880e8725b528ce7b3179926
Alexander Lichter <github@lichter.io>
2024-12-14 17:02:25 +0100

    feat: callOnce with navigation mode

--- test/nuxt/composables.test.ts
+++ test/nuxt/composables.test.ts
@@ -794,6 +794,22 @@ describe('callOnce', () => {
     await execute('second')
     expect(fn).toHaveBeenCalledTimes(2)
   })
+
+  it('should be executed multiple times when using mode: navigation with key', async () => {
+    const fn = vi.fn()
+    const execute = () => callOnce('abc', fn, { mode: 'navigation' })
+    await execute()
+    await execute()
+    expect(fn).toHaveBeenCalledTimes(2)
+  })
+
+  it('should work with mode and without key', async () => {
+    const fn = vi.fn()
+    const execute = () => callOnce(fn, { mode: 'navigation' })
+    await execute()
+    await execute()
+    expect(fn).toHaveBeenCalledTimes(2)
+  })
 })
 
 describe('route announcer', () => {
