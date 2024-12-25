69e92e78fc093b05641c5d10d185eb1b12b2b515
Matej Cerny <matej.cern@gmail.com>
2024-12-25 12:01:22 +0100

    fix(nuxt): use AsyncContextInjection plugin only on the server-side

--- packages/nuxt/src/app/composables/asyncContext.ts
+++ packages/nuxt/src/app/composables/asyncContext.ts
@@ -3,10 +3,6 @@ import { getCurrentInstance, withAsyncContext as withVueAsyncContext } from 'vue
 
 /** @since 3.8.0 */
 export function withAsyncContext (fn: () => PromiseLike<unknown>) {
-  if (import.meta.client) {
-    return withVueAsyncContext(fn)
-  }
-
   return withVueAsyncContext(() => {
     const nuxtApp = getCurrentInstance()?.appContext.app.$nuxt
     return nuxtApp ? nuxtApp.runWithContext(fn) : fn()
