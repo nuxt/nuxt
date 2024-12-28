6236c3938c5ceb255f34a0558750b8fc82120db1
userquin <userquin@gmail.com>
2024-12-28 20:36:40 +0100

    perf(vite): remove client entry from server warmup

--- packages/vite/src/client.ts
+++ packages/vite/src/client.ts
@@ -152,9 +152,6 @@ export async function buildClient (ctx: ViteBuildContext) {
     ],
     appType: 'custom',
     server: {
-      warmup: {
-        clientFiles: [ctx.entry],
-      },
       middlewareMode: true,
     },
   } satisfies vite.InlineConfig, ctx.nuxt.options.vite.$client || {}))
