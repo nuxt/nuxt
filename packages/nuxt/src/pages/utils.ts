0249c74bcd55e3cf0b179301399ac2600293fc6f
Daniel Roe <daniel@roe.dev>
2025-01-14 13:12:59 +0000

    fix(nuxt): do not warn about invalid characters in route groups/catchalls

--- packages/nuxt/src/pages/utils.ts
+++ packages/nuxt/src/pages/utils.ts
@@ -424,7 +424,7 @@ function parseSegment (segment: string, absolutePath: string) {
           state = SegmentParserState.initial
         } else if (c && PARAM_CHAR_RE.test(c)) {
           buffer += c
-        } else {
+        } else if (state === SegmentParserState.dynamic || state === SegmentParserState.optional) {
           logger.warn(`'\`${c}\`' is not allowed in a dynamic route parameter and has been ignored. Consider renaming \`${absolutePath}\`.`)
         }
         break
