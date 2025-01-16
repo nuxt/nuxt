5eb64113276b43cadf012e4ff5b716c260e9fa0f
Saeid Zareie <saeid.za98@gmail.com>
2025-01-16 10:07:37 +0330

    feat(nuxt): better formatting errors

---
+++ packages/nuxt/src/core/runtime/nitro/utils.ts
@@ -0,0 +1,28 @@
+import { join } from 'node:path'
+import { type H3Event, setResponseHeaders } from 'h3'
+import type { NitroErrorHandler } from 'nitro/types'
+
+export function defineNitroErrorHandler (
+  handler: NitroErrorHandler,
+): NitroErrorHandler {
+  return handler
+}
+
+export function setSecurityHeaders (event: H3Event, allowjs = false) {
+  setResponseHeaders(event, {
+    // Prevent browser from guessing the MIME types of resources.
+    'X-Content-Type-Options': 'nosniff',
+    // Prevent error page from being embedded in an iframe
+    'X-Frame-Options': 'DENY',
+    // Prevent browsers from sending the Referer header
+    'Referrer-Policy': 'no-referrer',
+    // Disable the execution of any js
+    'Content-Security-Policy': allowjs
+      ? 'script-src \'self\' \'unsafe-inline\'; object-src \'none\'; base-uri \'self\';'
+      : 'script-src \'none\'; frame-ancestors \'none\';',
+  })
+}
+
+export function resolveErrorHandler (isDev: boolean) {
+  return join('core/runtime/nitro/', (isDev ? 'dev' : 'prod') + '-error')
+}
