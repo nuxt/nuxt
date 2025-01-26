397368c83a89e217712158e89d3e06c8ca5394e8
Daniel Roe <daniel@roe.dev>
2025-01-26 22:17:33 +0100

    test: clear directory before running benchmarks

--- packages/kit/test/write-types.bench.ts
+++ packages/kit/test/write-types.bench.ts
@@ -1,14 +1,24 @@
 import { fileURLToPath } from 'node:url'
-import { afterAll, bench, describe } from 'vitest'
+import { rm } from 'node:fs/promises'
+import { afterAll, beforeAll, bench, describe } from 'vitest'
 import { join, normalize } from 'pathe'
 import { withoutTrailingSlash } from 'ufo'
 import { loadNuxt, writeTypes } from '@nuxt/kit'
+import type { Nuxt } from 'nuxt/schema'
 
-describe('writeTypes', async () => {
+describe('writeTypes', () => {
   const relativeDir = join('../../..', 'test/fixtures/basic-types')
   const path = withoutTrailingSlash(normalize(fileURLToPath(new URL(relativeDir, import.meta.url))))
 
-  const nuxt = await loadNuxt({ cwd: path })
+  let nuxt: Nuxt
+
+  beforeAll(async () => {
+    nuxt = await loadNuxt({ cwd: path })
+    await Promise.all([
+      rm(join(path, '.nuxt'), { recursive: true, force: true }),
+    ])
+  })
+
   afterAll(async () => {
     await nuxt.close()
   })
