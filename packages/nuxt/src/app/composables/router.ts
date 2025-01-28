2bdd84be2c767de45887f27a25fe0c1bc267a575
Daniel Roe <daniel@roe.dev>
2025-01-28 21:34:42 +0000

    refactor: move inside condition

--- packages/nuxt/src/app/composables/router.ts
+++ packages/nuxt/src/app/composables/router.ts
@@ -18,8 +18,7 @@ export const useRouter: typeof _useRouter = () => {
 
 /** @since 3.0.0 */
 export const useRoute: typeof _useRoute = () => {
-  const notInsideSFC = !getCurrentInstance()
-  if (import.meta.dev && isProcessingMiddleware() && notInsideSFC) {
+  if (import.meta.dev && !getCurrentInstance() && isProcessingMiddleware()) {
     console.warn('[nuxt] Calling `useRoute` within middleware may lead to misleading results. Instead, use the (to, from) arguments passed to the middleware to access the new and old routes.')
   }
   if (hasInjectionContext()) {
