b8b5fb8008b8c15da9074b6c241e322479144734
Daniel Roe <daniel@roe.dev>
2025-02-08 22:56:10 +0000

    Update packages/nuxt/src/core/templates.ts

--- packages/nuxt/src/core/templates.ts
+++ packages/nuxt/src/core/templates.ts
@@ -433,7 +433,7 @@ import { _replaceAppConfig } from '#app/config'
 // Vite - webpack is handled directly in #app/config
 if (import.meta.dev && !import.meta.nitro && import.meta.hot) {
   import.meta.hot.accept((newModule) => {
-   _replaceAppConfig(newModule.default)
+    _replaceAppConfig(newModule.default)
   })
 }
 /** client-end **/
