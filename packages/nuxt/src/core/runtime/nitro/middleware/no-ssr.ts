5bad8616590f0d2795f9ba7a3db2c31e6a4c9665
Julien Huang <julien.h.dev@gmail.com>
2025-02-25 22:44:24 +0100

    move no-ssr in middlewares

--- packages/nuxt/src/core/runtime/nitro/middleware/no-ssr.ts
+++ packages/nuxt/src/core/runtime/nitro/middleware/no-ssr.ts
@@ -1,5 +1,5 @@
 import { defineEventHandler, getRequestHeader } from 'h3'
-// @todo check for reference in the repo... this doesn't seems to be used
+
 export default defineEventHandler((event) => {
   if (getRequestHeader(event, 'x-nuxt-no-ssr')) {
     event.context.nuxt ||= {}
