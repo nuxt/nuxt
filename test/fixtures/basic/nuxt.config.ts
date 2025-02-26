fb9ee0a4ea385d5e8c77e8cf60a30bff7a987749
Daniel Roe <daniel@roe.dev>
2025-02-26 17:02:04 +0000

    perf(nuxt): migrate to use `exsolve` for module resolution (#31124)

--- test/fixtures/basic/nuxt.config.ts
+++ test/fixtures/basic/nuxt.config.ts
@@ -35,7 +35,7 @@ export default defineNuxtConfig({
         }
       })
     },
-    '~/modules/subpath',
+    '~/custom-modules/subpath',
     './modules/test',
     '~/modules/example',
     function (_, nuxt) {
