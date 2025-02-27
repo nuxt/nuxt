9ec94a4b1c86cef37c1f2f94a6af15e8f1f21607
xjccc <546534045@qq.com>
2025-02-27 18:07:48 +0800

    update: code

--- packages/nuxt/src/app/composables/asyncData.ts
+++ packages/nuxt/src/app/composables/asyncData.ts
@@ -304,8 +304,8 @@ export function useAsyncData<
           const { captureStackTrace } = await import('errx')
           const { fileURLToPath } = await import('node:url')
           const stack = captureStackTrace()
-          const caller = stack[stack.length - 1]
-          const explanation = caller ? ` (used at ${fileURLToPath(caller.source)}:${caller.line}:${caller.column})` : ''
+          const { source, line, column } = stack[stack.length - 1] ?? {}
+          const explanation = source ? ` (used at ${fileURLToPath(source)}:${line}:${column})` : ''
           // @ts-expect-error private property
           console.warn(`[nuxt] \`${options._functionName || 'useAsyncData'}${explanation}\` must return a value (it should not be \`undefined\`) or the request may be duplicated on the client side.`)
         }
