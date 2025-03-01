c398ce28173c673a2dac844900940c5ba49374d9
xjccc <546534045@qq.com>
2025-03-01 14:40:10 +0800

    feat(nuxt): show source file when warning about undefined useAsyncData (#31144)

--- packages/nuxt/src/app/composables/asyncData.ts
+++ packages/nuxt/src/app/composables/asyncData.ts
@@ -1,5 +1,6 @@
 import { computed, getCurrentInstance, getCurrentScope, onBeforeMount, onScopeDispose, onServerPrefetch, onUnmounted, ref, shallowRef, toRef, unref, watch } from 'vue'
 import type { MultiWatchSources, Ref } from 'vue'
+import { captureStackTrace } from 'errx'
 import type { NuxtApp } from '../nuxt'
 import { useNuxtApp } from '../nuxt'
 import { toArray } from '../utils'
@@ -301,8 +302,11 @@ export function useAsyncData<
         }
 
         if (import.meta.dev && import.meta.server && typeof result === 'undefined') {
+          const stack = captureStackTrace()
+          const { source, line, column } = stack[stack.length - 1] ?? {}
+          const explanation = source ? ` (used at ${source.replace(/^file:\/\//, '')}:${line}:${column})` : ''
           // @ts-expect-error private property
-          console.warn(`[nuxt] \`${options._functionName || 'useAsyncData'}\` must return a value (it should not be \`undefined\`) or the request may be duplicated on the client side.`)
+          console.warn(`[nuxt] \`${options._functionName || 'useAsyncData'}${explanation}\` must return a value (it should not be \`undefined\`) or the request may be duplicated on the client side.`)
         }
 
         nuxtApp.payload.data[key] = result
