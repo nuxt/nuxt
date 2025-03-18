202d083f23b987a23cac532eb925a6de5dfce04a
Daniel Roe <daniel@roe.dev>
2025-03-18 21:29:22 +0000

    fix(nuxt): fully resolve nuxt dependencies

---
+++ packages/nuxt/src/meta.mjs
@@ -0,0 +1,19 @@
+export const runtimeDependencies = [
+  // other deps
+  'devalue',
+  'klona',
+  // unjs ecosystem
+  'defu',
+  'ufo',
+  'h3',
+  'destr',
+  'consola',
+  'hookable',
+  'unctx',
+  'cookie-es',
+  'perfect-debounce',
+  'radix3',
+  'ohash',
+  'pathe',
+  'uncrypto',
+]
