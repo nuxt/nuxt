b7d66b3b8b46bd62d86a2d1e3353846de16e30ec
Daniel Roe <daniel@roe.dev>
2025-03-19 15:27:00 +0000

    test: move file to root dir

--- test/typed-router.test.ts
+++ test/typed-router.test.ts
@@ -1,10 +1,15 @@
 import { readFileSync } from 'node:fs'
 import { resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { x } from 'tinyexec'
 import { describe, expect, it } from 'vitest'
 
+const rootDir = fileURLToPath(new URL('./fixtures/basic-types', import.meta.url))
+
 describe('typed router integration', () => {
-  it('does not duplicate params in RouteNamedMap when a child route overrides the path with an absolute path', () => {
-    const typedRouterDtsFile = resolve(import.meta.dirname, '../.nuxt/types/typed-router.d.ts')
+  it('does not duplicate params in RouteNamedMap when a child route overrides the path with an absolute path', async () => {
+    await x('nuxi', ['prepare', rootDir])
+    const typedRouterDtsFile = resolve(rootDir, '.nuxt/types/typed-router.d.ts')
     const typedRouterDts = readFileSync(typedRouterDtsFile, 'utf8')
     expect(typedRouterDts).toContain(`'param-id-view-custom': RouteRecordInfo<'param-id-view-custom', '/param/:id()/view-custom', { id: ParamValue<true> }, { id: ParamValue<false> }>,`)
     expect(typedRouterDts).not.toContain(`'param-id-view-custom': RouteRecordInfo<'param-id-view-custom', '/param/:id()/view-custom', { id: ParamValue<true>, id: ParamValue<true> }, { id: ParamValue<false>, id: ParamValue<false> }>,`)
