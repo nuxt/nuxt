3f6a3b56ec105ccb0329fa10637fd5f0d0f946e0
Daniel Roe <daniel@roe.dev>
2025-03-25 15:10:54 +0000

    test: simplify

--- test/nuxt/loading-indicator.test.ts
+++ test/nuxt/loading-indicator.test.ts
@@ -60,15 +60,27 @@ describe('page loading indicator', () => {
   it('should hide nuxt page load indicator after navigating from nested page to other nested page', async () => {
     let startedLoading = 0
     let stoppedLoading = 0
-    nuxtApp.hook('page:loading:start', () => {
-      startedLoading++
-    })
-    nuxtApp.hook('page:loading:end', () => {
-      stoppedLoading++
-    })
+    nuxtApp.hook('page:loading:start', () => { startedLoading++ })
+    nuxtApp.hook('page:loading:end', () => { stoppedLoading++ })
 
     const route = useRoute()
-    const waitForLoad = async () => {
+
+    const el = await mountSuspended({ setup: () => () => h('div', [h(NuxtLoadingIndicator), h(NuxtPage)]) })
+
+    const getLoadingIndicator = () => el.getComponent('.nuxt-loading-indicator')
+    const getPage = () => el.getComponent('#page')
+
+    async function expectNavigatesWithLoading (path: string | RouteLocationRaw, onLoad?: () => void) {
+      startedLoading = 0
+      stoppedLoading = 0
+      await navigateTo(path)
+      expect(startedLoading).toBe(1)
+      expect(stoppedLoading).toBe(0)
+
+      expect(isLoading.value).toBe(true)
+      expect(getLoadingIndicator().attributes().style).toContain('opacity: 1;')
+      onLoad?.()
+
       // ensure loading hasn't already finished
       await nextTick(() => new Promise(r => setTimeout(r, 0)))
 
@@ -77,58 +89,35 @@ describe('page loading indicator', () => {
         await new Promise<void>(resolve => nuxtApp.hooks.hookOnce('page:loading:end', () => { resolve() }))
           .then(() => new Promise(r => setTimeout(r, 0)))
       }
-    }
 
-    const el = await mountSuspended({ setup: () => () => h('div', [h(NuxtLoadingIndicator), h(NuxtPage)]) })
+      expect(startedLoading).toBe(1)
+      expect(stoppedLoading).toBe(1)
 
-    const getLoadingIndicator = () => el.getComponent('.nuxt-loading-indicator')
-    const getPage = () => el.getComponent('#page')
-
-    function expectLoaded () {
       expect(isLoading.value).toBe(false)
       expect(getLoadingIndicator().attributes().style).toContain('opacity: 0;')
     }
-    function expectLoading () {
-      expect(isLoading.value).toBe(true)
-      expect(getLoadingIndicator().attributes().style).toContain('opacity: 1;')
-    }
-    async function onNavigation (path: string | RouteLocationRaw, onLoad?: () => void) {
-      startedLoading = 0
-      stoppedLoading = 0
-      await navigateTo(path)
-      expect(startedLoading).toBe(1)
-      expect(stoppedLoading).toBe(0)
-
-      expectLoading()
-      onLoad?.()
-      await waitForLoad()
-      expect(startedLoading).toBe(1)
-      expect(stoppedLoading).toBe(1)
-      expectLoaded()
-    }
 
     await flushPromises()
-    expectLoaded()
 
-    await onNavigation('/page-load-hook')
+    await expectNavigatesWithLoading('/page-load-hook')
     expect(getPage().html()).toMatchInlineSnapshot(`
       "<div id="page"><span>parent</span>
         <div>index</div>
       </div>"
     `)
 
-    await onNavigation('/page-load-hook/subpage')
+    await expectNavigatesWithLoading('/page-load-hook/subpage')
     expect(getPage().html()).toMatchInlineSnapshot(`
       "<div id="page"><span>parent</span>
         <div><span>child</span>/page-load-hook/subpage</div>
       </div>"
     `)
 
-    await onNavigation({ query: { someQuery: 'toto' } }, () => {
+    await expectNavigatesWithLoading({ query: { someQuery: 'toto' } }, () => {
       expect(route.fullPath).toMatchInlineSnapshot(`"/page-load-hook/subpage?someQuery=toto"`)
     })
 
-    await onNavigation('/page-load-hook/other-slug')
+    await expectNavigatesWithLoading('/page-load-hook/other-slug')
     expect(getPage().html()).toMatchInlineSnapshot(`
       "<div id="page"><span>parent</span>
         <div><span>child</span>/page-load-hook/other-slug</div>
