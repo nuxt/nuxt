8db4452c1da05759cdcfad42b47889b15423b462
Daniel Roe <daniel@roe.dev>
2025-03-25 15:07:12 +0000

    chore: remove e2e tests

--- test/fixtures/basic/pages/page-load-hook/[slug].vue
+++ test/fixtures/basic/pages/page-load-hook/[slug].vue
@@ -1,25 +1,7 @@
-<script lang="ts" setup>
-const route = useRoute()
-</script>
-
 <template>
   <div>
     <NuxtLink to="/page-load-hook">
       Back to parent
-    </NuxtLink>
-    <NuxtLink
-      :to="{
-        ...route,
-        query: {
-          ...route.query,
-          someQuery: 'toto',
-        },
-      }"
-    >
-      Add query
-    </NuxtLink>
-    <NuxtLink to="/page-load-hook/other-slug">
-      To other slug
     </NuxtLink>
   </div>
 </template>
