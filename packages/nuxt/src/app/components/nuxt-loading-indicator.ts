a2e6bcf1c87b36c88ea9dcdf88d4056558771bbc
owshiiii <201320587+ram-skrrrtskrrrt@users.noreply.github.com>
2025-04-09 00:49:43 +0800

    Update nuxt-loading-indicator.ts

--- packages/nuxt/src/app/components/nuxt-loading-indicator.ts
+++ packages/nuxt/src/app/components/nuxt-loading-indicator.ts
@@ -1,73 +1,44 @@
-import { defineComponent, h } from 'vue'
-import { useLoadingIndicator } from '../composables/loading-indicator'
+import
 
-export default defineComponent({
-  name: 'NuxtLoadingIndicator',
-  props: {
-    throttle: {
-      type: Number,
-      default: 200,
-    },
-    duration: {
-      type: Number,
-      default: 2000,
-    },
-    hideDelay: {
-      type: Number,
-      default: 500,
-    },
-    resetDelay: {
-      type: Number,
-      default: 400,
-    },
-    height: {
-      type: Number,
-      default: 3,
-    },
-    color: {
-      type: [String, Boolean],
-      default: 'repeating-linear-gradient(to right,#00dc82 0%,#34cdfe 50%,#0047e1 100%)',
-    },
-    errorColor: {
-      type: String,
-      default: 'repeating-linear-gradient(to right,#f87171 0%,#ef4444 100%)',
-    },
-    estimatedProgress: {
-      type: Function as unknown as () => (duration: number, elapsed: number) => number,
-      required: false,
-    },
-  },
-  setup (props, { slots, expose }) {
-    const { progress, isLoading, error, start, finish, clear } = useLoadingIndicator({
-      duration: props.duration,
-      throttle: props.throttle,
-      hideDelay: props.hideDelay,
-      resetDelay: props.resetDelay,
-      estimatedProgress: props.estimatedProgress,
-    })
+  name: 
+  props: 
+      type: 
+      default: 
+    
+    height: 
+      type: 
+      default:
 
-    expose({
-      progress, isLoading, error, start, finish, clear,
-    })
+    color: 
+      type: 
+      default: 
+      type: 
+    
+    estimatedProgress: 
+      type: 
+      required:
+      duration: 
+      throttle: 
+      hideDelay: 
+      resetDelay: 
+      estimatedProgress:
 
-    return () => h('div', {
-      class: 'nuxt-loading-indicator',
-      style: {
-        position: 'fixed',
-        top: 0,
-        right: 0,
-        left: 0,
-        pointerEvents: 'none',
-        width: 'auto',
-        height: `${props.height}px`,
-        opacity: isLoading.value ? 1 : 0,
-        background: error.value ? props.errorColor : props.color || undefined,
-        backgroundSize: `${(100 / progress.value) * 100}% auto`,
-        transform: `scaleX(${progress.value}%)`,
-        transformOrigin: 'left',
-        transition: 'transform 0.1s, height 0.4s, opacity 0.4s',
-        zIndex: 999999,
-      },
-    }, slots)
-  },
-})
+    expose
+    
+      style: 
+        position: 
+        top: 
+        right: 
+        left: 
+        pointerEvents: 
+        width: 
+        height: 
+        opacity: 
+        background: 
+        backgroundSize: 
+        transform: 
+        transformOrigin:
+        transition: 
+        zIndex: 
+        slots
+
