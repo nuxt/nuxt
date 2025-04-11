6eb6de820ebd666ea002f74cfd2c3771c19c95bd
Damian Glowala <damian.glowala.rebkow@gmail.com>
2025-04-11 16:47:54 +0200

    fix: wait for suspense to resolve before handling `NuxtErrorBoundary` error during hydration

--- packages/nuxt/src/app/components/nuxt-error-boundary.ts
+++ packages/nuxt/src/app/components/nuxt-error-boundary.ts
@@ -1,5 +1,6 @@
-import { defineComponent, onErrorCaptured, ref } from 'vue'
+import { type ComponentPublicInstance, defineComponent, onErrorCaptured, ref } from 'vue'
 import { useNuxtApp } from '../nuxt'
+import { onNuxtReady } from '../../app'
 
 export default defineComponent({
   name: 'NuxtErrorBoundary',
@@ -13,12 +14,24 @@ export default defineComponent({
     const error = ref<Error | null>(null)
     const nuxtApp = useNuxtApp()
 
+    function handleError (err: Error, instance: ComponentPublicInstance | null, info: string) {
+      emit('error', err)
+
+      nuxtApp.hooks.callHook('vue:error', err, instance, info)
+      error.value = err
+    }
+
     if (import.meta.client) {
-      onErrorCaptured((err, target, info) => {
+      onErrorCaptured((err, instance, info) => {
+        if (nuxtApp.isHydrating) {
+          onNuxtReady(() => handleError(err, instance, info))
+
+          return false
+        }
+
         if (!nuxtApp.isHydrating || !nuxtApp.payload.serverRendered) {
-          emit('error', err)
-          nuxtApp.hooks.callHook('vue:error', err, target, info)
-          error.value = err
+          handleError(err, instance, info)
+
           return false
         }
       })
