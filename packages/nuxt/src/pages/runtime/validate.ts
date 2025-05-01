3ba8d44bb680767e30fa0749dd7a8cbfa569f5d3
Daniel Roe <daniel@roe.dev>
2025-05-01 15:14:08 +0100

    fix: set as fatal error

--- packages/nuxt/src/pages/runtime/validate.ts
+++ packages/nuxt/src/pages/runtime/validate.ts
@@ -1,11 +1,8 @@
-import { createError, showError } from '#app/composables/error'
-import { useNuxtApp } from '#app/nuxt'
+import { createError } from '#app/composables/error'
 import { defineNuxtRouteMiddleware } from '#app/composables/router'
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
   if (!to.meta?.validate) { return }
-
-  const nuxtApp = useNuxtApp()
 
   const result = await Promise.resolve(to.meta.validate(to))
   if (result === true) {
@@ -13,14 +10,13 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   }
 
   const error = createError({
+    fatal: true,
     statusCode: (result && result.statusCode) || 404,
     statusMessage: (result && result.statusMessage) || `Page Not Found: ${to.fullPath}`,
     data: {
       path: to.fullPath,
     },
   })
-
-  await nuxtApp.runWithContext(() => showError(error))
 
   // We pretend to have navigated to the invalid route so
   // that the user can return to the previous page with
