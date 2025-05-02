1505fb5449f69960d90d78bdae841af719ac8036
Daniel Roe <daniel@roe.dev>
2025-05-02 09:07:17 +0100

    fix(nuxt): short circuit middleware when validate returns false (#31967)

--- packages/nuxt/src/pages/runtime/validate.ts
+++ packages/nuxt/src/pages/runtime/validate.ts
@@ -1,12 +1,8 @@
-import { createError, showError } from '#app/composables/error'
-import { useNuxtApp } from '#app/nuxt'
-import { defineNuxtRouteMiddleware, useRouter } from '#app/composables/router'
+import { createError } from '#app/composables/error'
+import { defineNuxtRouteMiddleware } from '#app/composables/router'
 
-export default defineNuxtRouteMiddleware(async (to) => {
+export default defineNuxtRouteMiddleware(async (to, from) => {
   if (!to.meta?.validate) { return }
-
-  const nuxtApp = useNuxtApp()
-  const router = useRouter()
 
   const result = await Promise.resolve(to.meta.validate(to))
   if (result === true) {
@@ -14,28 +10,20 @@ export default defineNuxtRouteMiddleware(async (to) => {
   }
 
   const error = createError({
+    fatal: import.meta.client,
     statusCode: (result && result.statusCode) || 404,
     statusMessage: (result && result.statusMessage) || `Page Not Found: ${to.fullPath}`,
     data: {
       path: to.fullPath,
     },
   })
-  const unsub = router.beforeResolve((final) => {
-    unsub()
-    if (final === to) {
-      const unsub = router.afterEach(async () => {
-        unsub()
-        await nuxtApp.runWithContext(() => showError(error))
-        // We pretend to have navigated to the invalid route so
-        // that the user can return to the previous page with
-        // the back button.
-        if (typeof window !== 'undefined') {
-          window.history.pushState({}, '', to.fullPath)
-        }
-      })
-      // We stop the navigation immediately before it resolves
-      // if there is no other route matching it.
-      return false
-    }
-  })
+
+  // We pretend to have navigated to the invalid route so
+  // that the user can return to the previous page with
+  // the back button.
+  if (typeof window !== 'undefined') {
+    window.history.pushState({}, '', from.fullPath)
+  }
+
+  return error
 })
