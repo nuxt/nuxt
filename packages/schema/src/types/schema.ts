f41eefbd178363a55386369d5d7a912fec503720
Daniel Roe <daniel@roe.dev>
2025-05-05 00:02:11 +0100

    fix(schema): broaden `warningIgnoreFilters`

--- packages/schema/src/types/schema.ts
+++ packages/schema/src/types/schema.ts
@@ -2204,7 +2204,7 @@ export interface ConfigSchema {
      * Filters to hide build warnings.
      *
      */
-    warningIgnoreFilters: Array<(warn: WebpackError) => boolean>
+    warningIgnoreFilters: Array<(warn: WebpackError | Error) => boolean>
 
     /**
      * Configure [webpack experiments](https://webpack.js.org/configuration/experiments/)
