82f03ebe26a9bfa0f3188c28e27534bfaecbc8f2
Daniel Roe <daniel@roe.dev>
2025-05-15 15:59:44 +0100

    test: use vitest workspaces

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,32 +1,72 @@
 import { resolve } from 'pathe'
+import { defineVitestProject } from '@nuxt/test-utils/config'
 import { configDefaults, coverageConfigDefaults, defineConfig } from 'vitest/config'
 import { isCI, isWindows } from 'std-env'
 import codspeedPlugin from '@codspeed/vitest-plugin'
 
 export default defineConfig({
   plugins: isCI ? [codspeedPlugin()] : [],
-  resolve: {
-    alias: {
-      '#build/nuxt.config.mjs': resolve('./test/mocks/nuxt-config'),
-      '#build/router.options': resolve('./test/mocks/router-options'),
-      '#internal/nuxt/paths': resolve('./test/mocks/paths'),
-      '#build/app.config.mjs': resolve('./test/mocks/app-config'),
-      '#app': resolve('./packages/nuxt/dist/app'),
-    },
-  },
   test: {
-    setupFiles: ['./test/setup-env.ts'],
     coverage: {
-      exclude: [...coverageConfigDefaults.exclude, 'packages/nuxt/src/app', 'playground', '**/test/', 'scripts', 'vitest.nuxt.config.ts'],
+      exclude: [...coverageConfigDefaults.exclude, 'playground', '**/test/', 'scripts', 'vitest.nuxt.config.ts'],
     },
-    testTimeout: isWindows ? 60000 : 10000,
-    // Excluded plugin because it should throw an error when accidentally loaded via Nuxt
-    exclude: [...configDefaults.exclude, 'test/e2e/**', 'e2e/**', 'nuxt/**', '**/test.ts', '**/this-should-not-load.spec.js'],
-    poolOptions: {
-      threads: {
-        maxThreads: process.env.TEST_ENV === 'dev' ? 1 : undefined,
-        minThreads: process.env.TEST_ENV === 'dev' ? 1 : undefined,
+    workspace: [
+      {
+        test: {
+          name: 'fixtures',
+          include: ['test/*.test.ts'],
+          setupFiles: ['./test/setup-env.ts'],
+          testTimeout: isWindows ? 60000 : 10000,
+          // Excluded plugin because it should throw an error when accidentally loaded via Nuxt
+          exclude: [...configDefaults.exclude, 'test/e2e/**', 'e2e/**', 'nuxt/**', '**/test.ts', '**/this-should-not-load.spec.js'],
+        },
       },
-    },
+      {
+        resolve: {
+          alias: {
+            '#build/nuxt.config.mjs': resolve('./test/mocks/nuxt-config'),
+            '#build/router.options': resolve('./test/mocks/router-options'),
+            '#internal/nuxt/paths': resolve('./test/mocks/paths'),
+            '#build/app.config.mjs': resolve('./test/mocks/app-config'),
+            '#app': resolve('./packages/nuxt/dist/app'),
+          },
+        },
+        test: {
+          name: 'unit',
+          setupFiles: ['./test/setup-env.ts'],
+          include: ['packages/**/*.test.ts'],
+          testTimeout: isWindows ? 60000 : 10000,
+          // Excluded plugin because it should throw an error when accidentally loaded via Nuxt
+          exclude: [...configDefaults.exclude, 'test/e2e/**', 'e2e/**', 'nuxt/**', '**/test.ts', '**/this-should-not-load.spec.js'],
+        },
+      },
+      await defineVitestProject({
+        test: {
+          name: 'nuxt',
+          dir: './test/nuxt',
+          environment: 'nuxt',
+          setupFiles: ['./test/setup-runtime.ts'],
+          environmentOptions: {
+            nuxt: {
+              overrides: {
+                pages: true,
+                routeRules: {
+                  '/specific-prerendered': { prerender: true },
+                  '/pre/test': { redirect: '/' },
+                  '/pre/**': { prerender: true },
+                },
+                experimental: {
+                  appManifest: process.env.TEST_MANIFEST !== 'manifest-off',
+                  alwaysRunFetchOnKeyChange: true,
+                },
+                imports: {
+                  polyfills: false,
+                },
+              },
+            },
+          },
+        },
+      }),
+    ],
   },
 })
