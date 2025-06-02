e535f880a76d29c10a4ebf83b51aa36ec401b0c3
Daniel Roe <daniel@roe.dev>
2025-06-02 16:48:02 +0100

    refactor: drop Node export

--- packages/nuxt/src/core/utils/parse.ts
+++ packages/nuxt/src/core/utils/parse.ts
@@ -1,8 +1,6 @@
-import type { Node } from 'estree-walker'
-import { type SameShape, type TransformOptions, type TransformResult, transform as esbuildTransform } from 'esbuild'
+import type { SameShape, TransformOptions, TransformResult } from 'esbuild'
+import { transform as esbuildTransform } from 'esbuild'
 import { tryUseNuxt } from '@nuxt/kit'
-
-export type { Node }
 
 export async function transform<T extends TransformOptions> (input: string | Uint8Array, options?: SameShape<TransformOptions, T>): Promise<TransformResult<T>> {
   return await esbuildTransform(input, { ...tryUseNuxt()?.options.esbuild.options, ...options })
