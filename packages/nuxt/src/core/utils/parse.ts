a048b9d251a2d7e8961f4c95414a87b60d3882fa
Daniel Roe <daniel@roe.dev>
2025-07-11 22:33:47 +0100

    chore: improve return type for `transformAndMinify`

--- packages/nuxt/src/core/utils/parse.ts
+++ packages/nuxt/src/core/utils/parse.ts
@@ -1,9 +1,9 @@
 import { tryUseNuxt } from '@nuxt/kit'
 import type { TransformOptions, TransformResult } from 'oxc-transform'
 import { transform as oxcTransform } from 'oxc-transform'
-import { minify } from 'oxc-minify'
+import { minify, type MinifyResult } from 'oxc-minify'
 
-export function transformAndMinify (input: string, options?: TransformOptions): TransformResult {
+export function transformAndMinify (input: string, options?: TransformOptions): TransformResult | MinifyResult {
   // not async until https://github.com/oxc-project/oxc/issues/10900
   const oxcOptions = tryUseNuxt()?.options.oxc
   const transformResult = oxcTransform('', input, { ...oxcOptions?.transform.options, ...options })
