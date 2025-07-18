2a1d79897153580e9d5c22d1f941b97e79b3937c
Stephen Jason Wang <stephenjasonwang@gmail.com>
2025-07-18 19:32:44 +0800

    refactor: update references to shared imports in type declaration templates

--- packages/nuxt/src/core/templates.ts
+++ packages/nuxt/src/core/templates.ts
@@ -366,7 +366,7 @@ export const nitroSchemaTemplate: NuxtTemplate = {
     return /* typescript */`
 ${lines.join('\n')}
 /// <reference path="./runtime-config.d.ts" />
-/// <reference path="./shared.d.ts" />
+/// <reference path="./shared-imports.d.ts" />
 
 import type { RuntimeConfig } from 'nuxt/schema'
 import type { H3Event } from 'h3'
