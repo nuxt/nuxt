ccfa026f4a800097363959b92a77c1dff9c14d11
Daniel Roe <daniel@roe.dev>
2025-07-18 16:34:41 +0200

    test add type tests

---
+++ test/fixtures/basic-types/shared/shared-types.ts
@@ -0,0 +1,13 @@
+import { describe, expectTypeOf, it } from 'vitest'
+
+// import { foo } from '#shared/other'
+
+describe('shared folder', () => {
+  // it('can reference its own aliases', () => {
+  //   expectTypeOf(foo).not.toBeAny()
+  //   expectTypeOf(foo).toEqualTypeOf<string>()
+  // })
+  it('can reference auto-imported utils', () => {
+    expectTypeOf(useSharedUtil()).toEqualTypeOf<string>()
+  })
+})
