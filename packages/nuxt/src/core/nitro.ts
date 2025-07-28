7f11348536da10ec97dc210e4d49c2834328dcf3
Damian Głowala <damian.glowala.rebkow@gmail.com>
2025-07-28 18:24:45 +0200

    fix(kit): use `mlly` to parse module paths (#32386)
    Co-authored-by: Daniel Roe <daniel@roe.dev>

--- packages/nuxt/src/core/nitro.ts
+++ packages/nuxt/src/core/nitro.ts
@@ -46,11 +46,15 @@ export async function initNitro (nuxt: Nuxt & { _nitro?: Nitro }) {
 
   const rootDirWithSlash = withTrailingSlash(nuxt.options.rootDir)
 
-  const modules = await resolveNuxtModule(rootDirWithSlash,
-    nuxt.options._installedModules
-      .filter(m => m.entryPath)
-      .map(m => m.entryPath!),
-  )
+  const moduleEntryPaths: string[] = []
+  for (const m of nuxt.options._installedModules) {
+    const path = m.meta?.rawPath || m.entryPath
+    if (path) {
+      moduleEntryPaths.push(path)
+    }
+  }
+
+  const modules = await resolveNuxtModule(rootDirWithSlash, moduleEntryPaths)
 
   const sharedDirs = new Set<string>()
   if (nuxt.options.nitro.imports !== false && nuxt.options.imports.scan !== false) {
@@ -145,7 +149,13 @@ export async function initNitro (nuxt: Nuxt & { _nitro?: Nitro }) {
         },
         include: [
           join(nuxt.options.buildDir, 'types/nitro-nuxt.d.ts'),
-          ...modules.map(m => join(relativeWithDot(nuxt.options.buildDir, m), 'runtime/server')),
+          ...modules.flatMap((m) => {
+            const moduleDir = relativeWithDot(nuxt.options.buildDir, m)
+            return [
+              join(moduleDir, 'runtime/server'),
+              join(moduleDir, 'dist/runtime/server'),
+            ]
+          }),
           ...nuxt.options._layers.map(layer =>
             relativeWithDot(
               nuxt.options.buildDir,
