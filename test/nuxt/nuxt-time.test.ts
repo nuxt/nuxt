a4e4857fcecbf4822784ada2ae942010fe114599
Daniel Roe <daniel@roe.dev>
2025-08-10 21:04:54 +0100

    test: add regression test for ssr/client-side differences

--- test/nuxt/nuxt-time.test.ts
+++ test/nuxt/nuxt-time.test.ts
@@ -1,6 +1,7 @@
 import { describe, expect, it } from 'vitest'
 import { defineComponent, h } from 'vue'
 import { mountSuspended } from '@nuxt/test-utils/runtime'
+import { injectHead } from '#unhead/composables'
 
 import { NuxtTime } from '#components'
 
@@ -72,5 +73,50 @@ describe('<NuxtTime>', () => {
     expect(thing.html()).toMatchInlineSnapshot(
       `"<time datetime="${new Date(datetime).toISOString()}" title="test">5 minutes ago</time>"`,
     )
+  })
+
+  it('should generate the correct hydrateable code', async () => {
+    const datetime = Date.now() - 32 * 24 * 60 * 60 * 1000
+    const thing = await mountSuspended(
+      defineComponent({
+        render: () =>
+          h(NuxtTime, {
+            // not a defined prop, but we use to switch on ssr behaviour in test
+            ssr: true,
+            datetime,
+            relative: true,
+            title: 'test',
+          }),
+      }),
+    )
+
+    const html = thing.html()
+    const id = html.match(/data-prehydrate-id="([^"]+)"/)?.[1]
+    expect(thing.html()).toMatchInlineSnapshot(
+      `"<time data-relative="true" data-title="test" datetime="${new Date(datetime).toISOString()}" title="test" ssr="true" data-prehydrate-id="${id}">1 month ago</time>"`,
+    )
+    const oldQuerySelector = document.querySelectorAll
+
+    // @ts-expect-error ignoring types here
+    document.querySelectorAll = (selector) => {
+      if (selector === `[data-prehydrate-id*="${id}"]`) {
+        return [thing.element]
+      }
+      return oldQuerySelector.call(document, selector)
+    }
+
+    const head = injectHead()
+    // @ts-expect-error craziness
+    const innerHTML = head.entries.get(1).input.script[0].innerHTML
+    const fn = new Function(innerHTML)
+    fn()
+
+    expect(window._nuxtTimeNow).toBeDefined()
+
+    expect(thing.html()).toMatchInlineSnapshot(
+      `"<time data-relative="true" data-title="test" datetime="${new Date(datetime).toISOString()}" title="test" ssr="true" data-prehydrate-id="${id}">1 month ago</time>"`,
+    )
+
+    document.querySelectorAll = oldQuerySelector
   })
 })
