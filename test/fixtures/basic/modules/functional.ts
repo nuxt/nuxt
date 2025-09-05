06ea9170517a4908e91b19a255f67a5617a0baea
Daniel Roe <daniel@roe.dev>
2025-09-05 22:44:33 +0100

    test: remove `app/` prefix for tree-shaken composables

--- test/fixtures/basic/modules/functional.ts
+++ test/fixtures/basic/modules/functional.ts
@@ -3,7 +3,7 @@ import { defineNuxtModule } from 'nuxt/kit'
 
 export default defineNuxtModule(
   function (_, nuxt) {
-    nuxt.options.optimization.treeShake.composables.server[join(nuxt.options.rootDir, 'app/composables/tree-shake.ts')] = ['useClientOnlyComposable', 'setTitleToPink']
-    nuxt.options.optimization.treeShake.composables.client[join(nuxt.options.rootDir, 'app/composables/tree-shake.ts')] = ['useServerOnlyComposable']
+    nuxt.options.optimization.treeShake.composables.server[join(nuxt.options.rootDir, 'composables/tree-shake.ts')] = ['useClientOnlyComposable', 'setTitleToPink']
+    nuxt.options.optimization.treeShake.composables.client[join(nuxt.options.rootDir, 'composables/tree-shake.ts')] = ['useServerOnlyComposable']
   },
 )
