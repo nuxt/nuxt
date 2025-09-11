591a2dfbf01aea3f1e6115e2461af46112bfbfc3
Daniel Roe <daniel@roe.dev>
2025-09-11 13:56:22 +0100

    perf: precompile transpile regexps

--- packages/nuxt/src/core/modules.ts
+++ packages/nuxt/src/core/modules.ts
@@ -1,11 +1,19 @@
 import { normalizeModuleTranspilePath } from '@nuxt/kit'
 import type { Nuxt } from '@nuxt/schema'
+import escapeStringRegexp from 'escape-string-regexp'
 
 export const addModuleTranspiles = (nuxt: Nuxt) => {
   // Try to sanitize modules to better match imports
-  function isTranspilePresent (mod: string) {
-    return nuxt.options.build.transpile
-      .some(t => !(t instanceof Function) && (t instanceof RegExp ? t.test(mod) : new RegExp(t).test(mod)))
+  const transpile: RegExp[] = []
+  for (const t of nuxt.options.build.transpile) {
+    if (t instanceof Function) {
+      continue
+    }
+    if (typeof t === 'string') {
+      transpile.push(new RegExp(escapeStringRegexp(t)))
+    } else {
+      transpile.push(t)
+    }
   }
 
   for (const m of [...nuxt.options.modules, ...nuxt.options._modules]) {
@@ -15,7 +23,7 @@ export const addModuleTranspiles = (nuxt: Nuxt) => {
     }
     const path = normalizeModuleTranspilePath(mod)
     // Automatically add used modules to the transpile
-    if (!isTranspilePresent(path)) {
+    if (!transpile.some(t => t.test(path))) {
       nuxt.options.build.transpile.push(path)
     }
   }
