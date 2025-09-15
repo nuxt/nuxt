73446994a39aeedd025c4b6d173fe8f58275c72e
Alex Liu <dsa1314@gmail.com>
2025-09-15 18:08:56 +0800

    refactor(nuxt): improve implementation of error composables (#33234)

--- packages/nuxt/src/app/composables/error.ts
+++ packages/nuxt/src/app/composables/error.ts
@@ -12,6 +12,7 @@ import { nuxtDefaultErrorValue } from '#build/nuxt.config.mjs'
 export const NUXT_ERROR_SIGNATURE = '__nuxt_error'
 
 /** @since 3.0.0 */
+/* @__NO_SIDE_EFFECTS__ */
 export const useError = (): Ref<NuxtPayload['error']> => toRef(useNuxtApp().payload, 'error')
 
 export interface NuxtError<DataT = unknown> extends H3Error<DataT> {
@@ -28,10 +29,10 @@ export const showError = <DataT = unknown>(
   const nuxtError = createError<DataT>(error)
 
   try {
-    const nuxtApp = useNuxtApp()
     const error = useError()
 
     if (import.meta.client) {
+      const nuxtApp = useNuxtApp()
       nuxtApp.hooks.callHook('app:error', nuxtError)
     }
 
