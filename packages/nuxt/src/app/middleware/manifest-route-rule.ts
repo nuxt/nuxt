dbb5e1facb8e377f8a02755342500dea40fe3c34
Julien Huang <julien.h.dev@gmail.com>
2025-09-16 23:11:49 +0200

    fix: use to.hash

--- packages/nuxt/src/app/middleware/manifest-route-rule.ts
+++ packages/nuxt/src/app/middleware/manifest-route-rule.ts
@@ -6,7 +6,7 @@ export default defineNuxtRouteMiddleware(async (to) => {
   if (import.meta.server || import.meta.test) { return }
   const rules = await getRouteRules({ path: to.path })
   if (rules.redirect) {
-    const hash = window.location.hash
+    const hash = to.hash
     const redirectHasHash = rules.redirect.includes('#')
     if (hasProtocol(rules.redirect, { acceptRelative: true })) {
       window.location.href = rules.redirect + (redirectHasHash ? '' : hash)
