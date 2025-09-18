f6a273b87be60c000a6858637a189d2ff4e1f225
Daniel Roe <daniel@roe.dev>
2025-09-18 17:15:21 +0100

    fix(nuxt): use single resolve imports plugin instance

--- packages/nuxt/src/core/nuxt.ts
+++ packages/nuxt/src/core/nuxt.ts
@@ -328,8 +328,7 @@ async function initNuxt (nuxt: Nuxt) {
   addWebpackPlugin(() => ImpoundPlugin.webpack(nuxtProtectionConfig))
 
   // add resolver for modules used in virtual files
-  addVitePlugin(() => ResolveDeepImportsPlugin(nuxt), { client: false })
-  addVitePlugin(() => ResolveDeepImportsPlugin(nuxt), { server: false })
+  addVitePlugin(() => ResolveDeepImportsPlugin(nuxt))
 
   addVitePlugin(() => ResolveExternalsPlugin(nuxt), { prepend: true })
 
