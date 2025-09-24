8beb9238ba4c833892cfea7ae0af8ef11edb8146
KazariEX <zinkawaii@qq.com>
2025-09-24 19:31:26 +0800

    fix: typo of `SharedRuntimeConfig` and `SharedPublicRuntimeConfig`

--- packages/nuxt/src/core/runtime-config.ts
+++ packages/nuxt/src/core/runtime-config.ts
@@ -126,8 +126,8 @@ declare module '@nuxt/schema' {
   interface PublicRuntimeConfig extends UserPublicRuntimeConfig {}
 }
 declare module 'nuxt/schema' {
-  interface RuntimeConfig extends UserRuntimeConfig {}
-  interface PublicRuntimeConfig extends UserPublicRuntimeConfig {}
+  interface RuntimeConfig extends SharedRuntimeConfig {}
+  interface PublicRuntimeConfig extends SharedPublicRuntimeConfig {}
 }
 declare module 'vue' {
   interface ComponentCustomProperties {
