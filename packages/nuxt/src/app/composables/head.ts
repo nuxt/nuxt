02c863094c84fb83edf379d49927bf61b4a550b7
Daniel Roe <daniel@roe.dev>
2025-09-27 13:12:31 +0300

    chore: add some lint ignores

--- packages/nuxt/src/app/composables/head.ts
+++ packages/nuxt/src/app/composables/head.ts
@@ -1,9 +1,2 @@
-export {
-  injectHead,
-  useHead,
-  useServerHead,
-  useSeoMeta,
-  useServerSeoMeta,
-  useHeadSafe,
-  useServerHeadSafe,
-} from '#unhead/composables'
+// eslint-disable-next-line @typescript-eslint/no-deprecated
+export { injectHead, useHead, useServerHead, useSeoMeta, useServerSeoMeta, useHeadSafe, useServerHeadSafe } from '#unhead/composables'
