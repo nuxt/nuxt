94c7094b5d4b32d2c142cc049720e6d9ba9b639a
Florian Heuberger <fh@flogersoft.de>
2025-10-13 21:37:18 +0200

    revert: keep original regex in scrollBehavior as equality check doesn't need normalization

--- packages/nuxt/src/pages/runtime/router.options.ts
+++ packages/nuxt/src/pages/runtime/router.options.ts
@@ -16,7 +16,7 @@ export default <RouterConfig> {
     const hashScrollBehaviour = useRouter().options?.scrollBehaviorType ?? 'auto'
 
     // Hash routes on the same page, no page hook is fired so resolve here
-    if (to.path.replace(/(?<=.)\/$/, '') === from.path.replace(/(?<=.)\/$/, '')) {
+    if (to.path.replace(/\/$/, '') === from.path.replace(/\/$/, '')) {
       if (from.hash && !to.hash) {
         return { left: 0, top: 0 }
       }
