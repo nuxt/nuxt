b976d05554615d526be2c2c611fe96bc57afc00d
KazariEX <zinkawaii@qq.com>
2025-10-24 00:30:17 +0900

    refactor: rename to `setDefaultHead`

--- packages/kit/src/head.ts
+++ packages/kit/src/head.ts
@@ -2,7 +2,7 @@ import type { NuxtAppConfig } from '@nuxt/schema'
 import { useNuxt } from './context'
 import { defu } from 'defu'
 
-export function addAppHead (head: NuxtAppConfig['head']) {
+export function setDefaultHead (head: NuxtAppConfig['head']) {
   const nuxt = useNuxt()
   nuxt.options.app.head = defu(head, nuxt.options.app.head)
 }
