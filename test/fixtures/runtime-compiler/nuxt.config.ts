1642868010422050ac6ee6eece60a4dc953a3d21
Hugo <hugo.richard@vercel.com>
2025-12-09 23:21:44 +0000

    docs: split and improve `Module Author Guides` (#33803)
    Co-authored-by: Matej Černý <matej.cern@gmail.com>
    Co-authored-by: Lucie <25330882+lihbr@users.noreply.github.com>

--- test/fixtures/runtime-compiler/nuxt.config.ts
+++ test/fixtures/runtime-compiler/nuxt.config.ts
@@ -1,6 +1,6 @@
 import { withMatrix } from '../../matrix'
 
-// https://nuxt.com/docs/4.x/api/nuxt-config
+// https://nuxt.com/docs/3.x/api/nuxt-config
 export default withMatrix({
   vue: {
     runtimeCompiler: true,
