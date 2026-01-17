f1ec5e66e02ecff1e0f1d7f708dce1e59a074e0c
OrbisK <robin.kehl@singular-it.de>
2026-01-17 22:49:46 +0100

    types: cast cookie[name] to T

--- packages/nuxt/src/app/composables/cookie.ts
+++ packages/nuxt/src/app/composables/cookie.ts
@@ -89,7 +89,7 @@ export function useCookie<T = string | null | undefined> (name: string, _opts?: 
     }
     const callback = (force = false) => {
       if (!force) {
-        if (opts.readonly || isEqual(cookie.value, cookies[name])) { return }
+        if (opts.readonly || isEqual(cookie.value, cookies[name] as T)) { return }
       }
       writeClientCookie(name, cookie.value, opts as CookieSerializeOptions)
 
@@ -153,11 +153,11 @@ export function useCookie<T = string | null | undefined> (name: string, _opts?: 
   } else if (import.meta.server) {
     const nuxtApp = useNuxtApp()
     const writeFinalCookieValue = () => {
-      if (opts.readonly || isEqual(cookie.value, cookies[name])) { return }
+      if (opts.readonly || isEqual(cookie.value, cookies[name] as T)) { return }
       nuxtApp._cookies ||= {}
       if (name in nuxtApp._cookies) {
         // do not append a second `set-cookie` header
-        if (isEqual(cookie.value, nuxtApp._cookies[name])) { return }
+        if (isEqual(cookie.value, nuxtApp._cookies[name] as T)) { return }
         // warn in dev mode
         if (import.meta.dev) {
           console.warn(`[nuxt] cookie \`${name}\` was previously set to \`${opts.encode(nuxtApp._cookies[name] as any)}\` and is being overridden to \`${opts.encode(cookie.value as any)}\`. This may cause unexpected issues.`)
