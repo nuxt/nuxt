fb4db7ac021030c0d8312324dac5870cc4a9c715
OrbisK <robin.kehl@singular-it.de>
2026-01-19 22:22:11 +0100

    fix(nuxt): encode cookie strings with quotes to prevent unwanted parse to object

--- test/nuxt/composables.test.ts
+++ test/nuxt/composables.test.ts
@@ -709,13 +709,13 @@ describe('useCookie', () => {
 
   it('should set cookie value when called on client', () => {
     useCookie('cookie-watch-false', { default: () => 'foo', watch: false })
-    expect(document.cookie).toContain('cookie-watch-false=foo')
+    expect(document.cookie).toContain('cookie-watch-false=%22foo%22')
 
     useCookie('cookie-watch-true', { default: () => 'foo', watch: true })
-    expect(document.cookie).toContain('cookie-watch-true=foo')
+    expect(document.cookie).toContain('cookie-watch-true=%22foo%22')
 
     useCookie('cookie-readonly', { default: () => 'foo', readonly: true })
-    expect(document.cookie).toContain('cookie-readonly=foo')
+    expect(document.cookie).toContain('cookie-readonly=%22foo%22')
   })
 
   it('should not parse json by default', () => {
