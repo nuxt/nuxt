fad2fb70b4de7aecc1201075d5b030077061a163
OrbisK <robin.kehl@singular-it.de>
2026-01-20 22:37:23 +0100

    test: retry failing failing test

--- test/nuxt/use-async-data.test.ts
+++ test/nuxt/use-async-data.test.ts
@@ -502,7 +502,7 @@ describe('useAsyncData', () => {
     expect(promiseFn).toHaveBeenCalledTimes(2)
   })
 
-  it.fails('should resolve to latest value when watched dependency is rapidly updated', async () => {
+  it.fails('should resolve to latest value when watched dependency is rapidly updated', { retry: 5 }, async () => {
     const route = ref('/')
     const promiseFn = vi.fn(() => Promise.resolve(route.value))
     const component = defineComponent({
