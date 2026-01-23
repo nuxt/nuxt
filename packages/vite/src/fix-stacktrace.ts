29e9f6d2796951a66d0c3acc2719d07427402d7b
Julien Huang <julien.h.dev@gmail.com>
2026-01-24 00:28:51 +0100

    fix: remove vite-ignore

--- packages/vite/src/fix-stacktrace.ts
+++ packages/vite/src/fix-stacktrace.ts
@@ -5,7 +5,7 @@ export default (nitroApp: NitroApp) => {
   let runner: ViteNodeRunner
   nitroApp.hooks.hook('error', async (error) => {
     // @ts-expect-error post-build file
-    runner ||= await import(/* @vite-ignore */'#build/dist/server/runner.mjs').then(m => m.default)
+    runner ||= await import('#build/dist/server/runner.mjs').then(m => m.default)
     if (error?.stack) {
       await runner.ssrFixStacktrace(error)
     }
