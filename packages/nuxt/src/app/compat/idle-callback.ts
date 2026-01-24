b0445f0a31da6b69548ac3182bcbba1aa17bce60
onmax <maximogarciamtnez@gmail.com>
2026-01-24 13:36:25 +0100

    refactor(nuxt): remove requestIdleCallback polyfill

--- packages/nuxt/src/app/compat/idle-callback.ts
+++ packages/nuxt/src/app/compat/idle-callback.ts
@@ -1,16 +1,7 @@
-// Polyfills for Safari support
-// https://caniuse.com/requestidlecallback
 export const requestIdleCallback: Window['requestIdleCallback'] = import.meta.server
   ? (() => {}) as any
-  : (globalThis.requestIdleCallback || ((cb) => {
-      const start = Date.now()
-      const idleDeadline = {
-        didTimeout: false,
-        timeRemaining: () => Math.max(0, 50 - (Date.now() - start)),
-      }
-      return setTimeout(() => { cb(idleDeadline) }, 1)
-    }))
+  : globalThis.requestIdleCallback
 
 export const cancelIdleCallback: Window['cancelIdleCallback'] = import.meta.server
   ? (() => {}) as any
-  : (globalThis.cancelIdleCallback || ((id) => { clearTimeout(id) }))
+  : globalThis.cancelIdleCallback
