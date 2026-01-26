9b5202e8e03a89af2a97b1a7481a1785935d89b8
Daniel Roe <daniel@roe.dev>
2026-01-26 22:11:29 +0000

    fix(kit): externalize nuxt/app to fix rolldown build with obuild 0.4.20

--- packages/kit/build.config.ts
+++ packages/kit/build.config.ts
@@ -2,6 +2,13 @@ import { defineBuildConfig } from 'obuild/config'
 
 export default defineBuildConfig({
   entries: [
-    { type: 'bundle', input: 'src/index', dts: { oxc: true } },
+    {
+      type: 'bundle',
+      input: 'src/index',
+      dts: { oxc: true },
+      rolldown: {
+        external: ['nuxt/app'],
+      },
+    },
   ],
 })
