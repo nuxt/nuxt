0c42bea90ecf02c0f5d6c62247464e4a53b23a29
onmax <maximogarciamtnez@gmail.com>
2026-01-29 09:41:40 +0100

    fix(nuxt): reactive query params in prerendered pages

--- packages/nuxt/src/app/components/route-provider.ts
+++ packages/nuxt/src/app/components/route-provider.ts
@@ -1,7 +1,9 @@
-import { defineComponent, h, nextTick, onMounted, provide, shallowReactive } from 'vue'
+import { defineComponent, h, nextTick, onMounted, provide, shallowReactive, shallowRef, watch } from 'vue'
 import type { Ref, VNode } from 'vue'
 import type { RouteLocationNormalizedLoaded } from 'vue-router'
 import { PageRouteSymbol } from './injections'
+import { useRouter } from '#app/composables/router'
+import { useNuxtApp } from '#app/nuxt'
 
 export const defineRouteProvider = (name = 'RouteProvider') => defineComponent({
   name,
@@ -19,17 +21,38 @@ export const defineRouteProvider = (name = 'RouteProvider') => defineComponent({
     // Prevent reactivity when the page will be rerendered in a different suspense fork
     const previousKey = props.renderKey
     const previousRoute = props.route
+    const routeVersion = shallowRef(0)
 
     // Provide a reactive route within the page
     const route = {} as RouteLocationNormalizedLoaded
     for (const key in props.route) {
       Object.defineProperty(route, key, {
-        get: () => previousKey === props.renderKey ? props.route[key as keyof RouteLocationNormalizedLoaded] : previousRoute[key as keyof RouteLocationNormalizedLoaded],
+        get: () => {
+          routeVersion.value
+          return previousKey === props.renderKey ? props.route[key as keyof RouteLocationNormalizedLoaded] : previousRoute[key as keyof RouteLocationNormalizedLoaded]
+        },
         enumerable: true,
       })
     }
 
     provide(PageRouteSymbol, shallowReactive(route))
+
+    // Force getter re-evaluation to sync query params during hydration
+    if (import.meta.client) {
+      const nuxtApp = useNuxtApp()
+      const router = useRouter()
+
+      if (nuxtApp.isHydrating) {
+        const unwatch = watch(
+          () => router.currentRoute.value,
+          () => {
+            routeVersion.value++
+          },
+          { flush: 'sync' },
+        )
+        nuxtApp.hooks.hookOnce('app:suspense:resolve', unwatch)
+      }
+    }
 
     let vnode: VNode
     if (import.meta.dev && import.meta.client && props.trackRootNodes) {
