782ee73882ae82d0c3a7f37b7e53babb5d293646
mrkaashee <mrkaashee@gmail.com>
2026-02-06 21:01:52 +0500

    docs: align server alias examples with `#server` and `rootDir` (#34259)

--- packages/nitro-server/src/augments.ts
+++ packages/nitro-server/src/augments.ts
@@ -236,7 +236,7 @@ declare module 'nuxt/schema' {
      * @example
      * ```js
      * serverHandlers: [
-     *   { route: '/path/foo/**:name', handler: '~/server/foohandler.ts' }
+     *   { route: '/path/foo/**:name', handler: '#server/foohandler.ts' }
      * ]
      * ```
      */
