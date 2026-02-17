9ed09342e54af91a09638e9c7df5473bd523e1c1
M-YasirGhaffar <yasirghaffar.nust@gmail.com>
2026-02-17 05:42:01 +0500

    fix(nuxt): populate `route.meta.layout` from `appLayout` route rules

--- test/fixtures/basic/app/pages/route-rules/layout.vue
+++ test/fixtures/basic/app/pages/route-rules/layout.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
 <template>
   <div>
     <div>Should use layout set in route-rules</div>
+    <div>route-meta-layout:{{ route.meta.layout }}</div>
   </div>
 </template>
