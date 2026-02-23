5e0117685394fd8ddff4ac787c17a393d9ab28a2
Daniel Roe <daniel@roe.dev>
2026-02-23 20:58:29 +0000

    fix: update `isJsonRequest`

--- packages/nitro-server/src/runtime/utils/error.ts
+++ packages/nitro-server/src/runtime/utils/error.ts
@@ -1,26 +1,27 @@
-import type { H3Event } from 'nitro/h3'
+import type { HTTPEvent } from 'nitro/h3'
+import { FastURL } from 'srvx'
 
 /**
  * Nitro internal functions extracted from https://github.com/nitrojs/nitro/blob/v2/src/runtime/internal/utils.ts
  */
 
-export function isJsonRequest (event: H3Event): boolean {
+export function isJsonRequest (event: HTTPEvent): boolean {
   // If the client specifically requests HTML, then avoid classifying as JSON.
   if (event.req.headers.get('accept')?.includes('text/html')) {
     return false
   }
-  const url = event.url.pathname
+  const url = new FastURL(event.req.url)
   return (
     hasReqHeader(event, 'accept', 'application/json') ||
     hasReqHeader(event, 'user-agent', 'curl/') ||
     hasReqHeader(event, 'user-agent', 'httpie/') ||
     hasReqHeader(event, 'sec-fetch-mode', 'cors') ||
-    url.startsWith('/api/') ||
-    url.endsWith('.json')
+    url.pathname.startsWith('/api/') ||
+    url.pathname.endsWith('.json')
   )
 }
 
-export function hasReqHeader (event: H3Event, name: string, includes: string): boolean {
+export function hasReqHeader (event: HTTPEvent, name: string, includes: string): boolean {
   const value = event.req.headers.get(name)
   return !!(
     value && value.toLowerCase().includes(includes)
