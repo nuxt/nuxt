92e050db223037d4b5d6e93009079ab6c237862a
Daniel Roe <daniel@roe.dev>
2026-03-05 11:46:15 +0000

    refactor(vite): use vite's `transformWithEsbuild` for analysis

--- packages/vite/src/plugins/analyze.ts
+++ packages/vite/src/plugins/analyze.ts
@@ -1,5 +1,5 @@
 import type { Plugin } from 'vite'
-import { transform } from 'esbuild'
+import { transformWithEsbuild } from 'vite'
 import { defu } from 'defu'
 import type { Nuxt, NuxtOptions } from '@nuxt/schema'
 import type { RenderedModule } from 'rollup'
@@ -32,7 +32,7 @@ export async function AnalyzePlugin (nuxt: Nuxt): Promise<Plugin | undefined> {
               const minifiedModuleEntryPromises: Array<Promise<[string, RenderedModule]>> = []
               for (const [moduleId, module] of Object.entries(bundle.modules)) {
                 minifiedModuleEntryPromises.push(
-                  transform(module.code || '', { minify: true })
+                  transformWithEsbuild(module.code || '', moduleId, { minify: true })
                     .then(result => [moduleId, { ...module, code: result.code }]),
                 )
               }
