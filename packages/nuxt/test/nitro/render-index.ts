ff284264b2c115283165a9d419e2df1609a63625
Daniel Roe <daniel@roe.dev>
2026-03-11 23:25:25 +0100

    test: avoid swallowing errors

--- packages/nuxt/test/nitro/render-index.ts
+++ packages/nuxt/test/nitro/render-index.ts
@@ -6,8 +6,14 @@ const nitroApp = useNitroApp()
 
 async function renderIndex () {
   const res = await nitroApp.fetch(new Request('/'))
+  if (!res.ok) {
+    throw new Error(`Failed to render /: ${res.status} ${res.statusText}`)
+  }
   // eslint-disable-next-line no-console
   console.log(await res.text())
 }
 
-renderIndex()
+renderIndex().catch((error) => {
+  console.error(error)
+  process.exitCode = 1
+})
