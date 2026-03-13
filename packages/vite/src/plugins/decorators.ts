91e5f10add12aada093b4bca3fee60979381e0b3
Daniel Roe <daniel@roe.dev>
2026-03-13 11:45:11 +0100

    refactor(kit): add `ensureDependencyInstalled` utility and consolidate install prompts

--- packages/vite/src/plugins/decorators.ts
+++ packages/vite/src/plugins/decorators.ts
@@ -1,48 +1,8 @@
-import process from 'node:process'
 import type { Plugin } from 'vite'
-import { addDependency } from 'nypm'
-import { logger } from '@nuxt/kit'
-import { hasTTY, isCI } from 'std-env'
+import { ensureDependencyInstalled, logger } from '@nuxt/kit'
 import type { Nuxt } from '@nuxt/schema'
 
-const BABEL_DECORATOR_DEPS = ['@babel/plugin-proposal-decorators', '@babel/plugin-syntax-jsx']
-
-async function ensureBabelDecoratorDeps (nuxt: Nuxt): Promise<boolean> {
-  for (const pkg of BABEL_DECORATOR_DEPS) {
-    try {
-      await import(pkg)
-    } catch (_err) {
-      const err = _err as NodeJS.ErrnoException
-
-      if (err.code !== 'ERR_MODULE_NOT_FOUND' && err.code !== 'MODULE_NOT_FOUND') {
-        throw err
-      }
-
-      if (!isCI && hasTTY) {
-        logger.info('Decorator support requires additional dependencies.')
-        const shouldInstall = await logger.prompt(`Install \`${BABEL_DECORATOR_DEPS.join('` and `')}\`?`, {
-          type: 'confirm',
-          initial: true,
-        })
-
-        if (shouldInstall) {
-          logger.start(`Installing ${BABEL_DECORATOR_DEPS.map(d => `\`${d}\``).join(' and ')}...`)
-          await addDependency(BABEL_DECORATOR_DEPS, {
-            dev: true,
-            cwd: nuxt.options.rootDir,
-            silent: true,
-          })
-          logger.info('Rerun Nuxt to enable decorator support.')
-          process.exit(1)
-        }
-      }
-
-      logger.warn(`Cannot find \`${pkg}\`. Install \`${BABEL_DECORATOR_DEPS.join('` and `')}\` to enable decorator support.`)
-      return false
-    }
-  }
-  return true
-}
+const BABEL_DECORATOR_DEPS = ['@babel/plugin-proposal-decorators', '@babel/plugin-syntax-jsx'] as const
 
 export function DecoratorsPlugin (nuxt: Nuxt): Plugin {
   let transformSync: typeof import('@babel/core').transformSync
@@ -51,9 +11,16 @@ export function DecoratorsPlugin (nuxt: Nuxt): Plugin {
     name: 'nuxt:decorators',
     apply: () => !!nuxt.options.experimental.decorators,
     async applyToEnvironment () {
-      if (!await ensureBabelDecoratorDeps(nuxt)) {
+      const result = await ensureDependencyInstalled([...BABEL_DECORATOR_DEPS], {
+        rootDir: nuxt.options.rootDir,
+        searchPaths: nuxt.options.modulesDir,
+      })
+
+      if (result !== true) {
+        logger.warn(`Install ${result.map(d => `\`${d}\``).join(' and ')} to enable decorator support.`)
         return false
       }
+
       transformSync = await import('@babel/core').then(r => r.transformSync)
       return true
     },
