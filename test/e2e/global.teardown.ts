64c7cc5df7822b2b4c93e74bdef8e95c6581b8e8
Daniel Roe <daniel@roe.dev>
2026-03-26 09:23:01 -0700

    test: add retries to tearing down hmr fixture directory

--- test/e2e/global.teardown.ts
+++ test/e2e/global.teardown.ts
@@ -6,7 +6,17 @@ import { test as teardown } from '@playwright/test'
 const fixtureDir = fileURLToPath(new URL('../fixtures-temp/hmr', import.meta.url))
 
 teardown('remove temporary hmr fixture directory', async () => {
-  if (existsSync(fixtureDir)) {
-    await rm(fixtureDir, { force: true, recursive: true })
+  if (!existsSync(fixtureDir)) { return }
+
+  // try to work around windows flakiness with file locks
+  const maxRetries = 5
+  for (let i = 0; i < maxRetries; i++) {
+    try {
+      await rm(fixtureDir, { force: true, recursive: true, maxRetries: 3, retryDelay: 500 })
+      return
+    } catch (err: unknown) {
+      if (i === maxRetries - 1) { throw err }
+      await new Promise(resolve => setTimeout(resolve, 1000 * (i + 1)))
+    }
   }
 })
