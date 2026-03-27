5ae421031eabde42d7d4c8b6dfa16fb53c392a0c
Eduardo San Martin Morote <posva13@gmail.com>
2026-03-27 16:55:06 -0700

    chore: add index

--- flow-demo/app/pages/index.vue
+++ flow-demo/app/pages/index.vue
@@ -2,16 +2,39 @@
   <div>
     <h1>Playground</h1>
     <ul>
-      <li><NuxtLink to="/async-page">Async Page</NuxtLink></li>
-      <li><NuxtLink to="/settings">Settings</NuxtLink></li>
-      <li><NuxtLink to="/delayed-redirect">Delayed Redirect</NuxtLink></li>
-      <li><NuxtLink to="/share">Share</NuxtLink></li>
-      <li><NuxtLink to="/cancel-checkout">Cancel Checkout</NuxtLink></li>
-      <li><NuxtLink to="/admin">Admin Dashboard</NuxtLink></li>
-      <li><NuxtLink to="/counter">Counter</NuxtLink></li>
-      <li><NuxtLink to="/user-profile">User Profile</NuxtLink></li>
-      <li><NuxtLink to="/blog-post">Blog Post</NuxtLink></li>
-      <li><NuxtLink to="/preferences">Preferences</NuxtLink></li>
+      <li>
+        <NuxtLink to="/async-page">Async Page</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/phoenix-weather">Phoenix weather</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/settings">Settings</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/delayed-redirect">Delayed Redirect</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/share">Share</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/cancel-checkout">Cancel Checkout</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/admin">Admin Dashboard</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/counter">Counter</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/user-profile">User Profile</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/blog-post">Blog Post</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/preferences">Preferences</NuxtLink>
+      </li>
     </ul>
   </div>
 </template>
