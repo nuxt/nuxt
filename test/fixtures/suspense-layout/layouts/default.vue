7482157ca36e0e36e994bb2b338cbda54aad830d
Daniel Roe <daniel@roe.dev>
2026-05-06 12:04:53 +0100

    fix(nuxt): only force suspense remount after first resolve (#34949)

---
+++ test/fixtures/suspense-layout/layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div data-testid="default-layout">
+    <header>Default Layout</header>
+    <slot />
+  </div>
+</template>
---
+++ test/fixtures/suspense-layout/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 data-testid="index-title">
+      Index Page
+    </h1>
+    <NuxtLink
+      to="/redirect"
+      data-testid="link-redirect"
+    >
+      Go via redirect
+    </NuxtLink>
+    <NuxtLink
+      to="/asyncdata/p0"
+      data-testid="link-async-start"
+    >
+      Async p0
+    </NuxtLink>
+  </div>
+</template>
