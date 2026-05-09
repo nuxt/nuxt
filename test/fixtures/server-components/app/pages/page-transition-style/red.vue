16fde3ab7e1039dc0dd32fb231984a42a7e0d648
Daniel Roe <daniel@roe.dev>
2026-05-09 19:33:35 +0100

    fix(nuxt): defer head DOM updates until page transition finishes (#35016)

---
+++ test/fixtures/server-components/app/pages/page-transition-style/red.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+definePageMeta({
+  pageTransition: { name: 'page', mode: 'out-in' },
+})
+
+useHead({
+  style: [{ innerHTML: '.bg-red { background-color: rgb(255, 0, 0); }' }],
+})
+</script>
+
+<template>
+  <div class="bg-red red-page">
+    <h1>red</h1>
+    <NuxtLink
+      id="to-blue"
+      to="/page-transition-style/blue"
+    >
+      to blue
+    </NuxtLink>
+  </div>
+</template>
+
+<style>
+.page-leave-active,
+.page-enter-active {
+  transition: opacity 500ms;
+}
+.page-leave-to,
+.page-enter-from {
+  opacity: 0;
+}
+</style>
