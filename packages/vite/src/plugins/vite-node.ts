b1dea174fd43ece68ad8cc9aa60a384ab61304d4
Daniel Roe <daniel@roe.dev>
2026-05-12 07:32:26 +0100

    fix(vite): don't strip buildAssetsDir from vite-node SSR ids (#35040)

--- packages/vite/src/plugins/vite-node.ts
+++ packages/vite/src/plugins/vite-node.ts
@@ -269,7 +269,7 @@ export function ViteNodePlugin (nuxt: Nuxt): VitePlugin | undefined {
           socketPath,
           root: nuxt.options.srcDir,
           entryPath: spaEntryPath ?? resolveServerEntry(ssrServer.config),
-          base: ssrServer.config.base || '/_nuxt/',
+          base: '/',
           maxRetryAttempts: nuxt.options.vite.viteNode?.maxRetryAttempts,
           baseRetryDelay: nuxt.options.vite.viteNode?.baseRetryDelay,
           maxRetryDelay: nuxt.options.vite.viteNode?.maxRetryDelay,
