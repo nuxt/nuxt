a3f0c205e41c0f84221b9eb6acda211da2da494f
Daniel Roe <daniel@roe.dev>
2026-05-19 16:36:43 +0100

    test: cover add regression test for hmr in sibling local layers (#35125)

--- test/e2e/global.setup.ts
+++ test/e2e/global.setup.ts
@@ -3,17 +3,27 @@ import { fileURLToPath } from 'node:url'
 import { cp, rm } from 'node:fs/promises'
 import { test as setup } from '@playwright/test'
 
-const fixtureDir = fileURLToPath(new URL('../fixtures-temp/hmr', import.meta.url))
-const sourceDir = fileURLToPath(new URL('../fixtures/hmr', import.meta.url))
+const fixtures = [
+  {
+    sourceDir: fileURLToPath(new URL('../fixtures/hmr', import.meta.url)),
+    fixtureDir: fileURLToPath(new URL('../fixtures-temp/hmr', import.meta.url)),
+  },
+  {
+    sourceDir: fileURLToPath(new URL('../fixtures/hmr-sibling-layer', import.meta.url)),
+    fixtureDir: fileURLToPath(new URL('../fixtures-temp/hmr-sibling-layer', import.meta.url)),
+  },
+]
 
 setup('create temporary hmr fixture directory', async () => {
-  if (existsSync(fixtureDir)) {
-    await rm(fixtureDir, { force: true, recursive: true })
+  for (const { sourceDir, fixtureDir } of fixtures) {
+    if (existsSync(fixtureDir)) {
+      await rm(fixtureDir, { force: true, recursive: true })
+    }
+    await cp(sourceDir, fixtureDir, {
+      recursive: true,
+      filter: (src) => {
+        return !src.includes('.cache') && !src.endsWith('.sock') && !src.includes('.output') && !src.includes('.nuxt-')
+      },
+    })
   }
-  await cp(sourceDir, fixtureDir, {
-    recursive: true,
-    filter: (src) => {
-      return !src.includes('.cache') && !src.endsWith('.sock') && !src.includes('.output') && !src.includes('.nuxt-')
-    },
-  })
 })
