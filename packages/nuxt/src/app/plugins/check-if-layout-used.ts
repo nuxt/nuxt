ab8d67f40c65b4bbd4ff1c476d241895ca1dda88
Anton Gorbunov <gorbunov.a@promail366.com>
2026-05-25 17:01:07 +0000

    fix(nuxt): clarify page and layout usage warnings

--- packages/nuxt/src/app/plugins/check-if-layout-used.ts
+++ packages/nuxt/src/app/plugins/check-if-layout-used.ts
@@ -14,7 +14,11 @@ const plugin: Plugin & ObjectPlugin = defineNuxtPlugin({
 
     function checkIfLayoutUsed () {
       if (!error.value && !nuxtApp._isNuxtLayoutUsed && Object.keys(layouts).length > 0) {
-        console.warn('[nuxt] Your project has layouts but the `<NuxtLayout />` component has not been used.')
+        console.warn(
+          '[nuxt] Your project has layouts but `<NuxtLayout />` has not been detected.' +
+          ' Make sure you are using `<NuxtLayout />` in your app to render layouts.' +
+          ' If `<NuxtLayout />` is rendered conditionally, this warning can be triggered before it is mounted.',
+        )
       }
     }
     if (import.meta.server) {
