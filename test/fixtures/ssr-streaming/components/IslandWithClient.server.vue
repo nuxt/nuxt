c331151e159256860032fa31bf50727bdca81905
Daniel Roe <daniel@roe.dev>
2026-05-26 16:53:10 +0100

    feat(nitro,nuxt,schema): add experimental SSR streaming (#34411)
    Co-authored-by: Harlan Wilton <harlan@harlanzw.com>

---
+++ test/fixtures/ssr-streaming/components/IslandWithClient.server.vue
@@ -0,0 +1,9 @@
+<template>
+  <div
+    class="client-island"
+    data-testid="client-island"
+  >
+    <p>server-rendered island shell</p>
+    <IslandCounter nuxt-client />
+  </div>
+</template>
