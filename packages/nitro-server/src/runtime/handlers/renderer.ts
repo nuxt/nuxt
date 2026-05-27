c3dba641606ed7e93963260c26f55c2dcc642bfa
Florian Heuberger <fh@flogersoft.de>
2026-05-27 12:26:48 +0200

    fix: local eslint config...

--- packages/nitro-server/src/runtime/handlers/renderer.ts
+++ packages/nitro-server/src/runtime/handlers/renderer.ts
@@ -356,6 +356,7 @@ async function renderRoute (event: H3Event, ssrError: (NuxtPayload['error'] & { 
   }
 
   // TODO: migrate to `ssrContext.head.render()` once `renderSSRHeadOptions` (e.g. `omitLineBreaks`) can be passed to `createServerHead` at construction time.
+  // eslint-disable-next-line @typescript-eslint/no-deprecated
   const { headTags, bodyTags, bodyTagsOpen, htmlAttrs, bodyAttrs } = renderSSRHead(ssrContext.head, renderSSRHeadOptions)
 
   // Create render context
