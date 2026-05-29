df8574cb3a674747e2815649a127952aa4f6ed9d
Luke Nelson <luke@nelson.zone>
2026-05-29 09:29:37 +0100

    chore(types): add type predicates to island key, string literal, and hash link guards

--- packages/nuxt/src/app/plugins/utils.ts
+++ packages/nuxt/src/app/plugins/utils.ts
@@ -1,4 +1,4 @@
 const VALID_ISLAND_KEY_RE = /^[a-z][a-z\d-]*_[a-z\d]+$/i
-export function isValidIslandKey (key: string): boolean {
+export function isValidIslandKey (key: string): key is string {
   return typeof key === 'string' && VALID_ISLAND_KEY_RE.test(key) && key.length <= 100
 }
