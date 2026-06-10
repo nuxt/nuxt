3fa00c89891d6cd6c6c2123cd4a87f624da28794
Luke Nelson <luke@nelson.zone>
2026-06-10 11:02:11 +0100

    fix(vite): inline CSS for lazy components imported from `#components` (#35215)

--- packages/vite/src/plugins/ssr-styles.ts
+++ packages/vite/src/plugins/ssr-styles.ts
@@ -275,7 +275,7 @@ export function SSRStylesPlugin (nuxt: Nuxt): Plugin | undefined {
 
             const relativePath = relativeToSrcDir(stripQuery(moduleId))
             if (relativePath in cssMap) {
-              cssMap[relativePath]!.inBundle = cssMap[relativePath]!.inBundle ?? ((isVue(moduleId) && !!relativePath) || isEntry)
+              cssMap[relativePath]!.inBundle = cssMap[relativePath]!.inBundle ?? ((isVue(stripQuery(moduleId)) && !!relativePath) || isEntry)
             }
           }
 
