9c91dcf68fe0d7c70240798cc4784d906b2b1f5f
Daniel Roe <daniel@roe.dev>
2026-06-11 21:15:21 +0000

    test: add failing test for #25317

---
+++ test/fixtures/basic/app/components/Issue25317/Wrapper.server.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+await new Promise<void>((resolve) => {
+  setTimeout(() => resolve(), 20)
+})
+</script>
+
+<template>
+  <Issue25317Inner />
+</template>
---
+++ test/fixtures/basic/app/pages/issue-25317.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: false,
+})
+</script>
+
+<template>
+  <div id="issue-25317-page">
+    <Suspense>
+      <Issue25317Wrapper />
+      <template #fallback>
+        <div id="issue-25317-outer-fallback">
+          outer-loading
+        </div>
+      </template>
+    </Suspense>
+  </div>
+</template>
