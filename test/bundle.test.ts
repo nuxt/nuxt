3d49a87f2c58660f6a4c1c01bc5c5a8ffbcc1160
Daniel Roe <daniel@roe.dev>
2026-07-16 16:23:01 +0000

    ci: drop build steps and dist artifacts from jobs that run from source

--- test/bundle.test.ts
+++ test/bundle.test.ts
@@ -54,7 +54,7 @@ describe.skipIf(process.env.SKIP_BUNDLE_SIZE === 'true' || process.env.ECOSYSTEM
     const serverDir = join(rootDir, '.output/server')
 
     const serverStats = await analyzeSizes(['**/*.mjs', '!_libs'], serverDir, rootDir)
-    expect.soft(roundToKilobytes(serverStats.totalBytes)).toMatchInlineSnapshot(`"69.1k"`)
+    expect.soft(roundToKilobytes(serverStats.totalBytes)).toMatchInlineSnapshot(`"69.0k"`)
 
     const modules = await analyzeSizes(['_libs/**/*'], serverDir, rootDir)
     expect.soft(roundToKilobytes(modules.totalBytes)).toMatchInlineSnapshot(`"495k"`)
