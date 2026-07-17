51b539d1ede0033e21da63b43cfc9e99af75a6ed
Daniel Roe <daniel@roe.dev>
2026-07-17 12:32:00 +0000

    feat(nuxt): add useLayout composable for resolved route layout

--- test/fixtures/basic/app/pages/route-rules/layout.vue
+++ test/fixtures/basic/app/pages/route-rules/layout.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+const layout = useLayout()
+</script>
+
 <template>
   <div>
     <div>Should use layout set in route-rules</div>
+    <div>useLayout: {{ layout }}</div>
   </div>
 </template>
