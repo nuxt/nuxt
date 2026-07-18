d9ae885731d869b4bf9c4d7a00d7a171ce5a9d25
Mateleo <mateleo78@gmail.com>
2026-07-18 17:39:38 +0200

    feat(schema): case-sensitive routing by default in v5

--- packages/schema/src/config/router.ts
+++ packages/schema/src/config/router.ts
@@ -5,6 +5,13 @@ export default defineResolvers({
     options: {
       hashMode: false,
       scrollBehaviorType: 'auto',
+      sensitive: {
+        $resolve: async (val, get) => {
+          // vue-router is case-insensitive by default, which is at variance with nitro.
+          // Default to case-sensitive routing to avoid a class of bugs (breaking change in v5).
+          return typeof val === 'boolean' ? val : (await get('future.compatibilityVersion')) >= 5
+        },
+      },
     },
   },
 })
