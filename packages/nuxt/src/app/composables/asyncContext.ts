6d59ea01f2338e516599581f5b53f799cb0d0e8f
Daniel Roe <daniel@roe.dev>
2026-07-20 17:21:10 +0000

    test: add vapor mode interop fixture

--- packages/nuxt/src/app/composables/asyncContext.ts
+++ packages/nuxt/src/app/composables/asyncContext.ts
@@ -1,4 +1,3 @@
-// @ts-expect-error withAsyncContext is internal API
 import { getCurrentInstance, withAsyncContext as withVueAsyncContext } from 'vue'
 
 /** @since 3.8.0 */
---
+++ test/fixtures/vapor/app/app.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <nav>
+      <NuxtLink
+        to="/"
+        data-testid="nav-index"
+      >
+        Home
+      </NuxtLink>
+      <NuxtLink
+        to="/vapor-page"
+        data-testid="nav-vapor-page"
+      >
+        Vapor page
+      </NuxtLink>
+      <NuxtLink
+        to="/mixed"
+        data-testid="nav-mixed"
+      >
+        Mixed
+      </NuxtLink>
+    </nav>
+    <NuxtPage />
+  </div>
+</template>
