1ff28284642975503de47135351a6918e431cab2
Daniel Roe <daniel@roe.dev>
2026-08-14 11:49:36 +0000

    fix(rspack,webpack): do not search filesystem for postcss config

--- packages/webpack/src/utils/postcss.ts
+++ packages/webpack/src/utils/postcss.ts
@@ -35,6 +35,7 @@ export async function getPostcssConfig (nuxt: Nuxt) {
   }
 
   const postcssOptions = defu({}, nuxt.options.postcss, {
+    config: false,
     plugins: defaultPlugins,
     sourceMap: nuxt.options.webpack.cssSourceMap,
   })
