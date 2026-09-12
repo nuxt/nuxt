cc035e70a227d230c1344c4e7e04d8de1da9e2c8
Matej Černý <dev@matejcerny.dev>
2026-09-13 01:55:41 +0200

    test: add inline styles test for child and parent styles in different chunks

--- test/fixtures/inline-styles/pages/shared-a.vue
+++ test/fixtures/inline-styles/pages/shared-a.vue
@@ -1,6 +1,12 @@
 <template>
   <main>
     shared a
-    <SharedBox />
+    <SharedBox class="shared-parent" />
   </main>
 </template>
+
+<style scoped>
+.shared-parent {
+  --inline-shared-parent-token: shared-parent;
+}
+</style>
